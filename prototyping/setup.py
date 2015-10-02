019395a7711cb96e2dc052e326f04fa4019433e2
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2015-10-03 02:09:16 +0300

    Update setup.py to reflect changes in typing 3.5.0

--- prototyping/setup.py
+++ prototyping/setup.py
@@ -4,15 +4,14 @@
 import sys
 from distutils.core import setup
 
-if sys.version_info < (2, 7, 0) or (3,) < sys.version_info < (3, 2, 0):
-    sys.stderr.write('ERROR: You need either Python2 2.7 or later '
-                     'or Python3 3.2 or later '
+if sys.version_info < (3, 2, 0):
+    sys.stderr.write('ERROR: You need Python 3.2 or later '
                      'to install the typing package.\n')
     exit(1)
 
-version = '3.5.0b1'
+version = '3.5.0'
 description = 'Type Hints for Python'
-long_description = '''
+long_description = '''\
 Typing -- Type Hints for Python
 
 This is a backport of the standard library typing module to Python
@@ -23,16 +22,14 @@ type annotations. The notation can be used for documenting code in a
 concise, standard format, and it has been designed to also be used by
 static and runtime type checkers, static analyzers, IDEs and other
 tools.
-'''.lstrip()
+'''
 
-package_dir = {2: 'python2', 3: '.'}[sys.version_info.major]
 classifiers = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: POSIX',
-    'Programming Language :: Python :: 2.7',
+    'License :: OSI Approved :: Python Software Foundation License',
+    'Operating System :: OS Independent',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
@@ -45,11 +42,10 @@ setup(name='typing',
       long_description=long_description,
       author=u'Guido van Rossum, Jukka Lehtosalo, Łukasz Langa',
       author_email='jukka.lehtosalo@iki.fi',
-      url='http://docs.python.org/dev/library/typing.html#typing',
+      url='https://docs.python.org/3.5/library/typing.html',
       license='PSF',
       keywords='typing function annotations type hints hinting checking '
                'checker typehints typehinting typechecking backport',
-      platforms=['POSIX', 'Windows'],
-      package_dir={'': package_dir},
+      package_dir={'': '.'},
       py_modules=['typing'],
       classifiers=classifiers)
