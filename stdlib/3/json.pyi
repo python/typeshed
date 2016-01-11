0735ec02512af4ee56d1c7e25e0f550a9cf6f089
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-10 23:30:10 -0800

    Add keyword args to stdlib's json.pyi

--- stdlib/3/json.pyi
+++ stdlib/3/json.pyi
@@ -1,4 +1,4 @@
-from typing import Any, IO
+from typing import Any, IO, Optional, Tuple, Callable
 
 class JSONDecodeError(object):
     def dumps(self, obj: Any) -> str: ...
@@ -6,7 +6,30 @@ class JSONDecodeError(object):
     def loads(self, s: str) -> Any: ...
     def load(self, fp: IO[str]) -> Any: ...
 
-def dumps(obj: Any) -> str: ...
-def dump(obj: Any, fp: IO[str], *args: Any, **kwds: Any) -> None: ...
+def dumps(obj: Any,
+    skipkeys: bool = ...,
+    ensure_ascii: bool = ...,
+    check_circular: bool = ...,
+    allow_nan: bool = ...,
+    cls: Any = ...,
+    indent: Optional[int] = ...,
+    separators: Optional[Tuple[str, str]] = ...,
+    default: Optional[Callable[[Any], Any]] = ...,
+    sort_keys: bool = ...,
+    **kwds: Any) -> str: ...
+
+def dump(obj: Any,
+    fp: IO[str],
+    skipkeys: bool = ...,
+    ensure_ascii: bool = ...,
+    check_circular: bool = ...,
+    allow_nan: bool = ...,
+    cls: Any = ...,
+    indent: Optional[int] = ...,
+    separators: Optional[Tuple[str, str]] = ...,
+    default: Optional[Callable[[Any], Any]] = ...,
+    sort_keys: bool = ...,
+    **kwds: Any) -> None: ...
+
 def loads(s: str) -> Any: ...
 def load(fp: IO[str]) -> Any: ...
