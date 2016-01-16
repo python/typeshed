3a674645a555221ab76c935d71849691f045637c
Ben Darnell <ben@bendarnell.com>
2016-01-16 16:52:01 -0500

    Assorted fixes to stubs

--- stdlib/2.7/inspect.pyi
+++ stdlib/2.7/inspect.pyi
@@ -9,3 +9,12 @@ _FrameRecord = Tuple[_Frame, str, int, str, List[str], int]
 
 def currentframe() -> _FrameRecord: ...
 def stack(context: int = ...) -> List[_FrameRecord]: ...
+
+# namedtuple('ArgSpec', 'args varargs keywords defaults')
+class ArgSpec(tuple):
+    args = ...  # type: List[str]
+    varargs = ...  # type: str
+    keywords = ...  # type: str
+    defaults = ...  # type: tuple
+
+def getargspec(func: object) -> ArgSpec: ...
