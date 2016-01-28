390b566a314b1bbeb25b085d154b2a3875d96893
Guido van Rossum <guido@dropbox.com>
2016-01-27 21:16:40 -0800

    Comment out references to modules without stubs introduced by accident.

--- third_party/2.7/six/moves/urllib/error.pyi
+++ third_party/2.7/six/moves/urllib/error.pyi
@@ -3,6 +3,6 @@
 #
 # Stubs for six.moves.urllib.error (Python 2.7)
 
-from urllib2 import URLError as URLError
-from urllib2 import HTTPError as HTTPError
+## from urllib2 import URLError as URLError
+## from urllib2 import HTTPError as HTTPError
 from urllib import ContentTooShortError as ContentTooShortError
