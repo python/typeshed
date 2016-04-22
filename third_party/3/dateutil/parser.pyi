2defab6dc45cae7848a4f3bc1c1e9c1a348e7eac
Julien Hebert <juke@free.fr>
2016-04-22 14:23:41 +0200

    tests ok

--- third_party/3/dateutil/parser.pyi
+++ third_party/3/dateutil/parser.pyi
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Optional, Callable, Union
+from typing import List, Tuple, Optional, Callable, Union, IO, Any
 from datetime import datetime
 
 __all__ = ...  # type: List[str]
