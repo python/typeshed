52dfbb2463b265862ba8f2c8bb8e11afd0c7fa5d
Dakkaron <dak1st@gmx.at>
2016-04-28 11:09:17 +0200

    stdlib/2.7/bisect.pyi

---
+++ stdlib/2.7/bisect.pyi
@@ -0,0 +1,17 @@
+def bisect(a,x,lo=0,hi=0): # type: (list,object,int,int) -> int
+	return 0
+
+def bisect_left(a,x,lo=0,hi=len(a)): # type: (list,object,int,int) -> int
+	return 0
+
+def bisect_right(a,x,lo=0,hi=len(a)): # type: (list,object,int,int) -> int
+	return 0
+
+def insort_left(a,x,lo=0,hi=len(a)): # type: (list,object,int,int) -> None
+	...
+
+def insort_right(a,x,lo=0,hi=len(a)): # type: (list,object,int,int) -> None
+	...
+
+def insort(a,x,lo=0,hi=len(a)): # type: (list,object,int,int) -> None
+	...
