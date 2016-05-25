f2b5189591338dee937bcc723bbda0c140e594c5
Dakkaron <dak1st@gmx.at>
2016-05-25 07:26:25 +0200

    Small fixes to stdlib/3/getopt.pyi

--- stdlib/3/getopt.pyi
+++ stdlib/3/getopt.pyi
@@ -5,15 +5,17 @@
 from typing import List, Tuple
 
 def getopt(args: List[str], shortopts: str,
-           longopts: List[str]) -> Tuple[List[Tuple[str, str]],
-                                         List[str]]: ...
+           longopts: List[str]=...) -> Tuple[List[Tuple[str, str]],
+                                             List[str]]: ...
 
 def gnu_getopt(args: List[str], shortopts: str,
-               longopts: List[str]) -> Tuple[List[Tuple[str, str]],
-                                             List[str]]: ...
+               longopts: List[str]=...) -> Tuple[List[Tuple[str, str]],
+                                                 List[str]]: ...
 
 class GetoptError(Exception):
     msg = ...  # type: str
     opt = ...  # type: str
+    def __init__(self, msg: str, opt: str=...) -> None: ...
+    def __str__(self) -> str: ...
 
 error = GetoptError
