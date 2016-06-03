b8fe9071047c678c791d2f0872077df973b7e672
tharvik <tharvik@users.noreply.github.com>
2016-06-03 13:31:47 +0200

    add types to logging

---
+++ stdlib/2and3/logging/config.pyi
@@ -0,0 +1,29 @@
+# Stubs for logging.config (Python 3.4)
+
+from typing import Any, Callable, Dict, Optional, TextIO, Union
+import sys
+# TODO uncomment when mypy handle conditionals
+#if sys.version_info >= (3,):
+#    from configparser import RawConfigParser
+#else:
+#    from ConfigParser import RawConfigParser
+# TODO add RawConfigParser to configparser stubs
+RawConfigParser = Any
+
+
+def dictConfig(config: Dict[str, Any]) -> None: ...
+if sys.version_info >= (3, 4):
+    def fileConfig(fname: Union[str, TextIO, RawConfigParser],
+                   defaults: Optional[Dict[str, str]] = ...,
+                   disable_existing_loggers: bool = ...) -> None: ...
+    def listen(port: int = ...,
+               verify: Optional[Callable[[bytes], Optional[bytes]]] = ...) \
+               -> None: ...
+else:
+    def fileConfig(  # type: ignore
+                   fname: Union[str, TextIO],
+                   defaults: Optional[Dict[str, str]] = ...,
+                   disable_existing_loggers: bool = ...) -> None: ...
+    def listen(  # type: ignore
+               port: int = ...) -> None: ...
+def stopListening() -> None: ...
