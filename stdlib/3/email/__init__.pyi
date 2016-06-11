4cb0a0a8bc170fcc12971079bd16309898dad236
pgjones <philip.graham.jones@googlemail.com>
2016-06-11 12:56:51 +0100

    Change email (Py3) message_from_file IO types
    Instead of using TextIO and BinaryIO use IO[str] and IO[bytes]
    respectively to match the type returned by the open builtin. This
    follows the recommendations in #283.
    
    The error that prompts this is reproduced via:
    ```
    from email import message_from_file
    
    with open('email') as file_:
        email = message_from_file(file_)
    ```
    Argument 1 to "message_from_file" has incompatible type IO[Any]; expected "TextIO"

--- stdlib/3/email/__init__.pyi
+++ stdlib/3/email/__init__.pyi
@@ -1,6 +1,6 @@
 # Stubs for email (Python 3.4)
 
-from typing import Callable, Optional, BinaryIO, TextIO
+from typing import Callable, Optional, IO
 import sys
 from email.message import Message, Policy
 
@@ -9,9 +9,9 @@ if sys.version_info >= (3, 3):
                             policy: Policy = ...) -> Message: ...
     def message_from_bytes(s: bytes, _class: Callable[[], Message] = ..., *,
                            policy: Policy = ...) -> Message: ...
-    def message_from_file(fp: TextIO, _class: Callable[[], Message] = ..., *,
+    def message_from_file(fp: IO[str], _class: Callable[[], Message] = ..., *,
                            policy: Policy = ...) -> Message: ...
-    def message_from_binary_file(fp: BinaryIO,
+    def message_from_binary_file(fp: IO[bytes],
                                  _class: Callable[[], Message] = ..., *,
                                  policy: Policy = ...) -> Message: ...
 elif sys.version_info >= (3, 2):
@@ -21,10 +21,10 @@ elif sys.version_info >= (3, 2):
     def message_from_bytes(s: bytes,  # type: ignore
                            _class: Callable[[], Message] = ..., *,
                            strict: Optional[bool] = ...) -> Message: ...
-    def message_from_file(fp: TextIO,  # type: ignore
+    def message_from_file(fp: IO[str],  # type: ignore
                           _class: Callable[[], Message] = ..., *,
                           strict: Optional[bool] = ...) -> Message: ...
-    def message_from_binary_file(fp: BinaryIO,  # type: ignore
+    def message_from_binary_file(fp: IO[bytes],  # type: ignore
                                  _class: Callable[[], Message] = ..., *,
                                  strict: Optional[bool] = ...) -> Message: ...
 
