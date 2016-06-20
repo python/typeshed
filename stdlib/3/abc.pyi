569b81c42adfed79f4a4a46db9b9b1a98c93ab5b
Håken Lid <haakenlid@gmail.com>
2016-06-20 16:05:57 +0200

    add stub for stdlib abc.ABC in python 3.4+ (issue #304)

--- stdlib/3/abc.pyi
+++ stdlib/3/abc.pyi
@@ -1,4 +1,5 @@
 from typing import Any
+import sys
 # Stubs for abc.
 
 # Thesee definitions have special processing in type checker.
@@ -6,3 +7,7 @@ class ABCMeta:
     def register(cls: "ABCMeta", subclass: Any) -> None: ...
 abstractmethod = object()
 abstractproperty = object()
+
+if sys.version_info >= (3, 4):
+    class ABC(metaclass=ABCMeta):
+        pass
