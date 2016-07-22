df7c5c20ff5d710e4b82413f6c5e254fe3f930d8
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2016-07-21 19:35:11 -0500

    Ignore issue with different sig from supertype

--- third_party/2and3/pytz/__init__.pyi
+++ third_party/2and3/pytz/__init__.pyi
@@ -1,22 +1,22 @@
 # Stubs for pytz (Python 3.5)
 
 import datetime as dt
-from pytz.lazy import LazyList, LazyDict  # NOQA
+from typing import Optional, List, Set, Dict  # NOQA
 
-all_timezones = ...  # type: LazyList
-all_timezones_set = ...  # type: LazySet
-common_timezones = ...  # type: LazyList
-common_timezones_set = ...  # type: LazySet
-country_timezones = ...  # type: LazyDict
-country_names = ...  # type: LazyDict
+all_timezones = ...  # type: List
+all_timezones_set = ...  # type: Set
+common_timezones = ...  # type: List
+common_timezones_set = ...  # type: Set
+country_timezones = ...  # type: Dict
+country_names = ...  # type: Dict
 
 
 class UTC(dt.tzinfo):
     zone = ...  # type: str
     def fromutc(self, dt: dt.datetime) -> dt.datetime: ...
-    def utcoffset(self, dt: dt.datetime) -> dt.timedelta: ...
-    def tzname(self, dt: dt.datetime) -> str: ...
-    def dst(self, dt: dt.datetime) -> dt.timedelta: ...
+    def utcoffset(self, dt: Optional[dt.datetime]) -> dt.timedelta: ...  # type: ignore
+    def tzname(self, dt: Optional[dt.datetime]) -> str: ...
+    def dst(self, dt: Optional[dt.datetime]) -> dt.timedelta: ...  # type: ignore
     def localize(self, dt: dt.datetime, is_dst: bool=...) -> dt.datetime: ...
     def normalize(self, dt: dt.datetime, is_dst: bool=...) -> dt.datetime: ...
 
--- third_party/2and3/pytz/lazy.pyi
+++ third_party/2and3/pytz/lazy.pyi
@@ -1,17 +1,13 @@
 # Stubs for pytz.lazy (Python 3.5)
 
-from typing import Any, Iterable, List, Dict  # NOQA
+from typing import Any, Iterable, List, Set, Dict  # NOQA
 from collections import Mapping
 
 class LazyDict(Mapping):
-    data = ...  # type: Dict[str, Any]
-    def __getitem__(self, key: str) -> Any: ...
-    def __contains__(self, key: str) -> bool: ...
-    def __iter__(self) -> Iterable: ...
-    def __len__(self) -> int: ...
-    def keys(self) -> List[str]: ...
-
-class LazyList(list):
     pass
-class LazySet(set):
+
+class LazyList(List):
+    pass
+
+class LazySet(Set):
     pass
