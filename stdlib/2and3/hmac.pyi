9cdffd856080fab8557ef09214d6d091d6681aba
tharvik <tharvik@users.noreply.github.com>
2016-07-25 18:23:55 +0200

    add py2

--- stdlib/2and3/hmac.pyi
+++ stdlib/2and3/hmac.pyi
@@ -4,19 +4,25 @@ from typing import Any, Callable, Optional, Union, overload
 from types import ModuleType
 import sys
 
+_B = Union[bytes, bytearray]
+
 # TODO more precise type for object of hashlib
 _Hash = Any
 
-def new(key: Union[bytes,bytearray],
-        msg: Optional[Union[bytes, bytearray]] = ...,
-        digestmod: Optional[Union[str, Callable[[], _Hash], ModuleType]] = ...) -> HMAC: ...
+if sys.version_info >= (3, 4):
+    def new(key: _B, msg: Optional[_B] = ...,
+            digestmod: Optional[Union[str, Callable[[], _Hash], ModuleType]] = ...) -> HMAC: ...
+else:
+    def new(key: _B, msg: Optional[_B] = ...,
+            digestmod: Optional[Union[str, Callable[[], _Hash], ModuleType]] = ...) -> HMAC: ...
 
 class HMAC:
-    digest_size = ...  # type: int
+    if sys.version_info >= (3,):
+        digest_size = ...  # type: int
     if sys.version_info >= (3, 4):
         block_size = ...  # type: int
         name = ...  # type: str
-    def update(self, msg: Union[bytes, bytearray]) -> None: ...
+    def update(self, msg: _B) -> None: ...
     def digest(self) -> bytes: ...
     def hexdigest(self) -> str: ...
     def copy(self) -> HMAC: ...
