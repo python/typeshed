f0d16f14c5b92a9cac5d1994767a9d29d8c19e3f
tharvik <tharvik@users.noreply.github.com>
2016-07-28 16:04:10 +0200

    remove old distutils

--- stdlib/3/distutils/spawn.pyi
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-# In Python, arguments have integer default values
-def spawn(cmd: List[str], search_path: bool = ..., verbose: bool = ...,
-           dry_run: bool = ...) -> None: ...
-def find_executable(executable: str, path: str = ...) -> str: ...
