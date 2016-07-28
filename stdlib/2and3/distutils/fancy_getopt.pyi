1ab87689cb91499512669b278d227aa42e0da156
tharvik <tharvik@users.noreply.github.com>
2016-07-28 18:43:37 +0200

    fancy_getopt done

---
+++ stdlib/2and3/distutils/fancy_getopt.pyi
@@ -0,0 +1,26 @@
+# Stubs for distutils.fancy_getopt
+
+from typing import (
+    Any, List, Mapping, Optional, Tuple, Union,
+    TypeVar, overload,
+)
+
+_Option = Tuple[str, str, str]
+
+def fancy_getopt(options: List[_Option],
+                 negative_opt: Mapping[_Option, _Option],
+                 object: Any, args: Optional[List[str]]) \
+                 -> Union[List[str], Tuple[List[str], OptionDummy]]: ...
+def wrap_text(text: str, width: int) -> List[str]: ...
+
+class FancyGetopt:
+    def __init__(self, option_table: Optional[List[_Option]] = ...) -> None: ...
+    # TODO kinda wrong, `getopt(object=object())` is invalid
+    @overload
+    def getopt(self, args: Optional[List[str]] = ...) -> Tuple[List[str], OptionDummy]: ...
+    @overload
+    def getopt(self, args: Optional[List[str]], object: Any) -> List[str]: ...
+    def get_option_order(self) -> List[Tuple[str, str]]: ...
+    def generate_help(self, header: Optional[str] = ...) -> List[str]: ...
+
+class OptionDummy: ...
