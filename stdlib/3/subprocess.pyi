103ae0552c44f62d750d9ef78d049e298bd5a3ec
Sebastian Meßmer <smessmer@users.noreply.github.com>
2016-10-15 11:08:15 +0200

    Add timeout parameter to subprocess.Popen.wait()

--- stdlib/3/subprocess.pyi
+++ stdlib/3/subprocess.pyi
@@ -12,12 +12,12 @@ if sys.version_info >= (3, 5):
         returncode = ... # type: int
         stdout = ... # type: Union[str, bytes]
         stderr = ... # type: Union[str, bytes]
-        def __init__(self, args: Union[List, str], 
-                     returncode: int, 
-                     stdout: Union[str, bytes], 
+        def __init__(self, args: Union[List, str],
+                     returncode: int,
+                     stdout: Union[str, bytes],
                      stderr: Union[str, bytes]) -> None: ...
         def check_returncode(self) -> None: ...
-    
+
     # Nearly same args as Popen.__init__ except for timeout, input, and check
     def run(args: Union[str, Sequence[str]],
 	    timeout: float = ...,
@@ -199,6 +199,8 @@ class CalledProcessError(Exception):
     def __init__(self, returncode: int, cmd: str, output: Optional[str] = ...,
                  stderr: Optional[str] = ...) -> None: ...
 
+class TimeoutExpired(SubprocessError): ...
+
 class Popen:
     stdin = ... # type: IO[Any]
     stdout = ... # type: IO[Any]
@@ -226,7 +228,11 @@ class Popen:
                   pass_fds: Any = ...) -> None: ...
 
     def poll(self) -> int: ...
-    def wait(self) -> int: ...
+    if sys.version_info >= (3, 3):
+        # 3.3 added timeout
+        def wait(self, timeout: float) -> int: ...
+    else:
+        def wait(self) ->int: ...
     # Return str/bytes
     if sys.version_info >= (3, 3):
         def communicate(self, input: Union[str, bytes] = ..., timeout: float = ...) -> Tuple[Any, Any]: ...
