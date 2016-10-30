d3435e51a8042d7ae467b133b9d55982659cc37d
Elazar Gershuni <elazarg@gmail.com>
2016-10-30 20:11:04 +0200

    get rid of `import abc`

--- stdlib/3/importlib/abc.pyi
+++ stdlib/3/importlib/abc.pyi
@@ -1,5 +1,4 @@
-import abc
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 if sys.version_info >= (3, 4):
     from _importlib_modulespec import ModuleSpec
 import sys
@@ -16,19 +15,19 @@ class Finder(metaclass=ABCMeta): ...
     # Technically this class defines the following method, but its subclasses
     # in this module violate its signature. Since this class is deprecated, it's
     # easier to simply ignore that this method exists.
-    #@abc.abstractmethod
+    #@abstractmethod
     #def find_module(self, fullname: str,
     #                path: Sequence[_Path] = None) -> Optional[Loader]: ...
 
 class ResourceLoader(Loader):
-    @abc.abstractmethod
+    @abstractmethod
     def get_data(self, path: _Path) -> bytes: ...
 
 class InspectLoader(Loader):
     def is_package(self, fullname: str) -> bool: ...
     def get_code(self, fullname: str) -> Optional[types.CodeType]: ...
     def load_module(self, fullname: str) -> types.ModuleType: ...
-    @abc.abstractmethod
+    @abstractmethod
     def get_source(self, fullname: str) -> Optional[str]: ...
     if sys.version_info >= (3, 4):
         def exec_module(self, module: types.ModuleType) -> None: ...
@@ -41,7 +40,7 @@ class InspectLoader(Loader):
                            path: str = '<string>') -> types.CodeType: ...
 
 class ExecutionLoader(InspectLoader):
-    @abc.abstractmethod
+    @abstractmethod
     def get_filename(self, fullname: str) -> _Path: ...
     def get_code(self, fullname: str) -> Optional[types.CodeType]: ...
 
