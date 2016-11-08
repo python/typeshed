37d89567e87870e598558afbe42871c0e7ef8be9
Hong Minhee <hong.minhee@gmail.com>
2016-11-09 01:20:39 +0900

    ABC types were moved to collections.abc since 3.3

---
+++ stdlib/3/collections/abc.pyi
@@ -0,0 +1,13 @@
+# Stubs for collections.abc (introduced from Python 3.3)
+#
+# https://docs.python.org/3.3/whatsnew/3.3.html#collections
+import sys
+
+if sys.version_info >= (3, 3):
+    from . import (
+        Container as Container,
+        MutableMapping as MutableMapping,
+        Sequence as Sequence,
+        MutableSequence as MutableSequence,
+        Set as Set,
+    )
