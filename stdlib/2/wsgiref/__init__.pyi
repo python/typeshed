ad7c4b37f6f868159c52a37b7df747b649e650e1
Roy Williams <rwilliams@lyft.com>
2016-11-30 12:03:14 -0800

    s/WSGIFunction/WSGIApplication and fix up comments

--- stdlib/2/wsgiref/__init__.pyi
+++ stdlib/2/wsgiref/__init__.pyi
@@ -1,7 +1,7 @@
 # Type declaration for a WSGI Function in Python 2
 #
-# This function actually exist, but we need a central place to define the type
-# of a WSGI Application.
+# WSGIApplication doesn't exist in wsgiref/__init__.py, it's a type provided for
+# type checking purposes.
 #
 # To correctly use this type stub, utilize the `TYPE_CHECKING` flag in
 # typing:
@@ -18,9 +18,9 @@ from types import TracebackType
 exc_info = Tuple[Optional[Type[BaseException]],
                  Optional[BaseException],
                  Optional[TracebackType]]
-WSGIFunction = Callable[[Dict[Union[unicode, str], Union[unicode, str]],
-                         Union[
-                           Callable[[Union[unicode, str], List[Tuple[Union[unicode, str], Union[unicode, str]]]], Callable[[Union[unicode, str]], None]],
-                           Callable[[Union[unicode, str], List[Tuple[Union[unicode, str], Union[unicode, str]]], exc_info], Callable[[Union[unicode, str]], None]]
-                         ]],
-                         Iterable[Union[unicode, str]]]
+WSGIApplication = Callable[[Dict[Union[unicode, str], Union[unicode, str]],
+                            Union[
+                                Callable[[Union[unicode, str], List[Tuple[Union[unicode, str], Union[unicode, str]]]], Callable[[Union[unicode, str]], None]],
+                                Callable[[Union[unicode, str], List[Tuple[Union[unicode, str], Union[unicode, str]]], exc_info], Callable[[Union[unicode, str]], None]]
+                            ]],
+                            Iterable[Union[unicode, str]]]
--- stdlib/3/wsgiref/__init__.pyi
+++ stdlib/3/wsgiref/__init__.pyi
@@ -1,7 +1,7 @@
 # Type declaration for a WSGI Function in Python 3
 #
-# This function actually exist, but we need a central place to define the type
-# of a WSGI Application.
+# WSGIApplication doesn't exist in wsgiref/__init__.py, it's a type provided for
+# type checking purposes.
 #
 # To correctly use this type stub, utilize the `TYPE_CHECKING` flag in
 # typing:
@@ -18,9 +18,9 @@ from types import TracebackType
 exc_info = Tuple[Optional[Type[BaseException]],
                  Optional[BaseException],
                  Optional[TracebackType]]
-WSGIFunction = Callable[[Dict[str, str],
-                         Union[
-                           Callable[[str, List[Tuple[str, str]]], Callable[[Union[bytes, str]], None]],
-                           Callable[[str, List[Tuple[str, str]], exc_info], Callable[[Union[bytes, str]], None]]
-                         ]],
-                         Iterable[Union[bytes, str]]]
+WSGIApplication = Callable[[Dict[str, str],
+                            Union[
+                                Callable[[str, List[Tuple[str, str]]], Callable[[Union[bytes, str]], None]],
+                                Callable[[str, List[Tuple[str, str]], exc_info], Callable[[Union[bytes, str]], None]]
+                            ]],
+                            Iterable[Union[bytes, str]]]
