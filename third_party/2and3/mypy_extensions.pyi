6516e1c4dabefd28c920c4913c9419907d86e3d3
David Fisher <ddfisher@dropbox.com>
2017-01-04 13:30:18 -0800

    Fix None alias

--- third_party/2and3/mypy_extensions.pyi
+++ third_party/2and3/mypy_extensions.pyi
@@ -5,5 +5,7 @@ T = TypeVar('T')
 
 def TypedDict(typename: str, fields: Dict[str, Type[T]]) -> Type[dict]: ...
 
-# Return type that indicates a function does not return
-NoReturn = None
+# Return type that indicates a function does not return.
+# This type is equivalent to the None type, but the no-op Union is necessary to
+# distinguish the None type from the None value.
+NoReturn = Union[None]
