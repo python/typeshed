6178ed3201b94f5cc0540cc57cd069b99858a5cc
Adam Marszałek <sceeter89@users.noreply.github.com>
2017-01-20 18:09:23 +0100

    Expansion of unions inside `params` definition (#848)
    Due to `Dict` / `Mapping` invariance it's necessary to include other variants of `Union[str, bytes], Union[str, bytes]`.

--- third_party/3/requests/api.pyi
+++ third_party/3/requests/api.pyi
@@ -1,16 +1,22 @@
 # Stubs for requests.api (Python 3)
 
-from typing import Optional, Union, Any, Iterable, Dict, Tuple
+from typing import Optional, Union, Any, Iterable, Mapping, Tuple
 
 from .models import Response
+
+ParamsMappingValueType = Union[str, bytes, int, float, Iterable[Union[str, bytes, int, float]]]
 
 def request(method: str, url: str, **kwargs) -> Response: ...
 def get(url: Union[str, bytes],
         params: Optional[
             Union[
-                Dict[Union[str, bytes, int, float], Union[str, bytes, int, float, Iterable[Union[str, bytes, int, float]]]],
+                Mapping[Union[str, bytes, int, float], ParamsMappingValueType],
                 Union[str, bytes],
-                Tuple[Union[str, bytes, int, float], Union[str, bytes, int, float, Iterable[Union[str, bytes, int, float]]]]]] = None,
+                Tuple[Union[str, bytes, int, float], ParamsMappingValueType],
+                Mapping[str, ParamsMappingValueType],
+                Mapping[bytes, ParamsMappingValueType],
+                Mapping[int, ParamsMappingValueType],
+                Mapping[float, ParamsMappingValueType]]]=None,
         **kwargs) -> Response: ...
 def options(url: str, **kwargs) -> Response: ...
 def head(url: str, **kwargs) -> Response: ...
