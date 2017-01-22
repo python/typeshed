207d52a045fbf7343405765e3f3d2ba0ccff7487
ptz <ptz@subtotal.ru>
2017-01-22 19:49:11 +0300

    Added partial stub for third_party/2and3/sqlalchemy/orm/util. Added incomplete stub for class third_party/2and3/sqlalchemy/sql/elements/ColumnElement

---
+++ third_party/2and3/sqlalchemy/orm/util.pyi
@@ -0,0 +1,13 @@
+# Stubs for sqlalchemy.orm.session (Python 2 and 3)
+from typing import Optional, Any, Text
+
+from ..sql.selectable import FromClause
+
+class AliasedClass(object):
+    def __init__(self, cls: Any, alias: Optional[FromClause] =None, name: Optional[Text] =None, flat: bool =False, adapt_on_names: bool =False,
+                 with_polymorphic_mappers: Any =(), with_polymorphic_discriminator: Any =None, base_alias: Any =None, use_mapper_path: bool =False): ...
+    def __getattr__(self, key): ...
+    def __repr__(self) -> Text: ...
+
+def aliased(element: Any, alias: Optional[FromClause] =None, name: Optional[Text] =None, flat: bool =False, adapt_on_names: bool =False) -> AliasedClass: ...
+
