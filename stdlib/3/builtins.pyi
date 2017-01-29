8ce64ce782037031e32210b2646f64975279c0bb
Andrey Vlasovskikh <vlan@mail.ru>
2017-01-29 22:16:23 +0300

    Updates to slice built-in (#891)
    Makes args and attributes optional.

--- stdlib/3/builtins.pyi
+++ stdlib/3/builtins.pyi
@@ -481,10 +481,13 @@ class bool(int, SupportsInt, SupportsFloat):
     def __init__(self, o: object = ...) -> None: ...
 
 class slice:
-    start = 0
-    step = 0
-    stop = 0
-    def __init__(self, start: Optional[int], stop: Optional[int] = 0, step: Optional[int] = 0) -> None: ...
+    start = ...  # type: Optional[int]
+    step = ...  # type: Optional[int]
+    stop = ...  # type: Optional[int]
+    @overload
+    def __init__(self, stop: int = None) -> None: ...
+    @overload
+    def __init__(self, start: int = None, stop: int = None, step: int = None) -> None: ...
 
 class tuple(Sequence[_T_co], Generic[_T_co]):
     def __init__(self, iterable: Iterable[_T_co] = ...) -> None: ...
