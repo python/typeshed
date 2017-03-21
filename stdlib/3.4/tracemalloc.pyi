43e4f2469c9106bdedace69fbde848c813278636
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-03-20 21:09:34 -0700

    add stubs for tracemalloc
    This one has especially nice documentation, even including type annotations.

---
+++ stdlib/3.4/tracemalloc.pyi
@@ -0,0 +1,62 @@
+# Stubs for tracemalloc (Python 3.4+)
+
+def clear_traces() -> None: ...
+def get_object_traceback(obj: object) -> Optional[Traceback]: ...
+def get_traceback_limit() -> int: ...
+def get_traced_memory() -> Tuple[int, int]: ...
+def get_tracemalloc_memory() -> int: ...
+def is_tracing() -> bool: ...
+def start(nframe: int = ...) -> None: ...
+def stop() -> None: ...
+def take_snapshot() -> Snapshot: ...
+
+if sys.version_info >= (3, 6):
+    class DomainFilter:
+        inclusive: bool
+        domain: int
+        def __init__(self, inclusive: bool, domain: int) -> None: ...
+
+class Filter:
+    if sys.version_info >= (3, 6):
+        domain: Optional[int]
+    inclusive: bool
+    lineno: Optional[int]
+    filename_pattern: str
+    all_frames: bool
+    def __init__(self, inclusive: bool, filename_pattern: str, lineno: Optional[int] = ..., all_frames: bool = ..., domain: Optional[int] = ...) -> None: ...
+
+class Frame:
+    filename: str
+    lineno: int
+
+class Snapshot:
+    def compare_to(self, old_snapshot: Snapshot, key_type: str, cumulative: bool = ...) -> List[StatisticDiff]: ...
+    def dump(self, filename: str) -> None: ...
+    if sys.version_info >= (3, 6):
+        def filter_traces(self, filters: Sequence[Union[DomainFilter, Filter]]) -> Snapshot: ...
+    else:
+        def filter_traces(self, filters: Sequence[Filter]) -> Snapshot: ...
+    @classmethod
+    def load(cls, filename: str) -> Snapshot: ...
+    def statistics(self, key_type: str, cumulative: bool = ...) -> List[Statistic]: ...
+    traceback_limit: int
+    traces: Sequence[Trace]
+
+class Statistic:
+    count: int
+    size: int
+    traceback: Traceback
+
+class StatisticDiff:
+    count: int
+    count_diff: int
+    size: int
+    size_diff: int
+    traceback: Traceback
+
+class Trace:
+    size: int
+    traceback: Traceback
+
+class Traceback(Sequence[Frame]):
+    def format(limit: Optional[int] = ...) -> List[str]: ...
