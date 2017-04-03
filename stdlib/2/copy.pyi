0042ada8e2245a2a97accffc514fb12b164c36fe
David Euresti <david@dropbox.com>
2017-04-03 17:27:56 -0400

    Simplify and add missing types for copy module.
    Error and error were missing.  I also removed the internal arg memo.

--- stdlib/2/copy.pyi
+++ stdlib/2/copy.pyi
@@ -1,10 +1,11 @@
 # Stubs for copy
 
-# NOTE: These are incomplete!
-
-from typing import TypeVar, Dict, Any
+from typing import TypeVar
 
 _T = TypeVar('_T')
 
-def deepcopy(x: _T, memo: Dict[Any, Any] = ...) -> _T: ...
+# Note: deepcopy has other kwargs but they are only meant to be used internally.
+def deepcopy(x: _T) -> _T: ...
 def copy(x: _T) -> _T: ...
+class Error(Exception): ...
+error = Error
