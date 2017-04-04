fc5545b4258f551424f464e27ef083becbd6408d
David Euresti <david@dropbox.com>
2017-04-04 07:42:11 -0400

    Bring back internal kwargs

--- stdlib/2and3/copy.pyi
+++ stdlib/2and3/copy.pyi
@@ -1,11 +1,11 @@
 # Stubs for copy
 
-from typing import TypeVar
+from typing import TypeVar, Optional, Dict, Any
 
 _T = TypeVar('_T')
 
-# Note: deepcopy has other kwargs but they are only meant to be used internally.
-def deepcopy(x: _T) -> _T: ...
+# Note: memo and _nil are internal kwargs.
+def deepcopy(x: _T, memo: Optional[Dict[int, _T]] = ..., _nil: Any = ...) -> _T: ...
 def copy(x: _T) -> _T: ...
 class Error(Exception): ...
 error = Error
