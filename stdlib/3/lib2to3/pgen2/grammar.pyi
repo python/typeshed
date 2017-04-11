4a22323c5434eba0d6237b72187646d42a729b9f
Lukasz Langa <ambv@fb.com>
2017-04-11 16:39:04 -0700

    Respond to review, pgen2/grammar

--- stdlib/3/lib2to3/pgen2/grammar.pyi
+++ stdlib/3/lib2to3/pgen2/grammar.pyi
@@ -1,24 +1,26 @@
 # Stubs for lib2to3.pgen2.grammar (Python 3.6)
 
+from lib2to3.pgen2 import _Path
+
 from typing import Any, Dict, List, Optional, Text, Tuple, TypeVar
 
 _P = TypeVar('_P')
 _Label = Tuple[int, Optional[Text]]
-_States = List[List[Tuple[int, int]]]
+_DFA = List[List[Tuple[int, int]]]
 
 class Grammar:
     symbol2number: Dict[Text, int]
     number2symbol: Dict[int, Text]
-    states: _States
-    dfas: Dict[int, _States]
+    states: List[_DFA]
+    dfas: Dict[int, Tuple[_DFA, Dict[int, int]]]
     labels: List[_Label]
     keywords: Dict[Text, int]
     tokens: Dict[int, int]
     symbol2label: Dict[Text, int]
     start: int
     def __init__(self) -> None: ...
-    def dump(self, filename: Text) -> None: ...
-    def load(self, filename: Text) -> None: ...
+    def dump(self, filename: _Path) -> None: ...
+    def load(self, filename: _Path) -> None: ...
     def copy(self: _P) -> _P: ...
     def report(self) -> None: ...
 
