python/typeshed

commit ee0f341ecb18a6278f598eca1c0b2285c199dc72

stub for poplib (#1211)
* stub for poplib

* poplib: str -> Text

stdlib/2and3/poplib.pyi
@@ -1,0 +1,68 @@
+# Stubs for poplib (Python 2 and 3)
+
+from mypy_extensions import NoReturn
+import socket
+import ssl
+import sys
+from typing import Any, BinaryIO, Dict, List, Optional, Pattern, Text, Tuple
+
+class error_proto(Exception): pass
+
+POP3_PORT: int
+POP3_SSL_PORT: int
+CR: bytes
+LF: bytes
+CRLF: bytes
+
+
+class POP3:
+    if sys.version_info >= (3, 0):
+        encoding: Text
+
+    host: Text
+    port: int
+    sock: socket.socket
+    file: BinaryIO
+    welcome: bytes
+
+    def __init__(self, host: Text, port: int = ..., timeout: float = ...) -> None: ...
+    def getwelcome(self) -> bytes: ...
+    def set_debuglevel(self, level: int) -> None: ...
+    def user(self, user: Text) -> bytes: ...
+    def pass_(self, pswd: Text) -> bytes: ...
+    def stat(self) -> Tuple[int, int]: ...
+    def list(self, which: Optional[Text] = ...) -> bytes: ...
+    def retr(self, which: Text) -> bytes: ...
+    def dele(self, which: Text) -> bytes: ...
+    def noop(self) -> bytes: ...
+    def rset(self) -> bytes: ...
+    def quit(self) -> bytes: ...
+    def close(self) -> None: ...
+    def rpop(self, user: Text) -> bytes: ...
+
+    timestamp: Pattern[Text]
+
+    if sys.version_info < (3, 0):
+        def apop(self, user: Text, secret: Text) -> bytes: ...
+    else:
+        def apop(self, user: Text, password: Text) -> bytes: ...
+    def top(self, which: Text, howmuch: int) -> bytes: ...
+    def uidl(self, which: Optional[Text] = ...) -> bytes: ...
+    if sys.version_info >= (3, 5):
+        def utf8(self) -> bytes: ...
+    if sys.version_info >= (3, 4):
+        def capa(self) -> Dict[Text, List[Text]]: ...
+        def stls(self, context: Optional[ssl.SSLContext] = ...) -> bytes: ...
+
+
+class POP3_SSL(POP3):
+    if sys.version_info >= (3, 0):
+        def __init__(self, host: Text, port: int = ..., keyfile: Optional[Text] = ..., certfile: Optional[Text] = ...,
+                     timeout: float = ..., context: Optional[ssl.SSLContext] = ...) -> None: ...
+    else:
+        def __init__(self, host: Text, port: int = ..., keyfile: Optional[Text] = ..., certfile: Optional[Text] = ...,
+                     timeout: float = ...) -> None: ...
+
+    if sys.version_info >= (3, 4):
+        # "context" is actually the last argument, but that breaks LSP and it doesn't really matter because all the arguments are ignored
+        def stls(self, context: Any = ..., keyfile: Any = ..., certfile: Any = ...) -> bytes: ...

commit 7be1a6f1fd87cd3f17cb8beebe0e1a75278ff803

fix some types in poplib (#1225)
Fixes a problem introduced in #1211.

stdlib/2and3/poplib.pyi
@@ -6,6 +6,8 @@ import ssl
 import sys
 from typing import Any, BinaryIO, Dict, List, Optional, Pattern, Text, Tuple
 
+_LongResp = Tuple[bytes, List[bytes], int]
+
 class error_proto(Exception): pass
 
 POP3_PORT: int
@@ -31,9 +33,9 @@ class POP3:
     def user(self, user: Text) -> bytes: ...
     def pass_(self, pswd: Text) -> bytes: ...
     def stat(self) -> Tuple[int, int]: ...
-    def list(self, which: Optional[Text] = ...) -> bytes: ...
-    def retr(self, which: Text) -> bytes: ...
-    def dele(self, which: Text) -> bytes: ...
+    def list(self, which: Optional[Any] = ...) -> _LongResp: ...
+    def retr(self, which: Any) -> _LongResp: ...
+    def dele(self, which: Any) -> bytes: ...
     def noop(self) -> bytes: ...
     def rset(self) -> bytes: ...
     def quit(self) -> bytes: ...
@@ -46,8 +48,8 @@ class POP3:
         def apop(self, user: Text, secret: Text) -> bytes: ...
     else:
         def apop(self, user: Text, password: Text) -> bytes: ...
-    def top(self, which: Text, howmuch: int) -> bytes: ...
-    def uidl(self, which: Optional[Text] = ...) -> bytes: ...
+    def top(self, which: Any, howmuch: int) -> _LongResp: ...
+    def uidl(self, which: Optional[Any] = ...) -> _LongResp: ...
     if sys.version_info >= (3, 5):
         def utf8(self) -> bytes: ...
     if sys.version_info >= (3, 4):