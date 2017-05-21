728b97772930a4b96580af457795331821197751
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-05-21 14:31:22 -0700

    Replace a number of default argument values with "..." (#1280)
    pytype apparently doesn't like default values that aren't ints or
    None/True/False.

--- stdlib/3/multiprocessing/pool.pyi
+++ stdlib/3/multiprocessing/pool.pyi
@@ -5,49 +5,49 @@
 from typing import Any, Callable, Iterable, Mapping, Optional, Dict, List
 
 class AsyncResult():
-    def get(self, timeout: float = -1) -> Any: ...
-    def wait(self, timeout: float = -1) -> None: ...
+    def get(self, timeout: float = ...) -> Any: ...
+    def wait(self, timeout: float = ...) -> None: ...
     def ready(self) -> bool: ...
     def successful(self) -> bool: ...
 
 class ThreadPool():
     def __init__(self, processes: Optional[int] = None,
                  initializer: Optional[Callable[..., None]] = None,
-                 initargs: Iterable[Any] = ()) -> None: ...
+                 initargs: Iterable[Any] = ...) -> None: ...
     def apply(self,
               func: Callable[..., Any],
-              args: Iterable[Any]=(),
-              kwds: Dict[str, Any]=...) -> Any: ...
+              args: Iterable[Any] = ...,
+              kwds: Dict[str, Any] = ...) -> Any: ...
     def apply_async(self,
                 func: Callable[..., Any],
-                args: Iterable[Any]=(),
-                kwds: Dict[str, Any]=...,
+                args: Iterable[Any] = ...,
+                kwds: Dict[str, Any] = ...,
                 callback: Callable[..., None] = None,
                 error_callback: Callable[[BaseException], None] = None) -> AsyncResult: ...
     def map(self,
             func: Callable[..., Any],
-            iterable: Iterable[Any]=(),
+            iterable: Iterable[Any] = ...,
             chunksize: Optional[int] = None) -> List[Any]: ...
     def map_async(self, func: Callable[..., Any],
-                  iterable: Iterable[Any] = (),
+                  iterable: Iterable[Any] = ...,
                   chunksize: Optional[int] = None,
                   callback: Callable[..., None] = None,
                   error_callback: Callable[[BaseException], None] = None) -> AsyncResult: ...
     def imap(self,
              func: Callable[..., Any],
-             iterable: Iterable[Any]=(),
+             iterable: Iterable[Any] = ...,
              chunksize: Optional[int] = None) -> Iterable[Any]: ...
     def imap_unordered(self,
                        func: Callable[..., Any],
-                       iterable: Iterable[Any]=(),
+                       iterable: Iterable[Any] = ...,
                        chunksize: Optional[int] = None) -> Iterable[Any]: ...
     def starmap(self,
                 func: Callable[..., Any],
-                iterable: Iterable[Iterable[Any]]=(),
+                iterable: Iterable[Iterable[Any]] = ...,
                 chunksize: Optional[int] = None) -> List[Any]: ...
     def starmap_async(self,
                       func: Callable[..., Any],
-                      iterable: Iterable[Iterable[Any]] = (),
+                      iterable: Iterable[Iterable[Any]] = ...,
                       chunksize: Optional[int] = None,
                       callback: Callable[..., None] = None,
                       error_callback: Callable[[BaseException], None] = None) -> AsyncResult: ...
