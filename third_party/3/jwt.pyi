493044b9bbc630f9420181801205631e8c7b99c7
Atul Varma <varmaa@gmail.com>
2017-05-21 14:23:44 -0700

    Make jwt.pyi more compact, fix flake8 errors.

--- third_party/3/jwt.pyi
+++ third_party/3/jwt.pyi
@@ -1,63 +1,34 @@
 from typing import Mapping, Any, Optional, Union
 
 
-def decode(
-    jwt: Union[str, bytes],
-    key: Union[str, bytes]=...,
-    verify: bool=...,
-    algorithms: Optional[Any]=...,
-    options: Optional[Mapping[Any, Any]]=...,
-    **kwargs: Any) -> Mapping[str, Any]:
-    ...
+def decode(jwt: Union[str, bytes], key: Union[str, bytes]=...,
+           verify: bool=..., algorithms: Optional[Any]=...,
+           options: Optional[Mapping[Any, Any]]=...,
+           **kwargs: Any) -> Mapping[str, Any]: ...
 
+def encode(payload: Mapping[str, Any], key: Union[str, bytes],
+           algorithm: str=..., headers: Optional[Mapping[str, Any]]=...,
+           json_encoder: Optional[Any]=...) -> bytes: ...
 
-def encode(
-    payload: Mapping[str, Any],
-    key: Union[str, bytes],
-    algorithm: str=...,
-    headers: Optional[Mapping[str, Any]]=...,
-    json_encoder: Optional[Any]=...) -> bytes:
-    ...
+class InvalidTokenError(Exception): pass
 
+class DecodeError(InvalidTokenError): pass
 
-class InvalidTokenError(Exception):
-    pass
+class ExpiredSignatureError(InvalidTokenError): pass
 
+class InvalidAudienceError(InvalidTokenError): pass
 
-class DecodeError(InvalidTokenError):
-    pass
+class InvalidIssuerError(InvalidTokenError): pass
 
+class InvalidIssuedAtError(InvalidTokenError): pass
 
-class ExpiredSignatureError(InvalidTokenError):
-    pass
+class ImmatureSignatureError(InvalidTokenError): pass
 
+class InvalidKeyError(Exception): pass
 
-class InvalidAudienceError(InvalidTokenError):
-    pass
+class InvalidAlgorithmError(InvalidTokenError): pass
 
-
-class InvalidIssuerError(InvalidTokenError):
-    pass
-
-
-class InvalidIssuedAtError(InvalidTokenError):
-    pass
-
-
-class ImmatureSignatureError(InvalidTokenError):
-    pass
-
-
-class InvalidKeyError(Exception):
-    pass
-
-
-class InvalidAlgorithmError(InvalidTokenError):
-    pass
-
-
-class MissingRequiredClaimError(InvalidTokenError):
-    ...
+class MissingRequiredClaimError(InvalidTokenError): ...
 
 # Compatibility aliases (deprecated)
 ExpiredSignature = ExpiredSignatureError
