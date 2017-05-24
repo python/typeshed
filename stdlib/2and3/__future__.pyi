1a56d47b77ea020bd7586108a68975265bd44dcb
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-05-23 17:49:20 -0700

    complete __future__ stubs
    We were missing the most important one.

--- stdlib/2and3/__future__.pyi
+++ stdlib/2and3/__future__.pyi
@@ -1,16 +1,21 @@
 import sys
+from typing import List
 
 class _Feature:
     def getOptionalRelease(self) -> sys._version_info: ...
     def getMandatoryRelease(self) -> sys._version_info: ...
 
-absolute_import = ...  # type: _Feature
-division = ...  # type: _Feature
-generators = ...  # type: _Feature
-nested_scopes = ...  # type: _Feature
-print_function = ...  # type: _Feature
-unicode_literals = ...  # type: _Feature
-with_statement = ...  # type: _Feature
+absolute_import: _Feature
+division: _Feature
+generators: _Feature
+nested_scopes: _Feature
+print_function: _Feature
+unicode_literals: _Feature
+with_statement: _Feature
+if sys.version_info >= (3, 0):
+    barry_as_FLUFL: _Feature
 
-if sys.version_info[:2] >= (3, 5):
-    generator_stop = ...  # type: _Feature
+if sys.version_info >= (3, 5):
+    generator_stop: _Feature
+
+all_feature_names: List[str]
