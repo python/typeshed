1c105fdeecb3da02367196934e7f8f9e824132cf
Paige Kordas <kordas@google.com>
2017-06-05 12:03:07 -0700

    corrected types of file objects

--- stdlib/2/popen2.pyi
+++ stdlib/2/popen2.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Iterable, List, Optional, Union, Tuple, TypeVar
+from typing import Any, Iterable, List, Optional, Union, TextIO, Tuple, TypeVar
 
 _T = TypeVar('_T')
 
@@ -8,22 +8,22 @@ class Popen3:
     sts = ...  # type: int
     cmd = ...  # type: Iterable
     pid = ...  # type: int
-    tochild = ...  # type: file
-    fromchild = ...  # type: file
-    childerr = ...  # type: Optional[file]
+    tochild = ...  # type: TextIO
+    fromchild = ...  # type: TextIO
+    childerr = ...  # type: Optional[TextIO]
     def __init__(self, cmd: Iterable = ..., capturestderr: bool = ..., bufsize: int = ...) -> None: ...
     def __del__(self) -> None: ...
     def poll(self, _deadstate: _T = ...) -> Union[int, _T]: ...
     def wait(self) -> int: ...
 
 class Popen4(Popen3):
-    childerr = ...  # type: Optional[file]
+    childerr = ...  # type: None
     cmd = ...  # type: Iterable
     pid = ...  # type: int
-    tochild = ...  # type: file
-    fromchild = ...  # type: file
+    tochild = ...  # type: TextIO
+    fromchild = ...  # type: TextIO
     def __init__(self, cmd: Iterable = ..., bufsize: int = ...) -> None: ...
 
-def popen2(cmd: Iterable = ..., bufsize: int = ..., mode: str = ...) -> Tuple[file, file]: ...
-def popen3(cmd: Iterable = ..., bufsize: int = ..., mode: str = ...) -> Tuple[file, file, file]: ...
-def popen4(cmd: Iterable = ..., bufsize: int = ..., mode: str = ...) -> Tuple[file, file]: ...
+def popen2(cmd: Iterable = ..., bufsize: int = ..., mode: str = ...) -> Tuple[TextIO, TextIO]: ...
+def popen3(cmd: Iterable = ..., bufsize: int = ..., mode: str = ...) -> Tuple[TextIO, TextIO, TextIO]: ...
+def popen4(cmd: Iterable = ..., bufsize: int = ..., mode: str = ...) -> Tuple[TextIO, TextIO]: ...
