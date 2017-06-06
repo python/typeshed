16ec7cbdf001bbf318d0afa77c4790b4baf9b8af
rchen152 <rchen152@gmail.com>
2017-06-06 15:36:01 -0700

    Add __new__ to time.struct_time (#1394)
    This is needed because otherwise pytype uses the `__new__` inherited from the `NamedTuple` base class.

--- stdlib/2/time.pyi
+++ stdlib/2/time.pyi
@@ -17,6 +17,9 @@ class struct_time(NamedTuple('_struct_time',
     def __init__(self, o: Tuple[int, int, int,
                                 int, int, int,
                                 int, int, int], _arg: Any = ...) -> None: ...
+    def __new__(cls, o: Tuple[int, int, int,
+                              int, int, int,
+                              int, int, int], _arg: Any = ...) -> struct_time: ...
 
 _TIME_TUPLE = Tuple[int, int, int, int, int, int, int, int, int]
 
