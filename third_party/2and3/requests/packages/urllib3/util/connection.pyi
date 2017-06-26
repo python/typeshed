40d85eccdf5dfe23bdccdb68f3b81a1db9a4d974
Max Rozentsveyg <mxr@users.noreply.github.com>
2017-06-26 11:13:52 -0700

    Add type stub for requests.packages.urllib3.util.connection.HAS_IPV6
    This is defined at https://github.com/requests/requests/blob/v2.14.2/requests/packages/urllib3/util/connection.py#L130

--- third_party/2and3/requests/packages/urllib3/util/connection.pyi
+++ third_party/2and3/requests/packages/urllib3/util/connection.pyi
@@ -2,6 +2,7 @@ from typing import Any
 
 poll = ...  # type: Any
 select = ...  # type: Any
+HAS_IPV6 = ... # type: bool
 
 def is_connection_dropped(conn): ...
 def create_connection(address, timeout=..., source_address=..., socket_options=...): ...
