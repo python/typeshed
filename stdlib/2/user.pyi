4203d2bc94c5569c1463027439b5641a894145d6
Ashwini Chaudhary <monty.sinngh@gmail.com>
2017-07-02 19:47:01 +0530

    Added stub for user module

---
+++ stdlib/2/user.pyi
@@ -0,0 +1,10 @@
+# Stubs for user (Python 2)
+
+# Docs: https://docs.python.org/2/library/user.html
+# Source: https://hg.python.org/cpython/file/2.7/Lib/user.py
+from typing import BinaryIO, Text 
+
+
+home: str
+pythonrc: Union[bytes, Text]
+f: BinaryIO
