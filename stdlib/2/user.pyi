9f25b5e16e930ec687dd2da1fbdddc28fef1389b
Ashwini Chaudhary <monty.sinngh@gmail.com>
2017-07-02 20:50:10 +0530

    flake8

--- stdlib/2/user.pyi
+++ stdlib/2/user.pyi
@@ -2,7 +2,7 @@
 
 # Docs: https://docs.python.org/2/library/user.html
 # Source: https://hg.python.org/cpython/file/2.7/Lib/user.py
-from typing import BinaryIO, Text, Union 
+from typing import BinaryIO, Text, Union
 
 
 home: str
