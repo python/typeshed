2fd6a9448da170c9787e2a0236fa00023c530b93
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-07-02 15:09:40 -0700

    unused import, less whitespace, consistent types

--- stdlib/2/user.pyi
+++ stdlib/2/user.pyi
@@ -2,9 +2,8 @@
 
 # Docs: https://docs.python.org/2/library/user.html
 # Source: https://hg.python.org/cpython/file/2.7/Lib/user.py
-from typing import Any, Union
-
+from typing import Any
 
 def __getattr__(name) -> Any: ...  # type: ignore
 home: str
-pythonrc: bytes
+pythonrc: str
