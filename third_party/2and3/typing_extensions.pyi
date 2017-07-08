b9326df68ca203fbafad8550c210c09947422130
Michael Lee <michael.lee.0x2a@gmail.com>
2017-07-07 22:09:29 -0700

    Remove Collections backport
    See latest comment in https://github.com/python/typing/pull/443
    for justification.

--- third_party/2and3/typing_extensions.pyi
+++ third_party/2and3/typing_extensions.pyi
@@ -16,16 +16,6 @@ if sys.version_info >= (3, 5):
 if sys.version_info >= (3, 6):
     from typing import AsyncGenerator
 
-if sys.version_info >= (3, 6):
-    from typing import Collection
-else:
-    _T_co = typing.TypeVar('_T_co', covariant=True)
-    class Collection(typing.Sized,
-                     typing.Iterable[_T_co],
-                     typing.Container[_T_co],
-                     typing.Generic[_T_co]):
-        ...
-
 # Return type that indicates a function does not return.
 # This type is equivalent to the None type, but the no-op Union is necessary to
 # distinguish the None type from the None value.
