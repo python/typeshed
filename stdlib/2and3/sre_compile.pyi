a165c37b81aee2bc4655f812aeb6f86ec8d5ca2b
Ashwini Chaudhary <monty.sinngh@gmail.com>
2017-07-19 05:52:43 +0530

    Made it 2and3 compatible

--- stdlib/2and3/sre_compile.pyi
+++ stdlib/2and3/sre_compile.pyi
@@ -1,10 +1,13 @@
 # Source: https://hg.python.org/cpython/file/2.7/Lib/sre_compile.py
+# and https://github.com/python/cpython/blob/master/Lib/sre_compile.py
 
+import sys
 from sre_parse import SubPattern
 from typing import Any, Pattern, Tuple, Type, Union
 
 MAXCODE = ...  # type: int
-STRING_TYPES = ...  # type: Union[Tuple[Type[str]], Tuple[Type[str], Type[unicode]]]
+if sys.version_info < (3, 0):
+    STRING_TYPES = ...  # type: Union[Tuple[Type[str]], Tuple[Type[str], Type[unicode]]]
 
 def isstring(obj: Any) -> int: ...
 def compile(p: Union[str, SubPattern], flags: int = ...) -> Pattern: ...
