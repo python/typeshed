ccb20c09b39a51b20f16839058fb311f73695b77
Michael Lee <michael.lee.0x2a@gmail.com>
2017-07-20 08:32:10 -0700

    Add missing imports and enhance NoReturn comment
    Changes made:
    
    - Add missing `Type` re-export
    - Add missing `Awaitable` re-export
    - Add TODO note to `NoReturn`.

--- third_party/2and3/typing_extensions.pyi
+++ third_party/2and3/typing_extensions.pyi
@@ -8,6 +8,7 @@ from typing import Deque as Deque
 from typing import NewType as NewType
 from typing import overload as overload
 from typing import Text as Text
+from typing import Type as Type
 from typing import TYPE_CHECKING as TYPE_CHECKING
 
 if sys.version_info >= (3, 3):
@@ -17,6 +18,7 @@ if sys.version_info >= (3, 5):
     from typing import AsyncIterable as AsyncIterable
     from typing import AsyncIterator as AsyncIterator
     from typing import AsyncContextManager as AsyncContextManager
+    from typing import Awaitable as Awaitable
     from typing import Coroutine as Coroutine
 
 if sys.version_info >= (3, 6):
@@ -25,4 +27,7 @@ if sys.version_info >= (3, 6):
 # Return type that indicates a function does not return.
 # This type is equivalent to the None type, but the no-op Union is necessary to
 # distinguish the None type from the None value.
+#
+# TODO: Replace with direct import from typing once NoReturn is added to
+# typing.pyi.
 NoReturn = typing.Union[None]
