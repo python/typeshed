ad652378581c4ca035cfdef8940b4a148778e4fe
Michael Lee <michael.lee.0x2a@gmail.com>
2017-07-20 23:07:48 -0700

    Re-export NoReturn from typing.pyi

--- third_party/2and3/typing_extensions.pyi
+++ third_party/2and3/typing_extensions.pyi
@@ -6,6 +6,7 @@ from typing import Counter as Counter
 from typing import DefaultDict as DefaultDict
 from typing import Deque as Deque
 from typing import NewType as NewType
+from typing import NoReturn as NoReturn
 from typing import overload as overload
 from typing import Text as Text
 from typing import Type as Type
@@ -24,10 +25,3 @@ if sys.version_info >= (3, 5):
 if sys.version_info >= (3, 6):
     from typing import AsyncGenerator as AsyncGenerator
 
-# Return type that indicates a function does not return.
-# This type is equivalent to the None type, but the no-op Union is necessary to
-# distinguish the None type from the None value.
-#
-# TODO: Replace with direct import from typing once NoReturn is added to
-# typing.pyi.
-NoReturn = typing.Union[None]
