426182fe87213edbacf02c65701c63c2ff6af2a3
Pavel Pletenev <cpp.create@gmail.com>
2017-07-30 18:17:10 +0300

    Add type annotations for emoji

---
+++ third_party/2and3/emoji.pyi
@@ -0,0 +1,16 @@
+from typing import Tuple, Pattern, List, Dict, Union
+_DEFAULT_DELIMITER=':'
+def emojize(
+        string: str,
+        use_aliases: bool=False,
+        delimiters: Tuple[str, str]=(_DEFAULT_DELIMITER,_DEFAULT_DELIMITER)
+    ) -> str: ...
+
+def demojize(
+        string:str,
+        delimiters: Tuple[str, str]=(_DEFAULT_DELIMITER,_DEFAULT_DELIMITER)
+    ) -> str: ...
+
+def get_emoji_regexp() -> Pattern: ...
+
+def emoji_lis(string: str) -> List[Dict[str, Union[int, str]]]: ...
