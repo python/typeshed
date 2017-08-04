14f319b952710ad38ba528f416a95d98adef637b
Ashwini Chaudhary <monty.sinngh@gmail.com>
2017-08-04 06:07:56 +0530

    Text instead of str for filename

--- stdlib/2/whichdb.pyi
+++ stdlib/2/whichdb.pyi
@@ -1,8 +1,8 @@
 # Source: https://hg.python.org/cpython/file/2.7/Lib/whichdb.py
 
 from types import ModuleType
-from typing import Optional
+from typing import Optional, Text
 
 dbm = ...  # type: Optional[ModuleType]
 
-def whichdb(filename: str) -> Optional[str]: ...
+def whichdb(filename: Text) -> Optional[str]: ...
