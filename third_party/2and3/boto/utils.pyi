a8ff43a12806c435f9ab9133e3b6f58f8e47c6e4
Max Rozentsveyg <mxr@lyft.com>
2017-09-05 21:40:09 +0100

    fixes

--- third_party/2and3/boto/utils.pyi
+++ third_party/2and3/boto/utils.pyi
@@ -1,94 +1,106 @@
+import _thread
+import datetime
 import logging
 import logging.handlers
-import datetime
 import subprocess
-import thread
 import time
 
-import boto.provider
 import boto.connection
-import six
-from typing import Any, List, Hashable, Optional, Text, Union, Tuple
+import boto.provider
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Hashable,
+    Iterable,
+    IO,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    Union,
+)
 
 
-JSONDecodeError = ...  # type: Exception
-qsa_of_interest = ...  # type: List[Text]
+JSONDecodeError = ...  # type: ValueError
+qsa_of_interest = ...  # type: List[str]
 
-def unquote_v(nv: Text) -> Union[Text, Tuple[Text, Text]]: ...
+def unquote_v(nv: str) -> Union[str, Tuple[str, str]]: ...
 def canonical_string(
-    method: Text,
-    path: Text,
-    headers: Dict[Text, Text],
+    method: str,
+    path: str,
+    headers: Dict[str, str],
     expires: Optional[int] = ...,
     provider: Optional[boto.provider.Provider] = ...,
 ): ...
 def merge_meta(
-    headers: Dict[Text, Text],
-    metadata: Dict[Text, Text],
+    headers: Dict[str, str],
+    metadata: Dict[str, str],
     provider: Optional[boto.provider.Provider] = ...,
 ): ...
-def get_aws_metadata(headers: Dict[Text, Text], provider: Optional[boto.provider.Provider] = ...): ...
+def get_aws_metadata(headers: Dict[str, str], provider: Optional[boto.provider.Provider] = ...): ...
 def retry_url(
-    url: Text,
+    url: str,
     retry_on_404: bool = ...,
     num_retries: int = ...,
     timeout: Optional[int] = ...,
 ): ...
 
-class LazyLoadMetadata(dict):
+class LazyLoadMetadata(Dict):
     def __init__(
         self,
-        url: Text,
+        url: str,
         num_retries: int,
         timeout: Optional[int] = ...,
     ) -> None: ...
     def __getitem__(self, key: Hashable): ...
     def get(self, key: Hashable, default: Optional[Any] = ...) -> Any: ...
-    def values(self) -> List[Any]: ...
-    def items(self) -> List[Tuple[Any, Any]]: ...
+    def values(self): ...
+    def items(self): ...
 
 def get_instance_metadata(
-    version: Text = ...,
-    url: Text = ...,
-    data: Text = ...,
+    version: str = ...,
+    url: str = ...,
+    data: str = ...,
     timeout: Optional[int] = ...,
     num_retries: int = ...
 ) -> Optional[LazyLoadMetadata]: ...
 def get_instance_identity(
-    version: Text = ...,
-    url: Text = ...,
+    version: str = ...,
+    url: str = ...,
     timeout: Optional[int] = ...,
     num_retries: int = ...,
-) -> Optional[Dict[Text, Any]]: ...
+) -> Optional[Dict[str, Any]]: ...
 def get_instance_userdata(
-    version: Text = ...,
-    sep: Optional[Text] = ...,
-    url: Text = ...,
+    version: str = ...,
+    sep: Optional[str] = ...,
+    url: str = ...,
     timeout: Optional[int] = ...,
     num_retries: int = ...,
-) -> Dict[Text, Text]: ...
+) -> Dict[str, str]: ...
 
-ISO8601 = ...  # type: Text
-ISO8601_MS = ...  # type: Text
-RFC1123 = ...  # type: Text
-LOCALE_LOCK = ...  # type: therad.LockType
+ISO8601 = ...  # type: str
+ISO8601_MS = ...  # type: str
+RFC1123 = ...  # type: str
+LOCALE_LOCK = ...  # type: _thread.LockType
 
-def setlocale(name: Union[Text, Tuple[Text, Text]]): ...
-def get_ts(ts: Optional[time.struct_time] = ...) -> Text: ...
-def parse_ts(ts: Text) -> datetime.datetime: ...
-def find_class(module_name: Text, class_name: Optional[Text] = ...) -> Optional[Any]: ...
-def update_dme(username: Text, password: Text, dme_id: Text, ip_address: Text) -> Text: ...
+def setlocale(name: Union[str, Tuple[str, str]]): ...
+def get_ts(ts: Optional[time.struct_time] = ...) -> str: ...
+def parse_ts(ts: str) -> datetime.datetime: ...
+def find_class(module_name: str, class_name: Optional[str] = ...) -> Optional[Type[object]]: ...
+def update_dme(username: str, password: str, dme_id: str, ip_address: str) -> str: ...
 def fetch_file(
-    uri: Text,
-    file: Optional[file] = ...,
-    username: Optional[Text] = ...,
-    password: Optional[Text] = ...,
-) -> file: ...
+    uri: str,
+    file: Optional[IO] = ...,
+    username: Optional[str] = ...,
+    password: Optional[str] = ...,
+) -> IO: ...
 
 class ShellCommand:
     exit_code = ...  # type: int
     command = ...  # type: subprocess._CMD
-    log_fp = ...  # type: six.StringIO
+    log_fp = ...  # type: IO
     wait = ...  # type: bool
     fail_fast = ...  # type: bool
 
@@ -98,7 +110,7 @@ class ShellCommand:
         wait: bool = ...,
         fail_fast: bool = ...,
         cwd: Optional[subprocess._TXT] = ...,
-    ) -> None: ....
+    ) -> None: ...
 
     process = ...  # type: subprocess.Popen
 
@@ -106,29 +118,29 @@ class ShellCommand:
     def setReadOnly(self, value) -> None: ...
     def getStatus(self) -> Optional[int]: ...
 
-    status = ...  # type: Any
+    status = ...  # type: Optional[int]
 
-    def getOutput(self) -> Text: ...
+    def getOutput(self) -> str: ...
 
-    output = ...  # type: Any
+    output = ...  # type: str
 
 class AuthSMTPHandler(logging.handlers.SMTPHandler):
-    username = ...  # type: Text
-    password = ...  # type: Text
+    username = ...  # type: str
+    password = ...  # type: str
     def __init__(
         self,
-        mailhost: Text,
-        username: Text,
-        password: Text,
-        fromaddr: Text,
-        toaddrs: Sequence[Text],
-        subject: Text,
+        mailhost: str,
+        username: str,
+        password: str,
+        fromaddr: str,
+        toaddrs: Sequence[str],
+        subject: str,
     ) -> None: ...
-    def emit(self, record: logging.Record) -> None: ...
+    def emit(self, record: logging.LogRecord) -> None: ...
 
-class LRUCache(dict):
+class LRUCache(Dict):
     class _Item:
-        previous = ...  # type: Optional[_Item]
+        previous = ...  # type: Optional[Any]
         key = ...  # type: Hashable
         value = ...  # type: Any
         def __init__(self, key, value) -> None: ...
@@ -138,7 +150,7 @@ class LRUCache(dict):
     tail = ...  # type: Optional[_Item]
 
     def __init__(self, capacity: int) -> None: ...
-    def __contains__(self, key: Hashable) -> bool: ...
+    def __contains__(self, key) -> bool: ...
     def __iter__(self): ...
     def __len__(self) -> int: ...
     def __getitem__(self, key: Hashable) -> Any: ...
@@ -146,48 +158,48 @@ class LRUCache(dict):
 
 class Password:
     hashfunc = ...  # type: Callable
-    str = ...  # type: Optional[Text]
+    str = ...  # type: Optional[str]
 
     def __init__(
         self,
-        str: Optional[Text] = ...,
+        str: Optional[str] = ...,
         hashfunc: Optional[Callable] = ...,
     ) -> None: ...
-    def set(self, value: Optional[bytes, Text]): ...
+    def set(self, value: Union[bytes, str]): ...
     def __eq__(self, other: Any) -> bool: ...
     def __len__(self) -> bool: ...
 
 def notify(
-    subject: Text,
-    body: Optional[Text] = ...,
-    html_body: Optional[Union[Sequence[Text], Text]] = ...,
-    to_string: Optional[Text] = ...,
+    subject: str,
+    body: Optional[str] = ...,
+    html_body: Optional[Union[Sequence[str], str]] = ...,
+    to_string: Optional[str] = ...,
     attachments: Optional[Iterable] = ...,
     append_instance_id: bool = ...,
 ): ...
-def get_utf8_value(value: Text) -> Text: ...
+def get_utf8_value(value: str) -> str: ...
 def mklist(value: Any) -> List: ...
-def pythonize_name(name: Text) -> Text: ...
+def pythonize_name(name: str) -> str: ...
 def write_mime_multipart(
-    content: List[Tuple[Text, Text]],
+    content: List[Tuple[str, str]],
     compress: bool = ...,
-    deftype: Text = ...,
-    delimiter: Text = ...,
+    deftype: str = ...,
+    delimiter: str = ...,
 ): ...
-def guess_mime_type(content: Text, deftype: Text) -> Text: ...
+def guess_mime_type(content: str, deftype: str) -> str: ...
 def compute_md5(
-    fp: file,
+    fp: IO,
     buf_size: int = ...,
     size: Optional[int] = ...,
-) -> Tuple[Text, Text, int]: ...
+) -> Tuple[str, str, int]: ...
 def compute_hash(
-    fp: file,
+    fp: IO,
     buf_size: int = ...,
     size: Optional[int] = ...,
     hash_algorithm: Any = ...,
-) -> Tuple[Text, Text, int]: ...
-def find_matching_headers(name: Text, headers: Dict[Text, Text]) -> List[Text]: ...
-def merge_headers_by_name(name: Text, headers: Dict[Text, Text]) -> Text: ...
+) -> Tuple[str, str, int]: ...
+def find_matching_headers(name: str, headers: Dict[str, str]) -> List[str]: ...
+def merge_headers_by_name(name: str, headers: Dict[str, str]) -> str: ...
 
 class RequestHook:
     def handle_request_data(
@@ -197,5 +209,5 @@ class RequestHook:
         error: bool = ...,
     ): ...
 
-def host_is_ipv6(hostname: Text) -> bool: ...
-def parse_host(hostname: Text) -> Text: ...
+def host_is_ipv6(hostname: str) -> bool: ...
+def parse_host(hostname: str) -> str: ...
