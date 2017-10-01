547ec21ae5ac29a76b3ae7c88da2a08300ed60dc
Michael Lee <mlee42@cs.washington.edu>
2017-09-30 20:16:25 -0700

    Address final flake8 errors

--- stdlib/2/textwrap.pyi
+++ stdlib/2/textwrap.pyi
@@ -12,16 +12,16 @@ class TextWrapper(object):
     break_on_hyphens: bool = ...
 
     def __init__(
-        self,
-        width: int = ...,
-        expand_tabs: bool = ...,
-        replace_whitespace: bool = ...,
-        drop_whitespace: bool = ...,
-        initial_indent: str = ...,
-        subsequent_indent: str = ...,
-        fix_sentence_endings: bool = ...,
-        break_long_words: bool = ...,
-        break_on_hyphens: bool = ...) -> None:
+            self,
+            width: int = ...,
+            expand_tabs: bool = ...,
+            replace_whitespace: bool = ...,
+            drop_whitespace: bool = ...,
+            initial_indent: str = ...,
+            subsequent_indent: str = ...,
+            fix_sentence_endings: bool = ...,
+            break_long_words: bool = ...,
+            break_on_hyphens: bool = ...) -> None:
         ...
 
     def wrap(self, text: AnyStr) -> List[AnyStr]:
