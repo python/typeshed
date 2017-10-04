0341d2197494f4d2cc5df2debaddc20f680de1ef
Brandon Carpenter <hashstat@yahoo.com>
2017-10-04 10:46:44 -0700

    salt argument to crypt() may be _Method

--- stdlib/2and3/crypt.pyi
+++ stdlib/2and3/crypt.pyi
@@ -1,5 +1,5 @@
 import sys
-from typing import List, NamedTuple, Optional
+from typing import List, NamedTuple, Optional, Union
 
 
 if sys.version_info >= (3, 3):
@@ -13,6 +13,6 @@ if sys.version_info >= (3, 3):
     methods: List[_Method]
 
     def mksalt(method: Optional[_Method] = ...) -> str: ...
-    def crypt(word: str, salt: Optional[str] = ...) -> str: ...
+    def crypt(word: str, salt: Optional[Union[str, _Method]] = ...) -> str: ...
 else:
     def crypt(word: str, salt: str) -> str: ...
