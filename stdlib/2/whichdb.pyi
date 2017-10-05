ee982e8413fb7f48b43afd73a8654a247afc9940
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-10-04 21:50:57 -0700

    remove unnecessary "dbm" attribute

--- stdlib/2/whichdb.pyi
+++ stdlib/2/whichdb.pyi
@@ -1,8 +1,5 @@
 # Source: https://hg.python.org/cpython/file/2.7/Lib/whichdb.py
 
-from types import ModuleType
 from typing import Optional, Text
-
-dbm = ...  # type: Optional[ModuleType]
 
 def whichdb(filename: Text) -> Optional[str]: ...
