99cc86c46701c86f9b3a3188fce0d6dcba6c9592
Josh Staiger <joshstaiger@gmail.com>
2017-11-03 12:12:59 -0700

    Base concurrent.futures.Executor on ContextManager
    Update the concurrent.futures so that Executor, ThreadPoolExecutor, and ProcessPoolExecutor inherit from the typing.ContextManager[T] generic introduced in Python 3.6.
    
    Specifically, this solves a problem in code such as:
    
    with ThreadPoolExecutor() as p: ...
    
    Where the p variable would be typed as an abstract `Executor`, rather than the specific `ThreadPoolExecutor` as expected.

--- stdlib/3/concurrent/futures/process.pyi
+++ stdlib/3/concurrent/futures/process.pyi
@@ -1,9 +1,17 @@
 from typing import Optional, Any
+
+import sys
+
 from ._base import Future, Executor
 
 EXTRA_QUEUED_CALLS = ...  # type: Any
 
 class BrokenProcessPool(RuntimeError): ...
 
-class ProcessPoolExecutor(Executor):
-    def __init__(self, max_workers: Optional[int] = ...) -> None: ...
+if sys.version_info >= (3,6):
+    class ProcessPoolExecutor(Executor[ProcessPoolExecutor]):
+        def __init__(self, max_workers: Optional[int] = ...) -> None: ...
+
+else:
+    class ProcessPoolExecutor(Executor):
+        def __init__(self, max_workers: Optional[int] = ...) -> None: ...
--- stdlib/3/concurrent/futures/thread.pyi
+++ stdlib/3/concurrent/futures/thread.pyi
@@ -2,9 +2,10 @@ from typing import Optional
 from ._base import Executor, Future
 import sys
 
-class ThreadPoolExecutor(Executor):
-    if sys.version_info >= (3, 6):
-        def __init__(self, max_workers: Optional[int] = ...,
-                     thread_name_prefix: str = ...) -> None: ...
-    else:
+if sys.version_info >= (3, 6):
+    class ThreadPoolExecutor(Executor[ThreadPoolExecutor]):
+        def __init__(self, max_workers: Optional[int] = ..., thread_name_prefix: str = ...) -> None: ...
+
+else:
+    class ThreadPoolExecutor(Executor):
         def __init__(self, max_workers: Optional[int] = ...) -> None: ...
