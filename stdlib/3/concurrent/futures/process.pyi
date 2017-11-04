89ec503d65d372dd78966b9f813101d2eefe556c
Josh Staiger <joshstaiger@gmail.com>
2017-11-04 13:11:34 -0700

    Revert "Base concurrent.futures.Executor on ContextManager"
    This reverts commit 99cc86c46701c86f9b3a3188fce0d6dcba6c9592.

--- stdlib/3/concurrent/futures/process.pyi
+++ stdlib/3/concurrent/futures/process.pyi
@@ -1,17 +1,9 @@
 from typing import Optional, Any
-
-import sys
-
 from ._base import Future, Executor
 
 EXTRA_QUEUED_CALLS = ...  # type: Any
 
 class BrokenProcessPool(RuntimeError): ...
 
-if sys.version_info >= (3,6):
-    class ProcessPoolExecutor(Executor[ProcessPoolExecutor]):
-        def __init__(self, max_workers: Optional[int] = ...) -> None: ...
-
-else:
-    class ProcessPoolExecutor(Executor):
-        def __init__(self, max_workers: Optional[int] = ...) -> None: ...
+class ProcessPoolExecutor(Executor):
+    def __init__(self, max_workers: Optional[int] = ...) -> None: ...
--- stdlib/3/concurrent/futures/thread.pyi
+++ stdlib/3/concurrent/futures/thread.pyi
@@ -2,10 +2,9 @@ from typing import Optional
 from ._base import Executor, Future
 import sys
 
-if sys.version_info >= (3, 6):
-    class ThreadPoolExecutor(Executor[ThreadPoolExecutor]):
-        def __init__(self, max_workers: Optional[int] = ..., thread_name_prefix: str = ...) -> None: ...
-
-else:
-    class ThreadPoolExecutor(Executor):
+class ThreadPoolExecutor(Executor):
+    if sys.version_info >= (3, 6):
+        def __init__(self, max_workers: Optional[int] = ...,
+                     thread_name_prefix: str = ...) -> None: ...
+    else:
         def __init__(self, max_workers: Optional[int] = ...) -> None: ...
