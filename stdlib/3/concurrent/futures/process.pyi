cacaa2c986d32967aa3d297927a37c86bc3ce0a3
Josh Staiger <joshstaiger@gmail.com>
2017-11-04 13:10:52 -0700

    Revert "Fix flake8 complaint."
    This reverts commit 260c1ba23d15360d85d210bb4d916a4cb31975e2.

--- stdlib/3/concurrent/futures/process.pyi
+++ stdlib/3/concurrent/futures/process.pyi
@@ -8,7 +8,7 @@ EXTRA_QUEUED_CALLS = ...  # type: Any
 
 class BrokenProcessPool(RuntimeError): ...
 
-if sys.version_info >= (3, 6):
+if sys.version_info >= (3,6):
     class ProcessPoolExecutor(Executor[ProcessPoolExecutor]):
         def __init__(self, max_workers: Optional[int] = ...) -> None: ...
 
