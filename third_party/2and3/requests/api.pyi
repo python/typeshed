2e9ce4c5ee7d21fefeb50e0c6cb4d0231cc20c1c
Roy Williams <rwilliams@lyft.com>
2017-12-07 14:16:23 -0800

    Fix lint

--- third_party/2and3/requests/api.pyi
+++ third_party/2and3/requests/api.pyi
@@ -12,17 +12,17 @@ else:
 
 _ParamsMappingValueType = Union[_Text, bytes, int, float, Iterable[Union[_Text, bytes, int, float]]]
 _Data = Union[
-        None,
-        bytes,
-        MutableMapping[str, str],
-        MutableMapping[str, Text],
-        MutableMapping[Text, str],
-        MutableMapping[Text, Text],
-        Iterable[Tuple[str, str]],
-        Iterable[Tuple[str, Text]],
-        Iterable[Tuple[Text, str]],
-        Iterable[Tuple[Text, Text]],
-        IO
+    None,
+    bytes,
+    MutableMapping[str, str],
+    MutableMapping[str, Text],
+    MutableMapping[Text, str],
+    MutableMapping[Text, Text],
+    Iterable[Tuple[str, str]],
+    Iterable[Tuple[str, Text]],
+    Iterable[Tuple[Text, str]],
+    Iterable[Tuple[Text, Text]],
+    IO
 ]
 
 def request(method: str, url: str, **kwargs) -> Response: ...
@@ -38,7 +38,7 @@ def get(url: Union[_Text, bytes],
         **kwargs) -> Response: ...
 def options(url: _Text, **kwargs) -> Response: ...
 def head(url: _Text, **kwargs) -> Response: ...
-def post(url:_Text, data: _Data=..., json=..., **kwargs) -> Response: ...
+def post(url: _Text, data: _Data=..., json=..., **kwargs) -> Response: ...
 def put(url: _Text, data: _Data=..., json=..., **kwargs) -> Response: ...
 def patch(url: _Text, data: _Data=..., json=..., **kwargs) -> Response: ...
 def delete(url: _Text, **kwargs) -> Response: ...
