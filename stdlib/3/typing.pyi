f07742b5f947a73d18daf60e07d552f8f3742728
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-02-08 19:11:51 +0100

    Add missing private field to NamedTuple
    It is very useful for doing introspection.

--- stdlib/3/typing.pyi
+++ stdlib/3/typing.pyi
@@ -535,6 +535,7 @@ def cast(tp: Type[_T], obj: Any) -> _T: ...
 
 # NamedTuple is special-cased in the type checker
 class NamedTuple(tuple):
+    _field_types = ...  # type: collections.OrderedDict[str, Type[Any]]
     _fields = ...  # type: Tuple[str, ...]
     _source = ...  # type: str
 
