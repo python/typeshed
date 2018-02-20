0190987e1a0dfcd1ca080aba64cc3dae5c73f80a
David Euresti <david@euresti.com>
2018-02-19 19:06:47 -0800

    Better type for click 'type' argument
    The call accepts tuples of types and callback methods

--- third_party/2and3/click/types.pyi
+++ third_party/2and3/click/types.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, IO, Iterable, List, Optional, TypeVar, Union
+from typing import Any, Callable, IO, Iterable, List, Optional, TypeVar, Union, Tuple as PyTuple
 import uuid
 
 from click.core import Context, Parameter
@@ -270,7 +270,9 @@ class UUIDParameterType(ParamType):
         ...
 
 
-def convert_type(ty: Any, default: Optional[Any] = ...) -> ParamType:
+_ConvertibleType = Union[type, ParamType, PyTuple[type, ...], Callable[[Optional[str]], Any]]
+
+def convert_type(ty: Optional[_ConvertibleType], default: Optional[Any] = ...) -> ParamType:
     ...
 
 # parameter type shortcuts
