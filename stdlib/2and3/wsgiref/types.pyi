2162b936c0a5f930e40183985c4c7866b569f581
Sebastian Rittau <srittau@rittau.biz>
2018-05-17 17:39:59 +0200

    Use typing.Text

--- stdlib/2and3/wsgiref/types.pyi
+++ stdlib/2and3/wsgiref/types.pyi
@@ -16,25 +16,23 @@
 # hinting your code.  Otherwise Python will raise NameErrors.
 
 import sys
-from typing import Callable, Dict, Iterable, List, Optional, Tuple, Type, Union, Any
+from typing import Callable, Dict, Iterable, List, Optional, Tuple, Type, Union, Any, Text
 from types import TracebackType
 
 _exc_info = Tuple[Optional[Type[BaseException]],
                   Optional[BaseException],
                   Optional[TracebackType]]
 if sys.version_info < (3,):
-    _Text = Union[unicode, str]
-    _BText = _Text
+    _BText = Text
 else:
-    _Text = str
     _BText = Union[bytes, str]
-WSGIEnvironment = Dict[_Text, Any]
+WSGIEnvironment = Dict[Text, Any]
 WSGIApplication = Callable[
     [
         WSGIEnvironment,
         Union[
-            Callable[[_Text, List[Tuple[_Text, _Text]]], Callable[[_BText], None]],
-            Callable[[_Text, List[Tuple[_Text, _Text]], _exc_info], Callable[[_BText], None]]
+            Callable[[Text, List[Tuple[Text, Text]]], Callable[[_BText], None]],
+            Callable[[Text, List[Tuple[Text, Text]], _exc_info], Callable[[_BText], None]]
         ]
     ],
     Iterable[_BText]
