48904699ec005a4b364eaa23dbe7952e9ebfd668
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-06-05 10:58:39 -0400

    urlretrieve can accept filename: pathlib.Path for Python>=3.6 (#2194)

--- stdlib/3/urllib/request.pyi
+++ stdlib/3/urllib/request.pyi
@@ -10,7 +10,7 @@ from email.message import Message
 from urllib.response import addinfourl
 import ssl
 import sys
-
+import os
 
 _T = TypeVar('_T')
 _UrlopenRet = Union[HTTPResponse, addinfourl]
@@ -187,10 +187,14 @@ class HTTPErrorProcessor(BaseHandler):
     def http_response(self) -> _UrlopenRet: ...
     def https_response(self) -> _UrlopenRet: ...
 
-
-def urlretrieve(url: str, filename: Optional[str] = ...,
-                reporthook: Optional[Callable[[int, int, int], None]] = ...,
-                data: Optional[bytes] = ...) -> Tuple[str, HTTPMessage]: ...
+if sys.version_info >= (3, 6):
+    def urlretrieve(url: str, filename: Optional[Union[str, os.PathLike]] = ...,
+                    reporthook: Optional[Callable[[int, int, int], None]] = ...,
+                    data: Optional[bytes] = ...) -> Tuple[str, HTTPMessage]: ...
+else:
+    def urlretrieve(url: str, filename: Optional[str] = ...,
+                    reporthook: Optional[Callable[[int, int, int], None]] = ...,
+                    data: Optional[bytes] = ...) -> Tuple[str, HTTPMessage]: ...
 def urlcleanup() -> None: ...
 
 class URLopener:
