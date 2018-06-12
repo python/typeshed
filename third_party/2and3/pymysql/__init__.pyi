054aabc2c9638b173db694cc7d090f20d00d627c
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2018-06-11 21:13:34 -0700

    split up long lines

--- third_party/2and3/pymysql/__init__.pyi
+++ third_party/2and3/pymysql/__init__.pyi
@@ -2,8 +2,27 @@ from typing import Union, Tuple, Callable
 from .connections import Connection
 from .constants import FIELD_TYPE as FIELD_TYPE
 from .converters import escape_dict as escape_dict, escape_sequence as escape_sequence, escape_string as escape_string
-from .err import Warning as Warning, Error as Error, InterfaceError as InterfaceError, DataError as DataError, DatabaseError as DatabaseError, OperationalError as OperationalError, IntegrityError as IntegrityError, InternalError as InternalError, NotSupportedError as NotSupportedError, ProgrammingError as ProgrammingError, MySQLError as MySQLError
-from .times import Date as Date, Time as Time, Timestamp as Timestamp, DateFromTicks as DateFromTicks, TimeFromTicks as TimeFromTicks, TimestampFromTicks as TimestampFromTicks
+from .err import (
+    Warning as Warning,
+    Error as Error,
+    InterfaceError as InterfaceError,
+    DataError as DataError,
+    DatabaseError as DatabaseError,
+    OperationalError as OperationalError,
+    IntegrityError as IntegrityError,
+    InternalError as InternalError,
+    NotSupportedError as NotSupportedError,
+    ProgrammingError as ProgrammingError,
+    MySQLError as MySQLError,
+)
+from .times import (
+    Date as Date,
+    Time as Time,
+    Timestamp as Timestamp,
+    DateFromTicks as DateFromTicks,
+    TimeFromTicks as TimeFromTicks,
+    TimestampFromTicks as TimestampFromTicks,
+)
 
 threadsafety: int
 apilevel: str
@@ -21,15 +40,12 @@ DATE: DBAPISet
 TIME: DBAPISet
 TIMESTAMP: DBAPISet
 ROWID: DBAPISet
-
 def Binary(x) -> Union[bytearray, bytes]: ...
 def Connect(*args, **kwargs) -> Connection: ...
 def get_client_info() -> str: ...
 
 connect: Callable[..., Connection]
 
-
 version_info: Tuple[int, int, int, str, int]
 NULL: str
-
 def install_as_MySQLdb() -> None: ...
