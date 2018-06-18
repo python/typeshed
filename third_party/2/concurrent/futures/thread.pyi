1a8455b444e9866e824da72c5c5ee59b0e4a4f66
Daniel Li <daniel.li@deshaw.com>
2018-06-17 21:33:37 -0400

    Add thread_name_prefix to ThreadPoolExecutor stub (#2249)
    The thread_name_prefix argument of ThreadPoolExecutor.__init__ was added
    to the Python 2 backport in agronholm/pythonfutures#64.

--- third_party/2/concurrent/futures/thread.pyi
+++ third_party/2/concurrent/futures/thread.pyi
@@ -3,7 +3,7 @@ from ._base import Executor, Future
 import sys
 
 class ThreadPoolExecutor(Executor):
-    if sys.version_info >= (3, 6):
+    if sys.version_info >= (3, 6) or sys.version_info < (3,):
         def __init__(self, max_workers: Optional[int] = ...,
                      thread_name_prefix: str = ...) -> None: ...
     else:
