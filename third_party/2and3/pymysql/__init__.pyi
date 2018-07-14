7d3ec1e0be987fdb0ad133f79540b651981e3cd8
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2018-07-13 19:43:09 -0700

    Improvements to pymysql (#2220)
    * don't use type comments
    
    * split up long lines
    
    * all the constants are ints
    
    * fixes to pymysql stub
    
    * drop __all__, fix tabs

--- third_party/2and3/pymysql/__init__.pyi
+++ third_party/2and3/pymysql/__init__.pyi
@@ -1,35 +1,61 @@
+import sys
 from typing import Union, Tuple, Callable
-from .connections import Connection
+
+from .connections import Connection as _Connection
 from .constants import FIELD_TYPE as FIELD_TYPE
 from .converters import escape_dict as escape_dict, escape_sequence as escape_sequence, escape_string as escape_string
-from .err import Warning as Warning, Error as Error, InterfaceError as InterfaceError, DataError as DataError, DatabaseError as DatabaseError, OperationalError as OperationalError, IntegrityError as IntegrityError, InternalError as InternalError, NotSupportedError as NotSupportedError, ProgrammingError as ProgrammingError, MySQLError as MySQLError
-from .times import Date as Date, Time as Time, Timestamp as Timestamp, DateFromTicks as DateFromTicks, TimeFromTicks as TimeFromTicks, TimestampFromTicks as TimestampFromTicks
+from .err import (
+    Warning as Warning,
+    Error as Error,
+    InterfaceError as InterfaceError,
+    DataError as DataError,
+    DatabaseError as DatabaseError,
+    OperationalError as OperationalError,
+    IntegrityError as IntegrityError,
+    InternalError as InternalError,
+    NotSupportedError as NotSupportedError,
+    ProgrammingError as ProgrammingError,
+    MySQLError as MySQLError,
+)
+from .times import (
+    Date as Date,
+    Time as Time,
+    Timestamp as Timestamp,
+    DateFromTicks as DateFromTicks,
+    TimeFromTicks as TimeFromTicks,
+    TimestampFromTicks as TimestampFromTicks,
+)
 
-threadsafety = ...  # type: int
-apilevel = ...  # type: str
-paramstyle = ...  # type: str
+threadsafety: int
+apilevel: str
+paramstyle: str
 
-class DBAPISet(frozenset):
+class DBAPISet(frozenset[int]):
     def __ne__(self, other) -> bool: ...
     def __eq__(self, other) -> bool: ...
     def __hash__(self) -> int: ...
 
-STRING = ...  # type: DBAPISet
-BINARY = ...  # type: DBAPISet
-NUMBER = ...  # type: DBAPISet
-DATE = ...  # type: DBAPISet
-TIME = ...  # type: DBAPISet
-TIMESTAMP = ...  # type: DBAPISet
-ROWID = ...  # type: DBAPISet
+STRING: DBAPISet
+BINARY: DBAPISet
+NUMBER: DBAPISet
+DATE: DBAPISet
+TIME: DBAPISet
+TIMESTAMP: DBAPISet
+DATETIME: DBAPISet
+ROWID: DBAPISet
 
-def Binary(x) -> Union[bytearray, bytes]: ...
-def Connect(*args, **kwargs) -> Connection: ...
+if sys.version_info >= (3, 0):
+    def Binary(x) -> bytes: ...
+else:
+    def Binary(x) -> bytearray: ...
+def Connect(*args, **kwargs) -> _Connection: ...
 def get_client_info() -> str: ...
 
-connect = ...  # type: Callable[..., Connection]
+connect: Callable[..., _Connection]
+Connection: Callable[..., _Connection]
+__version__: str
+version_info: Tuple[int, int, int, str, int]
+NULL: str
 
-
-version_info = ...  # type: Tuple[int, int, int, str, int]
-NULL = ...  # type: str
-
+def thread_safe() -> bool: ...
 def install_as_MySQLdb() -> None: ...
