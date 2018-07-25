46f34fed82538632fe05c810372b59010dc1c7ab
Chelsea Voss <voss.chelsea@gmail.com>
2018-07-25 11:38:24 -0700

    Allow DictReader values to be None (#2346)

--- stdlib/2and3/csv.pyi
+++ stdlib/2and3/csv.pyi
@@ -52,9 +52,9 @@ if sys.version_info >= (3,):
         quoting = ...  # type: int
 
 if sys.version_info >= (3, 6):
-    _DRMapping = OrderedDict[str, str]
+    _DRMapping = OrderedDict[str, Optional[str]]
 else:
-    _DRMapping = Dict[str, str]
+    _DRMapping = Dict[str, Optional[str]]
 
 
 class DictReader(Iterator[_DRMapping]):
