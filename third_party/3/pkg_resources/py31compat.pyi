12e0fe66252486423f20a109478f1613f89a5902
Teddy Sudol <tsudol@google.com>
2018-09-12 09:13:56 -0700

    Update version check in in py31compat.pyi
    _makedirs_31 is only used in the source file for Py2 and Py3.4.0. Since 3.4.0 is not supported, the version check can be simplified.

--- third_party/3/pkg_resources/py31compat.pyi
+++ third_party/3/pkg_resources/py31compat.pyi
@@ -8,7 +8,7 @@ def _makedirs_31(path: Text, exist_ok: bool = ...) -> None: ...
 
 # _makedirs_31 has special behavior to handle an edge case that was removed in
 # 3.4.1. No one should be using 3.4 instead of 3.4.1, so this should be fine.
-if sys.version_info >= (3, 4):
+if sys.version_info >= (3,):
     makedirs = os.makedirs
 else:
     makedirs = _makedirs_31
