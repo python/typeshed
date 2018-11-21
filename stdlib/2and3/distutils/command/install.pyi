f02b62f7edf7b1eca388e3ddb1aa2a86d03744a0
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2018-11-21 23:55:46 +0300

    add some distutils.dist stubs

---
+++ stdlib/2and3/distutils/command/install.pyi
@@ -0,0 +1,10 @@
+from distutils.cmd import Command
+from typing import Union, Optional
+
+
+class install(Command):
+    user: Union[int, bool]
+    prefix: Optional[str]
+    home: Optional[str]
+    root: Optional[str]
+    install_lib: Optional[str]
