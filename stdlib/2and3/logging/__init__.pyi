dc72d7118e16ac1283cf3961d59009d09425d25e
Florian Bruhin <me@the-compiler.org>
2018-11-26 21:11:21 +0100

    logging: Use _ArgsType for logging.makeRecord
    The "args" argument is passed to LogMessage, so passing a tuple in is fine as
    well.

--- stdlib/2and3/logging/__init__.pyi
+++ stdlib/2and3/logging/__init__.pyi
@@ -114,7 +114,7 @@ class Logger(Filterer):
     def handle(self, record: LogRecord) -> None: ...
     if sys.version_info >= (3,):
         def makeRecord(self, name: str, lvl: int, fn: str, lno: int, msg: Any,
-                       args: Mapping[str, Any],
+                       args: _ArgsType,
                        exc_info: Optional[_SysExcInfoType],
                        func: Optional[str] = ...,
                        extra: Optional[Mapping[str, Any]] = ...,
@@ -122,7 +122,7 @@ class Logger(Filterer):
     else:
         def makeRecord(self,
                        name: str, lvl: int, fn: str, lno: int, msg: Any,
-                       args: Mapping[str, Any],
+                       args: _ArgsType,
                        exc_info: Optional[_SysExcInfoType],
                        func: Optional[str] = ...,
                        extra: Optional[Mapping[str, Any]] = ...) -> LogRecord: ...
