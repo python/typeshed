fb8b77e71536fb51e62cdfeb7a226965b3441d88
Daniel Mouritzen <dmrtzn@gmail.com>
2018-12-21 04:44:30 +0100

    Set correct type for _subparsersaction.choices (#2702)
    choices=_name_parser_map, see https://github.com/python/cpython/blob/a11d44056e4f9b64d28efec295e1c1c45d4cb9e1/Lib/argparse.py#L1097

--- stdlib/2and3/argparse.pyi
+++ stdlib/2and3/argparse.pyi
@@ -383,6 +383,7 @@ class _SubParsersAction(Action):
     _prog_prefix: _Text
     _parser_class: Type[ArgumentParser]
     _name_parser_map: Dict[_Text, ArgumentParser]
+    choices: Dict[_Text, ArgumentParser]
     _choices_actions: List[Action]
     def __init__(self,
                  option_strings: Sequence[_Text],
