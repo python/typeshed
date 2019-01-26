9bd839fade86d39c4f9112fb4d4957c5297fe9a1
Hanaasagi <ambiguous404@gmail.com>
2019-01-26 17:50:37 +0900

    update annotation of cyaml

--- third_party/2and3/yaml/cyaml.pyi
+++ third_party/2and3/yaml/cyaml.pyi
@@ -1,8 +1,10 @@
-from typing import Text, Union
+from typing import Text, Union, IO, Optional, Sequence, Mapping, Any
 from typing_extensions import Protocol
 
-from yaml.constructor import BaseConstructor, SafeConstructor
+from yaml.constructor import BaseConstructor, Constructor, SafeConstructor
+from yaml.representer import BaseRepresenter, Representer, SafeRepresenter
 from yaml.resolver import BaseResolver, Resolver
+from yaml.serializer import Serializer
 
 class _Readable(Protocol):
     def read(self, size: int) -> Union[Text, bytes]: ...
@@ -18,3 +20,25 @@ class CLoader(CParser, SafeConstructor, Resolver):
 
 class CSafeLoader(CParser, SafeConstructor, Resolver):
     def __init__(self, stream: Union[str, bytes, _Readable]) -> None: ...
+
+class CDangerLoader(CParser, Constructor, Resolver): ...  # undocumented
+
+class CEmitter:
+    def __init__(self, stream: IO[Any], canonical: Any = ..., indent: Optional[int] = ...,
+                 width: Optional[int] = ..., allow_unicode: Any = ..., line_break: Optional[str] = ...,
+                 encoding: Optional[str] = ..., explicit_start: Any = ..., explicit_end: Any = ...,
+                 version: Optional[Sequence[int]] = ..., tags: Optional[Mapping[str, str]] = ...) -> None: ...
+
+class CBaseDumper(CEmitter, BaseRepresenter, BaseResolver):
+    def __init__(self, stream: IO[Any], default_style: Optional[str] = ...,
+                 default_flow_style: Optional[bool] = ..., canonical: Any = ...,
+                 indent: Optional[int] = ..., width: Optional[int] = ...,
+                 allow_unicode: Any = ..., line_break: Optional[str] = ...,
+                 encoding: Optional[str] = ..., explicit_start: Any = ..., explicit_end: Any = ...,
+                 version: Optional[Sequence[int]] = ..., tags: Optional[Mapping[str, str]] = ...) -> None: ...
+
+class CDumper(CEmitter, SafeRepresenter, Resolver): ...
+
+CSafeDumper = CDumper
+
+class CDangerDumper(CEmitter, Serializer, Representer, Resolver): ...  # undocumented
