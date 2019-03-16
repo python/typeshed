4c6ee70b2ef4791eaef67d1e7714a7d071f9a499
Rebecca Chen <rechen@google.com>
2019-03-15 20:07:52 -0700

    Add missing class pdb.Pdb.
    Based on: https://docs.python.org/3/library/pdb.html#pdb.Pdb,
    with the one difference that the `skip` argument to the
    constructor is present in 2.7 despite being listed as new in 3.1.

--- stdlib/2and3/pdb.pyi
+++ stdlib/2and3/pdb.pyi
@@ -1,7 +1,8 @@
 # NOTE: This stub is incomplete - only contains some global functions
 
+from cmd import Cmd
 import sys
-from typing import Any, Dict, Optional
+from typing import Any, Dict, IO, Iterable, Optional
 
 class Restart(Exception): ...
 
@@ -19,3 +20,41 @@ else:
 
 def post_mortem(t: Optional[Any] = ...) -> None: ...
 def pm() -> None: ...
+
+class Pdb(Cmd):
+    if sys.version_info >= (3, 6):
+        def __init__(
+            self,
+            completekey: str = ...,
+            stdin: Optional[IO[str]] = ...,
+            stdout: Optional[IO[str]] = ...,
+            skip: Iterable[str] = ...,
+            nosigint: bool = ...,
+            readrc: bool = ...,
+        ) -> None: ...
+    elif sys.version_info >= (3, 2):
+        def __init__(
+            self,
+            completekey: str = ...,
+            stdin: Optional[IO[str]] = ...,
+            stdout: Optional[IO[str]] = ...,
+            skip: Iterable[str] = ...,
+            nosigint: bool = ...,
+        ) -> None: ...
+    else:
+        def __init__(
+            self,
+            completekey: str = ...,
+            stdin: Optional[IO[str]] = ...,
+            stdout: Optional[IO[str]] = ...,
+            skip: Iterable[str] = ...,
+        ) -> None: ...
+    def run(self, statement: str, globals: Optional[Dict[str, Any]] = ...,
+            locals: Optional[Dict[str, Any]] = ...) -> None: ...
+    def runeval(self, expression: str, globals: Optional[Dict[str, Any]] = ...,
+                locals: Optional[Dict[str, Any]] = ...) -> Any: ...
+    def runcall(self, *args: Any, **kwds: Any) -> Any: ...
+    if sys.version_info >= (3, 7):
+        def set_trace(self, *, header: Optional[str] = ...) -> None: ...
+    else:
+        def set_trace(self) -> None: ...
