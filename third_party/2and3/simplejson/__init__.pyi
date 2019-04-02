887fa525bb18b4d052e93d8945b324b0e4d5c315
Shahar Evron <shahar.evron@gmail.com>
2019-04-02 15:45:55 +0300

    Removing version check following PR feedback
    Not needed as `simplejson` is not bound to a specific Python version. In Python 2.x `bytes` is equivalent to `str` which is included in `Text`.

--- third_party/2and3/simplejson/__init__.pyi
+++ third_party/2and3/simplejson/__init__.pyi
@@ -1,14 +1,10 @@
-import sys
 from typing import Any, IO, Text, Union
 
 from simplejson.scanner import JSONDecodeError as JSONDecodeError
 from simplejson.decoder import JSONDecoder as JSONDecoder
 from simplejson.encoder import JSONEncoder as JSONEncoder, JSONEncoderForHTML as JSONEncoderForHTML
 
-if sys.version_info >= (3, 6):
-    _LoadsString = Union[Text, bytes, bytearray]
-else:
-    _LoadsString = Text
+_LoadsString = Union[Text, bytes, bytearray]
 
 
 def dumps(obj: Any, *args: Any, **kwds: Any) -> str: ...
