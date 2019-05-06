e0b3cdd6fceb6d5a2b2dcac57064e6fe1ee06b1c
Chad Birch <deimos@tildes.net>
2019-05-06 11:50:29 -0600

    Bleach stubs: fix typos, use protocol classes (#2957)

--- third_party/2and3/bleach/__init__.pyi
+++ third_party/2and3/bleach/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import List, Any, Optional, Text
+from typing import Any, Container, Iterable, Optional, Text
 
 from bleach.linkifier import DEFAULT_CALLBACKS as DEFAULT_CALLBACKS, Linker as Linker
 from bleach.sanitizer import (
@@ -6,7 +6,7 @@ from bleach.sanitizer import (
     ALLOWED_PROTOCOLS as ALLOWED_PROTOCOLS,
     ALLOWED_STYLES as ALLOWED_STYLES,
     ALLOWED_TAGS as ALLOWED_TAGS,
-    Cleaner as Clear,
+    Cleaner as Cleaner,
 )
 
 from .linkifier import _Callback
@@ -17,13 +17,16 @@ VERSION: Any  # packaging.version.Version
 
 def clean(
     text: Text,
-    tags: List[Text] = ...,
+    tags: Container[Text] = ...,
     attributes: Any = ...,
-    styles: List[Text] = ...,
-    protocols: List[Text] = ...,
+    styles: Container[Text] = ...,
+    protocols: Container[Text] = ...,
     strip: bool = ...,
     strip_comments: bool = ...,
 ) -> Text: ...
 def linkify(
-    text: Text, callbacks: List[_Callback] = ..., skip_tags: Optional[List[Text]] = ..., parse_email: bool = ...
+    text: Text,
+    callbacks: Iterable[_Callback] = ...,
+    skip_tags: Optional[Container[Text]] = ...,
+    parse_email: bool = ...,
 ) -> Text: ...
