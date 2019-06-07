ca9fec3c4788f72289ab33c31d4fcb99cdf4139e
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-06-07 15:15:24 +0100

    Remove two redundant definitions star-imported from _curses

--- stdlib/2and3/curses/__init__.pyi
+++ stdlib/2and3/curses/__init__.pyi
@@ -1,12 +1,9 @@
-import _curses
 from _curses import *  # noqa: F403
-from typing import TypeVar, Callable, Any, Sequence, Mapping
+from typing import TypeVar, Callable, Any
 
 _T = TypeVar('_T')
 
 LINES: int
 COLS: int
 
-def initscr() -> _curses._CursesWindow: ...
-def start_color() -> None: ...
 def wrapper(func: Callable[..., _T], *arg: Any, **kwds: Any) -> _T: ...
