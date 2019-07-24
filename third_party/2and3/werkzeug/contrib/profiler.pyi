8aa5da6773d8bb9dad0ede30abcdd1a6d928bee3
Sebastian Rittau <srittau@rittau.biz>
2019-07-24 15:07:25 +0200

    Fix missing contravariance

--- third_party/2and3/werkzeug/contrib/profiler.pyi
+++ third_party/2and3/werkzeug/contrib/profiler.pyi
@@ -1,11 +1,14 @@
-from typing import Any, AnyStr, Generic, Optional, Protocol, Tuple
+from typing import Any, AnyStr, Generic, Optional, Protocol, Tuple, TypeVar
 
 from ..middleware.profiler import *
 
-class _Writable(Protocol[AnyStr]):
-    def write(self, __s: AnyStr) -> Any: ...
+_T = TypeVar("_T")
+_T_contra = TypeVar("_T_contra", contravariant=True)
 
-class MergeStream(Generic[AnyStr]):
-    streams: Tuple[_Writable[AnyStr]]
-    def __init__(self, *streams: _Writable[AnyStr]) -> None: ...
-    def write(self, data: AnyStr) -> None: ...
+class _Writable(Protocol[_T_contra]):
+    def write(self, __s: _T_contra) -> Any: ...
+
+class MergeStream(Generic[_T]):
+    streams: Tuple[_Writable[_T]]
+    def __init__(self, *streams: _Writable[_T]) -> None: ...
+    def write(self, data: _T) -> None: ...
