ec6f61c38c480f650c95a547dd368bbcd950a42e
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2019-07-25 14:14:52 -0700

    Add _ViewFuncReturnType

--- third_party/2and3/flask/app.pyi
+++ third_party/2and3/flask/app.pyi
@@ -44,13 +44,16 @@ _WSGICallable = Callable[[Dict[Text, Any], _StartResponse], Iterable[bytes]]
 _Status = Union[str, int]
 _Headers = Union[Dict[Any, Any], List[Tuple[Any, Any]]]
 _Body = Union[Text, ByteString, Dict[Text, Any], Response, _WSGICallable]
+_ViewFuncReturnType = [
+    _Body,
+    Tuple[_Body, _Status, _Headers],
+    Tuple[_Body, _Status],
+    Tuple[_Body, _Headers],
+]
 
 _ViewFunc = Union[
     Callable[..., NoReturn],
-    Callable[..., _Body],
-    Callable[..., Tuple[_Body, _Status, _Headers]],
-    Callable[..., Tuple[_Body, _Status]],
-    Callable[..., Tuple[_Body, _Headers]]
+    Callable[..., _ViewFuncReturnType],
 ]
 _VT = TypeVar('_VT', bound=_ViewFunc)
 
