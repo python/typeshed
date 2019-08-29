6b76ecda31ba2b8d77941c50fb15fcf50edb8937
Sebastian Rittau <srittau@rittau.biz>
2019-08-29 12:49:11 +0200

    Run requests/api.pyi through black

--- third_party/2and3/requests/api.pyi
+++ third_party/2and3/requests/api.pyi
@@ -6,27 +6,24 @@ from typing import Optional, Union, Any, Iterable, Mapping, MutableMapping, Tupl
 from .models import Response
 
 _ParamsMappingValueType = Union[Text, bytes, int, float, Iterable[Union[Text, bytes, int, float]]]
-_Data = Union[
-    None,
-    Text,
-    bytes,
-    MutableMapping[str, Any],
-    MutableMapping[Text, Any],
-    Iterable[Tuple[Text, Optional[Text]]],
-    IO
-]
+_Data = Union[None, Text, bytes, MutableMapping[str, Any], MutableMapping[Text, Any], Iterable[Tuple[Text, Optional[Text]]], IO]
 
 def request(method: str, url: str, **kwargs) -> Response: ...
-def get(url: Union[Text, bytes],
-        params: Optional[
-            Union[Mapping[Union[Text, bytes, int, float], _ParamsMappingValueType],
-                  Union[Text, bytes],
-                  Tuple[Union[Text, bytes, int, float], _ParamsMappingValueType],
-                  Mapping[Text, _ParamsMappingValueType],
-                  Mapping[bytes, _ParamsMappingValueType],
-                  Mapping[int, _ParamsMappingValueType],
-                  Mapping[float, _ParamsMappingValueType]]] = ...,
-        **kwargs) -> Response: ...
+def get(
+    url: Union[Text, bytes],
+    params: Optional[
+        Union[
+            Mapping[Union[Text, bytes, int, float], _ParamsMappingValueType],
+            Union[Text, bytes],
+            Tuple[Union[Text, bytes, int, float], _ParamsMappingValueType],
+            Mapping[Text, _ParamsMappingValueType],
+            Mapping[bytes, _ParamsMappingValueType],
+            Mapping[int, _ParamsMappingValueType],
+            Mapping[float, _ParamsMappingValueType],
+        ]
+    ] = ...,
+    **kwargs,
+) -> Response: ...
 def options(url: Union[Text, bytes], **kwargs) -> Response: ...
 def head(url: Union[Text, bytes], **kwargs) -> Response: ...
 def post(url: Union[Text, bytes], data: _Data = ..., json=..., **kwargs) -> Response: ...
