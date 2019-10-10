4232eb8bce11486df2e1269a746459c8fba43095
Sebastian Rittau <srittau@rittau.biz>
2019-10-10 11:39:27 +0200

    Work around timothycrosley/isort#1027

--- stdlib/3/encodings/__init__.pyi
+++ stdlib/3/encodings/__init__.pyi
@@ -1,3 +1,6 @@
+# This commit serves as a workaround for timothycrosley/isort#1027 and can
+# be removed when a release with a fix was released.
+
 import codecs
 
 def search_function(encoding: str) -> codecs.CodecInfo: ...
