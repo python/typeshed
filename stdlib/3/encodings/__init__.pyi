1730fd3f06b653b00ab6a93c38ace085004bccea
Sebastian Rittau <srittau@rittau.biz>
2019-10-11 08:21:27 +0200

    typo

--- stdlib/3/encodings/__init__.pyi
+++ stdlib/3/encodings/__init__.pyi
@@ -1,4 +1,4 @@
-# This commit serves as a workaround for timothycrosley/isort#1027 and can
+# This comment serves as a workaround for timothycrosley/isort#1027 and can
 # be removed when a release with a fix was released.
 
 import codecs
