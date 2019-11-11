66778639a52e882c04e853778aa3862bbe1076ba
Sebastian Rittau <srittau@rittau.biz>
2019-11-11 16:52:11 +0100

    Run CI tests against win32 and darwin platform (#3457)
    Also use Python 3.8 as default Python version for tests

--- tests/mypy_test.py
+++ tests/mypy_test.py
@@ -24,6 +24,8 @@ parser.add_argument('-n', '--dry-run', action='store_true', help="Don't actually
 parser.add_argument('-x', '--exclude', type=str, nargs='*', help="Exclude pattern")
 parser.add_argument('-p', '--python-version', type=str, nargs='*',
                     help="These versions only (major[.minor])")
+parser.add_argument('--platform',
+                    help="Run mypy for a certain OS platform (defaults to sys.platform)")
 parser.add_argument('--warn-unused-ignores', action='store_true',
                     help="Run mypy with --warn-unused-ignores "
                     "(hint: only get rid of warnings that are "
@@ -135,6 +137,8 @@ def main():
             flags.append('--disallow-any-generics')
             if args.warn_unused_ignores:
                 flags.append('--warn-unused-ignores')
+            if args.platform:
+                flags.extend(['--platform', args.platform])
             sys.argv = ['mypy'] + flags + files
             if args.verbose:
                 print("running", ' '.join(sys.argv))
