dae468c014fdb8f049f7f7188188cc2db9dd9e2a
CraftSpider <runetynan@gmail.com>
2019-11-23 16:20:52 -0500

    Make return of findmatch have optional values

--- stdlib/2and3/mailcap.pyi
+++ stdlib/2and3/mailcap.pyi
@@ -1,8 +1,8 @@
 
-from typing import Sequence, Dict, List, Union, Tuple
+from typing import Sequence, Dict, List, Union, Tuple, Optional
 
 _Cap = Dict[str, Union[str, int]]
 _CapDict = Dict[str, List[_Cap]]
 
-def findmatch(caps: _CapDict, MIMEtype: str, key: str = ..., filename: str = ..., plist: Sequence[str] = ...) -> Tuple[str, _Cap]: ...
+def findmatch(caps: _CapDict, MIMEtype: str, key: str = ..., filename: str = ..., plist: Sequence[str] = ...) -> Tuple[Optional[str], Optional[_Cap]]: ...
 def getcaps() -> _CapDict: ...
