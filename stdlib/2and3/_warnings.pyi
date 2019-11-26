26c9acb828c7df2f5165b93059d4c1c4028979ae
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2019-11-25 19:30:09 -0800

    merge 2and3 for _warnings

--- stdlib/2and3/_warnings.pyi
+++ stdlib/2and3/_warnings.pyi
@@ -1,8 +1,13 @@
+import sys
 from typing import Any, Dict, List, Optional, Tuple, Type
 
-default_action: str
+if sys.version_info >= (3, 0):
+    _defaultaction: str
+    _onceregistry: Dict[Any, Any]
+else:
+    default_action: str
+    once_registry: Dict[Any, Any]
 filters: List[Tuple[Any, ...]]
-once_registry: Dict[Any, Any]
 
 def warn(message: Warning, category: Optional[Type[Warning]] = ..., stacklevel: int = ...) -> None: ...
 def warn_explicit(
