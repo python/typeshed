9b6319239068952e464fb67558d82dafb90c3c62
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2019-12-06 09:59:27 -0500

    Update orjson stub from orjson repository(#3532)

--- third_party/3/orjson.pyi
+++ third_party/3/orjson.pyi
@@ -2,13 +2,19 @@
 
 from typing import Any, Callable, Optional, Union
 
+__version__ = str
+
 def dumps(
-    __obj: Any, default: Optional[Callable[[Any], Any]] = ..., option: Optional[int] = ...
+    __obj: Any,
+    default: Optional[Callable[[Any], Any]] = ...,
+    option: Optional[int] = ...,
 ) -> bytes: ...
-def loads(__obj: Union[bytes, str]) -> Any: ...
+def loads(__obj: Union[bytes, bytearray, str]) -> Any: ...
 
 class JSONDecodeError(ValueError): ...
 class JSONEncodeError(TypeError): ...
 
-OPT_STRICT_INTEGER: int
+OPT_SERIALIZE_DATACLASS: int
 OPT_NAIVE_UTC: int
+OPT_OMIT_MICROSECONDS: int
+OPT_STRICT_INTEGER: int
