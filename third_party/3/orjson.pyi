fea8ec1ed6f43781b9e82d63cd38b1833da7cfe7
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2019-12-06 09:00:40 -0500

    Updated orjson.pyi
    Copied current version from orjson repository

--- third_party/3/orjson.pyi
+++ third_party/3/orjson.pyi
@@ -1,14 +1,18 @@
-# https://github.com/ijl/orjson/blob/master/orjson.pyi
-
 from typing import Any, Callable, Optional, Union
 
+__version__ = str
+
 def dumps(
-    __obj: Any, default: Optional[Callable[[Any], Any]] = ..., option: Optional[int] = ...
+    __obj: Any,
+    default: Optional[Callable[[Any], Any]] = ...,
+    option: Optional[int] = ...,
 ) -> bytes: ...
-def loads(__obj: Union[bytes, str]) -> Any: ...
+def loads(__obj: Union[bytes, bytearray, str]) -> Any: ...
 
 class JSONDecodeError(ValueError): ...
 class JSONEncodeError(TypeError): ...
 
-OPT_STRICT_INTEGER: int
+OPT_SERIALIZE_DATACLASS: int
 OPT_NAIVE_UTC: int
+OPT_OMIT_MICROSECONDS: int
+OPT_STRICT_INTEGER: int
