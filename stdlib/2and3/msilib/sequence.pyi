cbeb3c013ad000927ecfdcb17f317815f009a93b
Rune Tynan <runetynan@gmail.com>
2020-01-19 17:04:01 -0500

    First commit, add basic typings for msilib

---
+++ stdlib/2and3/msilib/sequence.pyi
@@ -0,0 +1,12 @@
+
+from typing import List, Tuple, Optional
+
+_SequenceType = List[Tuple[str, Optional[str], int]]
+
+AdminExecuteSequence: _SequenceType
+AdminUISequence: _SequenceType
+AdvtExecuteSequence: _SequenceType
+InstallExecuteSequence: _SequenceType
+InstallUISequence: _SequenceType
+
+tables: List[str]
---
+++ stdlib/2and3/msilib/text.pyi
@@ -0,0 +1,7 @@
+
+from typing import List, Tuple, Optional
+
+ActionText: List[Tuple[str, str, Optional[str]]]
+UIText: List[Tuple[str, Optional[str]]]
+
+tables: List[str]
