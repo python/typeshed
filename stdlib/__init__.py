befcfd6dc8d8d24d21db30fca078159e46bc1980
Ethan Smith <ethan@ethanhs.me>
2020-01-25 14:50:04 -0800

    Package stdlib in its own package

---
+++ stdlib/__init__.py
@@ -0,0 +1,8 @@
+"""A simple package that returns the path to the stdlib stubs folder."""
+
+__version__ = '1.0a1'
+
+import pathlib
+
+def get_stdlib_dir() -> pathlib.Path:
+    return pathlib.Path().absolute()
