6aa51f82500cf5210fd39dd73216f93eaea082c4
hauntsaninja <>
2020-02-20 23:14:02 -0800

    resource: fix platform availability

--- stdlib/3/resource.pyi
+++ stdlib/3/resource.pyi
@@ -1,5 +1,6 @@
 # Stubs for resource
 
+import sys
 from typing import Tuple, Optional, NamedTuple
 
 RLIMIT_AS: int
@@ -8,20 +9,21 @@ RLIMIT_CPU: int
 RLIMIT_DATA: int
 RLIMIT_FSIZE: int
 RLIMIT_MEMLOCK: int
-RLIMIT_MSGQUEUE: int
-RLIMIT_NICE: int
 RLIMIT_NOFILE: int
 RLIMIT_NPROC: int
-RLIMIT_OFILE: int
 RLIMIT_RSS: int
-RLIMIT_RTPRIO: int
-RLIMIT_RTTIME: int
-RLIMIT_SIGPENDING: int
 RLIMIT_STACK: int
 RLIM_INFINITY: int
 RUSAGE_CHILDREN: int
 RUSAGE_SELF: int
-RUSAGE_THREAD: int
+if sys.platform == "linux":
+    RLIMIT_MSGQUEUE: int
+    RLIMIT_NICE: int
+    RLIMIT_OFILE: int
+    RLIMIT_RTPRIO: int
+    RLIMIT_RTTIME: int
+    RLIMIT_SIGPENDING: int
+    RUSAGE_THREAD: int
 
 class _RUsage(NamedTuple):
     ru_utime: float
@@ -44,7 +46,8 @@ class _RUsage(NamedTuple):
 def getpagesize() -> int: ...
 def getrlimit(__resource: int) -> Tuple[int, int]: ...
 def getrusage(__who: int) -> _RUsage: ...
-def prlimit(pid: int, resource: int, limits: Optional[Tuple[int, int]]) -> Tuple[int, int]: ...
 def setrlimit(__resource: int, __limits: Tuple[int, int]) -> None: ...
+if sys.platform == "linux":
+    def prlimit(pid: int, resource: int, limits: Optional[Tuple[int, int]]) -> Tuple[int, int]: ...
 
 error = OSError
