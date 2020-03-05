04011c53104866d0c92b8a45f96ce206d9f73bda
Lawrence Law <lawrence.law@canva.com>
2020-03-06 10:09:59 +1100

    Update imghdr to accept IO[bytes] instead of BinaryIO

--- stdlib/2and3/imghdr.pyi
+++ stdlib/2and3/imghdr.pyi
@@ -1,16 +1,16 @@
-from typing import overload, Union, Text, BinaryIO, Optional, Any, List, Callable
+from typing import overload, Union, Text, IO, Optional, Any, List, Callable
 import sys
 import os
 
 
 if sys.version_info >= (3, 6):
-    _File = Union[Text, os.PathLike[Text], BinaryIO]
+    _File = Union[Text, os.PathLike[Text], IO[bytes]]
 else:
-    _File = Union[Text, BinaryIO]
+    _File = Union[Text, IO[bytes]]
 
 
 @overload
 def what(file: _File) -> Optional[str]: ...
 @overload
 def what(file: Any, h: bytes) -> Optional[str]: ...
-tests: List[Callable[[bytes, BinaryIO], Optional[str]]]
+tests: List[Callable[[bytes, IO[bytes]], Optional[str]]]
