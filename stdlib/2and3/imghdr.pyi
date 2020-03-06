6f93fbc312fe7e46445ead877a001d5ed89db91e
Lawrence Law <lawrence.law@canva.com>
2020-03-06 21:50:47 +1100

    Fix type hinting of  parameter for tests object

--- stdlib/2and3/imghdr.pyi
+++ stdlib/2and3/imghdr.pyi
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import IO, Any, Callable, List, Optional, Text, Union, overload
+from typing import IO, Any, BinaryIO, Callable, List, Optional, Text, Union, overload
 
 if sys.version_info >= (3, 6):
     _File = Union[Text, os.PathLike[Text], IO[bytes]]
@@ -12,4 +12,4 @@ else:
 def what(file: _File) -> Optional[str]: ...
 @overload
 def what(file: Any, h: bytes) -> Optional[str]: ...
-tests: List[Callable[[bytes, IO[bytes]], Optional[str]]]
+tests: List[Callable[[bytes, Optional[BinaryIO]], Optional[str]]]
