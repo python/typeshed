9a84e2129ecb77bb000686fed0a9c6f5375fa909
Julin S <48789920+ju-sh@users.noreply.github.com>
2020-03-09 23:55:57 +0530

    add TypedDict

---
+++ third_party/2and3/chardet/__init__.pyi
@@ -0,0 +1,3 @@
+from .universaldetector import UniversalDetector as UniversalDetector
+
+def __getattr__(name: str) -> Any: ...  # incomplete
--- third_party/2and3/chardet/universaldetector.pyi
+++ third_party/2and3/chardet/universaldetector.pyi
@@ -1,14 +1,30 @@
-from typing import Dict, Union, AnyStr, Pattern
+from typing import Dict, Union, AnyStr, Pattern, Optional
 from logging import Logger
 
-class UniversalDetector:
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+
+    class FinalResultType(TypedDict):
+        encoding: str
+        confidence: float
+        language: str
+
+    class IntermediateResultType(TypedDict):
+        encoding: Optional[str]
+        confidence: float
+        language: Optional[str]
+
+class UniversalDetector(object):
     MINIMUM_THRESHOLD: float
     HIGH_BYTE_DETECTOR: Pattern[bytes]
     ESC_DETECTOR: Pattern[bytes]
     WIN_BYTE_DETECTOR: Pattern[bytes]
     ISO_WIN_MAP: Dict[str, str]
 
-    result: Dict[str, Union[str, float]]
+    if sys.version_info >= (3, 8):
+        result: IntermediateResultType
+    else:
+        result: Dict[str, Union[str, float]]
     done: bool
     lang_filter: int
     logger: Logger
@@ -16,4 +32,8 @@ class UniversalDetector:
     def __init__(self, lang_filter: int) -> None: ...
     def reset(self) -> None: ...
     def feed(self, byte_str: bytes) -> None: ...
-    def close(self) -> Dict[str, Union[str, float]]: ...
+
+    if sys.version_info >= (3, 8):
+        def close(self) -> FinalResultType: ...
+    else:
+        def close(self) -> Dict[str, Union[str, float]]: ...
