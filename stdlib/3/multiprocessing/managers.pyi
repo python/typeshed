24691fa03a8f83748b1723183f7b58d223c5305a
Debjyoti Biswas <45293921+lladhibhutall@users.noreply.github.com>
2020-03-20 16:41:53 +0530

    Adding stubs for multiprocessing.managers BaseProxy (#3868)

--- stdlib/3/multiprocessing/managers.pyi
+++ stdlib/3/multiprocessing/managers.pyi
@@ -7,7 +7,7 @@ import sys
 import threading
 from typing import (
     Any, Callable, ContextManager, Dict, Iterable, Generic, List, Mapping, Optional,
-    Sequence, Tuple, TypeVar, Union,
+    Sequence, Tuple, TypeVar, Union, AnyStr,
 )
 from .context import BaseContext
 
@@ -22,7 +22,22 @@ class Namespace: ...
 
 _Namespace = Namespace
 
-class BaseProxy: ...
+class BaseProxy(object):
+    _address_to_local: Dict[Any, Any]
+    _mutex: Any
+    if sys.version_info >= (3, 6):
+        def __init__(self, token: Any, serializer: str, manager: Any = ...,
+                     authkey: Optional[AnyStr] = ..., exposed: Any = ...,
+                     incref: bool = ..., manager_owned: bool = ...) -> None: ...
+    else:
+        def __init__(self, token: Any, serializer: str, manager: Any = ...,
+                     authkey: Optional[AnyStr] = ..., exposed: Any = ...,
+                     incref: bool = ...) -> None: ...
+    def __deepcopy__(self, memo: Optional[Any]) -> Any: ...
+    def _callmethod(self, methodname: str, args: Tuple[Any, ...] = ...,
+                    kwds: Dict[Any, Any] = ...) -> None: ...
+    def _getvalue(self) -> Any: ...
+    def __reduce__(self) -> Tuple[Any, Tuple[Any, Any, str, Dict[Any, Any]]]: ...
 
 class ValueProxy(BaseProxy, Generic[_T]):
     def get(self) -> _T: ...
