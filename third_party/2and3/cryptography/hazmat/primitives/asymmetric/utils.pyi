5980284af3534dffc174101908598e653228358e
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2020-05-25 22:56:41 +0200

    cryptography: modify signature of Prehashed
    mark internal attributes as undocumented
    add public attribute digest_size

--- third_party/2and3/cryptography/hazmat/primitives/asymmetric/utils.pyi
+++ third_party/2and3/cryptography/hazmat/primitives/asymmetric/utils.pyi
@@ -1,10 +1,16 @@
 from typing import Optional, Tuple
 from cryptography.hazmat.primitives.hashes import HashAlgorithm
 
+
 def decode_dss_signature(signature: bytes) -> Tuple[int, int]: ...
+
+
 def encode_dss_signature(r: int, s: int) -> bytes: ...
 
+
 class Prehashed(object):
-    _algorithm: HashAlgorithm
-    _digest_size: int
+    _algorithm: HashAlgorithm   # undocumented
+    _digest_size: int           # undocumented
     def __init__(self, algorithm: Optional[HashAlgorithm]) -> None: ...
+
+    digest_size: int
