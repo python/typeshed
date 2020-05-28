da0b404e21181b93918a574f5d223adadf303059
Rune Tynan <runetynan@gmail.com>
2020-05-28 00:22:05 -0400

    Add __new__, as it doesn't match type.__new__

--- stdlib/3.7/_py_abc.pyi
+++ stdlib/3.7/_py_abc.pyi
@@ -1,9 +1,10 @@
 
-from typing import Type, TypeVar
+from typing import Type, TypeVar, Tuple, Any, Dict
 
 _T = TypeVar('_T')
 
 def get_cache_token() -> object: ...
 
 class ABCMeta(type):
+    def __new__(mcls, __name: str, __bases: Tuple[Type[Any]], __namespace: Dict[str, object], **kwargs) -> ABCMeta: ...
     def register(cls: ABCMeta, subclass: Type[_T]) -> Type[_T]: ...
