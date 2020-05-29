7705ac3133744028de54feb1f8184d3fb0584fd9
Rune Tynan <runetynan@gmail.com>
2020-05-29 13:12:47 -0400

    Implement review changes

--- stdlib/3/_sitebuiltins.pyi
+++ stdlib/3/_sitebuiltins.pyi
@@ -1,6 +1,6 @@
 
 import sys
-from typing import Optional, NoReturn, ClassVar, Sequence
+from typing import Optional, NoReturn, ClassVar, Iterable
 
 if sys.version_info >= (3, 8):
     from typing import Literal
@@ -8,13 +8,16 @@ else:
     from typing_extensions import Literal
 
 class Quitter:
+    name: str
+    eof: str
+
     def __init__(self, name: str, eof: str) -> None: ...
     def __call__(self, code: Optional[int] = ...) -> NoReturn: ...
 
 class _Printer:
     MAXLINES: ClassVar[Literal[23]]
 
-    def __init__(self, name: str, data: str, files: Sequence[str] = ..., dirs: Sequence[str] = ...) -> None: ...
+    def __init__(self, name: str, data: str, files: Iterable[str] = ..., dirs: Iterable[str] = ...) -> None: ...
     def __call__(self) -> None: ...
 
 class _Helper:
