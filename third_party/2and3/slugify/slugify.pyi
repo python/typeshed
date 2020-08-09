adcfa8c811e72c7959f2f81420b5f62415b3ba7d
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2020-08-08 18:14:35 -0700

    Update third_party/2and3/slugify/slugify.pyi

--- third_party/2and3/slugify/slugify.pyi
+++ third_party/2and3/slugify/slugify.pyi
@@ -13,7 +13,7 @@ def slugify(
     separator: str = ...,
     save_order: bool = ...,
     stopwords: Iterable[str] = ...,
-    regex_pattern: str = ...,
+    regex_pattern: Optional[str] = ...,
     lowercase: bool = ...,
     replacements: Iterable[str] = ...,
 ) -> str: ...
