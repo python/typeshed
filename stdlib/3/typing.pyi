f20c565eb358ea3feb7cb3978b2d1fc12296259a
Eric Traut <eric@traut.com>
2020-08-17 13:49:58 -0700

    Added support in typing.pyi for PEP 612 (ParamSpec and Concatenate) (#4446)
    Co-authored-by: Eric Traut <erictr@microsoft.com>

--- stdlib/3/typing.pyi
+++ stdlib/3/typing.pyi
@@ -49,6 +49,12 @@ if sys.version_info >= (3, 8):
 if sys.version_info < (3, 7):
     class GenericMeta(type): ...
 
+if sys.version_info >= (3, 10):
+    class ParamSpec:
+        __name__: str
+        def __init__(self, name: str) -> None: ...
+    Concatenate: _SpecialForm = ...
+
 # Return type that indicates a function does not return.
 # This type is equivalent to the None type, but the no-op Union is necessary to
 # distinguish the None type from the None value.
