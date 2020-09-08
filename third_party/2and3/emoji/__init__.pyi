a0610637b14d96e44990ee3742dfe10adca00353
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-09-08 03:07:37 +0200

    Update `emoji` stubs

---
+++ third_party/2and3/emoji/__init__.pyi
@@ -0,0 +1,13 @@
+from .core import (
+    emojize as emojize,
+    demojize as demojize,
+    get_emoji_regexp as get_emoji_regexp,
+    emoji_lis as emoji_lis,
+    emoji_count as emoji_count,
+)
+from .unicode_codes import (
+    EMOJI_UNICODE as EMOJI_UNICODE,
+    EMOJI_UNICODE_ALIAS as EMOJI_UNICODE_ALIAS,
+    UNICODE_EMOJI as UNICODE_EMOJI,
+    UNICODE_EMOJI_ALIAS as UNICODE_EMOJI_ALIAS,
+)
---
+++ third_party/2and3/emoji/unicode_codes.pyi
@@ -0,0 +1,6 @@
+from typing import Dict, Text
+
+EMOJI_UNICODE: Dict[Text, Text]
+EMOJI_UNICODE_ALIAS: Dict[Text, Text]
+UNICODE_EMOJI: Dict[Text, Text]
+UNICODE_EMOJI_ALIAS: Dict[Text, Text]
