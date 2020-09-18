4fb1eb8439b32385e3811a37db948ed368766726
Rune Tynan <runetynan@gmail.com>
2020-09-18 17:31:42 -0400

    Use correct tk import for py2

--- stdlib/2and3/_typeshed/tkinter.pyi
+++ stdlib/2and3/_typeshed/tkinter.pyi
@@ -1,5 +1,10 @@
-from tkinter import Event, Misc, Widget
+import sys
 from typing import Optional, Protocol
+
+if sys.version_info < (3,):
+    from Tkinter import Event, Misc, Widget
+else:
+    from tkinter import Event, Misc, Widget
 
 class DndSource(Protocol):
     def dnd_end(self, target: Optional[Widget], event: Optional[Event[Misc]]) -> None: ...
