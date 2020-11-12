5313db680da628d600b53158754675cf9dba3843
Anders Kaseorg <andersk@mit.edu>
2020-11-12 14:34:47 -0800

    markdown: AbbrPreprocessor subclasses BlockProcessor
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- third_party/2and3/markdown/extensions/abbr.pyi
+++ third_party/2and3/markdown/extensions/abbr.pyi
@@ -1,15 +1,15 @@
 from typing import Any, Pattern
 
+from markdown.blockprocessors import BlockProcessor
 from markdown.extensions import Extension
 from markdown.inlinepatterns import InlineProcessor
-from markdown.preprocessors import Preprocessor
 
 ABBR_REF_RE: Pattern
 
 class AbbrExtension(Extension):
     def extendMarkdown(self, md) -> None: ...
 
-class AbbrPreprocessor(Preprocessor):
+class AbbrPreprocessor(BlockProcessor):
     def run(self, lines): ...
 
 class AbbrInlineProcessor(InlineProcessor):
