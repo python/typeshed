6fa5e724fabd97ca995e8edf9a9e6ebd7e8a9426
Stephanie Ding <sym@fb.com>
2020-11-19 11:40:35 -0500

    Added empty stubs for xml.dom.minidom.parse and xml.dom.minidom.parseString, and other modules under xml.dom

--- stdlib/2and3/xml/dom/minidom.pyi
+++ stdlib/2and3/xml/dom/minidom.pyi
@@ -1,3 +1,7 @@
-from typing import Any
+from typing import Any, Optional
+
+def parse(file: str, parser, bufsize: Optional[int]): ...
+
+def parseString(string: str, parser): ...
 
 def __getattr__(name: str) -> Any: ...  # incomplete
