3689722765e7991bb152bffc08db236ad2d5bb4d
Peter Scopes <peter.scopes@nccgroup.trust>
2020-11-26 14:05:37 +0000

    Add missing sub for cryptography 3.x pcks12

--- third_party/2and3/cryptography/hazmat/primitives/serialization/pkcs12.pyi
+++ third_party/2and3/cryptography/hazmat/primitives/serialization/pkcs12.pyi
@@ -1,3 +1,16 @@
-from typing import Any, Optional
+from typing import List, Optional, Union
 
-def load_key_and_certificates(data: bytes, password: Optional[bytes], backend: Optional[Any] = ...): ...
+from cryptography.hazmat.primitives.asymmetric.dsa import DSAPrivateKeyWithSerialization
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKeyWithSerialization
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKeyWithSerialization
+from cryptography.hazmat.primitives.serialization import KeySerializationEncryption
+from cryptography.x509 import Certificate
+
+def load_key_and_certificates(data: bytes, password: Optional[bytes], backend): ...
+def serialize_key_and_certificates(
+    name: bytes,
+    key: Union[RSAPrivateKeyWithSerialization, EllipticCurvePrivateKeyWithSerialization, DSAPrivateKeyWithSerialization],
+    cert: Optional[Certificate],
+    cas: Optional[List[Certificate]],
+    enc: KeySerializationEncryption,
+) -> bytes: ...
