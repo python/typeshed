e1a5474fa5da1d073d829d4f39fd467e8d56edca
Peter Scopes <peter.scopes@nccgroup.trust>
2020-11-30 16:34:44 +0000

    Resolves merge conflicts introduced by #4761

--- third_party/2and3/cryptography/hazmat/primitives/serialization/pkcs12.pyi
+++ third_party/2and3/cryptography/hazmat/primitives/serialization/pkcs12.pyi
@@ -1,4 +1,4 @@
-from typing import List, Optional, Union
+from typing import Any, List, Optional, Tuple, Union
 
 from cryptography.hazmat.primitives.asymmetric.dsa import DSAPrivateKeyWithSerialization
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKeyWithSerialization
@@ -6,7 +6,9 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKeyWithSeria
 from cryptography.hazmat.primitives.serialization import KeySerializationEncryption
 from cryptography.x509 import Certificate
 
-def load_key_and_certificates(data: bytes, password: Optional[bytes], backend): ...
+def load_key_and_certificates(
+    data: bytes, password: Optional[bytes], backend: Optional[Any] = ...
+) -> Tuple[Optional[Any], Optional[Certificate], List[Certificate]]: ...
 def serialize_key_and_certificates(
     name: bytes,
     key: Union[RSAPrivateKeyWithSerialization, EllipticCurvePrivateKeyWithSerialization, DSAPrivateKeyWithSerialization],
