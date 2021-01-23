6aa5cc691c4b83fe08d1f0dea1e5f68ed80da0ed
Rune Tynan <runetynan@gmail.com>
2021-01-23 10:17:57 -0500

    Add _collections_abc module (#4000)
    * Add _collections_abc module
    * Rearrange collections.abc and collections to re-export from _collections_abc
    
    Co-authored-by: Sebastian Rittau <srittau@rittau.biz>

--- stdlib/3/collections/__init__.pyi
+++ stdlib/3/collections/__init__.pyi
@@ -1,44 +1,9 @@
 import sys
 import typing
-from typing import (
-    AbstractSet,
-    Any,
-    AsyncGenerator as AsyncGenerator,
-    AsyncIterable as AsyncIterable,
-    AsyncIterator as AsyncIterator,
-    Awaitable as Awaitable,
-    ByteString as ByteString,
-    Callable as Callable,
-    Collection as Collection,
-    Container as Container,
-    Coroutine as Coroutine,
-    Dict,
-    Generator as Generator,
-    Generic,
-    Hashable as Hashable,
-    ItemsView as ItemsView,
-    Iterable as Iterable,
-    Iterator as Iterator,
-    KeysView as KeysView,
-    List,
-    Mapping as Mapping,
-    MappingView as MappingView,
-    MutableMapping as MutableMapping,
-    MutableSequence as MutableSequence,
-    MutableSet as MutableSet,
-    Optional,
-    Reversible as Reversible,
-    Sequence as Sequence,
-    Sized as Sized,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-    ValuesView as ValuesView,
-    overload,
-)
+from typing import Any, Dict, Generic, List, Optional, Tuple, Type, TypeVar, Union, overload
 
-Set = AbstractSet
+if sys.version_info < (3, 10):
+    from _collections_abc import *
 
 _S = TypeVar("_S")
 _T = TypeVar("_T")
