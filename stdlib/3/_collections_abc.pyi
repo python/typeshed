3c5f1d8a32b8b7557be65166103663d53199384c
Sebastian Rittau <srittau@rittau.biz>
2021-01-23 15:31:24 +0100

    Remove Python 3.5 compatibility

--- stdlib/3/_collections_abc.pyi
+++ stdlib/3/_collections_abc.pyi
@@ -1,37 +1,30 @@
-
-import sys
-
 from typing import (
+    AbstractSet as Set,
+    AsyncGenerator as AsyncGenerator,
     AsyncIterable as AsyncIterable,
     AsyncIterator as AsyncIterator,
     Awaitable as Awaitable,
     ByteString as ByteString,
+    Callable as Callable,
+    Collection as Collection,
     Container as Container,
     Coroutine as Coroutine,
     Generator as Generator,
     Hashable as Hashable,
+    ItemsView as ItemsView,
     Iterable as Iterable,
     Iterator as Iterator,
-    Sized as Sized,
-    Callable as Callable,
-    Mapping as Mapping,
-    MutableMapping as MutableMapping,
-    Sequence as Sequence,
-    MutableSequence as MutableSequence,
-    AbstractSet as Set,
-    MutableSet as MutableSet,
-    MappingView as MappingView,
-    ItemsView as ItemsView,
     KeysView as KeysView,
+    Mapping as Mapping,
+    MappingView as MappingView,
+    MutableMapping as MutableMapping,
+    MutableSequence as MutableSequence,
+    MutableSet as MutableSet,
+    Reversible as Reversible,
+    Sequence as Sequence,
+    Sized as Sized,
     ValuesView as ValuesView,
 )
-
-if sys.version_info >= (3, 6):
-    from typing import (
-        Collection as Collection,
-        Reversible as Reversible,
-        AsyncGenerator as AsyncGenerator,
-    )
 
 # Without the real definition, mypy and pytest both think that __all__ is empty, so re-exports nothing
 __all__ = [
