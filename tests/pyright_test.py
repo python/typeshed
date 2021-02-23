b244bf4968114bb5ac2a3d39af7ac138b3527551
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2021-02-23 09:41:22 -0800

    Use pyrightconfig.json, set node version in workflow

--- tests/pyright_test.py
+++ tests/pyright_test.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-import os
 import subprocess
 import sys
 from pathlib import Path
-from typing import Iterable, List
 
 
 _WELL_KNOWN_FILE = Path("tests", "pyright_test.py")
@@ -13,8 +11,7 @@ _PYRIGHT_COMMAND = ["npx", "-p", "pyright@1.1.113", "pyright"]
 
 def main() -> None:
     assert_npm_is_installed()
-    files = find_stubs()
-    ret = run_pyright(files)
+    ret = subprocess.run(_PYRIGHT_COMMAND).returncode
     sys.exit(ret)
 
 
@@ -27,25 +24,6 @@ def assert_npm_is_installed() -> None:
     except OSError:
         print("error running npx; is Node.js installed?", file=sys.stderr)
         sys.exit(1)
-
-
-def find_stubs() -> List[str]:
-    files: List[str] = []
-    for top in ["stdlib", "stubs"]:
-        for dirpath, dirnames, filenames in os.walk(top):
-            for fn in filenames:
-                if fn.endswith(".pyi"):
-                    files.append(os.path.join(dirpath, fn))
-            try:
-                dirnames.remove("@python2")
-            except ValueError:
-                pass
-    return files
-
-
-def run_pyright(files: Iterable[str]) -> int:
-    full_args = _PYRIGHT_COMMAND + list(files)
-    return subprocess.run(full_args).returncode
 
 
 if __name__ == "__main__":
