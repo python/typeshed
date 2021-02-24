e9227062e8b2def9cb197fc880a321f4aab55863
Guilhem Charles <gcharles@jwplayer.com>
2021-02-24 21:43:16 +0100

    redis: complete from_url function stubs (redis.utils.from_url)

--- stubs/redis/redis/utils.pyi
+++ stubs/redis/redis/utils.pyi
@@ -1,11 +1,14 @@
-from typing import Any, ContextManager
+from typing import Any, ContextManager, Optional, Text, overload
+from typing_extensions import Literal
 
 from . import Redis
 from .client import Pipeline
 
 HIREDIS_AVAILABLE: Any
-
-def from_url(url, db=..., **kwargs): ...
+@overload
+def from_url(url: Text, db: Optional[int] = ..., *, decode_responses: Literal[True], **kwargs) -> Redis[str]: ...
+@overload
+def from_url(url: Text, db: Optional[int] = ..., *, decode_responses: Literal[False] = ..., **kwargs) -> Redis[bytes]: ...
 def pipeline(redis_obj: Redis) -> ContextManager[Pipeline]: ...
 
 class dummy: ...
