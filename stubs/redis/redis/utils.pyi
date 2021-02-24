34b4f743323812ddd4f5c883a115c5d37c3fd427
Guilhem Charles <gcharles@jwplayer.com>
2021-02-24 21:29:29 +0100

    redis: complete pipeline function stub (redis.utils.pipeline)

--- stubs/redis/redis/utils.pyi
+++ stubs/redis/redis/utils.pyi
@@ -1,8 +1,11 @@
-from typing import Any
+from typing import Any, ContextManager
+
+from . import Redis
+from .client import Pipeline
 
 HIREDIS_AVAILABLE: Any
 
 def from_url(url, db=..., **kwargs): ...
-def pipeline(redis_obj): ...
+def pipeline(redis_obj: Redis) -> ContextManager[Pipeline]: ...
 
 class dummy: ...
