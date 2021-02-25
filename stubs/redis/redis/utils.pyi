9306cf7a4550bf733578f88469f6fb0dd1c79b5d
Guilhem Charles <gcharles@jwplayer.com>
2021-02-25 12:24:20 +0100

    redis: use a type var for one of str_if_bytes overload and silence mypy with a type ignore comment

--- stubs/redis/redis/utils.pyi
+++ stubs/redis/redis/utils.pyi
@@ -1,7 +1,9 @@
-from typing import Any, ContextManager, Optional, Text, overload
+from typing import Any, ContextManager, Optional, Text, TypeVar, overload
 from typing_extensions import Literal
 
 from .client import Pipeline, Redis
+
+_T = TypeVar("_T")
 
 HIREDIS_AVAILABLE: bool
 @overload
@@ -9,9 +11,9 @@ def from_url(url: Text, db: Optional[int] = ..., *, decode_responses: Literal[Tr
 @overload
 def from_url(url: Text, db: Optional[int] = ..., *, decode_responses: Literal[False] = ..., **kwargs: Any) -> Redis[bytes]: ...
 @overload
-def str_if_bytes(value: bytes) -> str: ...
+def str_if_bytes(value: bytes) -> str: ...  # type: ignore
 @overload
-def str_if_bytes(value: object) -> object: ...
+def str_if_bytes(value: _T) -> _T: ...
 def safe_str(value: object) -> str: ...
 def pipeline(redis_obj: Redis) -> ContextManager[Pipeline]: ...
 
