0cea325bc7a99a6beba93bb526b1ed7a0cd4b4a7
Sebastian Rittau <srittau@rittau.biz>
2021-03-23 03:26:43 +0100

    Modernize logging (#5068)

--- stdlib/@python2/logging/handlers.pyi
+++ stdlib/@python2/logging/handlers.pyi
@@ -1,16 +1,10 @@
 import datetime
 import ssl
-import sys
 from _typeshed import StrPath
-from collections.abc import Callable
 from logging import FileHandler, Handler, LogRecord
+from Queue import Queue
 from socket import SocketKind, SocketType
-from typing import Any, ClassVar, Optional, Union
-
-if sys.version_info >= (3, 7):
-    from queue import Queue, SimpleQueue
-else:
-    from queue import Queue
+from typing import Any, Callable, ClassVar, Dict, List, Optional, Tuple, Union
 
 DEFAULT_TCP_LOGGING_PORT: int
 DEFAULT_UDP_LOGGING_PORT: int
@@ -25,18 +19,10 @@ class WatchedFileHandler(FileHandler):
     def __init__(self, filename: StrPath, mode: str = ..., encoding: Optional[str] = ..., delay: bool = ...) -> None: ...
     def _statstream(self) -> None: ...
 
-class BaseRotatingHandler(FileHandler):
-    terminator: str
-    namer: Optional[Callable[[str], str]]
-    rotator: Optional[Callable[[str, str], None]]
-    def __init__(self, filename: StrPath, mode: str, encoding: Optional[str] = ..., delay: bool = ...) -> None: ...
-    def rotation_filename(self, default_name: str) -> None: ...
-    def rotate(self, source: str, dest: str) -> None: ...
-
-class RotatingFileHandler(BaseRotatingHandler):
+class RotatingFileHandler(Handler):
     def __init__(
         self,
-        filename: StrPath,
+        filename: str,
         mode: str = ...,
         maxBytes: int = ...,
         backupCount: int = ...,
@@ -45,17 +31,16 @@ class RotatingFileHandler(BaseRotatingHandler):
     ) -> None: ...
     def doRollover(self) -> None: ...
 
-class TimedRotatingFileHandler(BaseRotatingHandler):
+class TimedRotatingFileHandler(Handler):
     def __init__(
         self,
-        filename: StrPath,
+        filename: str,
         when: str = ...,
         interval: int = ...,
         backupCount: int = ...,
         encoding: Optional[str] = ...,
         delay: bool = ...,
         utc: bool = ...,
-        atTime: Optional[datetime.datetime] = ...,
     ) -> None: ...
     def doRollover(self) -> None: ...
 
@@ -63,7 +48,7 @@ class SocketHandler(Handler):
     retryStart: float
     retryFactor: float
     retryMax: float
-    def __init__(self, host: str, port: Optional[int]) -> None: ...
+    def __init__(self, host: str, port: int) -> None: ...
     def makeSocket(self, timeout: float = ...) -> SocketType: ...  # timeout is undocumented
     def makePickle(self, record: LogRecord) -> bytes: ...
     def send(self, s: bytes) -> None: ...
@@ -94,13 +79,6 @@ class SysLogHandler(Handler):
     LOG_CRON: int
     LOG_AUTHPRIV: int
     LOG_FTP: int
-
-    if sys.version_info >= (3, 9):
-        LOG_NTP: int
-        LOG_SECURITY: int
-        LOG_CONSOLE: int
-        LOG_SOLCRON: int
-
     LOG_LOCAL0: int
     LOG_LOCAL1: int
     LOG_LOCAL2: int
@@ -111,13 +89,12 @@ class SysLogHandler(Handler):
     LOG_LOCAL7: int
     unixsocket: bool  # undocumented
     socktype: SocketKind  # undocumented
-    ident: str  # undocumented
     facility: int  # undocumented
-    priority_names: ClassVar[dict[str, int]]  # undocumented
-    facility_names: ClassVar[dict[str, int]]  # undocumented
-    priority_map: ClassVar[dict[str, str]]  # undocumented
+    priority_names: ClassVar[Dict[str, int]]  # undocumented
+    facility_names: ClassVar[Dict[str, int]]  # undocumented
+    priority_map: ClassVar[Dict[str, str]]  # undocumented
     def __init__(
-        self, address: Union[tuple[str, int], str] = ..., facility: int = ..., socktype: Optional[SocketKind] = ...
+        self, address: Union[Tuple[str, int], str] = ..., facility: int = ..., socktype: Optional[SocketKind] = ...
     ) -> None: ...
     def encodePriority(self, facility: Union[int, str], priority: Union[int, str]) -> int: ...
     def mapPriority(self, levelName: str) -> str: ...
@@ -133,56 +110,24 @@ class SMTPHandler(Handler):
     # TODO `secure` can also be an empty tuple
     def __init__(
         self,
-        mailhost: Union[str, tuple[str, int]],
+        mailhost: Union[str, Tuple[str, int]],
         fromaddr: str,
-        toaddrs: list[str],
+        toaddrs: List[str],
         subject: str,
-        credentials: Optional[tuple[str, str]] = ...,
-        secure: Union[tuple[str], tuple[str, str], None] = ...,
-        timeout: float = ...,
+        credentials: Optional[Tuple[str, str]] = ...,
+        secure: Union[Tuple[str], Tuple[str, str], None] = ...,
     ) -> None: ...
     def getSubject(self, record: LogRecord) -> str: ...
 
 class BufferingHandler(Handler):
-    buffer: list[LogRecord]
+    buffer: List[LogRecord]
     def __init__(self, capacity: int) -> None: ...
     def shouldFlush(self, record: LogRecord) -> bool: ...
 
 class MemoryHandler(BufferingHandler):
-    def __init__(
-        self, capacity: int, flushLevel: int = ..., target: Optional[Handler] = ..., flushOnClose: bool = ...
-    ) -> None: ...
+    def __init__(self, capacity: int, flushLevel: int = ..., target: Optional[Handler] = ...) -> None: ...
     def setTarget(self, target: Handler) -> None: ...
 
 class HTTPHandler(Handler):
-    def __init__(
-        self,
-        host: str,
-        url: str,
-        method: str = ...,
-        secure: bool = ...,
-        credentials: Optional[tuple[str, str]] = ...,
-        context: Optional[ssl.SSLContext] = ...,
-    ) -> None: ...
-    def mapLogRecord(self, record: LogRecord) -> dict[str, Any]: ...
-
-class QueueHandler(Handler):
-    if sys.version_info >= (3, 7):
-        def __init__(self, queue: Union[SimpleQueue[Any], Queue[Any]]) -> None: ...
-    else:
-        def __init__(self, queue: Queue[Any]) -> None: ...
-    def prepare(self, record: LogRecord) -> Any: ...
-    def enqueue(self, record: LogRecord) -> None: ...
-
-class QueueListener:
-    if sys.version_info >= (3, 7):
-        def __init__(
-            self, queue: Union[SimpleQueue[Any], Queue[Any]], *handlers: Handler, respect_handler_level: bool = ...
-        ) -> None: ...
-    else:
-        def __init__(self, queue: Queue[Any], *handlers: Handler, respect_handler_level: bool = ...) -> None: ...
-    def dequeue(self, block: bool) -> LogRecord: ...
-    def prepare(self, record: LogRecord) -> Any: ...
-    def start(self) -> None: ...
-    def stop(self) -> None: ...
-    def enqueue_sentinel(self) -> None: ...
+    def __init__(self, host: str, url: str, method: str = ...) -> None: ...
+    def mapLogRecord(self, record: LogRecord) -> Dict[str, Any]: ...
