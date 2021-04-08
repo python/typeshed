4e3f9ebdcac381b394d34ab687932edae2f967c6
Sebastian Rittau <srittau@rittau.biz>
2021-04-08 14:25:06 +0200

    Populate docutils.frontend and .io

--- stubs/docutils/docutils/io.pyi
+++ stubs/docutils/docutils/io.pyi
@@ -1,3 +1,67 @@
-from typing import Any
+from typing import Any, ClassVar, Optional, Union
+from typing_extensions import Literal
 
-def __getattr__(name: str) -> Any: ...  # incomplete
+from . import TransformSpec
+
+__docformat__: str
+
+class InputError(IOError): ...
+class OutputError(IOError): ...
+
+def check_encoding(stream: Any, encoding: str) -> Optional[bool]: ...
+
+class Input(TransformSpec):
+    component_type: ClassVar[str]
+    default_source_path: ClassVar[Optional[str]]
+    def read(self) -> Any: ...
+    def __getattr__(self, name: str) -> Any: ...  # incomplete
+
+class Output(TransformSpec):
+    component_type: ClassVar[str]
+    default_destination_path: ClassVar[Optional[str]]
+    def __init__(
+        self,
+        destination: Optional[Any] = ...,
+        destination_path: Optional[Any] = ...,
+        encoding: Optional[str] = ...,
+        error_handler: str = ...,
+    ) -> None: ...
+    def write(self, data: str) -> Any: ...  # returns bytes or str
+    def encode(self, data: str) -> Any: ...  # returns bytes or str
+
+class FileInput(Input):
+    def __init__(
+        self,
+        source: Optional[Any] = ...,
+        source_path: Optional[Any] = ...,
+        encoding: Optional[str] = ...,
+        error_handler: str = ...,
+        autoclose: bool = ...,
+        mode: Literal["r", "rb"] = ...,
+    ) -> None: ...
+    def readlines(self) -> list[str]: ...
+    def close(self) -> None: ...
+
+class FileOutput(Output):
+    mode: ClassVar[Literal["w", "wb"]]
+    def __getattr__(self, name: str) -> Any: ...  # incomplete
+
+class BinaryFileOutput(FileOutput): ...
+
+class StringInput(Input):
+    default_source_path: ClassVar[str]
+
+class StringOutput(Output):
+    default_destination_path: ClassVar[str]
+    destination: Union[str, bytes]  # only defined after call to write()
+
+class NullInput(Input):
+    default_source_path: ClassVar[str]
+    def read(self) -> str: ...
+
+class NullOutput(Output):
+    default_destination_path: ClassVar[str]
+    def write(self, data: object) -> None: ...
+
+class DocTreeInput(Input):
+    default_source_path: ClassVar[str]
