ff8a1400f3f5412d905b0689ee4aa1ce2fca4d97
Sebastian Rittau <srittau@rittau.biz>
2021-04-20 14:35:11 +0200

    Remove docutils.nodes.reference, which has no resemblance to the implementation

--- stubs/docutils/docutils/nodes.pyi
+++ stubs/docutils/docutils/nodes.pyi
@@ -1,6 +1,3 @@
-from typing import Any, List
-
-class reference:
-    def __init__(self, rawsource: str = ..., text: str = ..., *children: List[Any], **attributes: Any) -> None: ...
+from typing import Any
 
 def __getattr__(name: str) -> Any: ...  # incomplete
