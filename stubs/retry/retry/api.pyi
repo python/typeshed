4d68cb9f85f086dea40e6489b54b7e258d4988a5
Frédéric Perrin <fred@graphiant.com>
2021-05-17 11:17:23 +0100

    Use IdentityFunction for decorator return type

--- stubs/retry/retry/api.pyi
+++ stubs/retry/retry/api.pyi
@@ -1,11 +1,8 @@
+from _typeshed import IdentityFunction
 from logging import Logger
-from typing import Any, Callable, Dict, Optional, Protocol, Sequence, Tuple, Type, TypeVar, Union
+from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Type, TypeVar, Union
 
 _R = TypeVar("_R")
-_T = TypeVar("_T", bound=Callable[..., Any])
-
-class _Decorator(Protocol):
-    def __call__(self, __x: _T) -> _T: ...
 
 def retry_call(
     f: Callable[..., _R],
@@ -27,4 +24,4 @@ def retry(
     backoff: float = ...,
     jitter: Union[Tuple[float, float], float] = ...,
     logger: Optional[Logger] = ...,
-) -> _Decorator: ...
+) -> IdentityFunction: ...
