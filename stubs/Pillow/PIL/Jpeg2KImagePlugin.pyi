a66d9b4a9a1628cb141d84c5d92f8024d4e180de
Sebastian Rittau <srittau@rittau.biz>
2021-06-12 21:53:11 +0200

    Work around a mypy warning

--- stubs/Pillow/PIL/Jpeg2KImagePlugin.pyi
+++ stubs/Pillow/PIL/Jpeg2KImagePlugin.pyi
@@ -5,9 +5,6 @@ from .ImageFile import ImageFile
 class Jpeg2KImageFile(ImageFile):
     format: str
     format_description: str
-    @property
-    def reduce(self): ...
-    @reduce.setter
-    def reduce(self, value) -> None: ...
+    reduce: Any
     tile: Any
     def load(self): ...
