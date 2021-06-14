e6e757a44bfc421876e8b94695b708c3c9526311
oleg.hoefling <oleg.hoefling@gmail.com>
2021-06-14 13:23:23 +0200

    revert adding incomplete marker in yaml.nodes to pass stubtest
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- stubs/PyYAML/yaml/nodes.pyi
+++ stubs/PyYAML/yaml/nodes.pyi
@@ -6,20 +6,17 @@ class Node:
     tag: str
     start_mark: Mark | Any
     end_mark: Mark | Any
-    def __getattr__(self, __name: str) -> Any: ...  # incomplete
     def __init__(self, tag: str, value, start_mark: Mark | None, end_mark: Mark | None) -> None: ...
 
 class ScalarNode(Node):
     id: ClassVar[str]
     style: str | Any
-    def __getattr__(self, __name: str) -> Any: ...  # incomplete
     def __init__(
         self, tag: str, value, start_mark: Mark | None = ..., end_mark: Mark | None = ..., style: str | None = ...
     ) -> None: ...
 
 class CollectionNode(Node):
     flow_style: bool | Any
-    def __getattr__(self, __name: str) -> Any: ...  # incomplete
     def __init__(
         self, tag: str, value, start_mark: Mark | None = ..., end_mark: Mark | None = ..., flow_style: bool | None = ...
     ) -> None: ...
