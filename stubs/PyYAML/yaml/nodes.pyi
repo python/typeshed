b77842bd55ede6a42af9d1b53efe87849e1301e3
oleg.hoefling <oleg.hoefling@gmail.com>
2021-06-14 11:38:24 +0200

    add stubs for yaml.nodes and yaml.representer
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- stubs/PyYAML/yaml/nodes.pyi
+++ stubs/PyYAML/yaml/nodes.pyi
@@ -1,31 +1,37 @@
-from typing import Any
+from typing import Any, ClassVar
+
+from yaml.error import Mark as Mark
 
 class Node:
-    tag: Any
+    tag: str
     value: Any
-    start_mark: Any
-    end_mark: Any
-    def __init__(self, tag, value, start_mark, end_mark) -> None: ...
+    start_mark: Mark | Any
+    end_mark: Mark | Any
+    def __init__(self, tag: str, value: Any, start_mark: Mark | None, end_mark: Mark | None) -> None: ...
 
 class ScalarNode(Node):
-    id: Any
-    tag: Any
+    id: ClassVar[str]
+    tag: str
     value: Any
-    start_mark: Any
-    end_mark: Any
-    style: Any
-    def __init__(self, tag, value, start_mark=..., end_mark=..., style=...) -> None: ...
+    start_mark: Mark | Any
+    end_mark: Mark | Any
+    style: str | Any
+    def __init__(
+        self, tag: str, value: Any, start_mark: Mark | None = ..., end_mark: Mark | None = ..., style: str | None = ...
+    ) -> None: ...
 
 class CollectionNode(Node):
-    tag: Any
+    tag: str
     value: Any
-    start_mark: Any
-    end_mark: Any
-    flow_style: Any
-    def __init__(self, tag, value, start_mark=..., end_mark=..., flow_style=...) -> None: ...
+    start_mark: Mark | Any
+    end_mark: Mark | Any
+    flow_style: bool | Any
+    def __init__(
+        self, tag: str, value: Any, start_mark: Mark | None = ..., end_mark: Mark | None = ..., flow_style: bool | None = ...
+    ) -> None: ...
 
 class SequenceNode(CollectionNode):
-    id: Any
+    id: ClassVar[str]
 
 class MappingNode(CollectionNode):
-    id: Any
+    id: ClassVar[str]
