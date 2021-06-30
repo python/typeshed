e7c38e5f57c0ddc0ff432de4fb78cbaf38969784
Sebastian Rittau <srittau@rittau.biz>
2021-06-30 11:15:03 +0200

    Add httplib2 third-party stubs

---
+++ stubs/httplib2/httplib2/auth.pyi
@@ -0,0 +1,17 @@
+from typing import Any
+
+from .error import *
+
+UNQUOTE_PAIRS: Any
+unquote: Any
+tchar: Any
+token: Any
+token68: Any
+quoted_string: Any
+auth_param_name: Any
+auth_param: Any
+params: Any
+scheme: Any
+challenge: Any
+authentication_info: Any
+www_authenticate: Any
---
+++ stubs/httplib2/httplib2/iri2uri.pyi
@@ -0,0 +1,7 @@
+from typing import Any
+
+__contributors__: Any
+escape_range: Any
+
+def encode(c): ...
+def iri2uri(uri): ...
