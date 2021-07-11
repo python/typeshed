094f85c7be974c673da0834d1acc021777f63d22
Akuli <akuviljanen17@gmail.com>
2021-07-11 11:11:12 +0300

    replace individual styles with __getattr__ in styles/__init__.pyi

--- stubs/Pygments/pygments/styles/abap.pyi
+++
@@ -1,7 +0,0 @@
-from pygments.style import Style as Style
-from pygments.token import Comment as Comment, Error as Error, Keyword as Keyword, Name as Name, Number as Number, Operator as Operator, String as String
-from typing import Any
-
-class AbapStyle(Style):
-    default_style: str = ...
-    styles: Any = ...
--- stubs/Pygments/pygments/styles/sas.pyi
+++
@@ -1,7 +0,0 @@
-from pygments.style import Style as Style
-from pygments.token import Comment as Comment, Error as Error, Generic as Generic, Keyword as Keyword, Name as Name, Number as Number, Other as Other, String as String, Whitespace as Whitespace
-from typing import Any
-
-class SasStyle(Style):
-    default_style: str = ...
-    styles: Any = ...
