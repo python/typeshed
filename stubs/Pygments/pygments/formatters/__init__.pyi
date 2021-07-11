06eeb10eb0844a62134ebe20b141a9379d633ee3
Akuli <akuviljanen17@gmail.com>
2021-07-11 11:18:18 +0300

    import-export each formatter into formatters/__init__.pyi

--- stubs/Pygments/pygments/formatters/__init__.pyi
+++ stubs/Pygments/pygments/formatters/__init__.pyi
@@ -6,24 +6,19 @@ def get_formatter_by_name(_alias: Any, **options: Any): ...
 def load_formatter_from_file(filename: Any, formattername: str = ..., **options: Any): ...
 def get_formatter_for_filename(fn: Any, **options: Any): ...
 
-class _automodule(types.ModuleType):
-    def __getattr__(self, name: Any): ...
-
-# Names in __all__ with no definition:
-#   BBCodeFormatter
-#   BmpImageFormatter
-#   GifImageFormatter
-#   HtmlFormatter
-#   IRCFormatter
-#   ImageFormatter
-#   JpgImageFormatter
-#   LatexFormatter
-#   NullFormatter
-#   PangoMarkupFormatter
-#   RawTokenFormatter
-#   RtfFormatter
-#   SvgFormatter
-#   Terminal256Formatter
-#   TerminalFormatter
-#   TerminalTrueColorFormatter
-#   TestcaseFormatter
+from .bbcode import BBCodeFormatter as BBCodeFormatter
+from .html import HtmlFormatter as HtmlFormatter
+from .img import (
+    BmpImageFormatter as BmpImageFormatter,
+    GifImageFormatter as GifImageFormatter,
+    ImageFormatter as ImageFormatter,
+    JpgImageFormatter as JpgImageFormatter,
+)
+from .irc import IRCFormatter as IRCFormatter
+from .latex import LatexFormatter as LatexFormatter
+from .other import NullFormatter as NullFormatter, RawTokenFormatter as RawTokenFormatter, TestcaseFormatter as TestcaseFormatter
+from .pangomarkup import PangoMarkupFormatter as PangoMarkupFormatter
+from .rtf import RtfFormatter as RtfFormatter
+from .svg import SvgFormatter as SvgFormatter
+from .terminal import TerminalFormatter as TerminalFormatter
+from .terminal256 import Terminal256Formatter as Terminal256Formatter, TerminalTrueColorFormatter as TerminalTrueColorFormatter
