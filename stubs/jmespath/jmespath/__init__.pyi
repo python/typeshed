4b4f00d9f35d437d02e91d4a52085e1cfd388fa8
Jukka Lehtosalo <jukka@dropbox.com>
2021-07-14 15:45:59 +0100

    Fix issue reported by stubtest

--- stubs/jmespath/jmespath/__init__.pyi
+++ stubs/jmespath/jmespath/__init__.pyi
@@ -2,7 +2,5 @@ from jmespath import parser as parser
 from jmespath.visitor import Options as Options
 from typing import Any
 
-python_ver: Any
-
 def compile(expression): ...
 def search(expression, data, options: Any | None = ...): ...
