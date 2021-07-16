d99dbb58206b57cb1c855295e86aa59204e8d1a1
Nsukami Patrick <ptrck@nskm.xyz>
2021-07-16 02:47:53 +0000

    Improve various type signatures (2)

--- stubs/dateparser/dateparser/__init__.pyi
+++ stubs/dateparser/dateparser/__init__.pyi
@@ -1,10 +1,11 @@
-from typing import Any, Optional
+from datetime import datetime
+from typing import Any, Optional, List, Dict
 
 def parse(
-    date_string: Any,
+    date_string: str,
     date_formats: Optional[Any] = ...,
-    languages: Optional[Any] = ...,
-    locales: Optional[Any] = ...,
-    region: Optional[Any] = ...,
-    settings: Optional[Any] = ...,
-): ...
+    languages: Optional[List[str]] = ...,
+    locales: Optional[List[str]] = ...,
+    region: Optional[str] = ...,
+    settings: Optional[Dict] = ...,
+) -> Optional[datetime]: ...
