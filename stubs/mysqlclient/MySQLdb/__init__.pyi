a1b203b834c5144a727aae8439a71f757b7f0019
Jukka Lehtosalo <jukka@dropbox.com>
2021-07-16 11:08:32 +0100

    Create stubs for mysqlclient

---
+++ stubs/mysqlclient/MySQLdb/__init__.pyi
@@ -0,0 +1,33 @@
+from ._mysql import DataError as DataError, DatabaseError as DatabaseError, Error as Error, IntegrityError as IntegrityError, InterfaceError as InterfaceError, InternalError as InternalError, MySQLError as MySQLError, NotSupportedError as NotSupportedError, OperationalError as OperationalError, ProgrammingError as ProgrammingError, Warning as Warning, debug as debug, escape as escape, escape_string as escape_string, get_client_info as get_client_info, string_literal as string_literal
+from MySQLdb.constants import FIELD_TYPE as FIELD_TYPE
+from MySQLdb.release import version_info as version_info
+from MySQLdb.times import Date as Date, DateFromTicks as DateFromTicks, Time as Time, TimeFromTicks as TimeFromTicks, Timestamp as Timestamp, TimestampFromTicks as TimestampFromTicks
+from sets import ImmutableSet as frozenset
+from typing import Any
+
+threadsafety: int
+apilevel: str
+paramstyle: str
+
+class DBAPISet(frozenset):
+    def __eq__(self, other): ...
+
+STRING: Any
+BINARY: Any
+NUMBER: Any
+DATE: Any
+TIME: Any
+TIMESTAMP: Any
+DATETIME = TIMESTAMP
+ROWID: Any
+
+def Binary(x): ...
+def Connect(*args, **kwargs): ...
+connect = Connect
+Connection = Connect
+
+# Names in __all__ with no definition:
+#   connections
+#   constants
+#   converters
+#   cursors
---
+++ stubs/mysqlclient/MySQLdb/constants/__init__.pyi
@@ -0,0 +1,6 @@
+# Names in __all__ with no definition:
+#   CLIENT
+#   CR
+#   ER
+#   FIELD_TYPE
+#   FLAG
