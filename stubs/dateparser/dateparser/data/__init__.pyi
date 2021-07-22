097f88c25c0575abca8b93f51ab5211b274a21d1
Nsukami Patrick <ptrck@nskm.xyz>
2021-07-22 14:06:53 +0000

    Remove directory containing translations data

--- stubs/dateparser/dateparser/data/__init__.pyi
+++ stubs/dateparser/dateparser/data/__init__.pyi
@@ -1,3 +1,1 @@
-from dateparser.data import date_translation_data as date_translation_data
-
 from .languages_info import language_locale_dict as language_locale_dict, language_order as language_order
--- stubs/dateparser/dateparser/data/date_translation_data/to.pyi
+++
@@ -1,3 +0,0 @@
-from typing import Any
-
-info: Any
