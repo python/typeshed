cd57b8f078dd43f103ff36729c705cb00dac8ace
Stephanie Ding <sym@fb.com>
2021-07-23 18:25:26 -0400

    Add partial type stubs for xml.pulldom

--- stdlib/xml/dom/pulldom.pyi
+++ stdlib/xml/dom/pulldom.pyi
@@ -1,3 +1,30 @@
-from typing import Any
+import xml.sax.handler
+from typing import Any, BytesIO, Optional
 
-def __getattr__(name: str) -> Any: ...  # incomplete
+START_ELEMENT: str
+END_ELEMENT: str
+COMMENT: str
+START_DOCUMENT: str
+END_DOCUMENT: str
+PROCESSING_INSTRUCTION: str
+IGNORABLE_WHITESPACE: str
+CHARACTERS: str
+
+class PullDOM(xml.sax.ContentHandler):
+    def __getattr__(self, name: str) -> Any: ...  # incomplete
+
+class ErrorHandler:
+    def warning(self, exception) -> None: ...
+    def error(self, exception) -> None: ...
+    def fatalError(self, exception) -> None: ...
+
+class DOMEventStream:
+    def __getattr__(self, name: str) -> Any: ...  # incomplete
+
+class SAX2DOM(PullDOM):
+    def __getattr__(self, name: str) -> Any: ...  # incomplete
+
+default_bufsize: int
+
+def parse(stream_or_string: str | BytesIO, parser: Optional[XMLReader] = ..., bufsize: Optional[int] = ...) -> DOMEventStream: ...
+def parseString(string: str, parser: Optional[XMLReader] = ...) -> DOMEventStream: ...
