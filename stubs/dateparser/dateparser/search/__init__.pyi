73abc80fa3474f88244c98f58da7eb09043cef3a
Nsukami Patrick <ptrck@nskm.xyz>
2021-07-25 16:23:49 +0000

    Fix error: Import Dict is not accessed

--- stubs/dateparser/dateparser/search/__init__.pyi
+++ stubs/dateparser/dateparser/search/__init__.pyi
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Any, Dict, List, Mapping, Optional, Set, Tuple, Union
+from typing import Any, List, Mapping, Optional, Set, Tuple, Union
 
 from dateparser.search.search import DateSearchWithDetection as DateSearchWithDetection
 
