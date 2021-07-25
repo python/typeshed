d28fbf7d3266ce60880d90dc524f281310eef37d
Nsukami Patrick <ptrck@nskm.xyz>
2021-07-25 15:36:40 +0000

    Improve dateparser.search.search_dates signature
    According to the checks in DateSearchWithDetection.detect_language,
    languages parameter can be a list but also a tuple or set

--- stubs/dateparser/dateparser/search/__init__.pyi
+++ stubs/dateparser/dateparser/search/__init__.pyi
@@ -1,8 +1,11 @@
 from datetime import datetime
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 from dateparser.search.search import DateSearchWithDetection as DateSearchWithDetection
 
 def search_dates(
-    text: str, languages: Optional[List[str]] = ..., settings: Optional[Dict[Any, Any]] = ..., add_detected_language: bool = ...
+    text: str,
+    languages: Optional[Union[List[str], Tuple[str], Set[str]]] = ...,
+    settings: Optional[Dict[Any, Any]] = ...,
+    add_detected_language: bool = ...,
 ) -> Union[List[Tuple[str, datetime]], List[Tuple[str, datetime, str]]]: ...
