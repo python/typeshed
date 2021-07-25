758501b599f1a3b048b5f409bdb6c65352298f31
Nsukami Patrick <ptrck@nskm.xyz>
2021-07-25 15:51:15 +0000

    Improve various type signatures (10)
    Whenever possible we prefer covariant types like Mapping in argument positions.
    So that, people will be able to pass every possible kind of dict.

--- stubs/dateparser/dateparser/search/__init__.pyi
+++ stubs/dateparser/dateparser/search/__init__.pyi
@@ -1,11 +1,11 @@
 from datetime import datetime
-from typing import Any, Dict, List, Optional, Set, Tuple, Union
+from typing import Any, Dict, List, Mapping, Optional, Set, Tuple, Union
 
 from dateparser.search.search import DateSearchWithDetection as DateSearchWithDetection
 
 def search_dates(
     text: str,
     languages: Optional[Union[List[str], Tuple[str], Set[str]]] = ...,
-    settings: Optional[Dict[Any, Any]] = ...,
+    settings: Optional[Mapping[Any, Any]] = ...,
     add_detected_language: bool = ...,
 ) -> Union[List[Tuple[str, datetime]], List[Tuple[str, datetime, str]]]: ...
