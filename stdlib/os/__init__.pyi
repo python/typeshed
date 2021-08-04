6f1a31ed1840c448a6e3b8e23ad3aa7767c82824
Stephanie Ding <sym@fb.com>
2021-08-04 12:42:06 -0400

    Add stubs for posix_spawn and posix_spawnp

--- stdlib/os/__init__.pyi
+++ stdlib/os/__init__.pyi
@@ -801,6 +801,32 @@ if sys.platform == "win32":
 
 else:
     # Unix only
+    def posix_spawn(
+        path: _FdOrAnyPath,
+        argv: _ExecVArgs,
+        env: _ExecEnv,
+        *,
+        file_actions: Optional[Sequence[Tuple]] = ...,
+        setpgroup: Optional[int] = ...,
+        resetids: bool = ...,
+        setsid: bool = ...,
+        setsigmask: Tuple = ...,
+        setsigdef: Tuple = ...,
+        scheduler: Optional[Tuple] = ...,
+    ) -> int: ...
+    def posix_spawnp(
+        path: _FdOrAnyPath,
+        argv: _ExecVArgs,
+        env: _ExecEnv,
+        *,
+        file_actions: Optional[Sequence[Tuple]] = ...,
+        setpgroup: Optional[int] = ...,
+        resetids: bool = ...,
+        setsid: bool = ...,
+        setsigmask: Tuple = ...,
+        setsigdef: Tuple = ...,
+        scheduler: Optional[Tuple] = ...,
+    ) -> int: ...
     def spawnlp(mode: int, file: StrOrBytesPath, arg0: StrOrBytesPath, *args: StrOrBytesPath) -> int: ...
     def spawnlpe(mode: int, file: StrOrBytesPath, arg0: StrOrBytesPath, *args: Any) -> int: ...  # Imprecise signature
     def spawnvp(mode: int, file: StrOrBytesPath, args: _ExecVArgs) -> int: ...
