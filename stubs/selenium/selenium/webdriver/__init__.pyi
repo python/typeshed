2c9f679c994c880d685d30fc653f77c3dbf522f9
Eric Traut <erictr@microsoft.com>
2021-08-05 19:34:05 -0700

    Added missing symbol in selenium stubs that was included in `__all__` list but not otherwise provided. This was identified by a recently-added diagnostic check in pyright that validates the `__all__` list.

--- stubs/selenium/selenium/webdriver/__init__.pyi
+++ stubs/selenium/selenium/webdriver/__init__.pyi
@@ -6,6 +6,7 @@ from .common.action_chains import ActionChains as ActionChains
 from .common.desired_capabilities import DesiredCapabilities as DesiredCapabilities
 from .common.proxy import Proxy as Proxy
 from .common.touch_actions import TouchActions as TouchActions
+from .edge.options import Options as EdgeOptions
 from .edge.webdriver import WebDriver as Edge
 from .firefox.firefox_profile import FirefoxProfile as FirefoxProfile
 from .firefox.options import Options as FirefoxOptions
