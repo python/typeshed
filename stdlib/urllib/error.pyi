92bff52851530d81eecc3381c95ce6575e2be382
Maarten ter Huurne <maarten@boxingbeetle.com>
2021-08-06 02:37:39 +0200

    Use Message for the headers parameter of HTTPError
    When the standard library constructs `HTTPError`, it will
    pass an `http.client.HTTPMessage`, which is a subclass of
    `email.message.Message`. Picking the superclass for the
    annotations gives users the flexibility to for example
    the result of the `email.message_from_X()` functions.
    
    The only thing unique to `HTTPMessage` is the undocumented
    `getallmatchingheaders()` method, which is only called by
    `http.server.CGIHTTPRequestHandler.run_cgi()`. That class
    gets its headers from `http.client.parse_headers()` and not
    from `HTTPError`, so I think it's safe to use `Message`
    as the annotation.

--- stdlib/urllib/error.pyi
+++ stdlib/urllib/error.pyi
@@ -1,5 +1,5 @@
 from email.message import Message
-from typing import IO, Mapping, Optional, Tuple, Union
+from typing import IO, Optional, Tuple, Union
 from urllib.response import addinfourl
 
 # Stubs for urllib.error
@@ -10,7 +10,7 @@ class URLError(IOError):
 
 class HTTPError(URLError, addinfourl):
     code: int
-    def __init__(self, url: str, code: int, msg: str, hdrs: Mapping[str, str], fp: Optional[IO[bytes]]) -> None: ...
+    def __init__(self, url: str, code: int, msg: str, hdrs: Message, fp: Optional[IO[bytes]]) -> None: ...
 
 class ContentTooShortError(URLError):
     content: Tuple[str, Message]
