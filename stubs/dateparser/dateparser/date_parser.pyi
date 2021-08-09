f4043d3b2cb7db89866b70a3d4c6b0bd18d576f1
Nsukami Patrick <ptrck@nskm.xyz>
2021-08-09 17:10:31 +0000

    Fix reportUnusedImport and please pyright

--- stubs/dateparser/dateparser/date_parser.pyi
+++ stubs/dateparser/dateparser/date_parser.pyi
@@ -1,9 +1,5 @@
 from typing import Any, Optional
 
-from .conf import apply_settings
-from .timezone_parser import pop_tz_offset_from_string
-from .utils import apply_timezone, localize_timezone, strip_braces
-
 class DateParser:
     def parse(self, date_string: Any, parse_method: Any, settings: Optional[Any] = ...): ...
 
