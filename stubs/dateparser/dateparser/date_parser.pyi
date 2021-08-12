d5eef776b58fe2c15ef1d46b315188a43db677c1
Nsukami Patrick <ptrck@nskm.xyz>
2021-08-12 06:54:01 +0000

    Remove 'Any' whenever necessary
    Any is reserved for cases where it really accepts anything,
    or there's no way to do better with the current type system.
    This only applies to function arguments.

--- stubs/dateparser/dateparser/date_parser.pyi
+++ stubs/dateparser/dateparser/date_parser.pyi
@@ -1,6 +1,6 @@
 from typing import Any, Optional
 
 class DateParser:
-    def parse(self, date_string: Any, parse_method: Any, settings: Optional[Any] = ...): ...
+    def parse(self, date_string, parse_method, settings: Optional[Any] = ...): ...
 
 date_parser: Any
