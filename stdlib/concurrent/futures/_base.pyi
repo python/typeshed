001c56e667311559528404975096c4f5c36e1984
HunterAP <etgarkaspi@gmail.com>
2021-09-02 15:10:37 -0400

    Reverted change to wait method and DoneAndNotDoneFutures class

--- stdlib/concurrent/futures/_base.pyi
+++ stdlib/concurrent/futures/_base.pyi
@@ -75,7 +75,7 @@ class Executor:
 def as_completed(fs: Iterable[Future[_T]], timeout: float | None = ...) -> Iterator[Future[_T]]: ...
 
 # Ideally this would be a namedtuple, but mypy doesn't support generic tuple types. See #1976
-class DoneAndNotDoneFutures(Sequence[Set[Future[_T]]]):
+class DoneAndNotDoneFutures(Sequence[_T]):
     done: Set[Future[_T]]
     not_done: Set[Future[_T]]
     def __new__(_cls, done: Set[Future[_T]], not_done: Set[Future[_T]]) -> DoneAndNotDoneFutures[_T]: ...
@@ -85,7 +85,7 @@ class DoneAndNotDoneFutures(Sequence[Set[Future[_T]]]):
     @overload
     def __getitem__(self, s: slice) -> DoneAndNotDoneFutures[_T]: ...
 
-def wait(fs: Set[Future[_T]], timeout: float | None = ..., return_when: str = ...) -> DoneAndNotDoneFutures[_T]: ...
+def wait(fs: Iterable[Future[_T]], timeout: float | None = ..., return_when: str = ...) -> DoneAndNotDoneFutures[_T]: ...
 
 class _Waiter:
     event: threading.Event
