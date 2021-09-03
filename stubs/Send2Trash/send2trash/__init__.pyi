555505e6f900a2c3561d2f71849049caaee502cf
Akuli <akuviljanen17@gmail.com>
2021-09-03 23:33:21 +0300

    imports

--- stubs/Send2Trash/send2trash/__init__.pyi
+++ stubs/Send2Trash/send2trash/__init__.pyi
@@ -1,3 +1,6 @@
+from _typeshed import AnyPath
+from typing import Any
+
 from .exceptions import TrashPermissionError as TrashPermissionError
 
 # The list should be list[AnyPath] but that doesn't work because invariance
