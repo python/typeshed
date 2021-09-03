88b7d3261f4879eaaac423082b1f221d548ea5d1
Akuli <akuviljanen17@gmail.com>
2021-09-03 23:29:22 +0300

    add typed send2trash to __init__

--- stubs/Send2Trash/send2trash/__init__.pyi
+++ stubs/Send2Trash/send2trash/__init__.pyi
@@ -1,1 +1,4 @@
 from .exceptions import TrashPermissionError as TrashPermissionError
+
+# The list should be list[AnyPath] but that doesn't work because invariance
+def send2trash(paths: list[Any] | AnyPath) -> None: ...
