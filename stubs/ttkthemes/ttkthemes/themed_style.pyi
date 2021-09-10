7f6aa0061a1108130a9bb066d5b93ac1a5172e44
Akuli <akuviljanen17@gmail.com>
2021-09-10 16:27:30 +0300

    default

--- stubs/ttkthemes/ttkthemes/themed_style.pyi
+++ stubs/ttkthemes/ttkthemes/themed_style.pyi
@@ -8,5 +8,5 @@ class ThemedStyle(ttk.Style, ThemedWidget):
         self, master: tkinter.Misc | None, *, theme: str | None = ..., gif_override: bool | None = ..., **kwargs
     ) -> None: ...
     # Unlike ttk.Style, can't return None
-    def theme_use(self, theme_name: str | None) -> str: ...  # type: ignore
+    def theme_use(self, theme_name: str | None = ...) -> str: ...  # type: ignore
     def theme_names(self) -> list[str]: ...  # type: ignore
