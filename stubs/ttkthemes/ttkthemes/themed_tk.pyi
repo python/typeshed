516321af9086b81c6e8f1d56a38c0e8c4dde9321
Akuli <akuviljanen17@gmail.com>
2021-09-10 16:42:37 +0300

    fix

--- stubs/ttkthemes/ttkthemes/themed_tk.pyi
+++ stubs/ttkthemes/ttkthemes/themed_tk.pyi
@@ -7,19 +7,19 @@ class ThemedTk(tkinter.Tk, ThemedWidget):
     def __init__(
         self,
         # non-keyword-only args copied from tkinter.Tk
-        toplevel: bool | None = ...,
-        themebg: bool | None = ...,
-        background: bool | None = ...,  # old alias for themebg
-        gif_override: bool = ...,
-        *,
-        theme: str | None = ...,
-        # fonts argument does nothing
         screenName: str | None = ...,
         baseName: str | None = ...,
         className: str = ...,
         useTk: bool = ...,
         sync: bool = ...,
         use: str | None = ...,
+        *,
+        theme: str | None = ...,
+        # fonts argument does nothing
+        toplevel: bool | None = ...,
+        themebg: bool | None = ...,
+        background: bool | None = ...,  # old alias for themebg
+        gif_override: bool = ...,
     ) -> None: ...
     def set_theme(self, theme_name, toplevel: bool | None = ..., themebg: bool | None = ...) -> None: ...
     # TODO: currently no good way to say "use the same big list of kwargs as parent class but also add these"
