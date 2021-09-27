c856898611586b6807bd0c08c00be6547b84d94b
cmckain <cmckain@uci.edu>
2021-09-26 23:58:05 -0700

    Update element.pyi

--- stubs/beautifulsoup4/bs4/element.pyi
+++ stubs/beautifulsoup4/bs4/element.pyi
@@ -319,7 +319,7 @@ class Tag(PageElement):
         text: _Strainable | None = ...,
         limit: int | None = ...,
         **kwargs: _Strainable,
-    ) -> ResultSet[Type[_PageElementT]]: ...
+    ) -> ResultSet[_PageElementT]: ...
     __call__ = find_all
     findAll = find_all
     findChildren = find_all
