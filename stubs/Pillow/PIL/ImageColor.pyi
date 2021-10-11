ca208d15d8876d3b09a73ae2c200a3e247d6cb0b
Nikita Sobolev <mail@sobolevn.me>
2021-10-11 16:10:00 +0300

    Update ImageColor.pyi

--- stubs/Pillow/PIL/ImageColor.pyi
+++ stubs/Pillow/PIL/ImageColor.pyi
@@ -1,6 +1,8 @@
-_RGB = tuple[int, int, int] | tuple[int, int, int, int]
+from typing import Tuple
+
+_RGB = Tuple[int, int, int] | Tuple[int, int, int, int]
 _Ink = str | int | _RGB
-_GreyScale = tuple[int, int]
+_GreyScale = Tuple[int, int]
 
 def getrgb(color: _Ink) -> _RGB: ...
 def getcolor(color: _Ink, mode: str) -> _RGB | _GreyScale: ...
