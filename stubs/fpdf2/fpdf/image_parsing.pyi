a866598d528a2bf06b3699e1cc3138aa558cf16d
Sebastian Rittau <srittau@rittau.biz>
2021-11-16 16:31:58 +0100

    Work around mypy bug

--- stubs/fpdf2/fpdf/image_parsing.pyi
+++ stubs/fpdf2/fpdf/image_parsing.pyi
@@ -1,9 +1,9 @@
-from typing import Any
+from typing import Any, Tuple
 from typing_extensions import Literal
 
 _ImageFilter = Literal["AUTO", "FlateDecode", "DCTDecode", "JPXDecode"]
 
-SUPPORTED_IMAGE_FILTERS: tuple[_ImageFilter, ...]
+SUPPORTED_IMAGE_FILTERS: Tuple[_ImageFilter, ...]
 
 def load_image(filename): ...
 
