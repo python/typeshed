6cb9979761b5fca8ac629f344fe942ba62b2ffb7
Sebastian Rittau <srittau@rittau.biz>
2021-11-16 09:48:53 +0100

    Recommend to use mypy error codes if applicable
    Use mypy error codes in stdlib/builtins.pyi.

--- tests/mypy_test.py
+++ tests/mypy_test.py
@@ -209,6 +209,7 @@ def get_mypy_flags(args, major: int, minor: int, temp_name: str, *, custom_types
             "--disallow-any-generics",
             "--disallow-subclassing-any",
             "--warn-incomplete-stub",
+            "--show-error-codes",
             "--no-error-summary",
         ]
     if custom_typeshed:
