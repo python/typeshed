2a6fcc5af24a6771bb5d8653c6ffe7095ec4dffb
Alex Waygood <Alex.Waygood@Gmail.com>
2021-11-25 01:52:53 +0000

    Update coroutines.pyi

--- stdlib/asyncio/coroutines.pyi
+++ stdlib/asyncio/coroutines.pyi
@@ -1,12 +1,14 @@
 import sys
 import types
-from collections.abc import Callable, Coroutine
-from typing import Any, TypeVar
+from collections.abc import Coroutine
+from typing import Any
 from typing_extensions import TypeGuard
 
-_F = TypeVar("_F", bound=Callable[..., Any])
-
 if sys.version_info < (3, 11):
+    from collections.abc import Callable
+    from typing import TypeVar
+    
+    _F = TypeVar("_F", bound=Callable[..., Any])
     def coroutine(func: _F) -> _F: ...
 
 def iscoroutinefunction(func: object) -> bool: ...
