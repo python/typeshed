61cbeaa8fdf7a20e546b058b18f99d1f3b5ac31c
Alex Waygood <Alex.Waygood@Gmail.com>
2021-11-25 20:51:42 +0000

    Revert all `contextlib`-related changes to `check_new_syntax.py`

--- tests/check_new_syntax.py
+++ tests/check_new_syntax.py
@@ -4,23 +4,10 @@ import ast
 import sys
 from itertools import chain
 from pathlib import Path
-from typing import TypedDict
-
-
-class ModuleImportsDict(TypedDict):
-    set_from_collections_abc: bool
-    context_manager_from_typing: bool
-    async_context_manager_from_typing: bool
-
-
-STUBS_SUPPORTING_PYTHON_2 = frozenset(
-    {path.parent for path in Path("stubs").rglob("METADATA.toml") if "python2 = true" in path.read_text().splitlines()}
-)
 
 
 def check_new_syntax(tree: ast.AST, path: Path) -> list[str]:
     errors = []
-    python_2_support_required = any(directory in path.parents for directory in STUBS_SUPPORTING_PYTHON_2)
 
     def unparse_without_tuple_parens(node: ast.AST) -> str:
         if isinstance(node, ast.Tuple) and node.elts:
@@ -30,15 +17,9 @@ def check_new_syntax(tree: ast.AST, path: Path) -> list[str]:
     def is_dotdotdot(node: ast.AST) -> bool:
         return isinstance(node, ast.Constant) and node.s is Ellipsis
 
-    def add_ContextManager_error(node: ast.Subscript | ast.Name, *, is_async: bool) -> None:
-        insert = "Async" if is_async else ""
-        errors.append(
-            f"{path}:{node.lineno}: Use `contextlib.Abstract{insert}ContextManager` instead of `typing.{insert}ContextManager`"
-        )
-
     class OldSyntaxFinder(ast.NodeVisitor):
-        def __init__(self, module_imports_info_dict: ModuleImportsDict) -> None:
-            self.module_imports_info_dict = module_imports_info_dict
+        def __init__(self, *, set_from_collections_abc: bool) -> None:
+            self.set_from_collections_abc = set_from_collections_abc
 
         def visit_Subscript(self, node: ast.Subscript) -> None:
             if isinstance(node.value, ast.Name):
@@ -51,7 +32,7 @@ def check_new_syntax(tree: ast.AST, path: Path) -> list[str]:
                 if node.value.id in {"List", "FrozenSet"}:
                     new_syntax = f"{node.value.id.lower()}[{ast.unparse(node.slice)}]"
                     errors.append(f"{path}:{node.lineno}: Use built-in generics, e.g. `{new_syntax}`")
-                if not self.module_imports_info_dict["set_from_collections_abc"] and node.value.id == "Set":
+                if not self.set_from_collections_abc and node.value.id == "Set":
                     new_syntax = f"set[{ast.unparse(node.slice)}]"
                     errors.append(f"{path}:{node.lineno}: Use built-in generics, e.g. `{new_syntax}`")
                 if node.value.id == "Deque":
@@ -72,14 +53,6 @@ def check_new_syntax(tree: ast.AST, path: Path) -> list[str]:
                 ):
                     new_syntax = f"tuple[{unparse_without_tuple_parens(node.slice)}]"
                     errors.append(f"{path}:{node.lineno}: Use built-in generics, e.g. `{new_syntax}`")
-                if not python_2_support_required:
-                    if self.module_imports_info_dict["context_manager_from_typing"] and node.value.id == "ContextManager":
-                        add_ContextManager_error(node, is_async=False)
-                    if (
-                        self.module_imports_info_dict["async_context_manager_from_typing"]
-                        and node.value.id == "AsyncContextManager"
-                    ):
-                        add_ContextManager_error(node, is_async=True)
 
             self.generic_visit(node)
 
@@ -89,69 +62,32 @@ def check_new_syntax(tree: ast.AST, path: Path) -> list[str]:
     # TODO: can use built-in generics in type aliases
     class AnnotationFinder(ast.NodeVisitor):
         def __init__(self) -> None:
-            self.module_imports_info_dict = ModuleImportsDict(
-                set_from_collections_abc=False, context_manager_from_typing=False, async_context_manager_from_typing=False
-            )
-
-        def old_syntax_finder(self) -> OldSyntaxFinder:
-            """Convenience method to create an `OldSyntaxFinder` instance with the correct state"""
-            return OldSyntaxFinder(self.module_imports_info_dict)
+            self.set_from_collections_abc = False
 
         def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
             if node.module == "collections.abc":
-                classes_from_collections_abc = node.names
-                if any(cls.name == "Set" for cls in classes_from_collections_abc):
-                    self.module_imports_info_dict["set_from_collections_abc"] = True
-
-            elif node.module == "typing":
-                classes_from_typing = node.names
-
-                for cls in classes_from_typing:
-                    cls_name = cls.name
-                    if cls_name == "ContextManager":
-                        self.module_imports_info_dict["context_manager_from_typing"] = True
-                    elif cls_name == "AsyncContextManager":
-                        self.module_imports_info_dict["async_context_manager_from_typing"] = True
+                imported_classes = node.names
+                if any(cls.name == "Set" for cls in imported_classes):
+                    self.set_from_collections_abc = True
 
             self.generic_visit(node)
 
         def visit_AnnAssign(self, node: ast.AnnAssign) -> None:
-            self.old_syntax_finder().visit(node.annotation)
+            OldSyntaxFinder(set_from_collections_abc=self.set_from_collections_abc).visit(node.annotation)
 
         def visit_arg(self, node: ast.arg) -> None:
             if node.annotation is not None:
-                self.old_syntax_finder().visit(node.annotation)
+                OldSyntaxFinder(set_from_collections_abc=self.set_from_collections_abc).visit(node.annotation)
 
         def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
             if node.returns is not None:
-                self.old_syntax_finder().visit(node.returns)
+                OldSyntaxFinder(set_from_collections_abc=self.set_from_collections_abc).visit(node.returns)
             self.generic_visit(node)
 
         def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
             if node.returns is not None:
-                self.old_syntax_finder().visit(node.returns)
+                OldSyntaxFinder(set_from_collections_abc=self.set_from_collections_abc).visit(node.returns)
             self.generic_visit(node)
-
-        if not python_2_support_required:
-
-            def visit_ClassDef(self, node: ast.ClassDef) -> None:
-                context_manager_from_typing = self.module_imports_info_dict["context_manager_from_typing"]
-                async_context_manager_from_typing = self.module_imports_info_dict["async_context_manager_from_typing"]
-
-                for base in node.bases:
-                    match base:
-                        case ast.Subscript(value=ast.Name(id="ContextManager")) if context_manager_from_typing:
-                            add_ContextManager_error(base, is_async=False)
-                        case ast.Name(id="ContextManager") if context_manager_from_typing:
-                            add_ContextManager_error(base, is_async=False)
-                        case ast.Subscript(value=ast.Name(id="AsyncContextManager")) if async_context_manager_from_typing:
-                            add_ContextManager_error(base, is_async=True)
-                        case ast.Name(id="AsyncContextManager") if async_context_manager_from_typing:
-                            add_ContextManager_error(base, is_async=True)
-                        case _:
-                            pass
-
-                self.generic_visit(node)
 
     AnnotationFinder().visit(tree)
     return errors
