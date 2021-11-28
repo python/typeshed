58ecccbab82ac48b284243a21bce61ff6d03c2c0
hauntsaninja <>
2021-11-27 19:26:24 -0800

    dummy_thread/ing: remove in 3.9
    https://bugs.python.org/issue37312

--- tests/check_consistent.py
+++ tests/check_consistent.py
@@ -19,7 +19,6 @@ import tomli
 
 consistent_files = [
     {"stdlib/@python2/builtins.pyi", "stdlib/@python2/__builtin__.pyi"},
-    {"stdlib/threading.pyi", "stdlib/_dummy_threading.pyi"},
 ]
 metadata_keys = {"version", "python2", "requires", "extra_description", "obsolete_since"}
 allowed_files = {"README.md"}
