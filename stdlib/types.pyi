652ee2fc34ceb774f8b88f60e42727da9de4a6a7
Alex Waygood <Alex.Waygood@Gmail.com>
2021-11-29 13:31:50 +0000

    Update `replace()`

--- stdlib/types.pyi
+++ stdlib/types.pyi
@@ -123,7 +123,7 @@ class CodeType:
             freevars: Tuple[str, ...] = ...,
             cellvars: Tuple[str, ...] = ...,
         ) -> None: ...
-    if sys.version_info >= (3, 8):
+    if sys.version_info >= (3, 8) and sys.version_info < (3, 10):
         def replace(
             self,
             *,
@@ -144,7 +144,28 @@ class CodeType:
             co_name: str = ...,
             co_lnotab: bytes = ...,
         ) -> CodeType: ...
-    if sys.version_info >= (3, 10):
+    elif sys.version_info >= (3, 10):
+        def replace(
+            self,
+            *,
+            co_argcount: int = ...,
+            co_posonlyargcount: int = ...,
+            co_kwonlyargcount: int = ...,
+            co_nlocals: int = ...,
+            co_stacksize: int = ...,
+            co_flags: int = ...,
+            co_firstlineno: int = ...,
+            co_code: bytes = ...,
+            co_consts: Tuple[Any, ...] = ...,
+            co_names: Tuple[str, ...] = ...,
+            co_varnames: Tuple[str, ...] = ...,
+            co_freevars: Tuple[str, ...] = ...,
+            co_cellvars: Tuple[str, ...] = ...,
+            co_filename: str = ...,
+            co_name: str = ...,
+            co_lnotab: bytes = ...,
+            co_linetable: object = ...,
+        ) -> CodeType: ...
         def co_lines(self) -> Iterator[tuple[int, int, int | None]]: ...
         co_linetable: object
     if sys.version_info >= (3, 11):
