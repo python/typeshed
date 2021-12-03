9dc75772d336c218b2f628fa1576b57d0169f265
kasium <15907922+kasium@users.noreply.github.com>
2021-12-03 11:41:49 +0100

    Annotate remaining opentracing fields, arguments, and return types (#6476)

--- stubs/opentracing/opentracing/mocktracer/text_propagator.pyi
+++ stubs/opentracing/opentracing/mocktracer/text_propagator.pyi
@@ -5,10 +5,10 @@ from .propagator import Propagator
 
 prefix_tracer_state: str
 prefix_baggage: str
-field_name_trace_id: Any
-field_name_span_id: Any
+field_name_trace_id: str
+field_name_span_id: str
 field_count: int
 
 class TextPropagator(Propagator):
-    def inject(self, span_context: SpanContext, carrier) -> None: ...
-    def extract(self, carrier): ...
+    def inject(self, span_context: SpanContext, carrier: dict[Any, Any]) -> None: ...
+    def extract(self, carrier: dict[Any, Any]) -> SpanContext: ...
--- stubs/opentracing/opentracing/tracer.pyi
+++ stubs/opentracing/opentracing/tracer.pyi
@@ -1,3 +1,7 @@
+from typing import Any, NamedTuple
+
+from .propagation import Format
+from .scope import Scope
 from .scope_manager import ScopeManager
 from .span import Span, SpanContext
 
@@ -6,35 +10,39 @@ class Tracer:
     @property
     def scope_manager(self) -> ScopeManager: ...
     @property
-    def active_span(self) -> Span: ...
+    def active_span(self) -> Span | None: ...
     def start_active_span(
         self,
         operation_name: str,
-        child_of=...,
-        references=...,
-        tags=...,
-        start_time=...,
+        child_of: Span | SpanContext | None = ...,
+        references: list[Reference] | None = ...,
+        tags: dict[Any, Any] | None = ...,
+        start_time: float | None = ...,
         ignore_active_span: bool = ...,
         finish_on_close: bool = ...,
-    ): ...
+    ) -> Scope: ...
     def start_span(
         self,
         operation_name: str | None = ...,
-        child_of=...,
-        references=...,
-        tags=...,
-        start_time=...,
+        child_of: Span | SpanContext | None = ...,
+        references: list[Reference] | None = ...,
+        tags: dict[Any, Any] | None = ...,
+        start_time: float | None = ...,
         ignore_active_span: bool = ...,
-    ): ...
-    def inject(self, span_context: SpanContext, format, carrier) -> None: ...
-    def extract(self, format, carrier): ...
+    ) -> Span: ...
+    def inject(self, span_context: SpanContext, format: Format, carrier: dict[Any, Any]) -> None: ...
+    def extract(self, format: Format, carrier: dict[Any, Any]) -> SpanContext: ...
 
 class ReferenceType:
     CHILD_OF: str
     FOLLOWS_FROM: str
 
-class Reference: ...
+class Reference(NamedTuple):
+    type: str
+    referenced_context: SpanContext | None
 
-def child_of(referenced_context=...): ...
-def follows_from(referenced_context=...): ...
-def start_child_span(parent_span: Span, operation_name: str, tags=..., start_time=...): ...
+def child_of(referenced_context: SpanContext | None = ...) -> Reference: ...
+def follows_from(referenced_context: SpanContext | None = ...) -> Reference: ...
+def start_child_span(
+    parent_span: Span, operation_name: str, tags: dict[Any, Any] | None = ..., start_time: float | None = ...
+) -> Span: ...
