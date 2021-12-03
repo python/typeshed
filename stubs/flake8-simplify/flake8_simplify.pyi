a93481f29b9755dcbd41f1ccbcd8378c816864a3
Kai Mueller <15907922+kasium@users.noreply.github.com>
2021-12-03 11:40:07 +0000

    Fix syntax

--- stubs/flake8-simplify/flake8_simplify.pyi
+++ stubs/flake8-simplify/flake8_simplify.pyi
@@ -1,8 +1,8 @@
 import ast
-from typing import Any, Generator, Tuple, Type
+from typing import Any, Generator, Type
 
 class Plugin:
     name: str
     version: str
     def __init__(self, tree: ast.AST) -> None: ...
-    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]: ...
+    def run(self) -> Generator[tuple[int, int, str, Type[Any]], None, None]: ...
