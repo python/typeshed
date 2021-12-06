b49d92233cb6e36d4c55cc46fe0684c85e6ba5a0
Kai Mueller <15907922+kasium@users.noreply.github.com>
2021-12-06 11:08:04 +0000

    Comments

--- stubs/flake8-docstrings/flake8_docstrings.pyi
+++ stubs/flake8-docstrings/flake8_docstrings.pyi
@@ -1,13 +1,19 @@
 import argparse
 import ast
-from typing import Any, Generator, Type
+from typing import Any, ClassVar, Generator, Iterable, Type
 
 class pep257Checker:
-    name: str
-    version: str
+    name: ClassVar[str]
+    version: ClassVar[str]
+    tree: ast.AST
+    filename: str
+    checker: Any
+    source: str
     def __init__(self, tree: ast.AST, filename: str, lines: Iterable[str]) -> None: ...
     @classmethod
     def add_options(cls, parser: Any) -> None: ...
     @classmethod
     def parse_options(cls, options: argparse.Namespace) -> None: ...
     def run(self) -> Generator[tuple[int, int, str, Type[Any]], None, None]: ...
+
+def __getattr__(name: str) -> Any: ...  # incomplete
