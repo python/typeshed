3dea05cb61689023c3b34aed1956cdfa8e1689c0
Akuli <akuviljanen17@gmail.com>
2021-12-06 14:50:01 +0200

    import overload

--- stdlib/traceback.pyi
+++ stdlib/traceback.pyi
@@ -1,7 +1,7 @@
 import sys
 from _typeshed import SupportsWrite
 from types import FrameType, TracebackType
-from typing import IO, Any, Generator, Iterable, Iterator, List, Mapping, Optional, Tuple, Type
+from typing import IO, Any, Generator, Iterable, Iterator, List, Mapping, Optional, Tuple, Type, overload
 
 _PT = Tuple[str, int, str, Optional[str]]
 
