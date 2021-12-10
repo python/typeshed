cea53fa0aa15bd3ae53da54f52d1501fc0f3e894
Alex Waygood <Alex.Waygood@Gmail.com>
2021-12-10 15:22:05 +0000

    Update pwd.pyi

--- stdlib/pwd.pyi
+++ stdlib/pwd.pyi
@@ -1,17 +1,30 @@
-from typing import ClassVar, Tuple
+from _typeshed import structseq
+from typing import Any, Sequence, overload
+from typing_extensions import Literal, SupportsIndex
 
-class struct_passwd(Tuple[str, str, int, int, str, str, str]):
-    pw_name: str
-    pw_passwd: str
-    pw_uid: int
-    pw_gid: int
-    pw_gecos: str
-    pw_dir: str
-    pw_shell: str
-
-    n_fields: ClassVar[int]
-    n_sequence_fields: ClassVar[int]
-    n_unnamed_fields: ClassVar[int]
+class struct_passwd(structseq[Any]):
+    @overload  # type: ignore[override]
+    def __getitem__(self, __i: Literal[0, 1, 4, 5, 6]) -> str: ...
+    @overload
+    def __getitem__(self, __i: Literal[2, 3]) -> int: ...
+    @overload
+    def __getitem__(self, __i: SupportsIndex) -> Any: ...
+    @overload
+    def __getitem__(self, __i: slice) -> Sequence[Any]: ...
+    @property
+    def pw_name(self) -> str: ...
+    @property
+    def pw_passwd(self) -> str: ...
+    @property
+    def pw_uid(self) -> int: ...
+    @property
+    def pw_gid(self) -> int: ...
+    @property
+    def pw_gecos(self) -> str: ...
+    @property
+    def pw_dir(self) -> str: ...
+    @property
+    def pw_shell(self) -> str: ...
 
 def getpwall() -> list[struct_passwd]: ...
 def getpwuid(__uid: int) -> struct_passwd: ...
