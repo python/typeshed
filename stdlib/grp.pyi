c2cdc4d05ce8cfbd5937412f481bbf62709cb1c5
Alex Waygood <Alex.Waygood@Gmail.com>
2021-12-10 15:52:49 +0000

    Update grp.pyi

--- stdlib/grp.pyi
+++ stdlib/grp.pyi
@@ -2,6 +2,7 @@ from _typeshed import structseq
 from typing import Any, Sequence, overload
 from typing_extensions import Literal, SupportsIndex
 
+# The constructor takes a sequence of any kind, which must have length of exactly 4.
 class struct_group(structseq[Any]):
     @overload  # type: ignore[override]
     def __getitem__(self, __i: Literal[0]) -> str: ...
