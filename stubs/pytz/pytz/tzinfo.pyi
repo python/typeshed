d67047682a3b97367fbd820e76a7df90d8a72f00
Sebastian Rittau <srittau@rittau.biz>
2021-12-13 17:07:53 +0100

    Add abstract methods to BaseTzInfo
    While these abstract methods don't exist at runtime, all sub-classes of
    BaseTzInfo implement them. It can be useful to annotate variables with
    BaseTzInfo and being able to call these methods on it.

--- stubs/pytz/pytz/tzinfo.pyi
+++ stubs/pytz/pytz/tzinfo.pyi
@@ -1,8 +1,21 @@
 import datetime
+from abc import abstractmethod
 from typing import Any
 
 class BaseTzInfo(datetime.tzinfo):
     zone: str | None  # Actually None but should be set on concrete subclasses
+    # The follow abstract methods don't exist in the implementation, but
+    # are implemented by all sub-classes.
+    @abstractmethod
+    def localize(self, dt: datetime.datetime) -> datetime.datetime: ...
+    @abstractmethod
+    def normalize(self, dt: datetime.datetime) -> datetime.datetime: ...
+    @abstractmethod
+    def tzname(self, dt: datetime.datetime | None) -> str: ...
+    @abstractmethod
+    def utcoffset(self, dt: datetime.datetime | None) -> datetime.timedelta | None: ...
+    @abstractmethod
+    def dst(self, dt: datetime.datetime | None) -> datetime.timedelta | None: ...
 
 class StaticTzInfo(BaseTzInfo):
     def fromutc(self, dt: datetime.datetime) -> datetime.datetime: ...
