1bbd90160161560fea951eb5e82810425598eaa2
Sebastian Rittau <srittau@rittau.biz>
2021-12-23 01:44:20 +0100

    stubtest fixes

--- stdlib/distutils/command/__init__.pyi
+++ stdlib/distutils/command/__init__.pyi
@@ -1,12 +1,18 @@
 from . import (
+    bdist_dumb as bdist_dumb,
+    bdist_rpm as bdist_rpm,
     build as build,
+    build_clib as build_clib,
+    build_ext as build_ext,
     build_py as build_py,
     build_scripts as build_scripts,
+    check as check,
     clean as clean,
     install as install,
     install_data as install_data,
     install_headers as install_headers,
     install_lib as install_lib,
+    register as register,
     sdist as sdist,
     upload as upload,
 )
