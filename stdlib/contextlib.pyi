b2cdc57feb5107db505c17425bfa0609285b0f05
Alex Waygood <Alex.Waygood@Gmail.com>
2021-12-23 19:11:07 +0000

    Remove `_GeneratorContextManagerBase`

--- stdlib/contextlib.pyi
+++ stdlib/contextlib.pyi
@@ -38,20 +38,15 @@ _CM_EF = TypeVar("_CM_EF", AbstractContextManager[Any], _ExitFunc)
 class ContextDecorator:
     def __call__(self, func: _F) -> _F: ...
 
-if sys.version_info >= (3, 7):
-    class _GeneratorContextManagerBase:
-        def __init__(self, func: Callable[_P, Iterator[Any] | AsyncIterator[Any]], args: _P.args, kwds: _P.kwds) -> None: ...  # type: ignore[name-defined]
-        gen: Generator[Any, Any, Any] | AsyncGenerator[Any, Any]
-        func: Callable[..., Generator[Any, Any, Any] | AsyncGenerator[Any, Any]]
-        args: tuple[Any, ...]
-        kwds: dict[str, Any]
-    class _GeneratorContextManager(_GeneratorContextManagerBase, AbstractContextManager[_T_co], ContextDecorator, Generic[_T_co]):
-        def __init__(self, func: Callable[_P, Iterator[_T_co]], args: _P.args, kwds: _P.kwds) -> None: ...  # type: ignore[name-defined]
-        gen: Generator[_T_co, Any, Any]
-        func: Callable[..., Generator[_T_co, Any, Any]]
-
-else:
-    class _GeneratorContextManager(AbstractContextManager[_T_co], ContextDecorator, Generic[_T_co]): ...
+class _GeneratorContextManager(AbstractContextManager[_T_co], ContextDecorator, Generic[_T_co]):
+    # In Python <= 3.6, __init__ and all instance attributes are defined directly on this class.
+    # In Python >= 3.7, __init__ and all instance attributes are inherited from _GeneratorContextManagerBase
+    # _GeneratorContextManagerBase is more trouble than it's worth to include in the stub; see #6676
+    def __init__(self, func: Callable[_P, Iterator[_T_co]], args: _P.args, kwds: _P.kwds) -> None: ...  # type: ignore[name-defined]
+    gen: Generator[_T_co, Any, Any]
+    func: Callable[..., Generator[_T_co, Any, Any]]
+    args: tuple[Any, ...]
+    kwds: dict[str, Any]
 
 def contextmanager(func: Callable[_P, Iterator[_T_co]]) -> Callable[_P, _GeneratorContextManager[_T_co]]: ...
 
@@ -59,15 +54,22 @@ if sys.version_info >= (3, 10):
     _AF = TypeVar("_AF", bound=Callable[..., Awaitable[Any]])
     class AsyncContextDecorator:
         def __call__(self, func: _AF) -> _AF: ...
-    class _AsyncGeneratorContextManager(
-        _GeneratorContextManagerBase, AbstractAsyncContextManager[_T_co], AsyncContextDecorator, Generic[_T_co]
-    ):
+    class _AsyncGeneratorContextManager(AbstractAsyncContextManager[_T_co], AsyncContextDecorator, Generic[_T_co]):
+        # __init__ and these attributes are actually defined in the base class _GeneratorContextManagerBase,
+        # which is more trouble than it's worth to include in the stub (see #6676)
         def __init__(self, func: Callable[_P, AsyncIterator[_T_co]], args: _P.args, kwds: _P.kwds) -> None: ...  # type: ignore[name-defined]
         gen: AsyncGenerator[_T_co, Any]
         func: Callable[..., AsyncGenerator[_T_co, Any]]
+        args: tuple[Any, ...]
+        kwds: dict[str, Any]
 
 elif sys.version_info >= (3, 7):
-    class _AsyncGeneratorContextManager(AbstractAsyncContextManager[_T_co], Generic[_T_co]): ...
+    class _AsyncGeneratorContextManager(AbstractAsyncContextManager[_T_co], Generic[_T_co]):
+        def __init__(self, func: Callable[_P, AsyncIterator[_T_co]], args: _P.args, kwds: _P.kwds) -> None: ...  # type: ignore[name-defined]
+        gen: AsyncGenerator[_T_co, Any]
+        func: Callable[..., AsyncGenerator[_T_co, Any]]
+        args: tuple[Any, ...]
+        kwds: dict[str, Any]
 
 if sys.version_info >= (3, 7):
     def asynccontextmanager(func: Callable[_P, AsyncIterator[_T_co]]) -> Callable[_P, _AsyncGeneratorContextManager[_T_co]]: ...
