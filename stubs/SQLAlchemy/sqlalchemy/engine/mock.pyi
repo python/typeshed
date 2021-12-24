a3dfffed66e1911f5724c60a924e93f70ff8ef8c
Sebastian Rittau <srittau@rittau.biz>
2021-12-24 16:06:00 +0100

    Further annotate sqlalchemy.engine

--- stubs/SQLAlchemy/sqlalchemy/engine/mock.pyi
+++ stubs/SQLAlchemy/sqlalchemy/engine/mock.pyi
@@ -1,18 +1,41 @@
-from typing import Any
+from _typeshed import Self
+from abc import abstractmethod
+from collections.abc import Mapping
+from typing import Any, overload
 
-from . import base
+from ..sql.compiler import Compiled
+from ..sql.ddl import DDLElement
+from ..sql.elements import ClauseElement
+from ..sql.functions import FunctionElement
+from ..sql.schema import DefaultGenerator
+from .cursor import CursorResult
+from .interfaces import Connectable, Dialect
+from .url import URL
 
-class MockConnection(base.Connectable):
-    def __init__(self, dialect, execute) -> None: ...
-    engine: Any
-    dialect: Any
-    name: Any
+class MockConnection(Connectable):
+    def __init__(self, dialect: Dialect, execute) -> None: ...
+    @property
+    def engine(self: Self) -> Self: ...  # type: ignore[override]
+    @property
+    def dialect(self) -> Dialect: ...
+    @property
+    def name(self) -> str: ...
     def schema_for_object(self, obj): ...
     def connect(self, **kwargs): ...
     def execution_options(self, **kw): ...
     def compiler(self, statement, parameters, **kwargs): ...
     def create(self, entity, **kwargs) -> None: ...
     def drop(self, entity, **kwargs) -> None: ...
-    def execute(self, object_, *multiparams, **params) -> None: ...
+    @abstractmethod
+    @overload
+    def execute(
+        self,
+        object_: ClauseElement | FunctionElement | DDLElement | DefaultGenerator | Compiled,
+        *multiparams: Mapping[str, Any],
+        **params: Any,
+    ) -> CursorResult: ...
+    @abstractmethod
+    @overload
+    def execute(self, object_: str, *multiparams: Any | tuple[Any, ...] | Mapping[str, Any], **params: Any) -> CursorResult: ...
 
-def create_mock_engine(url, executor, **kw): ...
+def create_mock_engine(url: URL | str, executor, **kw) -> MockConnection: ...
