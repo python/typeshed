f2b835717d42b9dbc906c0d5c0b9203ef7b41691
Sebastian Rittau <srittau@rittau.biz>
2021-12-27 12:48:38 +0100

    Update redis stubs to 4.1
    Move methods from class Redis to command classes

---
+++ stubs/redis/redis/commands/graph/node.pyi
@@ -0,0 +1,13 @@
+from typing import Any
+
+class Node:
+    id: Any
+    alias: Any
+    label: Any
+    labels: Any
+    properties: Any
+    def __init__(
+        self, node_id: Any | None = ..., alias: Any | None = ..., label: Any | None = ..., properties: Any | None = ...
+    ) -> None: ...
+    def toString(self): ...
+    def __eq__(self, rhs): ...
