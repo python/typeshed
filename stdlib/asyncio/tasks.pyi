018ecd836f32137c203a59e66fac4138dedb47b2
Nikita Sobolev <mail@sobolevn.me>
2022-01-02 13:30:39 +0300

    Update tasks.pyi

--- stdlib/asyncio/tasks.pyi
+++ stdlib/asyncio/tasks.pyi
@@ -21,9 +21,9 @@ _FT = TypeVar("_FT", bound=Future[Any])
 _FutureT = Union[Future[_T], Generator[Any, None, _T], Awaitable[_T]]
 _TaskYieldType = Optional[Future[object]]
 
-FIRST_EXCEPTION: Literal["FIRST_EXCEPTION"]
-FIRST_COMPLETED: Literal["FIRST_COMPLETED"]
-ALL_COMPLETED: Literal["ALL_COMPLETED"]
+FIRST_COMPLETED = concurrent.futures.FIRST_COMPLETED
+FIRST_EXCEPTION = concurrent.futures.FIRST_EXCEPTION
+ALL_COMPLETED = concurrent.futures.ALL_COMPLETED
 
 if sys.version_info >= (3, 10):
     def as_completed(fs: Iterable[_FutureT[_T]], *, timeout: float | None = ...) -> Iterator[Future[_T]]: ...
