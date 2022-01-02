79e75fb87fa8ec941b17a23cd67a7d3dacbc9909
Nikita Sobolev <mail@sobolevn.me>
2022-01-03 01:48:38 +0300

    Update mixins.pyi

--- stdlib/asyncio/mixins.pyi
+++ stdlib/asyncio/mixins.pyi
@@ -1,6 +1,8 @@
 import threading
+from typing import Any
 
 _global_lock: threading.Lock
 _marker: object
 
-class _LoopBoundMixin: ...
+class _LoopBoundMixin:
+    def __init__(self, *, loop: Any = ...) -> None: ...
