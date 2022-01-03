ac6c1bdf4fbc9ff1b769f346ea3c70d5a5a16ff0
Sebastian Rittau <srittau@rittau.biz>
2022-01-03 09:25:43 +0100

    Add WalImageFile

--- stubs/Pillow/PIL/WalImageFile.pyi
+++ stubs/Pillow/PIL/WalImageFile.pyi
@@ -1,3 +1,14 @@
+from typing import ClassVar
+
+from typing_extension import Literal
+
+from . import ImageFile
+
+class WalImageFile(ImageFile.ImageFile):
+    format: ClassVar[Literal["WAL"]]
+    format_description: ClassVar[str]
+    def load(self) -> None: ...
+
 def open(filename): ...
 
 quake2palette: bytes
