3c199c19cd85cb4483ce3c8575145774f8aecdbe
Sebastian Rittau <srittau@rittau.biz>
2022-01-03 18:49:16 +0100

    flake8 fixes

--- stubs/openpyxl/openpyxl/comments/comment_sheet.pyi
+++ stubs/openpyxl/openpyxl/comments/comment_sheet.pyi
@@ -1,3 +1,4 @@
+from collections.abc import Generator
 from typing import Any
 
 from openpyxl.descriptors.serialisable import Serialisable
--- stubs/openpyxl/openpyxl/formatting/rule.pyi
+++ stubs/openpyxl/openpyxl/formatting/rule.pyi
@@ -1,5 +1,6 @@
 from typing import Any
 
+from openpyxl.descriptors import Float
 from openpyxl.descriptors.serialisable import Serialisable
 
 class ValueDescriptor(Float):
