655a58931dae6fe01c4490611869a48956f7629f
Alex Waygood <Alex.Waygood@Gmail.com>
2022-01-04 17:56:00 +0000

    Improve `sys` stubs

--- stdlib/sys.pyi
+++ stdlib/sys.pyi
@@ -1,14 +1,29 @@
 import sys
-from _typeshed import structseq
 from builtins import object as _object
 from importlib.abc import PathEntryFinder
 from importlib.machinery import ModuleSpec
 from io import TextIOWrapper
 from types import FrameType, ModuleType, TracebackType
-from typing import Any, AsyncGenerator, Callable, NoReturn, Optional, Protocol, Sequence, TextIO, Type, TypeVar, Union, overload
+from typing import (
+    Any,
+    AsyncGenerator,
+    Callable,
+    Generic,
+    Iterable,
+    NoReturn,
+    Optional,
+    Protocol,
+    Sequence,
+    TextIO,
+    Type,
+    TypeVar,
+    Union,
+    overload,
+)
 from typing_extensions import Literal, final
 
 _T = TypeVar("_T")
+_T_co = TypeVar("_T_co", covariant=True)
 
 # The following type alias are stub-only and do not exist during runtime
 _ExcInfo = tuple[Type[BaseException], BaseException, TracebackType]
@@ -74,49 +89,142 @@ if sys.platform == "win32":
     winver: str
 _xoptions: dict[Any, Any]
 
+# Similar to _typeshed.structseq, but unlike most other `structseq` classes in the stdlib,
+# the `n_fields` ClassVar attributes in the sys-module structseq classes are read-only.
+class _sys_structseq:
+    @property
+    def n_fields(self) -> int: ...
+    @property
+    def n_sequence_fields(self) -> int: ...
+    @property
+    def n_unnamed_fields(self) -> int: ...
+
+class _immutable_structseq(_sys_structseq, Generic[_T_co]):
+    def __new__(cls: Type[_T], sequence: Iterable[_T_co], dict: dict[str, Any] = ...) -> _T: ...
+
+class _uninstantiable_structseq(_sys_structseq):
+    # type ignore because mypy doesn't like __new__ returning NoReturn
+    def __new__(cls, *args: Any, **kwargs: Any) -> NoReturn: ...  # type: ignore[misc]
+
 flags: _flags
 
-class _flags:
-    debug: int
-    division_warning: int
-    inspect: int
-    interactive: int
-    optimize: int
-    dont_write_bytecode: int
-    no_user_site: int
-    no_site: int
-    ignore_environment: int
-    verbose: int
-    bytes_warning: int
-    quiet: int
-    hash_randomization: int
-    if sys.version_info >= (3, 7):
-        dev_mode: int
-        utf8_mode: int
+if sys.version_info >= (3, 7):
+    @final
+    class _flags(
+        _uninstantiable_structseq, tuple[int, int, int, int, int, int, int, int, int, int, int, int, int, bool, int, int]
+    ):
+        @property
+        def debug(self) -> int: ...
+        @property
+        def inspect(self) -> int: ...
+        @property
+        def interactive(self) -> int: ...
+        @property
+        def optimize(self) -> int: ...
+        @property
+        def dont_write_bytecode(self) -> int: ...
+        @property
+        def no_user_site(self) -> int: ...
+        @property
+        def no_site(self) -> int: ...
+        @property
+        def ignore_environment(self) -> int: ...
+        @property
+        def verbose(self) -> int: ...
+        @property
+        def bytes_warning(self) -> int: ...
+        @property
+        def quiet(self) -> int: ...
+        @property
+        def hash_randomization(self) -> int: ...
+        @property
+        def isolated(self) -> int: ...
+        @property
+        def dev_mode(self) -> bool: ...
+        @property
+        def utf8_mode(self) -> int: ...
+        @property
+        def warn_default_encoding(self) -> int: ...  # undocumented
+
+else:
+    @final
+    class _flags(_uninstantiable_structseq, tuple[int, int, int, int, int, int, int, int, int, int, int, int, int]):
+        @property
+        def debug(self) -> int: ...
+        @property
+        def inspect(self) -> int: ...
+        @property
+        def interactive(self) -> int: ...
+        @property
+        def optimize(self) -> int: ...
+        @property
+        def dont_write_bytecode(self) -> int: ...
+        @property
+        def no_user_site(self) -> int: ...
+        @property
+        def no_site(self) -> int: ...
+        @property
+        def ignore_environment(self) -> int: ...
+        @property
+        def verbose(self) -> int: ...
+        @property
+        def bytes_warning(self) -> int: ...
+        @property
+        def quiet(self) -> int: ...
+        @property
+        def hash_randomization(self) -> int: ...
+        @property
+        def isolated(self) -> int: ...
 
 float_info: _float_info
 
-class _float_info:
-    epsilon: float  # DBL_EPSILON
-    dig: int  # DBL_DIG
-    mant_dig: int  # DBL_MANT_DIG
-    max: float  # DBL_MAX
-    max_exp: int  # DBL_MAX_EXP
-    max_10_exp: int  # DBL_MAX_10_EXP
-    min: float  # DBL_MIN
-    min_exp: int  # DBL_MIN_EXP
-    min_10_exp: int  # DBL_MIN_10_EXP
-    radix: int  # FLT_RADIX
-    rounds: int  # FLT_ROUNDS
+@final
+class _float_info(_immutable_structseq[float], tuple[float, int, int, float, int, int, float, int, int, int, int]):
+    @property
+    def max(self) -> float: ...  # DBL_MAX
+    @property
+    def max_exp(self) -> int: ...  # DBL_MAX_EXP
+    @property
+    def max_10_exp(self) -> int: ...  # DBL_MAX_10_EXP
+    @property
+    def min(self) -> float: ...  # DBL_MIN
+    @property
+    def min_exp(self) -> int: ...  # DBL_MIN_EXP
+    @property
+    def min_10_exp(self) -> int: ...  # DBL_MIN_10_EXP
+    @property
+    def dig(self) -> int: ...  # DBL_DIG
+    @property
+    def mant_dig(self) -> int: ...  # DBL_MANT_DIG
+    @property
+    def epsilon(self) -> float: ...  # DBL_EPSILON
+    @property
+    def radix(self) -> int: ...  # FLT_RADIX
+    @property
+    def rounds(self) -> int: ...  # FLT_ROUNDS
 
 hash_info: _hash_info
 
-class _hash_info:
-    width: int
-    modulus: int
-    inf: int
-    nan: int
-    imag: int
+@final
+class _hash_info(_immutable_structseq[Any | int], tuple[int, int, int, int, int, str, int, int, int]):
+    @property
+    def width(self) -> int: ...
+    @property
+    def modulus(self) -> int: ...
+    @property
+    def inf(self) -> int: ...
+    @property
+    def nan(self) -> int: ...
+    @property
+    def imag(self) -> int: ...
+    @property
+    def algorithm(self) -> str: ...
+    @property
+    def hash_bits(self) -> int: ...
+    @property
+    def seed_bits(self) -> int: ...
+    @property
+    def cutoff(self) -> int: ...
 
 implementation: _implementation
 
@@ -125,16 +233,18 @@ class _implementation:
     version: _version_info
     hexversion: int
     cache_tag: str
-    _multiarch: str
 
 int_info: _int_info
 
-class _int_info:
-    bits_per_digit: int
-    sizeof_digit: int
+@final
+class _int_info(_immutable_structseq[int], tuple[int, int]):
+    @property
+    def bits_per_digit(self) -> int: ...
+    @property
+    def sizeof_digit(self) -> int: ...
 
 @final
-class _version_info(structseq[Any | int], tuple[int, int, int, str, int]):
+class _version_info(_uninstantiable_structseq, tuple[int, int, int, str, int]):
     @property
     def major(self) -> int: ...
     @property
@@ -185,19 +295,30 @@ _TraceFunc = Callable[[FrameType, str, Any], Optional[Callable[[FrameType, str, 
 def gettrace() -> _TraceFunc | None: ...
 def settrace(tracefunc: _TraceFunc | None) -> None: ...
 
-class _WinVersion(tuple[int, int, int, int, str, int, int, int, int, tuple[int, int, int]]):
-    major: int
-    minor: int
-    build: int
-    platform: int
-    service_pack: str
-    service_pack_minor: int
-    service_pack_major: int
-    suite_mast: int
-    product_type: int
-    platform_version: tuple[int, int, int]
-
 if sys.platform == "win32":
+    # A tuple of length 5, even though it has more than 5 attributes.
+    @final
+    class _WinVersion(_uninstantiable_structseq, tuple[int, int, int, int, str]):
+        @property
+        def major(self) -> int: ...
+        @property
+        def minor(self) -> int: ...
+        @property
+        def build(self) -> int: ...
+        @property
+        def platform(self) -> int: ...
+        @property
+        def service_pack(self) -> str: ...
+        @property
+        def service_pack_minor(self) -> int: ...
+        @property
+        def service_pack_major(self) -> int: ...
+        @property
+        def suite_mast(self) -> int: ...
+        @property
+        def product_type(self) -> int: ...
+        @property
+        def platform_version(self) -> tuple[int, int, int]: ...
     def getwindowsversion() -> _WinVersion: ...
 
 def intern(__string: str) -> str: ...
@@ -220,21 +341,24 @@ if sys.version_info < (3, 9):
 
 if sys.version_info >= (3, 8):
     # not exported by sys
-    class UnraisableHookArgs:
+    class _UnraisableHookArgs:
         exc_type: Type[BaseException]
         exc_value: BaseException | None
         exc_traceback: TracebackType | None
         err_msg: str | None
         object: _object | None
-    unraisablehook: Callable[[UnraisableHookArgs], Any]
+    unraisablehook: Callable[[_UnraisableHookArgs], Any]
     def addaudithook(hook: Callable[[str, tuple[Any, ...]], Any]) -> None: ...
     def audit(__event: str, *args: Any) -> None: ...
 
 _AsyncgenHook = Optional[Callable[[AsyncGenerator[Any, Any]], None]]
 
-class _asyncgen_hooks(tuple[_AsyncgenHook, _AsyncgenHook]):
-    firstiter: _AsyncgenHook
-    finalizer: _AsyncgenHook
+@final
+class _asyncgen_hooks(_immutable_structseq[_AsyncgenHook], tuple[_AsyncgenHook, _AsyncgenHook]):
+    @property
+    def firstiter(self) -> _AsyncgenHook: ...
+    @property
+    def finalizer(self) -> _AsyncgenHook: ...
 
 def get_asyncgen_hooks() -> _asyncgen_hooks: ...
 def set_asyncgen_hooks(firstiter: _AsyncgenHook = ..., finalizer: _AsyncgenHook = ...) -> None: ...
