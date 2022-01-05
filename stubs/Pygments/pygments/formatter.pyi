2febfaf4c7b17a2c4cefac42a18214d355a261d2
Sam Bull <aa6bs0@sambull.org>
2022-01-05 17:28:46 +0000

    Make formatter generic

--- stubs/Pygments/pygments/formatter.pyi
+++ stubs/Pygments/pygments/formatter.pyi
@@ -1,6 +1,8 @@
-from typing import Any
+from typing import Any, Generic, TypeVar
 
-class Formatter:
+_T = TypeVar("_T", str, bytes)
+
+class Formatter(Generic[_T]):
     name: Any
     aliases: Any
     filenames: Any
@@ -10,6 +12,11 @@ class Formatter:
     title: Any
     encoding: Any
     options: Any
-    def __init__(self, **options) -> None: ...
+    @overload
+    def __init__(self: Formatter[str], *, encoding: None = ..., **options) -> None: ...
+    @overload
+    def __init__(self: Formatter[bytes], *, encoding: str, **options) -> None: ...
+    @overload
+    def __init__(self: Formatter[bytes], *, outencoding: str, **options) -> None: ...
     def get_style_defs(self, arg: str = ...): ...
     def format(self, tokensource, outfile): ...
