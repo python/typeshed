b6a5f9e96d70a2bf287fae6a2e268eb383b16415
Nikita Sobolev <mail@sobolevn.me>
2022-01-09 18:30:38 +0300

    Addresses review

--- stdlib/codecs.pyi
+++ stdlib/codecs.pyi
@@ -115,14 +115,14 @@ def iterdecode(iterator: Iterable[bytes], encoding: str, errors: str = ...) -> G
 if sys.version_info >= (3, 10):
     def unregister(__search_function: Callable[[str], CodecInfo | None]) -> None: ...
 
-BOM: bytes  # can be `BOM_UTF16_LE` or `BOM_UTF16_BE`, depends on `sys.byteorder`
+BOM: Literal[b"\xff\xfe", b"\xfe\xff"]  # depends on `sys.byteorder`
 BOM_BE: Literal[b"\xfe\xff"]
 BOM_LE: Literal[b"\xff\xfe"]
 BOM_UTF8: Literal[b"\xef\xbb\xbf"]
-BOM_UTF16: bytes  # depends on `sys.byteorder`
+BOM_UTF16: Literal[b"\xff\xfe", b"\xfe\xff"]  # depends on `sys.byteorder`
 BOM_UTF16_BE: Literal[b"\xfe\xff"]
 BOM_UTF16_LE: Literal[b"\xff\xfe"]
-BOM_UTF32: bytes  # depends on `sys.byteorder`
+BOM_UTF32: Literal[b"\xff\xfe\x00\x00", b"\x00\x00\xfe\xff"]  # depends on `sys.byteorder`
 BOM_UTF32_BE: Literal[b"\x00\x00\xfe\xff"]
 BOM_UTF32_LE: Literal[b"\xff\xfe\x00\x00"]
 
