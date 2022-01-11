80642adff877f8822b609d95ce12242a006068af
Nikita Sobolev <mail@sobolevn.me>
2022-01-11 15:09:15 +0300

    Update winsound.pyi

--- stdlib/winsound.pyi
+++ stdlib/winsound.pyi
@@ -3,20 +3,20 @@ from typing import overload
 from typing_extensions import Literal
 
 if sys.platform == "win32":
-    SND_FILENAME: Literal[0]
-    SND_ALIAS: Literal[0]
-    SND_LOOP: Literal[0]
-    SND_MEMORY: Literal[0]
-    SND_PURGE: Literal[0]
-    SND_ASYNC: Literal[0]
-    SND_NODEFAULT: Literal[0]
-    SND_NOSTOP: Literal[0]
-    SND_NOWAIT: Literal[0]
+    SND_FILENAME: Literal[131072]
+    SND_ALIAS: Literal[65536]
+    SND_LOOP: Literal[8]
+    SND_MEMORY: Literal[4]
+    SND_PURGE: Literal[64]
+    SND_ASYNC: Literal[1]
+    SND_NODEFAULT: Literal[2]
+    SND_NOSTOP: Literal[16]
+    SND_NOWAIT: Literal[8192]
 
-    MB_ICONASTERISK: Literal[0]
-    MB_ICONEXCLAMATION: Literal[0]
-    MB_ICONHAND: Literal[0]
-    MB_ICONQUESTION: Literal[0]
+    MB_ICONASTERISK: Literal[64]
+    MB_ICONEXCLAMATION: Literal[48]
+    MB_ICONHAND: Literal[16]
+    MB_ICONQUESTION: Literal[32]
     MB_OK: Literal[0]
     def Beep(frequency: int, duration: int) -> None: ...
     # Can actually accept anything ORed with 4, and if not it's definitely str, but that's inexpressible
