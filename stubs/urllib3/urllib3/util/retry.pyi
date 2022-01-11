7bcf37232c2fd4bab5d6af38e7f0b68cbadf337d
Matteo Centenaro <bugant@centenaro.me>
2022-01-11 15:47:52 +0100

    Make urllib3 stubs consistent with the latest release

--- stubs/urllib3/urllib3/util/retry.pyi
+++ stubs/urllib3/urllib3/util/retry.pyi
@@ -1,4 +1,4 @@
-from typing import Any, ClassVar, Collection
+from typing import Any, ClassVar
 
 from .. import exceptions
 from ..response import HTTPResponse
@@ -11,34 +11,26 @@ ResponseError = exceptions.ResponseError
 
 log: Any
 
-class RequestHistory:
-    method: str | None
-    url: str | None
-    error: Exception | None
-    status: int | None
-    redirect_location: str | None
-
 class Retry:
     DEFAULT_ALLOWED_METHODS: ClassVar[frozenset[str]]
     RETRY_AFTER_STATUS_CODES: ClassVar[frozenset[int]]
     DEFAULT_REMOVE_HEADERS_ON_REDIRECT: ClassVar[frozenset[str]]
     DEFAULT_BACKOFF_MAX: ClassVar[int]
 
-    total: bool | int | None
-    connect: int | None
-    read: int | None
-    redirect: bool | int | None
-    status: int | None
-    other: int | None
-    allowed_methods: Collection[str] | None
-    status_forcelist: Collection[int] | None
-    backoff_factor: float
-    backoff_max: float
-    raise_on_redirect: bool
-    raise_on_status: bool
-    history: tuple[RequestHistory, ...] | None
-    respect_retry_after_header: bool
-    remove_headers_on_redirect: Collection[str]
+    total: Any
+    connect: Any
+    read: Any
+    redirect: Any
+    status: Any
+    other: Any
+    allowed_methods: Any
+    status_forcelist: Any
+    backoff_factor: Any
+    raise_on_redirect: Any
+    raise_on_status: Any
+    history: Any
+    respect_retry_after_header: Any
+    remove_headers_on_redirect: Any
     def __init__(
         self,
         total=...,
@@ -50,21 +42,21 @@ class Retry:
         allowed_methods=...,
         status_forcelist=...,
         backoff_factor=...,
-        backoff_max=...,
         raise_on_redirect=...,
         raise_on_status=...,
         history=...,
         respect_retry_after_header=...,
         remove_headers_on_redirect=...,
+        method_whitelist=...,
     ) -> None: ...
     def new(self, **kw): ...
     @classmethod
-    def from_int(cls, retries, redirect=..., default=...) -> Retry: ...
-    def get_backoff_time(self) -> float: ...
-    def parse_retry_after(self, retry_after: str) -> float: ...
-    def get_retry_after(self, response: HTTPResponse) -> float | None: ...
-    def sleep_for_retry(self, response: HTTPResponse) -> bool: ...
-    def sleep(self, response: HTTPResponse | None = ...) -> None: ...
-    def is_retry(self, method: str, status_code: int, has_retry_after: bool = ...) -> bool: ...
-    def is_exhausted(self) -> bool: ...
-    def increment(self, method=..., url=..., response=..., error=..., _pool=..., _stacktrace=...) -> Retry: ...
+    def from_int(cls, retries, redirect=..., default=...): ...
+    def get_backoff_time(self): ...
+    def parse_retry_after(self, retry_after: str): ...
+    def get_retry_after(self, response: HTTPResponse): ...
+    def sleep_for_retry(self, response: HTTPResponse | None = ...): ...
+    def sleep(self, response: HTTPResponse | None = ...): ...
+    def is_retry(self, method: str, status_code: int, has_retry_after: bool = ...): ...
+    def is_exhausted(self): ...
+    def increment(self, method=..., url=..., response=..., error=..., _pool=..., _stacktrace=...): ...
