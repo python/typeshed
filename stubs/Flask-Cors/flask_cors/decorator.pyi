3bf13ce0af095cbd3f6b8f9ccd26cff07866b6ff
Kai Mueller <15907922+kasium@users.noreply.github.com>
2022-01-17 18:20:17 +0000

    Add stubs for flask-cors

---
+++ stubs/Flask-Cors/flask_cors/decorator.pyi
@@ -0,0 +1,17 @@
+from datetime import timedelta
+from logging import Logger
+from typing import Any, Callable
+
+LOG: Logger
+
+def cross_origin(
+    origins: str | list[str],
+    methods: str | list[str],
+    expose_headers: str | list[str],
+    allow_headers: str | list[str],
+    supports_credentials: bool,
+    max_age: timedelta | int | str | None,
+    send_wildcard: bool,
+    vary_header: bool,
+    automatic_options: bool,
+) -> Callable[..., Any]: ...
