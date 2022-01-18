e6d0882a6c07a8724bd6892ada58088e1ca8cfff
Nikita Sobolev <mail@sobolevn.me>
2022-01-18 17:08:42 +0300

    Update gc.pyi

--- stdlib/gc.pyi
+++ stdlib/gc.pyi
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Callable, Mapping
+from typing import Any, Callable
 from typing_extensions import Literal
 
 DEBUG_COLLECTABLE: Literal[2]
@@ -8,7 +8,7 @@ DEBUG_SAVEALL: Literal[32]
 DEBUG_STATS: Literal[1]
 DEBUG_UNCOLLECTABLE: Literal[4]
 
-_CallbackType = Callable[[Literal["start", "stop"], Mapping[str, int]], Any]
+_CallbackType = Callable[[Literal["start", "stop"], dict[str, int]], Any]
 
 callbacks: list[_CallbackType]
 garbage: list[Any]
