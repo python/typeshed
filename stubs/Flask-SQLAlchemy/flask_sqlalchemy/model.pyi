3cb580b9e5e6be1f425db236a6e59755c2ac1a64
Kai Mueller <15907922+kasium@users.noreply.github.com>
2022-01-18 10:05:47 +0000

    Add stubs for Flask-SQLAlchemy

---
+++ stubs/Flask-SQLAlchemy/flask_sqlalchemy/model.pyi
@@ -0,0 +1,20 @@
+from typing import Any, Pattern
+
+def should_set_tablename(cls: object) -> bool: ...
+
+camelcase_re: Pattern[str]
+
+def camel_to_snake_case(name: str) -> str: ...
+
+class NameMetaMixin(type):
+    def __init__(cls, name, bases, d) -> None: ...
+    def __table_cls__(cls, *args, **kwargs): ...
+
+class BindMetaMixin(type):
+    def __init__(cls, name, bases, d) -> None: ...
+
+class DefaultMeta(NameMetaMixin, BindMetaMixin): ...
+
+class Model:
+    query_class: Any | None
+    query: Any | None
