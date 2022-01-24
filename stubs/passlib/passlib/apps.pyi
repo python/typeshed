7669ce713a784b95f6085e58e51d70186011c3cf
Sebastian Rittau <srittau@rittau.biz>
2022-01-24 15:22:15 +0100

    Use CryptContext instead of LazyCryptContext

--- stubs/passlib/passlib/apps.pyi
+++ stubs/passlib/passlib/apps.pyi
@@ -1,4 +1,4 @@
-from .context import LazyCryptContext
+from .context import CryptContext
 
 __all__ = [
     "custom_app_context",
@@ -13,23 +13,23 @@ __all__ = [
     "postgres_context",
 ]
 
-master_context: LazyCryptContext
-custom_app_context: LazyCryptContext
-django10_context: LazyCryptContext
-django14_context: LazyCryptContext
-django16_context: LazyCryptContext
-django110_context: LazyCryptContext
-django21_context: LazyCryptContext
+master_context: CryptContext
+custom_app_context: CryptContext
+django10_context: CryptContext
+django14_context: CryptContext
+django16_context: CryptContext
+django110_context: CryptContext
+django21_context: CryptContext
 django_context = django21_context  # noqa: F821
 std_ldap_schemes: list[str]
-ldap_nocrypt_context: LazyCryptContext
-ldap_context: LazyCryptContext
-mysql3_context: LazyCryptContext
-mysql4_context: LazyCryptContext
+ldap_nocrypt_context: CryptContext
+ldap_context: CryptContext
+mysql3_context: CryptContext
+mysql4_context: CryptContext
 mysql_context = mysql4_context  # noqa: F821
-postgres_context: LazyCryptContext
-phpass_context: LazyCryptContext
-phpbb3_context: LazyCryptContext
-roundup10_context: LazyCryptContext
-roundup15_context: LazyCryptContext
+postgres_context: CryptContext
+phpass_context: CryptContext
+phpbb3_context: CryptContext
+roundup10_context: CryptContext
+roundup15_context: CryptContext
 roundup_context = roundup15_context  # noqa: F821
