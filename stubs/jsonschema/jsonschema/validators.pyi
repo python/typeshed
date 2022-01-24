9fda9dd7e62aa8e0519f516b08440369d2da71de
Sebastian Rittau <srittau@rittau.biz>
2022-01-24 15:54:52 +0100

    Annotate parts of jsonschema.validators
    Bump jsonschema version to 4.4.*

--- stubs/jsonschema/jsonschema/validators.pyi
+++ stubs/jsonschema/jsonschema/validators.pyi
@@ -1,28 +1,54 @@
-from typing import Any, Callable
+from _typeshed import SupportsKeysAndGetItem
+from collections.abc import Callable, Generator, Mapping
+from typing import Any, ClassVar
+
+# This class does not exist runtime. Compatible classes are created at
+# runtime by create().
+class _Validator:
+    VALIDATORS: ClassVar[dict[Any, Any]]
+    META_SCHEMA: ClassVar[dict[Any, Any]]
+    TYPE_CHECKER: Any
+    @staticmethod
+    def ID_OF(): ...
+    schema: Any
+    resolver: Any
+    format_checker: Any
+    evolve: Any
+    @classmethod
+    def check_schema(cls, schema) -> None: ...
+    def iter_errors(self, instance, _schema: Any | None = ...) -> Generator[Any, None, None]: ...
+    def descend(self, instance, schema, path: Any | None = ..., schema_path: Any | None = ...) -> Generator[Any, None, None]: ...
+    def validate(self, *args, **kwargs) -> None: ...
+    def is_type(self, instance, type): ...
+    def is_valid(self, instance, _schema: Any | None = ...) -> bool: ...
 
 def validates(version: str) -> Callable[..., Any]: ...
-def create(meta_schema, validators=..., version: Any | None = ..., type_checker=..., id_of=..., applicable_validators=...): ...
+def create(
+    meta_schema, validators=..., version: Any | None = ..., type_checker=..., id_of=..., applicable_validators=...
+) -> type[_Validator]: ...
 def extend(validator, validators=..., version: Any | None = ..., type_checker: Any | None = ...): ...
 
-Draft3Validator: Any
-Draft4Validator: Any
-Draft6Validator: Any
-Draft7Validator: Any
-Draft201909Validator: Any
-Draft202012Validator: Any
+Draft3Validator: type[_Validator]
+Draft4Validator: type[_Validator]
+Draft6Validator: type[_Validator]
+Draft7Validator: type[_Validator]
+Draft201909Validator: type[_Validator]
+Draft202012Validator: type[_Validator]
+
+_Handler = Callable[[str], Any]
 
 class RefResolver:
     referrer: Any
     cache_remote: Any
-    handlers: Any
+    handlers: dict[str, _Handler]
     store: Any
     def __init__(
         self,
-        base_uri,
-        referrer,
-        store=...,
+        base_uri: str,
+        referrer: Any,
+        store: Mapping[str, Any] = ...,
         cache_remote: bool = ...,
-        handlers=...,
+        handlers: SupportsKeysAndGetItem[str, _Handler] | Iterable[tuple[str, _Handler]] = ...,
         urljoin_cache: Any | None = ...,
         remote_cache: Any | None = ...,
     ) -> None: ...
@@ -41,5 +67,5 @@ class RefResolver:
     def resolve_fragment(self, document, fragment): ...
     def resolve_remote(self, uri): ...
 
-def validate(instance, schema, cls: Any | None = ..., *args, **kwargs) -> None: ...
+def validate(instance: object, schema: object, cls: type[_Validator] | None = ..., *args: Any, **kwargs: Any) -> None: ...
 def validator_for(schema, default=...): ...
