6e4ef6ddb452a0eaa3cf90b16787bc35e78a4e9b
Sebastian Rittau <srittau@rittau.biz>
2022-01-24 14:16:32 +0100

    Manually annotate passlib.apps

--- stubs/passlib/passlib/apps.pyi
+++ stubs/passlib/passlib/apps.pyi
@@ -1,12 +1,37 @@
 from typing import Any
 
-custom_app_context: Any
-django_context = django21_context
-ldap_nocrypt_context: Any
-ldap_context: Any
-mysql3_context: Any
-mysql4_context: Any
-mysql_context = mysql4_context
-postgres_context: Any
-phpass_context: Any
-phpbb3_context: Any
+from .context import LazyCryptContext
+
+__all__ = [
+    "custom_app_context",
+    "django_context",
+    "ldap_context",
+    "ldap_nocrypt_context",
+    "mysql_context",
+    "mysql4_context",
+    "mysql3_context",
+    "phpass_context",
+    "phpbb3_context",
+    "postgres_context",
+]
+
+master_context: LazyCryptContext
+custom_app_context: LazyCryptContext
+django10_context: LazyCryptContext
+django14_context: LazyCryptContext
+django16_context: LazyCryptContext
+django110_context: LazyCryptContext
+django21_context: LazyCryptContext
+django_context = django21_context  # noqa: F821
+std_ldap_schemes: list[str]
+ldap_nocrypt_context: LazyCryptContext
+ldap_context: LazyCryptContext
+mysql3_context: LazyCryptContext
+mysql4_context: LazyCryptContext
+mysql_context = mysql4_context  # noqa: F821
+postgres_context: LazyCryptContext
+phpass_context: LazyCryptContext
+phpbb3_context: LazyCryptContext
+roundup10_context: LazyCryptContext
+roundup15_context: LazyCryptContext
+roundup_context = roundup15_context  # noqa: F821
