fbe2448061facb782130e3bd319f5d9498e8be5b
Kai Mueller <15907922+kasium@users.noreply.github.com>
2022-01-25 07:09:13 +0000

    Add stubs for flake8_rst_docstrings

---
+++ stubs/flake8-rst-docstrings/flake8_rst_docstrings.pyi
@@ -0,0 +1,26 @@
+import ast
+from argparse import Namespace
+from typing import Any
+
+rst_prefix: str
+rst_fail_load: int
+rst_fail_lint: int
+code_mapping_info: dict[str, int]
+code_mapping_warning: dict[str, int]
+code_mapping_error: dict[str, int]
+code_mapping_severe: dict[str, int]
+code_mappings_by_level: dict[int, dict[str, int]]
+
+def code_mapping(level: int, msg: str, extra_directives: list[str], extra_roles: list[str], default: int = ...) -> int: ...
+
+class reStructuredTextChecker:
+    name: str
+    version: str
+    tree: ast.AST
+    filename: str
+    def __init__(self, tree, filename: str = ...) -> None: ...
+    @classmethod
+    def add_options(cls, parser: Any) -> None: ...
+    @classmethod
+    def parse_options(cls, options: Namespace) -> None: ...
+    def run(self) -> None: ...
