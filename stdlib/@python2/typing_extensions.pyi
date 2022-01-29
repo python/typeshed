b81767e48c1d3c5ba7c726c01132dbb693f2e178
Alex Waygood <Alex.Waygood@Gmail.com>
2022-01-29 13:40:11 +0000

    Update typing_extensions.pyi

--- stdlib/@python2/typing_extensions.pyi
+++ stdlib/@python2/typing_extensions.pyi
@@ -20,13 +20,16 @@ from typing import (  # noqa Y022
     TypeVar,
     ValuesView,
     _Alias,
-    _SpecialForm,
     overload as overload,
     runtime_checkable as runtime_checkable,
 )
 
 _T = TypeVar("_T")
 _F = TypeVar("_F", bound=Callable[..., Any])
+
+# unfortunately we have to duplicate this class definition from typing.pyi or we break pytype
+class _SpecialForm:
+    def __getitem__(self, typeargs: Any) -> object: ...
 
 # This alias for above is kept here for backwards compatibility.
 runtime = runtime_checkable
