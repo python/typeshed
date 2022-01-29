c8680094c7eb33c9832abfcdd56f9306b1699787
Nikita Sobolev <mail@sobolevn.me>
2022-01-29 12:41:39 +0300

    Use type alias

--- stdlib/socketserver.pyi
+++ stdlib/socketserver.pyi
@@ -6,10 +6,11 @@ from typing import Any, BinaryIO, Callable, ClassVar, Union
 
 _RequestType = Union[_socket, tuple[bytes, _socket]]
 _AddressType = Union[tuple[str, int], str]
+_RequestHandlerClassCallable = Callable[[_RequestType, _AddressType, BaseServer], BaseRequestHandler]
 
 class BaseServer:
     address_family: int
-    RequestHandlerClass: Callable[[_RequestType, _AddressType, BaseServer], BaseRequestHandler]
+    RequestHandlerClass: _RequestHandlerClassCallable
     server_address: tuple[str, int]
     socket: _socket
     allow_reuse_address: bool
@@ -17,7 +18,7 @@ class BaseServer:
     socket_type: int
     timeout: float | None
     def __init__(
-        self, server_address: Any, RequestHandlerClass: Callable[[_RequestType, _AddressType, BaseServer], BaseRequestHandler]
+        self, server_address: Any, RequestHandlerClass: _RequestHandlerClassCallable
     ) -> None: ...
     def fileno(self) -> int: ...
     def handle_request(self) -> None: ...
@@ -46,7 +47,7 @@ class TCPServer(BaseServer):
     def __init__(
         self,
         server_address: tuple[str, int],
-        RequestHandlerClass: Callable[[_RequestType, _AddressType, BaseServer], BaseRequestHandler],
+        RequestHandlerClass: _RequestHandlerClassCallable,
         bind_and_activate: bool = ...,
     ) -> None: ...
     def get_request(self) -> tuple[_socket, Any]: ...
