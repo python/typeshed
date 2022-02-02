0adbb11fa85645822b4559e6e6dc32b8fa3d9ced
Ilya Kulakov <kulakov.ilya@gmail.com>
2022-02-02 09:41:46 -0800

    bisect: elements of a are bounded by SupportsRichComparison

--- stdlib/_bisect.pyi
+++ stdlib/_bisect.pyi
@@ -1,8 +1,9 @@
 import sys
-from _typeshed import SupportsRichComparison, SupportsRichComparisonT
+from _typeshed import SupportsRichComparison
 from typing import Callable, MutableSequence, Sequence, TypeVar, overload
 
-_T = TypeVar("_T")
+_T = TypeVar("_T", bound=SupportsRichComparison)
+_KeyReturnT = TypeVar("_KeyReturnT", bound=SupportsRichComparison)
 
 if sys.version_info >= (3, 10):
     @overload
@@ -10,22 +11,22 @@ if sys.version_info >= (3, 10):
     @overload
     def bisect_left(
         a: Sequence[_T],
-        x: SupportsRichComparisonT,
+        x: _KeyReturnT,
         lo: int = ...,
         hi: int | None = ...,
         *,
-        key: Callable[[_T], SupportsRichComparisonT] = ...,
+        key: Callable[[_T], _KeyReturnT] = ...,
     ) -> int: ...
     @overload
     def bisect_right(a: Sequence[_T], x: _T, lo: int = ..., hi: int | None = ..., *, key: None = ...) -> int: ...
     @overload
     def bisect_right(
         a: Sequence[_T],
-        x: SupportsRichComparisonT,
+        x: _KeyReturnT,
         lo: int = ...,
         hi: int | None = ...,
         *,
-        key: Callable[[_T], SupportsRichComparisonT] = ...,
+        key: Callable[[_T], _KeyReturnT] = ...,
     ) -> int: ...
     def insort_left(
         a: MutableSequence[_T],
@@ -33,7 +34,7 @@ if sys.version_info >= (3, 10):
         lo: int = ...,
         hi: int | None = ...,
         *,
-        key: Callable[[_T], SupportsRichComparison] | None = ...,
+        key: Callable[[_T], _KeyReturnT] | None = ...,
     ) -> None: ...
     def insort_right(
         a: MutableSequence[_T],
@@ -41,7 +42,7 @@ if sys.version_info >= (3, 10):
         lo: int = ...,
         hi: int | None = ...,
         *,
-        key: Callable[[_T], SupportsRichComparison] | None = ...,
+        key: Callable[[_T], _KeyReturnT] | None = ...,
     ) -> None: ...
 
 else:
