fc695a3378580f69c038258cc7b2839dc543c0f2
Fares Ahmed <fares@duck.com>
2022-02-05 21:18:57 +0200

    Improve type hints for `chevron`

--- stubs/chevron/chevron/main.pyi
+++ stubs/chevron/chevron/main.pyi
@@ -1,9 +1,6 @@
 from _typeshed import StrOrBytesPath
 from typing import Any
 
-from metadata import version as version
-from renderer import render as render
-
 _OpenFile = StrOrBytesPath | int
 
 def main(template: _OpenFile, data: _OpenFile | None = ..., **kwargs: Any) -> str: ...
--- stubs/chevron/chevron/renderer.pyi
+++ stubs/chevron/chevron/renderer.pyi
@@ -1,19 +1,19 @@
-from _typeshed import StrOrBytesPath, SupportsRead
-from typing import Any, Sequence
+from _typeshed import StrPath, SupportsRead
+from typing import Any, MutableSequence, Sequence
 
 from tokenizer import tokenize as tokenize
 
 g_token_cache: dict[str, list[tuple[str, str]]]  # undocumented
 
 def render(
-    template: SupportsRead[str] | str = ...,
+    template: SupportsRead[str] | str | Sequence[tuple[str, str]] = ...,
     data: dict[str, Any] = ...,
-    partials_path: StrOrBytesPath | None = ...,
+    partials_path: StrPath | None = ...,
     partials_ext: str = ...,
     partials_dict: dict[str, str] = ...,
     padding: str = ...,
     def_ldel: str | None = ...,
     def_rdel: str | None = ...,
-    scopes: Sequence[int] = ...,
+    scopes: MutableSequence[int] = ...,
     warn: bool = ...,
 ) -> str: ...
