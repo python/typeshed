52efab27e5b9c72c69398204aced4ec37b2dfe4a
Dmitry Volodin <mr.molkree@gmail.com>
2022-02-08 17:16:21 +0300

    Fix zero exponent for `int`s
    It is typed as `int` instead of `Any` now

--- stdlib/builtins.pyi
+++ stdlib/builtins.pyi
@@ -176,7 +176,7 @@ class super:
     @overload
     def __init__(self) -> None: ...
 
-_PositiveInteger = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+_PositiveInteger = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 _NegativeInteger = Literal[-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20]
 
 class int:
