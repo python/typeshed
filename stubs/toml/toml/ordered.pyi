733c6fa5bdbf5f002121cab12ca8439a04c2cbf9
Fares Ahmed <fares@duck.com>
2022-02-09 10:38:50 +0200

    Fix `toml.ordered` types

--- stubs/toml/toml/ordered.pyi
+++ stubs/toml/toml/ordered.pyi
@@ -1,12 +1,13 @@
-from typing import TypeVar
+from typing import Any, Mapping, MutableMapping, TypeVar
 
 from .decoder import TomlDecoder
 from .encoder import TomlEncoder
 
-_T = TypeVar("_T")
+_MutableMappingT = TypeVar("_MutableMappingT", bound=MutableMapping[str, Any])
+_MappingT = TypeVar("_MappingT", bound=Mapping[str, Any])
 
-class TomlOrderedDecoder(TomlDecoder[_T]):
+class TomlOrderedDecoder(TomlDecoder[_MutableMappingT]):
     def __init__(self) -> None: ...
 
-class TomlOrderedEncoder(TomlEncoder[_T]):
+class TomlOrderedEncoder(TomlEncoder[_MappingT]):
     def __init__(self) -> None: ...
