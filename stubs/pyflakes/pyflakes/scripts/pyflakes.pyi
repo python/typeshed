949f9c794f4dff377ebd221a31f4ddb94aae3b7b
Alex Waygood <Alex.Waygood@Gmail.com>
2022-02-11 11:03:14 +0000

    Fix alphabetisation, delete pyflakes/scripts

--- stubs/pyflakes/pyflakes/scripts/pyflakes.pyi
+++
@@ -1,9 +0,0 @@
-from pyflakes.api import (
-    check as check,
-    checkPath as checkPath,
-    checkRecursive as checkRecursive,
-    iterSourceCode as iterSourceCode,
-    main as main,
-)
-
-__all__ = ["check", "checkPath", "checkRecursive", "iterSourceCode", "main"]
