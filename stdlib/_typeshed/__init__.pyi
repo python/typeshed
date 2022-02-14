ee5573abb58cb458e7150c8c5976580080664b16
Itai Steinherz <itaisteinherz@gmail.com>
2022-02-15 00:11:41 +0200

    Fix linting

--- stdlib/_typeshed/__init__.pyi
+++ stdlib/_typeshed/__init__.pyi
@@ -190,6 +190,8 @@ WriteableBuffer = Union[bytearray, memoryview, array.array[Any], mmap.mmap, ctyp
 # Same as _WriteableBuffer, but also includes read-only buffer types (like bytes).
 ReadableBuffer = Union[ReadOnlyBuffer, WriteableBuffer]  # stable
 
+StrOrBuffer = TypeVar("StrOrBuffer", str, ReadOnlyBuffer, WriteableBuffer)
+
 # stable
 if sys.version_info >= (3, 10):
     from types import NoneType as NoneType
