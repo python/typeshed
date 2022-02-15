bfd9c4655be1d84cbfe90926f0e2886d741ca95b
Yejia Chen <yejia@stripe.com>
2022-02-15 14:47:46 -0500

    Add overload annotation for DeletableAPIResource delete classmethod

--- stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
+++ stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
@@ -1,4 +1,13 @@
+from _typeshed import Self
+from typing import TypeVar, overload
+
 from stripe.api_resources.abstract.api_resource import APIResource as APIResource
 
+_T = TypeVar("_T", bound="DeletableAPIResource")
+
 class DeletableAPIResource(APIResource):
-    def delete(self, **params): ...
+    @overload
+    def delete(self: Self, **params) -> Self: ...
+    @overload
+    @classmethod
+    def delete(cls: type[_T], sid: str, **params) -> _T: ...
