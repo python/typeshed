e01a8640219809df9b062a467fe14f3c432a1bf5
Yejia Chen <yejia@stripe.com>
2022-02-23 15:03:19 -0500

    use @classmethod for both overloads

--- stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
+++ stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
@@ -1,4 +1,3 @@
-from _typeshed import Self
 from typing import TypeVar, overload
 
 from stripe.api_resources.abstract.api_resource import APIResource as APIResource
@@ -7,7 +6,8 @@ _T = TypeVar("_T", bound=DeletableAPIResource)
 
 class DeletableAPIResource(APIResource):
     @overload
-    def delete(self: Self, **params) -> Self: ...
+    @classmethod
+    def delete(cls: type[_T], **params) -> _T: ...
     @overload
     @classmethod
     def delete(cls: type[_T], sid: str, **params) -> _T: ...
