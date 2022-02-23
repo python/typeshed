52e740f1adcb039a9eca38687e8f1b54f03e3609
Yejia Chen <yejia@stripe.com>
2022-02-23 15:12:26 -0500

    use _typeshed.Self instead of TypeVar _T

--- stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
+++ stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
@@ -1,13 +1,12 @@
-from typing import TypeVar, overload
+from _typeshed import Self
+from typing import overload
 
 from stripe.api_resources.abstract.api_resource import APIResource as APIResource
-
-_T = TypeVar("_T", bound=DeletableAPIResource)
 
 class DeletableAPIResource(APIResource):
     @overload
     @classmethod
-    def delete(cls: type[_T], **params) -> _T: ...
+    def delete(cls: type[Self], **params) -> Self: ...
     @overload
     @classmethod
-    def delete(cls: type[_T], sid: str, **params) -> _T: ...
+    def delete(cls: type[Self], sid: str, **params) -> Self: ...
