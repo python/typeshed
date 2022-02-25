39587a604746d0b1cbd672babfb61c080a8c0286
yejia-stripe <90646909+yejia-stripe@users.noreply.github.com>
2022-02-25 10:06:20 -0500

    Update stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
+++ stubs/stripe/stripe/api_resources/abstract/deletable_api_resource.pyi
@@ -4,9 +4,5 @@ from typing import overload
 from stripe.api_resources.abstract.api_resource import APIResource as APIResource
 
 class DeletableAPIResource(APIResource):
-    @overload
     @classmethod
-    def delete(cls: type[Self], **params) -> Self: ...
-    @overload
-    @classmethod
-    def delete(cls: type[Self], sid: str, **params) -> Self: ...
+    def delete(cls: type[Self], sid: str = ..., **params) -> Self: ...
