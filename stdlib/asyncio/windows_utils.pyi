b98775d99ca017eba1176ec0302d52f4033c9b43
Alex Waygood <Alex.Waygood@Gmail.com>
2022-02-27 17:46:01 +0000

    Revert changes to allowlists and `windows_utils`

--- stdlib/asyncio/windows_utils.pyi
+++ stdlib/asyncio/windows_utils.pyi
@@ -7,7 +7,7 @@ from typing_extensions import Literal
 
 if sys.platform == "win32":
     if sys.version_info >= (3, 7):
-        __all__ = ("pipe", "PIPE", "PipeHandle")
+        __all__ = ("pipe", "Popen", "PIPE", "PipeHandle")
     else:
         __all__ = ["socketpair", "pipe", "Popen", "PIPE", "PipeHandle"]
         import socket
