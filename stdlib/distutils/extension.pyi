eb33ffc94eae040711381ba9fe29619ab9be3cee
Bas van Beek <b.f.van.beek@vu.nl>
2022-03-02 00:27:13 +0100

    Add annotations for missing `Extension` attributes

--- stdlib/distutils/extension.pyi
+++ stdlib/distutils/extension.pyi
@@ -1,4 +1,20 @@
 class Extension:
+    name: str
+    sources: list[str]
+    include_dirs: list[str]
+    define_macros: list[tuple[str, str | None]]
+    undef_macros: list[str]
+    library_dirs: list[str]
+    libraries: list[str]
+    runtime_library_dirs: list[str]
+    extra_objects: list[str]
+    extra_compile_args: list[str]
+    extra_link_args: list[str]
+    export_symbols: list[str]
+    swig_opts: list[str]
+    depends: list[str]
+    language: str | None
+    optional: bool | None
     def __init__(
         self,
         name: str,
