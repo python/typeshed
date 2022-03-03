fbde20797e0c7518cc5df92c5d7c3ebb313ffd2c
Alex Waygood <Alex.Waygood@Gmail.com>
2022-03-03 23:25:30 +0000

    Add tomllib.__all__ (#7433)

--- stdlib/tomllib.pyi
+++ stdlib/tomllib.pyi
@@ -2,6 +2,8 @@ from _typeshed import SupportsRead
 from collections.abc import Callable
 from typing import Any
 
+__all__ = ("loads", "load", "TOMLDecodeError")
+
 class TOMLDecodeError(ValueError): ...
 
 def load(__fp: SupportsRead[bytes], *, parse_float: Callable[[str], Any] = ...) -> dict[str, Any]: ...
