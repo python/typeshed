8e56c5262791b180ecc25c85fa3b99cc55e21900
hauntsaninja <>
2022-03-05 00:46:24 -0800

    unfix braintree

--- stubs/braintree/braintree/attribute_getter.pyi
+++ stubs/braintree/braintree/attribute_getter.pyi
@@ -2,4 +2,3 @@ from typing import Any
 
 class AttributeGetter:
     def __init__(self, attributes: Any | None = ...) -> None: ...
-    def __repr__(self, detail_list: list[str] | None = ...) -> None: ...  # type: ignore[override]
