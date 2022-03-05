cd862ea93c01ac28423160393a235bbb9d952a80
Alex Waygood <Alex.Waygood@Gmail.com>
2022-03-05 14:25:03 +0000

    Update contextlib.pyi

--- stdlib/contextlib.pyi
+++ stdlib/contextlib.pyi
@@ -117,8 +117,14 @@ if sys.version_info >= (3, 10):
 
     class AsyncContextDecorator:
         def __call__(self, func: _AF) -> _AF: ...
+            
+    _AsyncGeneratorContextManagerBase = AsyncContextDecorator
 
-    class _AsyncGeneratorContextManager(AbstractAsyncContextManager[_T_co], AsyncContextDecorator, Generic[_T_co]):
+elif sys.version_info >= (3, 7):
+    _AsyncGeneratorContextManagerBase = object
+
+if sys.version_info >= (3, 7):
+    class _AsyncGeneratorContextManager(AbstractAsyncContextManager[_T_co], _AsyncGeneratorContextManagerBase, Generic[_T_co]):
         # __init__ and these attributes are actually defined in the base class _GeneratorContextManagerBase,
         # which is more trouble than it's worth to include in the stub (see #6676)
         def __init__(self, func: Callable[..., AsyncIterator[_T_co]], args: tuple[Any, ...], kwds: dict[str, Any]) -> None: ...
@@ -130,15 +136,6 @@ if sys.version_info >= (3, 10):
             self, typ: type[BaseException] | None, value: BaseException | None, traceback: TracebackType | None
         ) -> bool | None: ...
 
-elif sys.version_info >= (3, 7):
-    class _AsyncGeneratorContextManager(AbstractAsyncContextManager[_T_co], Generic[_T_co]):
-        def __init__(self, func: Callable[..., AsyncIterator[_T_co]], args: tuple[Any, ...], kwds: dict[str, Any]) -> None: ...
-        gen: AsyncGenerator[_T_co, Any]
-        func: Callable[..., AsyncGenerator[_T_co, Any]]
-        args: tuple[Any, ...]
-        kwds: dict[str, Any]
-
-if sys.version_info >= (3, 7):
     def asynccontextmanager(func: Callable[_P, AsyncIterator[_T_co]]) -> Callable[_P, _AsyncGeneratorContextManager[_T_co]]: ...
 
 class _SupportsClose(Protocol):
