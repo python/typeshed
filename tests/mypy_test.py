228089e0be63b5cbcf6109a1db9998f0b6cdddff
AlexWaygood <alex.waygood@gmail.com>
2022-03-28 15:48:15 +0100

    `mypy_test`: Add two more config options

--- tests/mypy_test.py
+++ tests/mypy_test.py
@@ -207,6 +207,9 @@ def get_mypy_flags(args, major: int, minor: int, temp_name: str, *, custom_types
         "--warn-incomplete-stub",
         "--show-error-codes",
         "--no-error-summary",
+        "--enable-error-code",
+        "ignore-without-code",
+        "--strict-equality"
     ]
     if custom_typeshed:
         # Setting custom typeshed dir prevents mypy from falling back to its bundled
