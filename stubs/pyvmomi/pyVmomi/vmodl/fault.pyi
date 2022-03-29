0971b0a92987bf3637a33bef9dbd4288a3f76a4d
Hans Aarne Liblik <Hans.Liblik@gmail.com>
2022-03-29 17:37:45 +0300

    Update pyVmomi stubs

--- stubs/pyvmomi/pyVmomi/vmodl/fault.pyi
+++ stubs/pyvmomi/pyVmomi/vmodl/fault.pyi
@@ -1,5 +1,10 @@
 from typing import Any
 
+from stubs.pyvmomi.pyVmomi.vmodl import ManagedObject
+
 def __getattr__(name: str) -> Any: ...  # incomplete
 
 class InvalidArgument(Exception): ...
+
+class ManagedObjectNotFound:
+    obj: ManagedObject
