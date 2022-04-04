457a4975f9b3f68453e5a4e393929ae2a6691cba
AlexWaygood <alex.waygood@gmail.com>
2022-04-05 00:25:12 +0100

    Third-party stubs: make many attributes read-only properties

--- stubs/babel/babel/messages/plurals.pyi
+++ stubs/babel/babel/messages/plurals.pyi
@@ -1,12 +1,13 @@
-from typing import Any
+from typing import Any, NamedTuple
 
 LC_CTYPE: Any
 PLURALS: Any
 DEFAULT_PLURAL: Any
 
-class _PluralTuple(tuple[int, str]):
-    num_plurals: Any
-    plural_expr: Any
-    plural_forms: Any
+class _PluralTuple(NamedTuple):
+    num_plurals: int
+    plural_expr: str
+    @property
+    def plural_forms(self) -> str: ...
 
 def get_plural(locale=...): ...
