8f5b5c86ebc149c19bdefc8d30bbd757b619ed74
hauntsaninja <>
2022-04-16 16:59:27 -0700

    hashlib: update for py311

--- stdlib/hashlib.pyi
+++ stdlib/hashlib.pyi
@@ -1,28 +1,51 @@
 import sys
 from _typeshed import ReadableBuffer, Self
-from typing import AbstractSet
+from typing import AbstractSet, BinaryIO, Callable
 from typing_extensions import final
 
-__all__ = (
-    "md5",
-    "sha1",
-    "sha224",
-    "sha256",
-    "sha384",
-    "sha512",
-    "blake2b",
-    "blake2s",
-    "sha3_224",
-    "sha3_256",
-    "sha3_384",
-    "sha3_512",
-    "shake_128",
-    "shake_256",
-    "new",
-    "algorithms_guaranteed",
-    "algorithms_available",
-    "pbkdf2_hmac",
-)
+if sys.version_info >= (3, 11):
+    __all__ = (
+        "md5",
+        "sha1",
+        "sha224",
+        "sha256",
+        "sha384",
+        "sha512",
+        "blake2b",
+        "blake2s",
+        "sha3_224",
+        "sha3_256",
+        "sha3_384",
+        "sha3_512",
+        "shake_128",
+        "shake_256",
+        "new",
+        "algorithms_guaranteed",
+        "algorithms_available",
+        "pbkdf2_hmac",
+        "file_digest",
+    )
+else:
+    __all__ = (
+        "md5",
+        "sha1",
+        "sha224",
+        "sha256",
+        "sha384",
+        "sha512",
+        "blake2b",
+        "blake2s",
+        "sha3_224",
+        "sha3_256",
+        "sha3_384",
+        "sha3_512",
+        "shake_128",
+        "shake_256",
+        "new",
+        "algorithms_guaranteed",
+        "algorithms_available",
+        "pbkdf2_hmac",
+    )
 
 class _Hash:
     @property
@@ -143,3 +166,6 @@ class _BlakeHash(_Hash):
 
 blake2b = _BlakeHash
 blake2s = _BlakeHash
+
+if sys.version_info >= (3, 11):
+    def file_digest(__fileobj: BinaryIO, __digest: str | Callable[[], _Hash], *, _bufsize: int = ...): ...
