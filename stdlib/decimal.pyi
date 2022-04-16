bbbdc90cda6e65ab2a5041613e98c990e6769de5
Alex Waygood <Alex.Waygood@Gmail.com>
2022-04-16 16:36:18 +0100

    Give up on importing `__doc__`

--- stdlib/decimal.pyi
+++ stdlib/decimal.pyi
@@ -1,2 +1,2 @@
 from _decimal import *
-from _decimal import __doc__ as __doc__, __libmpdec_version__ as __libmpdec_version__, __version__ as __version__
+from _decimal import __libmpdec_version__ as __libmpdec_version__, __version__ as __version__
