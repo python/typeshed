5aa49a27487743e9d5e7585e5f612a2a56a66e4f
Alex Waygood <Alex.Waygood@Gmail.com>
2022-04-20 18:09:57 +0100

    Delete weird, unidiomatic tests

--- test_cases/stdlib/builtins/test_pow.py
+++ test_cases/stdlib/builtins/test_pow.py
@@ -7,6 +7,7 @@ assert_type(pow(1, 0), Literal[1])  # See #7163
 assert_type(pow(1, 0, None), Literal[1])  # See #7163
 assert_type(pow(2, 4, 0), NoReturn)
 assert_type(pow(2, 4), int)
+assert_type(pow(4, 6, None), int)
 assert_type(pow(5, -7), float)
 assert_type(pow(2, 4, 5), int)  # pow(<smallint>, <smallint>, <smallint>)
 assert_type(pow(2, 35, 3), int)  # pow(<smallint>, <bigint>, <smallint>)
@@ -20,10 +21,6 @@ assert_type(pow(complex(4, -8), Fraction(2, 3)), complex)
 assert_type(pow(Decimal("1.0"), Decimal("1.6")), Decimal)
 assert_type(pow(Decimal("1.0"), Decimal("1.0"), Decimal("1.0")), Decimal)
 assert_type(pow(Decimal("4.6"), 7, None), Decimal)
-assert_type((4).__pow__(7, 4), int)
-assert_type((4).__pow__(6, None), int)
-assert_type(complex(9).__pow__(3.1, None), complex)
-assert_type(Decimal("2.6").__pow__(5, None), Decimal)
 
 # These would ideally be more precise, but `Any` is acceptable
 # They have to be `Any` due to the fact that type-checkers can't distinguish between positive and negative numbers for the second argument to `pow()`
@@ -39,6 +36,5 @@ assert_type(pow(-9.8, 8.3), Any)
 assert_type(pow(-9.3, -88.2), Any)
 assert_type(pow(8.2, -9.8), Any)
 assert_type(pow(4.7, 9.2, None), Any)
-assert_type((6.2).__pow__(5.2, None), Any)
 # See #7046 -- float for a positive 1st arg, complex otherwise
 assert_type((-2) ** 0.5, Any)
