468d8adff131c77c599a730736380c893ddbb529
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2022-05-10 23:08:02 -0700

    stubtest_third_party: show package versions on failure (#7826)
    Co-authored-by: hauntsaninja <>

--- tests/stubtest_third_party.py
+++ tests/stubtest_third_party.py
@@ -93,6 +93,10 @@ def run_stubtest(dist: Path) -> bool:
         except subprocess.CalledProcessError:
             print(f"stubtest failed for {dist.name}", file=sys.stderr)
             print("\n\n", file=sys.stderr)
+
+            print("Ran with the following environment:", file=sys.stderr)
+            subprocess.run([pip_exe, "freeze"])
+
             if allowlist_path.exists():
                 print(
                     f'To fix "unused allowlist" errors, remove the corresponding entries from {allowlist_path}', file=sys.stderr
