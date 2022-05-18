05c56a5b1277e398141841a5f715f37ed676cdc6
Sebastian Rittau <srittau@rittau.biz>
2022-05-18 21:05:45 +0200

    Ignore some flake8 warning in test

--- test_cases/stdlib/typing/all.py
+++ test_cases/stdlib/typing/all.py
@@ -3,11 +3,11 @@ This tests that star imports work when using "all += " syntax.
 """
 
 import sys
-from typing import *
-from zipfile import *
+from typing import *  # noqa: F403
+from zipfile import *  # noqa: F403
 
 if sys.version_info >= (3, 9):
-    x: Annotated[int, 42]
+    x: Annotated[int, 42]  # noqa: F405
 
 if sys.version_info >= (3, 8):
-    p: Path
+    p: Path  # noqa: F405
