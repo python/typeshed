aa5afb32efe3e37c16c4d51ec7e98e8ef41c35df
Sebastian Rittau <srittau@rittau.biz>
2022-05-18 21:00:43 +0200

    Use all += in zipfile (+ test)

--- test_cases/stdlib/typing/all.py
+++ test_cases/stdlib/typing/all.py
@@ -4,6 +4,10 @@ This tests that star imports work when using "all += " syntax.
 
 import sys
 from typing import *
+from zipfile import *
 
 if sys.version_info >= (3, 9):
     x: Annotated[int, 42]
+
+if sys.version_info >= (3, 8):
+    p: Path
