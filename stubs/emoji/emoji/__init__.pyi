951982e26cc8eb8436a08b84a25dbd09c27e3753
Rens Oliemans <hallo@rensoliemans.nl>
2022-05-19 22:36:09 +0200

    Update __init__.pyi
    Not sure if this is the correct method, but it seemed the most logical thing.

--- stubs/emoji/emoji/__init__.pyi
+++ stubs/emoji/emoji/__init__.pyi
@@ -2,8 +2,13 @@ from .core import (
     demojize as demojize,
     emoji_count as emoji_count,
     emoji_lis as emoji_lis,
+    emoji_list as emoji_list,
+    replace_emoji as replace_emoji,
+    distinct_emoji_lis as distinct_emoji_lis,
+    distinct_emoji_list as distinct_emoji_list,
     emojize as emojize,
     get_emoji_regexp as get_emoji_regexp,
+    version as version,
 )
 from .unicode_codes import (
     EMOJI_ALIAS_UNICODE_ENGLISH as EMOJI_ALIAS_UNICODE_ENGLISH,
@@ -11,9 +16,14 @@ from .unicode_codes import (
     EMOJI_UNICODE_ITALIAN as EMOJI_UNICODE_ITALIAN,
     EMOJI_UNICODE_PORTUGUESE as EMOJI_UNICODE_PORTUGUESE,
     EMOJI_UNICODE_SPANISH as EMOJI_UNICODE_SPANISH,
+    EMOJI_UNICODE_GERMAN as EMOJI_UNICODE_GERMAN,
+    EMOJI_UNICODE_FRENCH as EMOJI_UNICODE_FRENCH,
     UNICODE_EMOJI_ALIAS_ENGLISH as UNICODE_EMOJI_ALIAS_ENGLISH,
     UNICODE_EMOJI_ENGLISH as UNICODE_EMOJI_ENGLISH,
     UNICODE_EMOJI_ITALIAN as UNICODE_EMOJI_ITALIAN,
     UNICODE_EMOJI_PORTUGUESE as UNICODE_EMOJI_PORTUGUESE,
     UNICODE_EMOJI_SPANISH as UNICODE_EMOJI_SPANISH,
+    UNICODE_EMOJI_GERMAN as UNICODE_EMOJI_GERMAN,
+    UNICODE_EMOJI_FRENCH as UNICODE_EMOJI_FRENCH,
+    EMOJI_DATA as EMOJI_DATA,
 )
