29c438d9bf2c710708bc657fd76be61d2a90da01
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-05-19 18:13:52 -0700

    fix condition

--- stdlib/traceback.pyi
+++ stdlib/traceback.pyi
@@ -113,7 +113,7 @@ class TracebackException:
     text: str
     offset: int
     msg: str
-    if sys.version_info >= (3, 10):
+    if sys.version_info >= (3, 11):
         def __init__(
             self,
             exc_type: type[BaseException],
