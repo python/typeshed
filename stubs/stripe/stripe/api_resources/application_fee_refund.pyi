910367719e144ab067fc154b02eb6af740637943
Steve Dignam <steve@dignam.xyz>
2022-05-28 16:55:22 -0400

    fix fmt

--- stubs/stripe/stripe/api_resources/application_fee_refund.pyi
+++ stubs/stripe/stripe/api_resources/application_fee_refund.pyi
@@ -7,7 +7,7 @@ from stripe.api_resources.abstract import UpdateableAPIResource as UpdateableAPI
 class ApplicationFeeRefund(UpdateableAPIResource):
     OBJECT_NAME: str
     @classmethod
-    def modify(cls: type[Self], fee, sid: str, **params) -> Self: ... # type: ignore[override]
+    def modify(cls: type[Self], fee, sid: str, **params) -> Self: ...  # type: ignore[override]
     def instance_url(self) -> str: ...
     @classmethod
     def retrieve(cls, id, api_key: Any | None = ..., **params) -> NoReturn: ...
