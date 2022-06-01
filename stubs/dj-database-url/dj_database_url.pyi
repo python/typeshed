8ae634c993c6d0d7651e78a7c4570aa79dd90885
Tom Parker-Shemilt <tom.parker-shemilt@babylonhealth.com>
2022-06-01 16:24:11 +0100

    Add the missing fields to dj-database-url config type

--- stubs/dj-database-url/dj_database_url.pyi
+++ stubs/dj-database-url/dj_database_url.pyi
@@ -4,15 +4,21 @@ from typing_extensions import TypedDict
 DEFAULT_ENV: str
 SCHEMES: dict[str, str]
 
+# From https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 class _DBConfig(TypedDict, total=False):
-    ENGINE: str
-    NAME: str
-    USER: str
-    PASSWORD: str
-    HOST: str
-    PORT: str
+    ATOMIC_REQUESTS: bool
+    AUTOCOMMIT: bool
     CONN_MAX_AGE: int
+    DISABLE_SERVER_SIDE_CURSORS: bool
+    ENGINE: str
+    HOST: str
+    NAME: str
     OPTIONS: dict[str, Any]
+    PASSWORD: str
+    PORT: str
+    TEST: dict[str, Any]
+    TIME_ZONE: str
+    USER: str
 
 def parse(url: str, engine: str | None = ..., conn_max_age: int = ..., ssl_require: bool = ...) -> _DBConfig: ...
 def config(
