2c720aba64ae69b45209350d07946eccda6af2f8
AlexWaygood <alex.waygood@gmail.com>
2022-06-11 16:54:19 +0100

    `inspect`, `asyncio`: Use more `TypeGuard`s

--- stdlib/asyncio/coroutines.pyi
+++ stdlib/asyncio/coroutines.pyi
@@ -1,8 +1,8 @@
 import sys
 import types
 from collections.abc import Coroutine
-from typing import Any
-from typing_extensions import TypeGuard
+from typing import Any, Callable, overload
+from typing_extensions import ParamSpec, TypeGuard
 
 if sys.version_info >= (3, 11):
     __all__ = ("iscoroutinefunction", "iscoroutine")
@@ -11,6 +11,8 @@ elif sys.version_info >= (3, 7):
 else:
     __all__ = ["coroutine", "iscoroutinefunction", "iscoroutine"]
 
+_P = ParamSpec("_P")
+
 if sys.version_info < (3, 11):
     from collections.abc import Callable
     from typing import TypeVar
@@ -18,7 +20,10 @@ if sys.version_info < (3, 11):
     _F = TypeVar("_F", bound=Callable[..., Any])
     def coroutine(func: _F) -> _F: ...
 
-def iscoroutinefunction(func: object) -> bool: ...
+@overload
+def iscoroutinefunction(func: Callable[_P, Any]) -> TypeGuard[Callable[_P, Coroutine[Any, Any, Any]]]: ...
+@overload
+def iscoroutinefunction(func: object) -> TypeGuard[Callable[..., Coroutine[Any, Any, Any]]]: ...
 
 if sys.version_info >= (3, 8):
     def iscoroutine(obj: object) -> TypeGuard[Coroutine[Any, Any, Any]]: ...
