70c2420c2c195cbd4af24a33e1377a1d38b47cd9
AlexWaygood <alex.waygood@gmail.com>
2022-06-12 22:52:05 +0100

    Improve tests

--- test_cases/stdlib/test_unittest.py
+++ test_cases/stdlib/test_unittest.py
@@ -1,3 +1,5 @@
+# pyright: reportUnnecessaryTypeIgnoreComment=true
+
 from datetime import datetime
 from decimal import Decimal
 from fractions import Fraction
@@ -9,3 +11,6 @@ def test_assertAlmostEqual(case: TestCase) -> None:
     case.assertAlmostEqual(Fraction(49, 50), Fraction(48, 50))
     case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1))
     case.assertAlmostEqual(Decimal("1.1"), Decimal("1.11"))
+    case.assertAlmostEqual(2.4, 2.41, places=8)
+    case.assertAlmostEqual(2.4, 2.41, delta=0.02)
+    case.assertAlmostEqual(2.4, 2.41, places=9, delta=0.02)  # type: ignore[call-overload]
