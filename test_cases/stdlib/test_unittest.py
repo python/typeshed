5add91d6bb316bddca055bdd6e4f3d495ce3ad82
Alex Waygood <Alex.Waygood@Gmail.com>
2022-06-14 16:24:51 +0100

    `unittest`: Improve `self.assert(Not)AlmostEqual(s)` (#8066)

---
+++ test_cases/stdlib/test_unittest.py
@@ -0,0 +1,29 @@
+# pyright: reportUnnecessaryTypeIgnoreComment=true
+
+import unittest
+from datetime import datetime, timedelta
+from decimal import Decimal
+from fractions import Fraction
+
+case = unittest.TestCase()
+
+case.assertAlmostEqual(2.4, 2.41)
+case.assertAlmostEqual(Fraction(49, 50), Fraction(48, 50))
+case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), delta=timedelta(hours=1))
+case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), None, "foo", timedelta(hours=1))
+case.assertAlmostEqual(Decimal("1.1"), Decimal("1.11"))
+case.assertAlmostEqual(2.4, 2.41, places=8)
+case.assertAlmostEqual(2.4, 2.41, delta=0.02)
+case.assertAlmostEqual(2.4, 2.41, None, "foo", 0.02)
+
+case.assertAlmostEqual(2.4, 2.41, places=9, delta=0.02)  # type: ignore[call-overload]
+case.assertAlmostEqual("foo", "bar")  # type: ignore[call-overload]
+case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1))  # type: ignore[arg-type]
+
+case.assertNotAlmostEqual(Fraction(49, 50), Fraction(48, 50))
+case.assertNotAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), delta=timedelta(hours=1))
+case.assertNotAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), None, "foo", timedelta(hours=1))
+
+case.assertNotAlmostEqual(2.4, 2.41, places=9, delta=0.02)  # type: ignore[call-overload]
+case.assertNotAlmostEqual("foo", "bar")  # type: ignore[call-overload]
+case.assertNotAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1))  # type: ignore[arg-type]
