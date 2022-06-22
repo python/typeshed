9847924547d50f49bc37a2284b45c46d80ad2af9
AlexWaygood <alex.waygood@gmail.com>
2022-06-22 18:26:17 +0100

    Fix `assertAlmostEqual` regression on mixed numeric types

--- test_cases/stdlib/test_unittest.py
+++ test_cases/stdlib/test_unittest.py
@@ -11,8 +11,10 @@ case = unittest.TestCase()
 # Tests for assertAlmostEqual
 ###
 
+case.assertAlmostEqual(1, 2.4)
 case.assertAlmostEqual(2.4, 2.41)
 case.assertAlmostEqual(Fraction(49, 50), Fraction(48, 50))
+case.assertAlmostEqual(3.14, complex(5, 6))
 case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), delta=timedelta(hours=1))
 case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), None, "foo", timedelta(hours=1))
 case.assertAlmostEqual(Decimal("1.1"), Decimal("1.11"))
@@ -23,18 +25,24 @@ case.assertAlmostEqual(2.4, 2.41, None, "foo", 0.02)
 case.assertAlmostEqual(2.4, 2.41, places=9, delta=0.02)  # type: ignore
 case.assertAlmostEqual("foo", "bar")  # type: ignore
 case.assertAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1))  # type: ignore
+case.assertAlmostEqual(Decimal("0.4"), Fraction(1, 2))  # type: ignore
+case.assertAlmostEqual(complex(2, 3), Decimal("0.9"))  # type: ignore
 
 ###
 # Tests for assertNotAlmostEqual
 ###
 
+case.assertAlmostEqual(1, 2.4)
 case.assertNotAlmostEqual(Fraction(49, 50), Fraction(48, 50))
+case.assertAlmostEqual(3.14, complex(5, 6))
 case.assertNotAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), delta=timedelta(hours=1))
 case.assertNotAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1), None, "foo", timedelta(hours=1))
 
 case.assertNotAlmostEqual(2.4, 2.41, places=9, delta=0.02)  # type: ignore
 case.assertNotAlmostEqual("foo", "bar")  # type: ignore
 case.assertNotAlmostEqual(datetime(1999, 1, 2), datetime(1999, 1, 2, microsecond=1))  # type: ignore
+case.assertNotAlmostEqual(Decimal("0.4"), Fraction(1, 2))  # type: ignore
+case.assertNotAlmostEqual(complex(2, 3), Decimal("0.9"))  # type: ignore
 
 ###
 # Tests for assertGreater
