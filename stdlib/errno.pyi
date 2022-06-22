88117be66ae0b715f91914ed50622de35b7198db
AlexWaygood <alex.waygood@gmail.com>
2022-06-22 23:19:34 +0100

    Improve `errno` module on Windows

--- stdlib/errno.pyi
+++ stdlib/errno.pyi
@@ -1,3 +1,4 @@
+import sys
 from collections.abc import Mapping
 
 errorcode: Mapping[int, str]
@@ -16,7 +17,8 @@ EAGAIN: int
 ENOMEM: int
 EACCES: int
 EFAULT: int
-ENOTBLK: int
+if sys.platform != "win32":
+    ENOTBLK: int
 EBUSY: int
 EEXIST: int
 EXDEV: int
@@ -45,49 +47,56 @@ ELOOP: int
 EWOULDBLOCK: int
 ENOMSG: int
 EIDRM: int
-ECHRNG: int
-EL2NSYNC: int
-EL3HLT: int
-EL3RST: int
-ELNRNG: int
-EUNATCH: int
-ENOCSI: int
-EL2HLT: int
-EBADE: int
-EBADR: int
-EXFULL: int
-ENOANO: int
-EBADRQC: int
-EBADSLT: int
+if sys.platform != "win32":
+    ECHRNG: int
+    EL2NSYNC: int
+    EL3HLT: int
+    EL3RST: int
+    ELNRNG: int
+    EUNATCH: int
+    ENOCSI: int
+    EL2HLT: int
+    EBADE: int
+    EBADR: int
+    EXFULL: int
+    ENOANO: int
+    EBADRQC: int
+    EBADSLT: int
 EDEADLOCK: int
-EBFONT: int
+if sys.platform != "win32":
+    EBFONT: int
 ENOSTR: int
 ENODATA: int
 ETIME: int
 ENOSR: int
-ENONET: int
-ENOPKG: int
+if sys.platform != "win32":
+    ENONET: int
+    ENOPKG: int
 EREMOTE: int
 ENOLINK: int
-EADV: int
-ESRMNT: int
-ECOMM: int
+if sys.platform != "win32":
+    EADV: int
+    ESRMNT: int
+    ECOMM: int
 EPROTO: int
-EMULTIHOP: int
-EDOTDOT: int
+if sys.platform != "win32":
+    EMULTIHOP: int
+    EDOTDOT: int
 EBADMSG: int
 EOVERFLOW: int
-ENOTUNIQ: int
-EBADFD: int
-EREMCHG: int
-ELIBACC: int
-ELIBBAD: int
-ELIBSCN: int
-ELIBMAX: int
-ELIBEXEC: int
+if sys.platform != "win32":
+    ENOTUNIQ: int
+    EBADFD: int
+    EREMCHG: int
+    ELIBACC: int
+    ELIBBAD: int
+    ELIBSCN: int
+    ELIBMAX: int
+    ELIBEXEC: int
 EILSEQ: int
-ERESTART: int
-ESTRPIPE: int
+if sys.platform != "win32":
+    ERESTART: int
+    ESTRPIPE: int
 EUSERS: int
 ENOTSOCK: int
 EDESTADDRREQ: int
@@ -119,40 +128,94 @@ EHOSTUNREACH: int
 EALREADY: int
 EINPROGRESS: int
 ESTALE: int
-EUCLEAN: int
-ENOTNAM: int
-ENAVAIL: int
-EISNAM: int
-EREMOTEIO: int
+if sys.platform != "win32":
+    EUCLEAN: int
+    ENOTNAM: int
+    ENAVAIL: int
+    EISNAM: int
+    EREMOTEIO: int
 EDQUOT: int
 ECANCELED: int  # undocumented
-EKEYEXPIRED: int  # undocumented
-EKEYREJECTED: int  # undocumented
-EKEYREVOKED: int  # undocumented
-EMEDIUMTYPE: int  # undocumented
-ENOKEY: int  # undocumented
-ENOMEDIUM: int  # undocumented
+if sys.platform != "win32":
+    EKEYEXPIRED: int  # undocumented
+    EKEYREJECTED: int  # undocumented
+    EKEYREVOKED: int  # undocumented
+    EMEDIUMTYPE: int  # undocumented
+    ENOKEY: int  # undocumented
+    ENOMEDIUM: int  # undocumented
 ENOTRECOVERABLE: int  # undocumented
 EOWNERDEAD: int  # undocumented
-ERFKILL: int  # undocumented
-EAUTH: int  # undocumented
-EBADARCH: int  # undocumented
-EBADEXEC: int  # undocumented
-EBADMACHO: int  # undocumented
-EBADRPC: int  # undocumented
-EDEVERR: int  # undocumented
-EFTYPE: int  # undocumented
-EL: int  # undocumented
-ELOCKUNMAPPED: int  # undocumented
-ENEEDAUTH: int  # undocumented
-ENOATTR: int  # undocumented
-ENOPOLICY: int  # undocumented
-ENOTACTIVE: int  # undocumented
-EPROCLIM: int  # undocumented
-EPROCUNAVAIL: int  # undocumented
-EPROGMISMATCH: int  # undocumented
-EPROGUNAVAIL: int  # undocumented
-EPWROFF: int  # undocumented
-EQFULL: int  # undocumented
-ERPCMISMATCH: int  # undocumented
-ESHLIBVERS: int  # undocumented
+if sys.platform != "win32":
+    ERFKILL: int  # undocumented
+    EAUTH: int  # undocumented
+    EBADARCH: int  # undocumented
+    EBADEXEC: int  # undocumented
+    EBADMACHO: int  # undocumented
+    EBADRPC: int  # undocumented
+    EDEVERR: int  # undocumented
+    EFTYPE: int  # undocumented
+    EL: int  # undocumented
+    ELOCKUNMAPPED: int  # undocumented
+    ENEEDAUTH: int  # undocumented
+    ENOATTR: int  # undocumented
+    ENOPOLICY: int  # undocumented
+    ENOTACTIVE: int  # undocumented
+    EPROCLIM: int  # undocumented
+    EPROCUNAVAIL: int  # undocumented
+    EPROGMISMATCH: int  # undocumented
+    EPROGUNAVAIL: int  # undocumented
+    EPWROFF: int  # undocumented
+    EQFULL: int  # undocumented
+    ERPCMISMATCH: int  # undocumented
+    ESHLIBVERS: int  # undocumented
+
+if sys.platform == "win32":
+    # All of these are undocumented
+    WSABASEERR: int
+    WSAEACCES: int
+    WSAEADDRINUSE: int
+    WSAEADDRNOTAVAIL: int
+    WSAEAFNOSUPPORT: int
+    WSAEALREADY: int
+    WSAEBADF: int
+    WSAECONNABORTED: int
+    WSAECONNREFUSED: int
+    WSAECONNRESET: int
+    WSAEDESTADDRREQ: int
+    WSAEDISCON: int
+    WSAEDQUOT: int
+    WSAEFAULT: int
+    WSAEHOSTDOWN: int
+    WSAEHOSTUNREACH: int
+    WSAEINPROGRESS: int
+    WSAEINTR: int
+    WSAEINVAL: int
+    WSAEISCONN: int
+    WSAELOOP: int
+    WSAEMFILE: int
+    WSAEMSGSIZE: int
+    WSAENAMETOOLONG: int
+    WSAENETDOWN: int
+    WSAENETRESET: int
+    WSAENETUNREACH: int
+    WSAENOBUFS: int
+    WSAENOPROTOOPT: int
+    WSAENOTCONN: int
+    WSAENOTEMPTY: int
+    WSAENOTSOCK: int
+    WSAEOPNOTSUPP: int
+    WSAEPFNOSUPPORT: int
+    WSAEPROCLIM: int
+    WSAEPROTONOSUPPORT: int
+    WSAEPROTOTYPE: int
+    WSAEREMOTE: int
+    WSAESHUTDOWN: int
+    WSAESOCKTNOSUPPORT: int
+    WSAESTALE: int
+    WSAETIMEDOUT: int
+    WSAETOOMANYREFS: int
+    WSAEUSERS: int
+    WSAEWOULDBLOCK: int
+    WSANOTINITIALISED: int
+    WSASYSNOTREADY: int
+    WSAVERNOTSUPPORTED: int
