3c9005ef2e1c3114cfab65b0d1e64dc5089c9c3e
hauntsaninja <>
2022-06-29 23:00:48 +0530

    updates

---
+++ stubs/prettytable/prettytable/colortable.pyi
@@ -0,0 +1,41 @@
+from _typeshed import Incomplete
+from collections.abc import Callable
+
+from .prettytable import PrettyTable as PrettyTable
+
+RESET_CODE: str
+
+init: Callable[[], object]
+
+class Theme:
+    default_color: str
+    vertical_char: str
+    vertical_color: str
+    horizontal_char: str
+    horizontal_color: str
+    junction_char: str
+    junction_color: str
+    def __init__(
+        self,
+        default_color: str = ...,
+        vertical_char: str = ...,
+        vertical_color: str = ...,
+        horizontal_char: str = ...,
+        horizontal_color: str = ...,
+        junction_char: str = ...,
+        junction_color: str = ...,
+    ) -> None: ...
+    def format_code(s: str) -> str: ...  # type: ignore
+
+class Themes:
+    DEFAULT: Theme
+    OCEAN: Theme
+
+class ColorTable(PrettyTable):
+    def __init__(self, field_names: Incomplete | None = ..., **kwargs) -> None: ...
+    @property
+    def theme(self) -> Theme: ...
+    @theme.setter
+    def theme(self, value: Theme): ...
+    def update_theme(self) -> None: ...
+    def get_string(self, **kwargs): ...
