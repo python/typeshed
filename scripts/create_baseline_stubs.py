f7c32d0055e996ddc7acd15e8c4a53ce916799ab
hauntsaninja <>
2022-07-02 00:44:23 +0530

    create_baseline_stubs: check all defs by default
    Since stubgen will usually create complete stubs, we should have
    stubtest check missing definitions so we can know to reflect any future
    additions.

--- scripts/create_baseline_stubs.py
+++ scripts/create_baseline_stubs.py
@@ -85,7 +85,14 @@ def create_metadata(stub_dir: str, version: str) -> None:
     assert not os.path.exists(fnam)
     print(f"Writing {fnam}")
     with open(fnam, "w") as f:
-        f.write(f'version = "{version}.*"\n')
+        f.write(
+            f"""\
+version = "{version}.*"
+
+[tool.stubtest]
+ignore_missing_stub = false
+"""
+        )
 
 
 def add_pyright_exclusion(stub_dir: str) -> None:
