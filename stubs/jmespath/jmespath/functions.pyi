0fe3da7bf5e5e703584b0702e7e1ecea2b6e4128
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-07-07 16:16:31 +0200

    Update stubs/jmespath/jmespath/functions.pyi
    Co-authored-by: Sebastian Rittau <srittau@rittau.biz>

--- stubs/jmespath/jmespath/functions.pyi
+++ stubs/jmespath/jmespath/functions.pyi
@@ -3,7 +3,7 @@ from typing import Any
 from typing_extensions import NotRequired, TypedDict
 
 TYPES_MAP: dict[str, str]
-REVERSE_TYPES_MAP: dict[str, str]
+REVERSE_TYPES_MAP: dict[str, tuple[str, ...]]
 
 class _Signature(TypedDict):
     types: list[str]
