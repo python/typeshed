0d0b99e0730f9b0eebedd3f42cbc1f2fba3364ac
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-07-12 12:14:45 -0700

    add test case

---
+++ test_cases/stdlib/builtins/test_tuple.py
@@ -0,0 +1,9 @@
+from typing_extensions import assert_type
+
+
+# Empty tuples, see #8275
+class TupleSub(tuple[int, ...]):
+    pass
+
+assert_type(TupleSub(), TupleSub)
+assert_type(TupleSub([1, 2, 3]), TupleSub)
