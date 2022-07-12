5ccf178ba2b3355db68e38b0d0e7658fc9993fa9
AlexWaygood <alex.waygood@gmail.com>
2022-07-12 15:07:33 +0100

    Improve test

--- test_cases/stdlib/asyncio/test_gather.py
+++ test_cases/stdlib/asyncio/test_gather.py
@@ -4,44 +4,21 @@ from typing_extensions import assert_type
 
 
 async def coro1() -> int:
-    ...
+    return 42
 
 
 async def coro2() -> str:
-    ...
+    return "spam"
 
 
-async def coro3() -> int:
-    ...
-
-
-async def coro4() -> int:
-    ...
-
-
-async def coro5() -> int:
-    ...
-
-
-async def coro6() -> int:
-    ...
-
-
-async def test_gather(
-    awaitable1: Awaitable[int],
-    awaitable2: Awaitable[str],
-    awaitable3: Awaitable[int],
-    awaitable4: Awaitable[int],
-    awaitable5: Awaitable[int],
-    awaitable6: Awaitable[int],
-) -> None:
+async def test_gather(awaitable1: Awaitable[int], awaitable2: Awaitable[str]) -> None:
     a = await asyncio.gather(awaitable1)
     assert_type(a, Tuple[int])
 
     b = await asyncio.gather(awaitable1, awaitable2, return_exceptions=True)
     assert_type(b, Tuple[Union[int, BaseException], Union[str, BaseException]])
 
-    c = await asyncio.gather(awaitable1, awaitable2, awaitable3, awaitable4, awaitable5, awaitable6)
+    c = await asyncio.gather(awaitable1, awaitable2, awaitable1, awaitable1, awaitable1, awaitable1)
     assert_type(c, List[Any])
 
     awaitables_list: List[Awaitable[int]] = [awaitable1]
@@ -52,4 +29,4 @@ async def test_gather(
     assert_type(e, List[Any])
 
 
-asyncio.run(test_gather(coro1(), coro2(), coro3(), coro4(), coro5(), coro6()))
+asyncio.run(test_gather(coro1(), coro2()))
