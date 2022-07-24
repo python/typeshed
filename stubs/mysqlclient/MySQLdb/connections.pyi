8a769cf7bba09b12db2f16106c1c91b436ba465b
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-24 07:30:57 -0400

    Update types of exceptions in mysqlclient’s connection class
    This updates the types of the classes used in mysqlclient’s Connection class. On this page we can see that by default each value is set to the exception itself.
    
    https://github.com/PyMySQL/mysqlclient/blob/main/MySQLdb/connections.py#L321-L330
    
    These were imported from `._exceptions` as seen here:
    https://github.com/PyMySQL/mysqlclient/blob/main/MySQLdb/connections.py#L10-L21
    
    If we look at `_exceptions.py`, we can see that these are all `Exception`'s, so we can type these as `BaseException` to align with the expectation that these will be raisable objects.
    https://github.com/PyMySQL/mysqlclient/blob/main/MySQLdb/_exceptions.py

--- stubs/mysqlclient/MySQLdb/connections.pyi
+++ stubs/mysqlclient/MySQLdb/connections.pyi
@@ -36,13 +36,13 @@ class Connection(_mysql.connection):
     def set_character_set(self, charset) -> None: ...
     def set_sql_mode(self, sql_mode) -> None: ...
     def show_warnings(self): ...
-    Warning: Any
-    Error: Any
-    InterfaceError: Any
-    DatabaseError: Any
-    DataError: Any
-    OperationalError: Any
-    IntegrityError: Any
-    InternalError: Any
-    ProgrammingError: Any
-    NotSupportedError: Any
+    Warning: BaseException
+    Error: BaseException
+    InterfaceError: BaseException
+    DatabaseError: BaseException
+    DataError: BaseException
+    OperationalError: BaseException
+    IntegrityError: BaseException
+    InternalError: BaseException
+    ProgrammingError: BaseException
+    NotSupportedError: BaseException
