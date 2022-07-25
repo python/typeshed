97f85816a61e804a38247b102e470c18da415a49
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-25 15:05:13 -0400

    refactor: add missing imports and vars to requests.compat

--- stubs/requests/requests/compat.pyi
+++ stubs/requests/requests/compat.pyi
@@ -1,3 +1,29 @@
 import collections
+import sys
+import urllib.parse
+import urllib.request
+
+_ver = sys.version_info
+ispy2: bool
+
+is_py2: bool
+is_py3: bool
+has_simplejson: bool
 
 OrderedDict = collections.OrderedDict
+
+quote = urllib.parse.quote
+quote_plus = urllib.parse.quote_plus
+unquote = urllib.parse.unquote
+unquote_plus = urllib.parse.unquote_plus
+urldefrag = urllib.parse.urldefrag
+urlencode = urllib.parse.urlencode
+urljoin = urllib.parse.urljoin
+urlparse = urllib.parse.urlparse
+urlsplit = urllib.parse.urlsplit
+urlunparse = urllib.parse.urlunparse
+
+# urllib.request
+getproxies = urllib.request.getproxies
+parse_http_list = urllib.request.parse_http_list
+proxy_bypass = urllib.request.proxy_bypass
