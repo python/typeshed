f2a17be9e66439a0f5d6489c4e4774064560a6ed
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-27 16:49:07 -0400

    feat: add support for resource-sharer type hints

---
+++ stdlib/multiprocessing/resource_sharer.pyi
@@ -0,0 +1,51 @@
+from _typeshed import Incomplete
+from collections.abc import Callable
+import socket
+import threading
+import sys
+
+from .connection import Connection, Listener
+from typing import Union
+from typing_extensions import TypeAlias
+
+__all__ = ["stop"]
+
+# https://docs.python.org/3/library/multiprocessing.html#address-formats
+_Address: TypeAlias = Union[str, tuple[str, int]]
+
+if sys.platform == "win32":
+    __all__ += ["DupSocket"]
+
+    class DupSocket:
+        _id: tuple[_Address, int]
+
+        def __init__(self, sock: socket.socket) -> None: ...
+        def detach(self) -> bytes: ...
+
+else:
+    __all__ += ["DupFd"]
+
+    class DupFd:
+        _id: tuple[_Address, int]
+
+        def __init__(self, fd: int) -> None: ...
+        def detach(self) -> None: ...
+
+class _ResourceSharer:
+    _key: int
+    _cache: dict[int, tuple[Incomplete, Incomplete]]
+    _lock: threading.Lock
+    _listener: Listener | None
+    _address: _Address | None
+    _thread: threading.Thread | None
+    def __init__(self) -> None: ...
+    def register(self, send: Callable[[Connection, int], None], close: Callable[[], None]) -> tuple[_Address, int]: ...
+    @staticmethod
+    def get_connection(ident: tuple[_Address, int]) -> Connection: ...
+    def stop(self, timeout: float | None = ...) -> None: ...
+    def _afterfork(self) -> None: ...
+    def _start(self) -> None: ...
+    def _serve(self) -> None: ...
+
+_resource_sharer: _ResourceSharer
+stop = _resource_sharer.stop
