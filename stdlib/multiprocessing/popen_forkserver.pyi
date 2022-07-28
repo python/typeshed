07dd381077229e04250dd0e3a2e7b2c647a54da5
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-28 12:02:29 -0400

    fix: protect module on non win32 systems

--- stdlib/multiprocessing/popen_forkserver.pyi
+++ stdlib/multiprocessing/popen_forkserver.pyi
@@ -1,20 +1,23 @@
+import sys
+
 from multiprocessing.process import BaseProcess
 from typing import ClassVar
 
 from . import popen_fork
 from .util import Finalize
 
-__all__ = ["Popen"]
+if sys.platform != "win32":
+    __all__ = ["Popen"]
 
-class _DupFd:
-    def __init__(self, ind: int) -> None: ...
-    def detach(self) -> int: ...
+    class _DupFd:
+        def __init__(self, ind: int) -> None: ...
+        def detach(self) -> int: ...
 
-class Popen(popen_fork.Popen):
-    DupFd: ClassVar[type[_DupFd]]
-    finalizer: Finalize
-    sentinel: int
+    class Popen(popen_fork.Popen):
+        DupFd: ClassVar[type[_DupFd]]
+        finalizer: Finalize
+        sentinel: int
 
-    def __init__(self, process_obj: BaseProcess) -> None: ...
-    def duplicate_for_child(self, fd: int) -> int: ...
-    def poll(self, flag: int = ...) -> int | None: ...
+        def __init__(self, process_obj: BaseProcess) -> None: ...
+        def duplicate_for_child(self, fd: int) -> int: ...
+        def poll(self, flag: int = ...) -> int | None: ...
