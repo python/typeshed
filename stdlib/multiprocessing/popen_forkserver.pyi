827bbce74eb9ac908da32e089ab1ffbdfc9321cc
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-28 11:41:19 -0400

    fix: add missing multiprocessing.popen_forkserver.pyi stubs

---
+++ stdlib/multiprocessing/popen_forkserver.pyi
@@ -0,0 +1,21 @@
+from multiprocessing.process import BaseProcess
+from typing import ClassVar, Protocol
+
+from . import popen_fork
+from .util import Finalize
+
+__all__ = ["Popen"]
+
+class _SupportsDetach(Protocol):
+    def detach(self) -> int: ...
+
+class Popen(popen_fork.Popen):
+    DupFd: ClassVar[_SupportsDetach]
+    finalizer: Finalize
+    method: ClassVar[str]
+    pid: int
+    sentinel: int
+
+    def __init__(self, process_obj: BaseProcess) -> None: ...
+    def duplicate_for_child(self, fd: int) -> int: ...
+    def poll(self, flag: int = ...) -> int | None: ...
