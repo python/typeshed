5b36eb54a7b1a63ed5e35e6a4e03ee9245497de0
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-28 13:16:18 -0400

    fix: feedback RE: Incomplete return, import, and version

--- stubs/emoji/emoji/unicode_codes/__init__.pyi
+++ stubs/emoji/emoji/unicode_codes/__init__.pyi
@@ -1,8 +1,6 @@
-from _typeshed import Incomplete
-
-from .data_dict import EMOJI_DATA as EMOJI_DATA, LANGUAGES as LANGUAGES, STATUS as STATUS
+from .data_dict import *
 
 __all__ = ["get_emoji_unicode_dict", "get_aliases_unicode_dict", "EMOJI_DATA", "STATUS", "LANGUAGES"]
 
-def get_emoji_unicode_dict(lang: str) -> Incomplete: ...
+def get_emoji_unicode_dict(lang: str) -> dict[str, str]: ...
 def get_aliases_unicode_dict() -> dict[str, str]: ...
