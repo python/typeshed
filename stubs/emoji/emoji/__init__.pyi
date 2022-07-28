57edd4d662a6832f7c070402c203fa11a9b75111
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-28 12:13:30 -0400

    refactor: update emoji package to 2.0.0
    Fix: #8359

--- stubs/emoji/emoji/__init__.pyi
+++ stubs/emoji/emoji/__init__.pyi
@@ -1,30 +1,32 @@
-from .core import (
-    demojize as demojize,
-    distinct_emoji_lis as distinct_emoji_lis,
-    distinct_emoji_list as distinct_emoji_list,
-    emoji_count as emoji_count,
-    emoji_lis as emoji_lis,
-    emoji_list as emoji_list,
+from emoji.core import (
     emojize as emojize,
-    get_emoji_regexp as get_emoji_regexp,
-    is_emoji as is_emoji,
+    demojize as demojize,
+    emoji_count as emoji_count,
+    emoji_list as emoji_list,
+    distinct_emoji_list as distinct_emoji_list,
     replace_emoji as replace_emoji,
     version as version,
+    is_emoji as is_emoji,
 )
-from .unicode_codes import (
-    EMOJI_ALIAS_UNICODE_ENGLISH as EMOJI_ALIAS_UNICODE_ENGLISH,
-    EMOJI_DATA as EMOJI_DATA,
-    EMOJI_UNICODE_ENGLISH as EMOJI_UNICODE_ENGLISH,
-    EMOJI_UNICODE_FRENCH as EMOJI_UNICODE_FRENCH,
-    EMOJI_UNICODE_GERMAN as EMOJI_UNICODE_GERMAN,
-    EMOJI_UNICODE_ITALIAN as EMOJI_UNICODE_ITALIAN,
-    EMOJI_UNICODE_PORTUGUESE as EMOJI_UNICODE_PORTUGUESE,
-    EMOJI_UNICODE_SPANISH as EMOJI_UNICODE_SPANISH,
-    UNICODE_EMOJI_ALIAS_ENGLISH as UNICODE_EMOJI_ALIAS_ENGLISH,
-    UNICODE_EMOJI_ENGLISH as UNICODE_EMOJI_ENGLISH,
-    UNICODE_EMOJI_FRENCH as UNICODE_EMOJI_FRENCH,
-    UNICODE_EMOJI_GERMAN as UNICODE_EMOJI_GERMAN,
-    UNICODE_EMOJI_ITALIAN as UNICODE_EMOJI_ITALIAN,
-    UNICODE_EMOJI_PORTUGUESE as UNICODE_EMOJI_PORTUGUESE,
-    UNICODE_EMOJI_SPANISH as UNICODE_EMOJI_SPANISH,
-)
+from emoji.unicode_codes import EMOJI_DATA, STATUS, LANGUAGES
+
+__all__ = [
+    # emoji.core
+    "emojize",
+    "demojize",
+    "emoji_count",
+    "emoji_list",
+    "distinct_emoji_list",
+    "replace_emoji",
+    "version",
+    "is_emoji",
+    # emoji.unicode_codes
+    "EMOJI_DATA",
+    "STATUS",
+    "LANGUAGES",
+]
+__version__: str
+__author__: str
+__email__: str
+__source__: str
+__license__: str
