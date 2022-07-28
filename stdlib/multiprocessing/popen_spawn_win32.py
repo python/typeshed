11f3c02f5906e8df889525edcc28e60c853f559c
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-28 14:15:49 -0400

    fix: add missing multiprocessing.popen_spawn_win32

---
+++ stdlib/multiprocessing/popen_spawn_win32.py
@@ -0,0 +1,41 @@
+import sys
+from multiprocessing.process import BaseProcess
+from typing import ClassVar
+from .util import Finalize
+
+
+if sys.platform == "win32":
+    __all__ = ["Popen"]
+
+    TERMINATE: int
+    WINEXE: bool
+    WINSERVICE: bool
+    WINENV: bool
+
+    class Popen:
+        method: ClassVar[str]
+        pid: int
+        returncode: int | None
+        _handle: int
+        sentinel: int
+        finalizer: Finalize
+
+        def __init__(self, process_obj: BaseProcess) -> None:
+            ...
+
+        def duplicate_for_child(self, handle: int) -> int:
+            ...
+
+        def wait(self, timeout: float | None = ...) -> int | None:
+            ...
+
+        def poll(self) -> int | None:
+            ...
+
+        def terminate(self) -> None:
+            ...
+
+        kill = terminate
+
+        def close(self) -> None:
+            ...
