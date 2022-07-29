b0cea4b2a48ee8be7494f10a33b7be12590f9191
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-29 11:23:53 -0400

    refactor: add help and version to requests

---
+++ stubs/requests/requests/help.pyi
@@ -0,0 +1,35 @@
+from typing import TypedDict
+
+class VersionDict(TypedDict):
+    version: str
+
+class OptionalVersionDict(TypedDict):
+    version: str | None
+
+class PlatformDict(TypedDict):
+    system: str
+    release: str
+
+class ImplementationDict(VersionDict):
+    name: str
+
+class PyOpenSSLDict(OptionalVersionDict):
+    openssl_version: str
+
+class InfoDict(TypedDict):
+    platform: PlatformDict
+    implementation: ImplementationDict
+    system_ssl: VersionDict
+    using_pyopenssl: bool
+    using_charset_normalizer: bool
+    pyOpenSSL: PyOpenSSLDict
+    urllib3: VersionDict
+    chardet: OptionalVersionDict
+    charset_normalizer: OptionalVersionDict
+    cryptography: VersionDict
+    idna: VersionDict
+    requests: VersionDict
+
+def _implementation() -> dict[str, str]: ...
+def info() -> InfoDict: ...
+def main() -> None: ...
