28ceaa6aea50483402e7c5d8c3f64ca3e8f21e33
Alex Waygood <Alex.Waygood@Gmail.com>
2022-07-30 14:28:39 +0100

    Add comment

--- stdlib/lib2to3/pgen2/__init__.pyi
+++ stdlib/lib2to3/pgen2/__init__.pyi
@@ -4,4 +4,5 @@ from lib2to3.pytree import _RawNode
 from typing import Any
 from typing_extensions import TypeAlias
 
+# This is imported in several lib2to3/pgen2 submodules
 _Convert: TypeAlias = Callable[[Grammar, _RawNode], Any]
