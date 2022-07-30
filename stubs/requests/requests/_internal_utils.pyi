3bc9fdb512015bcffbfa7ca7749c859caad3138c
Alex Waygood <Alex.Waygood@Gmail.com>
2022-07-30 12:29:16 +0100

    Fix flake8/pytype

--- stubs/requests/requests/_internal_utils.pyi
+++ stubs/requests/requests/_internal_utils.pyi
@@ -1,14 +1,7 @@
 from re import Pattern
+from typing import Any
 
-_VALID_HEADER_NAME_RE_BYTE: Pattern[bytes]
-_VALID_HEADER_NAME_RE_STR: Pattern[str]
-_VALID_HEADER_VALUE_RE_BYTE: Pattern[bytes]
-_VALID_HEADER_VALUE_RE_STR: Pattern[str]
-
-HEADER_VALIDATORS = {
-    bytes: (_VALID_HEADER_NAME_RE_BYTE, _VALID_HEADER_VALUE_RE_BYTE),
-    str: (_VALID_HEADER_NAME_RE_STR, _VALID_HEADER_VALUE_RE_STR),
-}
+HEADER_VALIDATORS: dict[type, tuple[Pattern[Any], Pattern[Any]]]
 
 def to_native_string(string: str | bytes, encoding: str = ...) -> str: ...
 def unicode_is_ascii(u_string: str) -> bool: ...
