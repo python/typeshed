33e7aa95ed1eac96d3dbed91845fc00bb97988a8
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-08-01 08:25:05 -0400

    fix: optional syntax

--- stubs/urllib3/urllib3/contrib/socks.pyi
+++ stubs/urllib3/urllib3/contrib/socks.pyi
@@ -1,45 +1,5 @@
-"""
-This module contains provisional support for SOCKS proxies from within
-urllib3. This module supports SOCKS4, SOCKS4A (an extension of SOCKS4), and
-SOCKS5. To enable its functionality, either install PySocks or install this
-module with the ``socks`` extra.
-
-The SOCKS implementation supports the full range of urllib3 features. It also
-supports the following SOCKS features:
-
-- SOCKS4A (``proxy_url='socks4a://...``)
-- SOCKS4 (``proxy_url='socks4://...``)
-- SOCKS5 with remote DNS (``proxy_url='socks5h://...``)
-- SOCKS5 with local DNS (``proxy_url='socks5://...``)
-- Usernames and passwords for the SOCKS proxy
-
-.. note::
-   It is recommended to use ``socks5h://`` or ``socks4a://`` schemes in
-   your ``proxy_url`` to ensure that DNS resolution is done from the remote
-   server instead of client-side when connecting to a domain name.
-
-SOCKS4 supports IPv4 and domain names with the SOCKS4A extension. SOCKS5
-supports IPv4, IPv6, and domain names.
-
-When connecting to a SOCKS4 proxy the ``username`` portion of the ``proxy_url``
-will be sent as the ``userid`` section of the SOCKS request:
-
-.. code-block:: python
-
-    proxy_url="socks4a://<userid>@proxy-host"
-
-When connecting to a SOCKS5 proxy the ``username`` and ``password`` portion
-of the ``proxy_url`` will be sent as the username/password to authenticate
-with the proxy:
-
-.. code-block:: python
-
-    proxy_url="socks5h://<username>:<password>@proxy-host"
-
-"""
-
-from typing import Any, ClassVar, Mapping, Optional
 from typing_extensions import TypedDict
+from typing import Any, ClassVar, Mapping
 
 from ..connection import HTTPConnection, HTTPSConnection
 from ..connectionpool import HTTPConnectionPool, HTTPSConnectionPool
@@ -47,10 +7,10 @@ from ..poolmanager import PoolManager
 
 class _SockOptions(TypedDict):
     socks_version: int
-    proxy_host: Optional[str]
-    proxy_port: Optional[str]
-    username: Optional[str]
-    password: Optional[str]
+    proxy_host: str | None
+    proxy_port: str | None
+    username: str | None
+    password: str | None
     rdns: bool
 
 class SOCKSConnection(HTTPConnection):
