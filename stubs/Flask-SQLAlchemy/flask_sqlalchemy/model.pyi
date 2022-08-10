89f096b8252766d21e7b98221aab570a29c94b6f
Alex Waygood <Alex.Waygood@Gmail.com>
2022-08-10 18:04:14 +0100

    Update stubs/Flask-SQLAlchemy/flask_sqlalchemy/model.pyi

--- stubs/Flask-SQLAlchemy/flask_sqlalchemy/model.pyi
+++ stubs/Flask-SQLAlchemy/flask_sqlalchemy/model.pyi
@@ -3,6 +3,7 @@ from typing import Any
 
 from sqlalchemy import Table
 from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.orm import Query
 
 def should_set_tablename(cls: type) -> bool: ...
 
