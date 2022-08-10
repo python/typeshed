45f46f9453853aff4099337276a589e4d7e6be60
Nikita Sobolev <mail@sobolevn.me>
2022-08-10 12:18:39 +0300

    Create test_dataclasses.py

---
+++ test_cases/stdlib/test_dataclasses.py
@@ -0,0 +1,14 @@
+# pyright: reportUnnecessaryTypeIgnoreComment=true
+
+import dataclasses
+from typing import Any
+from typing_extensions import assert_type
+
+@dataclassses.dataclass
+class D: ...
+
+assert_type(dataclasses.astuple(D()), tuple[Any, ...])
+assert_type(dataclasses.astuple(D(), tuple_factory=tuple), tuple[Any, ...])
+
+assert_type(dataclasses.asdict(D()), dict[str, Any])
+assert_type(dataclasses.asdict(D(), dict_factory=dict), dict[str, Any])
