5e1f9306bc50e2cf6b37fd4e058a1f069f6c3ec2
AlexWaygood <alex.waygood@gmail.com>
2022-08-10 19:20:57 +0100

    Fix flake8

--- test_cases/stdlib/test_threading.py
+++ test_cases/stdlib/test_threading.py
@@ -1,11 +1,11 @@
 import threading
 import _threading_local
 
-l = threading.local()
-l.foo = 42
-del l.foo
-l.baz = ["spam", "eggs"]
-del l.baz
+loc = threading.local()
+loc.foo = 42
+del loc.foo
+loc.baz = ["spam", "eggs"]
+del loc.baz
 
 l2 = _threading_local.local()
 l2.asdfasdf = 56
