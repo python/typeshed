5700f7bb8a3938828fe31fa396db455b99a868f9
sobolevn <mail@sobolevn.me>
2022-08-10 15:33:32 +0300

    Try another hack

--- test_cases/stdlib/builtins/test_dict.py
+++ test_cases/stdlib/builtins/test_dict.py
@@ -3,9 +3,6 @@
 from typing import Dict, Generic, Iterable, Tuple, TypeVar
 from typing_extensions import assert_type
 
-# Hack, so some checks will only work in mypy.
-MYPY: bool = False
-
 # These do follow `__init__` overloads order:
 bad = dict()  # type: ignore
 good: Dict[str, str] = dict()
@@ -32,13 +29,17 @@ dict(kt1, arg="a")  # type: ignore
 kt2: KeysAndGetItem[str, int] = KeysAndGetItem()
 assert_type(dict(kt2, arg=1), Dict[str, int])
 
-if MYPY:
-    # Hack to skip `pyright`. It was failing like:
-    # expected "Dict[int, str]" but received "dict[Literal[1, 2], Literal['a', 'b']]"
-    # See https://github.com/python/typeshed/pull/8517
-    i1: Iterable[Tuple[int, str]] = [(1, "a"), (2, "b")]
-    assert_type(dict(i1), Dict[int, str])
-    dict(i1, arg="a")  # type: ignore
+i1: Iterable[Tuple[int, str]] = [(1, "a"), (2, "b")]
+# Hack to pass `pyright`. It was failing like:
+# expected "Dict[int, str]" but received "dict[Literal[1, 2], Literal['a', 'b']]"
+# See https://github.com/python/typeshed/pull/8517
+# It used to be:
+# assert_type(dict(i1), Dict[int, str])
+d = dict(i1)
+assert_type(next(iter(d.keys())), int)
+assert_type(d[1], str)
+
+dict(i1, arg="a")  # type: ignore
 
 i2: Iterable[Tuple[str, int]] = [("a", 1), ("b", 2)]
 assert_type(dict(i2, arg=1), Dict[str, int])
