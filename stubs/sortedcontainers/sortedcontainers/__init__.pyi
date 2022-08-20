d569e1aeb3bde81391fb060e75c6666fd34ae7b5
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2022-08-20 21:59:00 +0200

    Auto-generate stub files for sortedcontainers

---
+++ stubs/sortedcontainers/sortedcontainers/__init__.pyi
@@ -0,0 +1,8 @@
+from .sorteddict import (
+    SortedDict as SortedDict,
+    SortedItemsView as SortedItemsView,
+    SortedKeysView as SortedKeysView,
+    SortedValuesView as SortedValuesView,
+)
+from .sortedlist import SortedKeyList as SortedKeyList, SortedList as SortedList, SortedListWithKey as SortedListWithKey
+from .sortedset import SortedSet as SortedSet
