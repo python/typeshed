a71115abbc7b6d5bd61770ebc371b5e599c43580
junkmd <jun.k.job.finding12@gmail.com>
2022-08-21 09:14:37 +0900

    add `_ctypes.pyi`

---
+++ stdlib/_ctypes.pyi
@@ -0,0 +1,23 @@
+import sys
+from ctypes import _CArgObject, _PointerLike
+from typing import Any
+from typing_extensions import TypeAlias
+
+if sys.platform == "win32":
+    # Description, Source, HelpFile, HelpContext, scode
+    _COMError_Details: TypeAlias = tuple[str | None, str | None, str | None, int | None, int | None]
+
+    class COMError(Exception):
+        """Raised when a COM method call failed."""
+
+        hresult: int
+        text: str | None
+        details: _COMError_Details
+
+        def __init__(self, hresult: int, text: str | None, details: _COMError_Details) -> None: ...
+
+    def CopyComPointer(src: _PointerLike, dst: _PointerLike | _CArgObject) -> int:
+        """CopyComPointer(src, dst) -> HRESULT value"""
+        ...
+
+def __getattr__(name: str) -> Any: ...  # incomplete
