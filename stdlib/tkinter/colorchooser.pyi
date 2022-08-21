c1b8657c5bfa445bd8ed70e5efe22aab22543607
Akuli <akuviljanen17@gmail.com>
2022-08-21 23:13:51 +0300

    Add types to tkinter.colorchooser.askcolor

--- stdlib/tkinter/colorchooser.pyi
+++ stdlib/tkinter/colorchooser.pyi
@@ -1,6 +1,7 @@
 import sys
 from tkinter.commondialog import Dialog
 from typing import ClassVar
+from tkinter import _Color, Misc
 
 if sys.version_info >= (3, 9):
     __all__ = ["Chooser", "askcolor"]
@@ -8,4 +9,8 @@ if sys.version_info >= (3, 9):
 class Chooser(Dialog):
     command: ClassVar[str]
 
-def askcolor(color: str | bytes | None = ..., **options) -> tuple[None, None] | tuple[tuple[float, float, float], str]: ...
+def askcolor(color: str | bytes | None = ..., *,
+initialcolor: _Color=...,
+parent: Misc=...,
+title: str=...
+) -> tuple[None, None] | tuple[tuple[float, float, float], str]: ...
