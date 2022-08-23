fed419b8f10f3ce1f6d529f2c29b016478b0c2a5
Nikita Sobolev <mail@sobolevn.me>
2022-08-24 00:52:16 +0300

    Add `CTYPES_MAX_ARGCOUNT` to `_ctypes` (#8601)

--- stdlib/_ctypes.pyi
+++ stdlib/_ctypes.pyi
@@ -2,6 +2,9 @@ import sys
 from ctypes import _CArgObject, _PointerLike
 from typing_extensions import TypeAlias
 
+if sys.version_info >= (3, 11):
+    CTYPES_MAX_ARGCOUNT: int
+
 if sys.platform == "win32":
     # Description, Source, HelpFile, HelpContext, scode
     _COMError_Details: TypeAlias = tuple[str | None, str | None, str | None, int | None, int | None]
