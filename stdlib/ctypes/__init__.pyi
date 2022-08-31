3e5808a99095180c931e970ed9d52f121654d86e
Alex Waygood <Alex.Waygood@Gmail.com>
2022-08-31 12:20:11 +0100

    Bump flake8-pyi and pytype test dependencies (#8656)

--- stdlib/ctypes/__init__.pyi
+++ stdlib/ctypes/__init__.pyi
@@ -1,13 +1,10 @@
 import sys
+from _ctypes import RTLD_GLOBAL as RTLD_GLOBAL, RTLD_LOCAL as RTLD_LOCAL
 from _typeshed import ReadableBuffer, Self, WriteableBuffer
 from abc import abstractmethod
 from collections.abc import Callable, Iterable, Iterator, Mapping, Sequence
 from typing import Any, ClassVar, Generic, TypeVar, Union as _UnionT, overload
 from typing_extensions import TypeAlias
-
-# TODO: import these from _ctypes once it no longer breaks pytype
-RTLD_GLOBAL: int
-RTLD_LOCAL: int
 
 if sys.version_info >= (3, 9):
     from types import GenericAlias
