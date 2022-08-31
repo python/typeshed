84fc12a248b89ff3db5a25e3c3a83dcf254ab323
Avasam <samuel.06@hotmail.com>
2022-08-31 10:37:05 -0400

    stubtest

--- stubs/D3DShot/d3dshot/dll/user32.pyi
+++ stubs/D3DShot/d3dshot/dll/user32.pyi
@@ -1,10 +1,17 @@
 import ctypes
 from _typeshed import Incomplete
+from ctypes import wintypes
 from typing_extensions import TypeAlias
 
 _Pointer: TypeAlias = Incomplete
 
-class DISPLAY_DEVICE(ctypes.Structure): ...
+class DISPLAY_DEVICE(ctypes.Structure):
+    cb: wintypes.DWORD
+    DeviceName: wintypes.WCHAR
+    DeviceString: wintypes.WCHAR
+    StateFlags: wintypes.DWORD
+    DeviceID: wintypes.WCHAR
+    DeviceKey: wintypes.WCHAR
 
 def get_display_device_name_mapping() -> dict[str, tuple[str, bool]]: ...
 def get_hmonitor_by_point(x: int, y: int) -> _Pointer: ...  # ctypes.windll.user32.MonitorFromPoint(point, 0)
