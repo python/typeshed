61831c0164ecfb70dcb2ea113e635a3c03dc78ed
James Hilton-Balfe <gobot1234yt@gmail.com>
2022-09-03 12:04:48 +0100

    Add some undocumented constants to gzip.pyi (#8675)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- stdlib/gzip.pyi
+++ stdlib/gzip.pyi
@@ -15,8 +15,14 @@ _ReadBinaryMode: TypeAlias = Literal["r", "rb"]
 _WriteBinaryMode: TypeAlias = Literal["a", "ab", "w", "wb", "x", "xb"]
 _OpenTextMode: TypeAlias = Literal["rt", "at", "wt", "xt"]
 
-READ: Literal[1]
-WRITE: Literal[2]
+READ: Literal[1]  # undocumented
+WRITE: Literal[2]  # undocumented
+
+FTEXT: int  # actually Literal[1] # undocumented
+FHCRC: int  # actually Literal[2] # undocumented
+FEXTRA: int  # actually Literal[4] # undocumented
+FNAME: int  # actually Literal[8] # undocumented
+FCOMMENT: int  # actually Literal[16] # undocumented
 
 class _ReadableFileobj(Protocol):
     def read(self, __n: int) -> bytes: ...
