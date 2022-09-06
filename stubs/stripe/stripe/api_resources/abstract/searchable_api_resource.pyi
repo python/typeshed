56f5dc9c7716e5d1bd603941c4424b6421cbee50
Yejia Chen <yejia@stripe.com>
2022-09-06 15:05:30 -0400

    Add SearchableAPIResource and SearchResultObject for types-stripe

---
+++ stubs/stripe/stripe/api_resources/abstract/searchable_api_resource.pyi
@@ -0,0 +1,23 @@
+from _typeshed import Self
+from typing import Iterator
+
+from stripe.api_resources.abstract.api_resource import APIResource as APIResource
+from stripe.api_resources.search_result_object import SearchResultObject
+
+class SearchableAPIResource(APIResource):
+    @classmethod
+    def _search(
+        cls: type[Self],
+        search_url: str,
+        api_key: str | None = ...,
+        stripe_version: str | None = ...,
+        stripe_account: str | None = ...,
+        **params,
+    ) -> SearchResultObject[Self]: ...
+
+    @classmethod
+    def search(cls: type[Self], *args: str | None, **kwargs) -> SearchResultObject[Self]: ...
+
+    @classmethod
+    def search_auto_paging_iter(cls: type[Self], *args: str | None, **kwargs) -> Iterator[Self]: ...
+
---
+++ stubs/stripe/stripe/api_resources/search_result_object.pyi
@@ -0,0 +1,32 @@
+from _typeshed import Self
+from typing import Any, Generic, Iterator, List, TypeVar
+
+from stripe.stripe_object import StripeObject
+
+_T = TypeVar("_T")
+
+class SearchResultObject(StripeObject, Generic[_T]):
+    OBJECT_NAME = "search_result"
+    url: str
+    has_more: bool
+    data: List[_T]
+    next_page: str
+    total_count: int
+
+    def search(
+        self: Self, api_key: str | None = ..., stripe_version: str | None = ..., stripe_account: str | None = ..., **params
+    ) -> Self: ...
+    def __getitem__(self, k) -> Any: ...
+    def __iter__(self) -> Iterator[_T]: ...
+    def __len__(self) -> int: ...
+    def auto_paging_iter(self) -> Iterator[_T]: ...
+    @classmethod
+    def empty_search_result(
+        cls: type[Self], api_key: str | None = ..., stripe_version: str | None = ..., stripe_account: str | None = ...
+    ) -> Self: ...
+    @property
+    def is_empty(self) -> bool: ...
+    def next_search_result_page(
+        self: Self, api_key: str | None = ..., stripe_version: str | None = ..., stripe_account: str | None = ..., **params
+    ) -> Self: ...
+
