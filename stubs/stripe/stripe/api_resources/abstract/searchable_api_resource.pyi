aea6720903aaf279ef7c7945ae022a9016dcb8c5
Yejia Chen <yejia@stripe.com>
2022-09-13 09:54:41 -0400

    Address review feedback

--- stubs/stripe/stripe/api_resources/abstract/searchable_api_resource.pyi
+++ stubs/stripe/stripe/api_resources/abstract/searchable_api_resource.pyi
@@ -6,15 +6,6 @@ from stripe.api_resources.search_result_object import SearchResultObject
 
 class SearchableAPIResource(APIResource):
     @classmethod
-    def _search(
-        cls: type[Self],
-        search_url: str,
-        api_key: str | None = ...,
-        stripe_version: str | None = ...,
-        stripe_account: str | None = ...,
-        **params,
-    ) -> SearchResultObject[Self]: ...
-    @classmethod
     def search(cls: type[Self], *args: str | None, **kwargs) -> SearchResultObject[Self]: ...
     @classmethod
     def search_auto_paging_iter(cls: type[Self], *args: str | None, **kwargs) -> Iterator[Self]: ...
--- stubs/stripe/stripe/api_resources/search_result_object.pyi
+++ stubs/stripe/stripe/api_resources/search_result_object.pyi
@@ -1,6 +1,6 @@
 from _typeshed import Self
 from collections.abc import Iterator
-from typing import Any, Generic, TypeVar
+from typing import Any, ClassVar, Generic, TypeVar
 from typing_extensions import Literal
 
 from stripe.stripe_object import StripeObject
@@ -8,7 +8,7 @@ from stripe.stripe_object import StripeObject
 _T = TypeVar("_T")
 
 class SearchResultObject(StripeObject, Generic[_T]):
-    OBJECT_NAME: Literal["search_result"] = ...
+    OBJECT_NAME: ClassVar[Literal["search_result"]] = ...
     url: str
     has_more: bool
     data: list[_T]
