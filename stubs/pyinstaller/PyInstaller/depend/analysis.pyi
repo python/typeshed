72788598d751f8803e2cf75b9d9cefc38c59e2b2
Avasam <samuel.06@hotmail.com>
2022-09-13 23:43:10 -0400

    Fix "stub does not have argument"

--- stubs/pyinstaller/PyInstaller/depend/analysis.pyi
+++ stubs/pyinstaller/PyInstaller/depend/analysis.pyi
@@ -2,4 +2,17 @@
 
 # The documentation explicitely mentions that "Normally you do not need to know about the module-graph."
 # However, some PyiModuleGraph typed class attributes are still documented as existing in imphookapi.
-class PyiModuleGraph: ...  # incomplete
+from _typeshed import Incomplete
+
+class PyiModuleGraph:  # incomplete
+    def __init__(
+        self,
+        pyi_homepath,
+        user_hook_dirs=...,
+        excludes=...,
+        path: Incomplete | None = ...,
+        replace_paths=...,
+        implies=...,
+        graph: Incomplete | None = ...,
+        debug: int = ...,
+    ) -> None: ...
