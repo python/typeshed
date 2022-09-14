e35ee77799e38cabe782c90602d9f26dde9a17a7
Avasam <samuel.06@hotmail.com>
2022-09-13 23:51:13 -0400

    pyi_homepath path

--- stubs/pyinstaller/PyInstaller/depend/analysis.pyi
+++ stubs/pyinstaller/PyInstaller/depend/analysis.pyi
@@ -3,11 +3,12 @@
 # The documentation explicitely mentions that "Normally you do not need to know about the module-graph."
 # However, some PyiModuleGraph typed class attributes are still documented as existing in imphookapi.
 from _typeshed import Incomplete
+from typing_extensions import LiteralString
 
 class PyiModuleGraph:  # incomplete
     def __init__(
         self,
-        pyi_homepath,
+        pyi_homepath: LiteralString,
         user_hook_dirs=...,
         excludes=...,
         path: Incomplete | None = ...,
