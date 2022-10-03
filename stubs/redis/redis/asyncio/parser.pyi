68bd7ade53b8d761da0f258ac83a628bea1f4742
Nikita Sobolev <mail@sobolevn.me>
2022-10-03 20:23:22 +0300

    Update parser.pyi

--- stubs/redis/redis/asyncio/parser.pyi
+++ stubs/redis/redis/asyncio/parser.pyi
@@ -1,7 +1,10 @@
-from redis.asyncio.cluster import ClusterNode
+from typing import Any
+
+# TODO: define and use:
+# from redis.asyncio.cluster import ClusterNode
 
 class CommandsParser:
     __slots__ = ("commands", "node")
 
-    async def initialize(self, node: ClusterNode | None = ...) -> None: ...
+    async def initialize(self, node: Any | None = ...) -> None: ...  # TODO: ClusterNode
     async def get_keys(self, *args: Any) -> tuple[str, ...] | None: ...
