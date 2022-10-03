7e2c8b3700a93ad073acc2b4161f15b84ad6ec54
Avasam <samuel.06@hotmail.com>
2022-10-02 20:57:07 -0400

    Fix some things I missed

--- stubs/pywin32/win32comext/axscript/axscript.pyi
+++ stubs/pywin32/win32comext/axscript/axscript.pyi
@@ -1,46 +1,5 @@
 import _win32typing
 
-def OleCreate(
-    clsid,
-    clsid1,
-    obCLSID: _win32typing.PyIID,
-    obIID: _win32typing.PyIID,
-    renderopt,
-    obFormatEtc,
-    obOleClientSite: _win32typing.PyIOleClientSite,
-    obStorage: _win32typing.PyIStorage,
-) -> _win32typing.PyIOleObject: ...
-def OleLoadPicture(
-    stream: _win32typing.PyIStream, size, runMode, arg: _win32typing.PyIID, arg1: _win32typing.PyIID
-) -> _win32typing.PyIUnknown: ...
-def OleLoadPicturePath(
-    url_or_path: str, unk, reserved, clr, arg: _win32typing.PyIID, arg1: _win32typing.PyIID
-) -> _win32typing.PyIUnknown: ...
-def OleSetContainedObject(unk: _win32typing.PyIUnknown, fContained) -> None: ...
-def OleTranslateAccelerator(frame: _win32typing.PyIOleInPlaceFrame, frame_info, msg: _win32typing.PyMSG) -> None: ...
-
-EMBDHLP_CREATENOW: int
-EMBDHLP_DELAYCREATE: int
-EMBDHLP_INPROC_HANDLER: int
-EMBDHLP_INPROC_SERVER: int
-OLECLOSE_NOSAVE: int
-OLECLOSE_PROMPTSAVE: int
-OLECLOSE_SAVEIFDIRTY: int
-OLECMDF_ENABLED: int
-OLECMDF_LATCHED: int
-OLECMDF_NINCHED: int
-OLECMDF_SUPPORTED: int
-OLECMDTEXTF_NAME: int
-OLECMDTEXTF_NONE: int
-OLECMDTEXTF_STATUS: int
-OLECREATE_LEAVERUNNING: int
-OLEIVERB_DISCARDUNDOSTATE: int
-OLEIVERB_HIDE: int
-OLEIVERB_INPLACEACTIVATE: int
-OLEIVERB_OPEN: int
-OLEIVERB_PRIMARY: int
-OLEIVERB_SHOW: int
-OLEIVERB_UIACTIVATE: int
 CATID_ActiveScript: _win32typing.PyIID
 CATID_ActiveScriptParse: _win32typing.PyIID
 IID_IActiveScript: _win32typing.PyIID
