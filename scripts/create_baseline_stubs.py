e5d52a39d4a5e10ee40b940421f262668bb96514
Samuel T <samuel.06@hotmail.com>
2022-10-04 10:21:10 -0400

    Script to run all checks locally (#8798)
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- scripts/create_baseline_stubs.py
+++ scripts/create_baseline_stubs.py
@@ -171,11 +171,8 @@ def main() -> None:
 
     print("\nDone!\n\nSuggested next steps:")
     print(f" 1. Manually review the generated stubs in {stub_dir}")
-    print(f' 2. Run "MYPYPATH={stub_dir} python3 -m mypy.stubtest {package}" to check the stubs against runtime')
-    print(f' 3. Run "mypy {stub_dir}" to check for errors')
-    print(f' 4. Run "black {stub_dir}" and "isort {stub_dir}" (if you\'ve made code changes)')
-    print(f' 5. Run "flake8 {stub_dir}" to check for e.g. unused imports')
-    print(" 6. Commit the changes on a new branch and create a typeshed PR")
+    print(" 2. Optionally run tests and autofixes (see tests/README.md for details")
+    print(" 3. Commit the changes on a new branch and create a typeshed PR (don't force-push!)")
 
 
 if __name__ == "__main__":
