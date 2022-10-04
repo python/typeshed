2a0aa2c2529ec3adcb23cb18a3cc97df0cc489f0
AlexWaygood <alex.waygood@gmail.com>
2022-10-04 10:51:24 -0700

    Use stubgen's `--export-less` option in `create_baseline_stubs.py`

--- scripts/create_baseline_stubs.py
+++ scripts/create_baseline_stubs.py
@@ -47,7 +47,7 @@ def get_installed_package_info(project: str) -> tuple[str, str] | None:
 
 def run_stubgen(package: str, output: str) -> None:
     print(f"Running stubgen: stubgen -o {output} -p {package}")
-    subprocess.run(["stubgen", "-o", output, "-p", package], check=True)
+    subprocess.run(["stubgen", "-o", output, "-p", package, "--export-less"], check=True)
 
 
 def run_black(stub_dir: str) -> None:
