4579094f77b817c0f3d0e5ae0f04271b2be98c1c
Nikita Sobolev <mail@sobolevn.me>
2022-10-07 16:03:18 +0300

    Bump tabulate to `0.9` version (#8865)

--- stubs/tabulate/tabulate/__init__.pyi
+++ stubs/tabulate/tabulate/__init__.pyi
@@ -2,6 +2,8 @@ from collections.abc import Callable, Container, Iterable, Mapping, Sequence
 from typing import Any, NamedTuple
 from typing_extensions import TypeAlias
 
+__version__: str
+
 LATEX_ESCAPE_RULES: dict[str, str]
 MIN_PADDING: int
 PRESERVE_WHITESPACE: bool
@@ -39,6 +41,7 @@ def tabulate(
     headers: str | dict[str, str] | Sequence[str] = ...,
     tablefmt: str | TableFormat = ...,
     floatfmt: str | Iterable[str] = ...,
+    intfmt: str | Iterable[str] = ...,
     numalign: str | None = ...,
     stralign: str | None = ...,
     missingval: str | Iterable[str] = ...,
@@ -46,4 +49,6 @@ def tabulate(
     disable_numparse: bool | Iterable[int] = ...,
     colalign: Iterable[str | None] | None = ...,
     maxcolwidths: int | Iterable[int | None] | None = ...,
+    rowalign: str | Iterable[str] | None = ...,
+    maxheadercolwidths: int | Iterable[int] | None = ...,
 ) -> str: ...
