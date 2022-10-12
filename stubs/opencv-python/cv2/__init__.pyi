f75c2f9077086d7b347d4fd92250bcf2d5c0f748
Avasam <samuel.06@hotmail.com>
2022-10-12 18:14:40 -0400

    fixed cv2.error and cv2.Error

--- stubs/opencv-python/cv2/__init__.pyi
+++ stubs/opencv-python/cv2/__init__.pyi
@@ -1,6 +1,14 @@
 from typing_extensions import TypeAlias
 
-from cv2 import data as data, gapi as gapi, mat_wrapper as mat_wrapper, misc as misc, utils as utils, version as version
+from cv2 import (
+    Error as Error,
+    data as data,
+    gapi as gapi,
+    mat_wrapper as mat_wrapper,
+    misc as misc,
+    utils as utils,
+    version as version,
+)
 from cv2.cv2 import *
 from cv2.mat_wrapper import Mat as WrappedMat, _NDArray
 
