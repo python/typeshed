b82ae957b836d810987ef0145669ca48ca79e589
Stanislav Zmiev <szmiev2000@gmail.com>
2022-10-18 23:28:33 +0400

    Clean up after fixes

--- stdlib/pathlib.pyi
+++ stdlib/pathlib.pyi
@@ -12,15 +12,13 @@ from collections.abc import Callable, Generator, Iterator, Sequence
 from io import BufferedRandom, BufferedReader, BufferedWriter, FileIO, TextIOWrapper
 from os import PathLike, stat_result
 from types import TracebackType
-from typing import IO, Any, AnyStr, BinaryIO, overload
-from typing_extensions import Literal, TypeAlias
+from typing import IO, Any, BinaryIO, overload
+from typing_extensions import Literal
 
 if sys.version_info >= (3, 9):
     from types import GenericAlias
 
 __all__ = ["PurePath", "PurePosixPath", "PureWindowsPath", "Path", "PosixPath", "WindowsPath"]
-
-_OnError: TypeAlias = Callable[[OSError], object]
 
 class PurePath(PathLike[str]):
     @property
