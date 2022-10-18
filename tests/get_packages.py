61deba65bf47cb5dd7cb6939878b962036eb6cd1
sobolevn <mail@sobolevn.me>
2022-10-18 14:39:46 +0300

    Run stubtest on stubs on different platforms

---
+++ tests/get_packages.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+import tomli
+
+platform = sys.platform
+distributions = sys.argv[1:]
+if not distributions:
+    distributions = os.listdir("stubs")
+
+metadata_mapping = {
+    "linux": "apt_dependencies",
+    # TODO: support others:
+    # "darwin": "brew_dependencies",
+    # "win32": "choco_dependencies",
+}
+
+if platform in metadata_mapping:
+    for distribution in distributions:
+        with open(f"stubs/{distribution}/METADATA.toml", "rb") as file:
+            for package in tomli.load(file).get("tool", {}).get("stubtest", {}).get(metadata_mapping[platform], []):
+                print(package)
--- tests/stubtest_third_party.py
+++ tests/stubtest_third_party.py
@@ -34,6 +34,11 @@ def run_stubtest(dist: Path, *, verbose: bool = False) -> bool:
         print(colored("skipping", "yellow"))
         return True
 
+    platform = metadata.get("tool", {}).get("plaform", None)
+    if platform is not None and sys.platform not in platform:
+        print(colored(f"skipping, unsupported platform: {sys.platform}, supported: {platform}", "yellow"))
+        return True
+
     with tempfile.TemporaryDirectory() as tmp:
         venv_dir = Path(tmp)
         venv.create(venv_dir, with_pip=True, clear=True)
