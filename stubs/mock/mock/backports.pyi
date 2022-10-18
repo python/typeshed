5e273eded7f1d7fbf7b1d1eb4ddaa997c0c7e3d3
sobolevn <mail@sobolevn.me>
2022-10-18 13:11:15 +0300

    Fix pyright

--- stubs/mock/mock/backports.pyi
+++ stubs/mock/mock/backports.pyi
@@ -1,11 +1,11 @@
 import sys
 
 if sys.version_info[:2] >= (3, 8):
-    from asyncio import iscoroutinefunction as iscoroutinefunction
-    from unittest import IsolatedAsyncioTestCase as IsolatedAsyncioTestCase
+    from asyncio import iscoroutinefunction as iscoroutinefunction  # pyright: ignore[reportGeneralTypeIssues]
+    from unittest import IsolatedAsyncioTestCase as IsolatedAsyncioTestCase  # pyright: ignore[reportGeneralTypeIssues]
 else:
     import unittest
 
-    class IsolatedAsyncioTestCase(unittest.TestCase): ...  # pyright: ignore[reportGeneralTypeIssues]
+    class IsolatedAsyncioTestCase(unittest.TestCase): ...  
     # It is a typeguard, but its signature is to complex to duplicate.
-    def iscoroutinefunction(obj: object) -> bool: ...  # pyright: ignore[reportGeneralTypeIssues]
+    def iscoroutinefunction(obj: object) -> bool: ...
