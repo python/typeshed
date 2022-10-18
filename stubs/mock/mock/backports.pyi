5e5ad922994955a590a3b3b72291608b7cd90fb6
sobolevn <mail@sobolevn.me>
2022-10-18 12:58:28 +0300

    Fix CI

--- stubs/mock/mock/backports.pyi
+++ stubs/mock/mock/backports.pyi
@@ -1,2 +1,10 @@
-from asyncio import iscoroutinefunction as iscoroutinefunction
-from unittest import IsolatedAsyncioTestCase as IsolatedAsyncioTestCase
+if sys.version_info[:2] < (3, 8):
+    import unittest
+
+    class IsolatedAsyncioTestCase(unittest.TestCase): ...
+
+    # It is a typeguard, but its signature is to complex to duplicate.
+    def iscoroutinefunction(obj: object) -> bool: ...
+else:
+    from asyncio import iscoroutinefunction as iscoroutinefunction
+    from unittest import IsolatedAsyncioTestCase as IsolatedAsyncioTestCase
