79d331286f9c9d889e224d87d1cc590df9ceda79
Nikita Sobolev <mail@sobolevn.me>
2022-10-20 11:37:42 +0300

    Mark `python-crontab` as completed (#8938)

--- stubs/python-crontab/cronlog.pyi
+++ stubs/python-crontab/cronlog.pyi
@@ -2,7 +2,9 @@ from _typeshed import Self, StrOrBytesPath
 from codecs import StreamReaderWriter
 from collections.abc import Generator, Iterator
 from types import TracebackType
+from typing_extensions import Literal
 
+PY3: Literal[True]
 MATCHER: str
 
 class LogReader:
