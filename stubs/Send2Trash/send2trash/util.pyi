3f91db3ef9a64a2bca875c65f364c8abc0a74469
Nikita Sobolev <mail@sobolevn.me>
2022-10-20 17:21:07 +0300

    Update util.pyi

--- stubs/Send2Trash/send2trash/util.pyi
+++ stubs/Send2Trash/send2trash/util.pyi
@@ -1,3 +1,4 @@
-from _typeshed import Incomplete
+from _typeshed import StrOrBytesPath
+from collections.abc import Sequence
 
-def preprocess_paths(paths: Incomplete) -> Incomplete: ...
+def preprocess_paths(paths: StrOrBytesPath | Sequence[StrOrBytesPath]) -> list[str | bytes]: ...
