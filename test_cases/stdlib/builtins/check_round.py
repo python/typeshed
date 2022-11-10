1ffa3d0cf85132105d7a11e509e4caf13659f0ed
sobolevn <mail@sobolevn.me>
2022-11-10 15:42:37 +0300

    Add special case and tests for `round()`

---
+++ test_cases/stdlib/builtins/check_round.py
@@ -0,0 +1,37 @@
+from typing_extensions import assert_type
+
+# float:
+
+assert_type(round(5.5), int)
+assert_type(round(5.5, None), int)
+assert_type(round(5.5, 0), float)
+assert_type(round(5.5, 1), float)
+
+n: int
+assert_type(round(5.5, n), float)
+
+# Protocols:
+
+class CustomIndex:
+    def __index__(self) -> int:
+        return 1
+
+class WithCustomRound1:
+    def __round__(self) -> str:
+        return 'a'
+
+assert_type(round(WithCustomRound1()), str)
+assert_type(round(WithCustomRound1(), None), str)
+# Errors:
+round(WithCustomRound1(), 1)  # type: ignore
+round(WithCustomRound1(), CustomIndex())  # type: ignore
+
+class WithCustomRound2:
+    def __round__(self, digits: int) -> str:
+        return 'a'
+
+assert_type(round(WithCustomRound2(), 1), str)
+assert_type(round(WithCustomRound2(), CustomIndex()), str)
+# Errors:
+round(WithCustomRound2(), None)  # type: ignore
+round(WithCustomRound2())  # type: ignore
