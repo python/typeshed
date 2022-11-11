f3b5ba3d7676cd7953fa1c7007609761af58001e
Nikita Sobolev <mail@sobolevn.me>
2022-11-11 21:10:10 +0300

    Update test_cases/stdlib/builtins/check_round.py
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- test_cases/stdlib/builtins/check_round.py
+++ test_cases/stdlib/builtins/check_round.py
@@ -54,7 +54,7 @@ round(WithCustomRound2())  # type: ignore
 
 class WithOverloadedRound:
     @overload
-    def __round__(self) -> str:
+    def __round__(self, ndigits: None = ...) -> str:
         ...
 
     @overload
