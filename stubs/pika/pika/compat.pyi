6d643190b759f3fe50f80b42e8facef38191e553
Sebastian Rittau <srittau@rittau.biz>
2022-11-15 13:46:21 +0100

    Annotate pika.compat partially

--- stubs/pika/pika/compat.pyi
+++ stubs/pika/pika/compat.pyi
@@ -1,19 +1,26 @@
-from _typeshed import Incomplete
+from abc import ABCMeta
 from io import StringIO as StringIO
-from urllib.parse import urlencode as urlencode, urlparse as urlparse
+from re import Pattern
+from typing import Final, Literal
+from urllib.parse import parse_qs, quote, unquote, urlencode as urlencode, urlparse as urlparse
 
-PY2: Incomplete
-PY3: Incomplete
-RE_NUM: Incomplete
-ON_LINUX: Incomplete
-ON_OSX: Incomplete
-ON_WINDOWS: Incomplete
-AbstractBase: Incomplete
-SOCKET_ERROR: Incomplete
+url_quote = quote
+url_unquote = unquote
+url_parse_qs = parse_qs
+
+PY2: Final[Literal[False]]
+PY3: Final[Literal[True]]
+RE_NUM: Final[Pattern[str]]
+ON_LINUX: Final[bool]
+ON_OSX: Final[bool]
+ON_WINDOWS: Final[bool]
+
+class AbstractBase(metaclass=ABCMeta): ...
+
 SOCKET_ERROR = OSError
-SOL_TCP: Incomplete
-basestring: Incomplete
-str_or_bytes: Incomplete
+SOL_TCP: Final[int]
+basestring: Final[tuple[type[str]]]
+str_or_bytes: Final[tuple[type[str], type[bytes]]]
 xrange = range
 unicode_type = str
 
@@ -30,8 +37,8 @@ def canonical_str(value): ...
 def is_integer(value): ...
 def as_bytes(value): ...
 def to_digit(value): ...
-def get_linux_version(release_str): ...
+def get_linux_version(release_str: str) -> tuple[int, int, int]: ...
 
-HAVE_SIGNAL: Incomplete
-EINTR_IS_EXPOSED: Incomplete
-LINUX_VERSION: Incomplete
+HAVE_SIGNAL: Final[bool]
+EINTR_IS_EXPOSED: Final[Literal[False]]
+LINUX_VERSION: tuple[int, int, int] | None
