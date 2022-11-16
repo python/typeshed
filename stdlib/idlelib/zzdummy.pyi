c996d82790e7dde3f2e38d3b68554ded79202de5
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-11-15 20:35:25 -0600

    Pycln keeps removing Event[Any]

--- stdlib/idlelib/zzdummy.pyi
+++ stdlib/idlelib/zzdummy.pyi
@@ -1,10 +1,10 @@
 from idlelib.config import idleConf as idleConf
 from idlelib.editor import EditorWindow
 from idlelib.format import FormatRegion
-from tkinter import Text
-from typing import Callable
+from tkinter import Event, Text
+from typing import Any, Callable
 
-def format_selection(format_line: Callable[["ZzDummy", str], str]) -> Callable[["Event[Any] | None"], str]: ...
+def format_selection(format_line: Callable[["ZzDummy", str], str]) -> Callable[[Event[Any] | None], str]: ...
 
 class ZzDummy:
     menudefs: list[tuple[str, list[tuple[str, str]]]]
