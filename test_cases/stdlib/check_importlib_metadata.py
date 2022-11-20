7cf67b0a0f8af9d80e151beb59c16dadc658e21d
layday <layday@protonmail.com>
2022-11-20 11:18:19 +0200

    Fix import

--- test_cases/stdlib/check_importlib_metadata.py
+++ test_cases/stdlib/check_importlib_metadata.py
@@ -1,8 +1,8 @@
 import sys
-from importlib.metadata._meta import SimplePath
 from pathlib import Path
 
-if sys.version_info >= (3, 9):
+if sys.version_info >= (3, 10):
+    from importlib.metadata._meta import SimplePath
     from zipfile import Path as ZipPath
 
     _: SimplePath = Path()
