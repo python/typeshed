e080430c2829a4abcb152860d104f4d8fc650a75
Sebastian Rittau <srittau@rittau.biz>
2022-11-21 18:45:58 +0100

    Tighten AMQPObject.__eq__

--- stubs/pika/pika/amqp_object.pyi
+++ stubs/pika/pika/amqp_object.pyi
@@ -3,7 +3,7 @@ from typing import ClassVar
 class AMQPObject:
     NAME: ClassVar[str]
     INDEX: ClassVar[int | None]
-    def __eq__(self, other: object) -> bool: ...
+    def __eq__(self, other: AMQPObject | None) -> bool: ...  # type: ignore[override]
 
 class Class(AMQPObject): ...
 
