13bd8da528719e31f15c93c36c142fa7f6fd7ceb
Alexander Burchenko <aabur@mail.ru>
2022-11-22 14:26:26 +0200

    boltons: ecoutils - fix stubtest errors

--- stubs/boltons/boltons/ecoutils.pyi
+++ stubs/boltons/boltons/ecoutils.pyi
@@ -2,28 +2,33 @@
 
 from typing import Any, NoReturn
 
-# ECO_VERSION: str
-# PY_GT_2: Incomplete
-# getrandbits: Incomplete
-# HAVE_URANDOM: bool
-# INSTANCE_ID: Incomplete
-# IS_64BIT: Incomplete
-# HAVE_UCS4: Incomplete
-# HAVE_READLINE: bool
-# SQLITE_VERSION: Incomplete
-# OPENSSL_VERSION: Incomplete
-# TKINTER_VERSION: Incomplete
-# ZLIB_VERSION: Incomplete
-# EXPAT_VERSION: Incomplete
-# CPU_COUNT: Incomplete
-# HAVE_THREADING: bool
-# HAVE_IPV6: Incomplete
-# RLIMIT_NOFILE: Incomplete
-# RLIMIT_FDS_SOFT: Incomplete
-# RLIMIT_FDS_HARD: Incomplete
-# START_TIME_INFO: Incomplete
+ECO_VERSION: str
+PY_GT_2: bool
+getrandbits: int
+HAVE_URANDOM: bool
+INSTANCE_ID: str
+IS_64BIT: bool
+HAVE_UCS4: bool
+HAVE_READLINE: bool
+SQLITE_VERSION: str
+OPENSSL_VERSION: str
+TKINTER_VERSION: str
+ZLIB_VERSION: str
+EXPAT_VERSION: str
+CPU_COUNT: int
+HAVE_THREADING: bool
+HAVE_IPV6: bool
+RLIMIT_NOFILE: int
+RLIMIT_FDS_SOFT: int
+RLIMIT_FDS_HARD: int
+START_TIME_INFO: dict[str, str | float]
 
 def get_python_info() -> dict[str, Any]: ...
 def get_profile(**kwargs) -> dict[str, Any]: ...
 def get_profile_json(indent: bool = ...) -> str: ...
 def main() -> NoReturn: ...
+def dumps(val: Any, indent: int) -> str: ...
+
+# def _escape_shell_args(args, sep=' ', style=None) -> str: ...
+# def _args2sh(args, sep=' ') -> str: ...
+# def _args2cmd(args, sep=' ') -> str: ...
