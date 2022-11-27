1f1d8de5c7f054644f6a90bfcb6b95b484b68114
sobolevn <mail@sobolevn.me>
2022-11-27 11:33:32 +0300

    Use overloads

--- stubs/simplejson/simplejson/__init__.pyi
+++ stubs/simplejson/simplejson/__init__.pyi
@@ -1,6 +1,6 @@
 from _typeshed import SupportsRichComparison
 from collections.abc import Callable
-from typing import IO, Any, TypeVar
+from typing import IO, Any, TypeVar, overload
 from typing_extensions import TypeAlias
 
 from simplejson.decoder import JSONDecoder as JSONDecoder
@@ -11,6 +11,33 @@ from simplejson.scanner import JSONDecodeError as JSONDecodeError
 _LoadsString: TypeAlias = str | bytes | bytearray
 _T = TypeVar("_T")
 
+@overload
+def dumps(
+    obj: Any,
+    skipkeys: bool = ...,
+    ensure_ascii: bool = ...,
+    check_circular: bool = ...,
+    allow_nan: bool = ...,
+    indent: str | int | None = ...,
+    separators: tuple[str, str] | None = ...,
+    encoding: str = ...,
+    default: Callable[[Any], Any] | None = ...,
+    use_decimal: bool = ...,
+    namedtuple_as_object: bool = ...,
+    tuple_as_array: bool = ...,
+    bigint_as_string: bool = ...,
+    sort_keys: bool = ...,
+    item_sort_key: Callable[[Any], SupportsRichComparison] | None = ...,
+    for_json: bool = ...,
+    ignore_nan: bool = ...,
+    int_as_string_bitcount: int | None = ...,
+    iterable_as_array: bool = ...,
+    *,
+    # Technically: you cannot pass extra `kw` without passing custom `cls`
+    cls: type[JSONEncoder],
+    **kw: Any,
+) -> str: ...
+@overload
 def dumps(
     obj: Any,
     skipkeys: bool = ...,
@@ -32,9 +59,35 @@ def dumps(
     ignore_nan: bool = ...,
     int_as_string_bitcount: int | None = ...,
     iterable_as_array: bool = ...,
-    # Technically: you cannot pass extra `kw` without passing custom `cls`
-    **kw: Any,
 ) -> str: ...
+@overload
+def dump(
+    obj: Any,
+    fp: IO[str],
+    skipkeys: bool = ...,
+    ensure_ascii: bool = ...,
+    check_circular: bool = ...,
+    allow_nan: bool = ...,
+    indent: str | int | None = ...,
+    separators: tuple[str, str] | None = ...,
+    encoding: str = ...,
+    default: Callable[[Any], Any] | None = ...,
+    use_decimal: bool = ...,
+    namedtuple_as_object: bool = ...,
+    tuple_as_array: bool = ...,
+    bigint_as_string: bool = ...,
+    sort_keys: bool = ...,
+    item_sort_key: Callable[[Any], SupportsRichComparison] | None = ...,
+    for_json: bool = ...,
+    ignore_nan: bool = ...,
+    int_as_string_bitcount: int | None = ...,
+    iterable_as_array: bool = ...,
+    # Technically: you cannot pass extra `kw` without passing custom `cls`
+    *,
+    cls: type[JSONEncoder],
+    **kw: Any,
+) -> None: ...
+@overload
 def dump(
     obj: Any,
     fp: IO[str],
@@ -57,9 +110,23 @@ def dump(
     ignore_nan: bool = ...,
     int_as_string_bitcount: int | None = ...,
     iterable_as_array: bool = ...,
-    # Technically: you cannot pass extra `kw` without passing custom `cls`
-    **kw: Any,
 ) -> None: ...
+@overload
+def loads(
+    s: _LoadsString,
+    encoding: str | None = ...,
+    object_hook: Callable[[dict[Any, Any]], Any] | None = ...,
+    parse_float: Callable[[str], Any] | None = ...,
+    parse_int: Callable[[str], Any] | None = ...,
+    parse_constant: Callable[[str], Any] | None = ...,
+    object_pairs_hook: Callable[[list[tuple[Any, Any]]], Any] | None = ...,
+    use_decimal: bool = ...,
+    # Technically: you cannot pass extra `kw` without passing custom `cls`
+    *,
+    cls: type[JSONDecoder],
+    **kw: Any,
+) -> Any: ...
+@overload
 def loads(
     s: _LoadsString,
     encoding: str | None = ...,
@@ -70,9 +137,25 @@ def loads(
     parse_constant: Callable[[str], Any] | None = ...,
     object_pairs_hook: Callable[[list[tuple[Any, Any]]], Any] | None = ...,
     use_decimal: bool = ...,
+) -> Any: ...
+@overload
+def load(
+    fp: IO[str],
+    encoding: str | None = ...,
+    object_hook: Callable[[dict[Any, Any]], Any] | None = ...,
+    parse_float: Callable[[str], Any] | None = ...,
+    parse_int: Callable[[str], Any] | None = ...,
+    parse_constant: Callable[[str], Any] | None = ...,
+    object_pairs_hook: Callable[[list[tuple[Any, Any]]], Any] | None = ...,
+    use_decimal: bool = ...,
+    namedtuple_as_object: bool = ...,
+    tuple_as_array: bool = ...,
     # Technically: you cannot pass extra `kw` without passing custom `cls`
+    *,
+    cls: type[JSONDecoder],
     **kw: Any,
 ) -> Any: ...
+@overload
 def load(
     fp: IO[str],
     encoding: str | None = ...,
@@ -85,7 +168,5 @@ def load(
     use_decimal: bool = ...,
     namedtuple_as_object: bool = ...,
     tuple_as_array: bool = ...,
-    # Technically: you cannot pass extra `kw` without passing custom `cls`
-    **kw: Any,
 ) -> Any: ...
 def simple_first(kv: tuple[_T, object]) -> tuple[bool, _T]: ...
