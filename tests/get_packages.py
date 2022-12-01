69e813817ef90adcf02ff4c7cd3d85842a9c49ed
Nikita Sobolev <mail@sobolevn.me>
2022-12-01 21:22:41 +0300

    Use `__name__ == "__main__"` in `get_packages.py`

--- tests/get_packages.py
+++ tests/get_packages.py
@@ -5,13 +5,18 @@ import sys
 import tomli
 from utils import METADATA_MAPPING
 
-platform = sys.platform
-distributions = sys.argv[1:]
-if not distributions:
-    distributions = os.listdir("stubs")
+def main() -> None:
+    platform = sys.platform
+    distributions = sys.argv[1:]
+    if not distributions:
+        distributions = os.listdir("stubs")
 
-if platform in METADATA_MAPPING:
-    for distribution in distributions:
-        with open(f"stubs/{distribution}/METADATA.toml", "rb") as file:
-            for package in tomli.load(file).get("tool", {}).get("stubtest", {}).get(METADATA_MAPPING[platform], []):
-                print(package)
+    if platform in METADATA_MAPPING:
+        for distribution in distributions:
+            with open(f"stubs/{distribution}/METADATA.toml", "rb") as file:
+                for package in tomli.load(file).get("tool", {}).get("stubtest", {}).get(METADATA_MAPPING[platform], []):
+                    print(package)
+
+
+if __name__ == "__main__":
+    main()
