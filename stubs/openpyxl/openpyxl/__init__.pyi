a1ab347c6daf97600784ed958561762573372947
Benjamin Jones <109316494+bpjatfb@users.noreply.github.com>
2022-12-02 15:00:33 -0800

    Alias load_workbook with open, ignoring pyright error.

--- stubs/openpyxl/openpyxl/__init__.pyi
+++ stubs/openpyxl/openpyxl/__init__.pyi
@@ -1,4 +1,5 @@
 from openpyxl.compat.numbers import NUMPY as NUMPY
+from openpyxl.reader.excel import load_workbook as open # pyright: ignore[reportUnusedImport]
 from openpyxl.reader.excel import load_workbook as load_workbook
 from openpyxl.workbook import Workbook as Workbook
 from openpyxl.xml import DEFUSEDXML as DEFUSEDXML, LXML as LXML
