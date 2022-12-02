f48ab6d6ef5ee74c28bd07ef684da27138ca61a8
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2022-12-02 15:53:32 -0500

    add test

---
+++ stubs/protobuf/@tests/struct.py
@@ -0,0 +1,15 @@
+from google.protobuf.struct_pb2 import ListValue, Value, Struct
+
+list_value = ListValue()
+
+lst = list(list_value)  # ensure ListValue's __len__ + __getitem__ make it iterable
+
+list_value[0] = 42.42
+list_value[0] = '42'
+list_value[0] = None
+list_value[0] = True
+list_value[0] = [42.42, '42', None, True, [42.42, '42', None, True], {"42": 42}]
+list_value[0] = ListValue()
+list_value[0] = Struct()
+
+list_element = list_value[0]
