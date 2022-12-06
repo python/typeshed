87de3377ea4619c9bb44f10b2ba18eea078fc105
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-12-06 21:46:19 +0100

    Add types for jmespath internals

--- stubs/jmespath/jmespath/lexer.pyi
+++ stubs/jmespath/jmespath/lexer.pyi
@@ -1,11 +1,20 @@
-from typing import Any
+from typing import Iterator
+from typing_extensions import TypedDict
 
 from jmespath.exceptions import EmptyExpressionError as EmptyExpressionError, LexerError as LexerError
 
+class LexerTokenizeResult(TypedDict):
+    type: str
+    value: str
+    start: int
+    end: int
+
+LexerTokenizeResults = Iterator[LexerTokenizeResult]
+
 class Lexer:
-    START_IDENTIFIER: Any
-    VALID_IDENTIFIER: Any
-    VALID_NUMBER: Any
-    WHITESPACE: Any
-    SIMPLE_TOKENS: Any
-    def tokenize(self, expression) -> None: ...
+    START_IDENTIFIER: set[str]
+    VALID_IDENTIFIER: set[str]
+    VALID_NUMBER: set[str]
+    WHITESPACE: set[str]
+    SIMPLE_TOKENS: dict[str, str]
+    def tokenize(self, expression: str) -> LexerTokenizeResults: ...
