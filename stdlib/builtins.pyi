8a329ba6eb3e393d376c6d1ab62c9a96a805ec68
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-12-10 09:56:50 -0800

    Update stdlib/builtins.pyi

--- stdlib/builtins.pyi
+++ stdlib/builtins.pyi
@@ -804,7 +804,7 @@ class bytearray(MutableSequence[int], ByteString):
     def __alloc__(self) -> int: ...
 
 @final
-class memoryview(Sized, Sequence[int]):
+class memoryview(Sequence[int]):
     @property
     def format(self) -> str: ...
     @property
