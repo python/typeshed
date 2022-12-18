14d09e90b288ba15c51acdf2361a8cf7989816a0
Alex Waygood <Alex.Waygood@Gmail.com>
2022-12-18 16:55:41 +0000

    Revert changes to `__init__.py`

--- stubs/python-dateutil/dateutil/__init__.pyi
+++ stubs/python-dateutil/dateutil/__init__.pyi
@@ -1,14 +1,1 @@
-# These are all lazy-loaded on demand at runtime
-from . import (
-    easter as easter,
-    parser as parser,
-    relativedelta as relativedelta,
-    rrule as rrule,
-    tz as tz,
-    utils as utils,
-    zoneinfo as zoneinfo,
-)
 
-__all__ = ["easter", "parser", "relativedelta", "rrule", "tz", "utils", "zoneinfo"]
-
-def __dir__() -> list[str]: ...
