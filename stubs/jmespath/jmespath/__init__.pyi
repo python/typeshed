0659e3c5748abac5e83dc7179f489714d480e46d
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-12-21 13:02:33 +0100

    Fix return type of `search` in jmespath stubs (#9394)

--- stubs/jmespath/jmespath/__init__.pyi
+++ stubs/jmespath/jmespath/__init__.pyi
@@ -1,7 +1,7 @@
 from typing import Any
 
 from jmespath import parser as parser
-from jmespath.visitor import Options as Options, _TreeNode
+from jmespath.visitor import Options as Options
 
 def compile(expression: str) -> parser.ParsedResult: ...
-def search(expression: str, data: Any, options: Options | None = ...) -> _TreeNode: ...
+def search(expression: str, data: Any, options: Options | None = ...) -> Any: ...
