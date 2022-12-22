40a780520f99c309c5a914a7158b3e28dc0151a6
Eugene Vilensky <eugene.vilensky@cambiahealth.com>
2022-12-22 08:13:56 -0600

    set session.headers to MutableMapping or None

--- stubs/requests/requests/sessions.pyi
+++ stubs/requests/requests/sessions.pyi
@@ -115,7 +115,7 @@ class Session(SessionRedirectMixin):
     __attrs__: Any
     # See https://github.com/psf/requests/issues/5020#issuecomment-989082461:
     # requests sets this as a CaseInsensitiveDict, but users may set it to any MutableMapping
-    headers: MutableMapping[str, str | bytes | None]
+    headers: MutableMapping[str, str | bytes ] | None
     auth: _Auth | None
     proxies: _TextMapping
     # Don't complain if:
