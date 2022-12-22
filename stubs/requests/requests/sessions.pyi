d1ac37a66114eee915d8bcd4f459ac8313eceb04
Eugene Vilensky <evilensky@gmail.com>
2022-12-22 08:49:34 -0600

    remove comment about none

--- stubs/requests/requests/sessions.pyi
+++ stubs/requests/requests/sessions.pyi
@@ -115,7 +115,6 @@ class Session(SessionRedirectMixin):
     __attrs__: Any
     # See https://github.com/psf/requests/issues/5020#issuecomment-989082461:
     # requests sets this as a CaseInsensitiveDict, but users may set it to any MutableMapping
-    # or None.
     headers: MutableMapping[str, str | bytes]
     auth: _Auth | None
     proxies: _TextMapping
