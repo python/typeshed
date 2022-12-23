b05a526d85cc36a1e1c455091f3c60063dd15914
Sebastian Rittau <srittau@rittau.biz>
2022-12-23 12:09:44 +0100

    General cleanups

--- stubs/ExifRead/exifread/__init__.pyi
+++ stubs/ExifRead/exifread/__init__.pyi
@@ -1,16 +1,7 @@
 from _typeshed import Incomplete
-from typing import BinaryIO
-
-from exifread.utils import make_string as make_string
 
 logger: Incomplete
 
 def process_file(
-    fh: BinaryIO,
-    stop_tag=...,
-    details: bool = ...,
-    strict: bool = ...,
-    debug: bool = ...,
-    truncate_tags: bool = ...,
-    auto_seek: bool = ...,
+    fh, stop_tag=..., details: bool = ..., strict: bool = ..., debug: bool = ..., truncate_tags: bool = ..., auto_seek: bool = ...
 ): ...
--- stubs/ExifRead/exifread/jpeg.pyi
+++ stubs/ExifRead/exifread/jpeg.pyi
@@ -1,6 +1,5 @@
 from _typeshed import Incomplete
-from typing import BinaryIO
 
 logger: Incomplete
 
-def find_jpeg_exif(fh: BinaryIO, data, fake_exif) -> tuple: ...
+def find_jpeg_exif(fh, data, fake_exif) -> tuple[Incomplete, Incomplete, Incomplete]: ...
