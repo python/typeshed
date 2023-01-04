b6dc873d6cd39b73de5e9b2eac98a5ea6e106da5
Spencer Brown <spencerb21@live.com>
2023-01-04 18:17:23 +1000

    Add complete stubs for babel.messages.extract

--- stubs/babel/babel/messages/extract.pyi
+++ stubs/babel/babel/messages/extract.pyi
@@ -1,30 +1,72 @@
-from collections.abc import Callable
-from typing import Any
+from collections.abc import Callable, Iterable, Collection
+from typing import Any, Protocol, TypedDict, AnyStr
+from os import PathLike
+from _typeshed import SupportsRead, SupportsReadline
+
+_Keyword: tuple[int | tuple[int, int] | tuple[int, str], ...]
 
 GROUP_NAME: str
-DEFAULT_KEYWORDS: Any
-DEFAULT_MAPPING: Any
+DEFAULT_KEYWORDS: dict[str, _Keyword]
+DEFAULT_MAPPING: list[tuple[str, str]]
 empty_msgid_warning: str
 
 def extract_from_dir(
-    dirname: Any | None = ...,
-    method_map=...,
+    dirname: AnyStr | PathLike[AnyStr] | None = ...,
+    method_map: list[tuple[str, str]] = ...,
     options_map: Any | None = ...,
-    keywords=...,
-    comment_tags=...,
-    callback: Any | None = ...,
+    keywords: dict[str, _Keyword] = ...,
+    comment_tags: Collection[str] = ...,
+    callback: Callable[[AnyStr, str, dict[str, Any]]] | None = ...,
     strip_comment_tags: bool = ...,
     directory_filter: Callable[[str], bool] | None = ...,
-) -> None: ...
+) -> list[tuple[AnyStr, int, str | tuple[str, ...], list[str], str | None]]: ...
 def check_and_call_extract_file(
-    filepath, method_map, options_map, callback, keywords, comment_tags, strip_comment_tags, dirpath: Any | None = ...
-) -> None: ...
+    filepath: AnyStr | PathLike,
+    method_map: list[tuple[str, str]],
+    options_map: dict[str, dict[str, Any]],
+    callback: Callable[[AnyStr, str, dict[str, Any]]] | None,
+    keywords: dict[str, _Keyword],
+    comment_tags: Collection[str],
+    strip_comment_tags,
+    dirpath: Any | None = ...,
+) -> list[tuple[AnyStr, int, str | tuple[str, ...], list[str], str | None]]: ...
 def extract_from_file(
-    method, filename, keywords=..., comment_tags=..., options: Any | None = ..., strip_comment_tags: bool = ...
-): ...
+    method,
+    filename: AnyStr | PathLike,
+    keywords: dict[str, _Keyword] = ...,
+    comment_tags: Collection[str] = ...,
+    options: dict[str, Any] | None = ...,
+    strip_comment_tags: bool = ...,
+) -> list[tuple[AnyStr, int, str | tuple[str, ...], list[str], str | None]]: ...
+
+class _FileObj(SupportsRead[bytes], SupportsReadline[bytes], Protocol):
+    def seek(self, __offset: int, __whence: int = ...) -> int: ...
+    def tell(self) -> int: ...
+
 def extract(
-    method, fileobj, keywords=..., comment_tags=..., options: Any | None = ..., strip_comment_tags: bool = ...
-) -> None: ...
-def extract_nothing(fileobj, keywords, comment_tags, options): ...
-def extract_python(fileobj, keywords, comment_tags, options): ...
-def extract_javascript(fileobj, keywords, comment_tags, options) -> None: ...
+    method,
+    fileobj: _FileObj,
+    keywords: dict[str, _Keyword] = ...,
+    comment_tags: Collection[str] = ...,
+    options: dict[str, Any] | None = ...,
+    strip_comment_tags: bool = ...,
+) -> Iterable[tuple[int, str | tuple[str, ...], list[str], str | None]]: ...
+def extract_nothing(
+    fileobj: _FileObj, keywords: dict[str, _Keyword], comment_tags: Collection[str], options: dict[str, Any]
+) -> Iterable[tuple[int, str | tuple[str, ...], list[str], str | None]]: ...
+
+class _PyOptions(TypedDict, total=False):
+    encoding: str
+
+def extract_python(
+    fileobj: _FileObj, keywords: dict[str, _Keyword], comment_tags: Collection[str], options: _PyOptions
+) -> Iterable[tuple[int, str | tuple[str, ...], list[str], str | None]]: ...
+
+class _JSOptions(TypedDict, total=False):
+    encoding: str
+    jsx: bool
+    template_string: bool
+
+def extract_javascript(
+    fileobj: _FileObj, keywords: dict[str, _Keyword], comment_tags: Collection[str], options: _JSOptions
+) -> Iterable[tuple[int, str | tuple[str, ...], list[str], str | None]]: ...
