07ad378e64546f621c6e75d48a6b420e50c00b18
Avasam <samuel.06@hotmail.com>
2023-01-11 06:08:15 -0500

    Removed stubs/SQLAlchemy/sqlalchemy/ext/mypy

--- stubs/SQLAlchemy/sqlalchemy/ext/mypy/decl_class.pyi
+++
@@ -1,8 +0,0 @@
-# mypy_test can't find mypy import
-from mypy.plugin import SemanticAnalyzerPluginInterface  # type: ignore[import]
-
-from . import util
-
-def scan_declarative_assignments_and_apply_types(
-    cls, api: SemanticAnalyzerPluginInterface, is_mixin_scan: bool = ...
-) -> list[util.SQLAlchemyAttribute] | None: ...
