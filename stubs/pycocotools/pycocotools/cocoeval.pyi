5302580b21fd2766657bb1f66a2f42dfff3ea7dc
AlexWaygood <alex.waygood@gmail.com>
2023-01-13 00:25:20 +0000

    `pycocotools`: Add `numpy` as a dependency

--- stubs/pycocotools/pycocotools/cocoeval.pyi
+++ stubs/pycocotools/pycocotools/cocoeval.pyi
@@ -1,13 +1,10 @@
-from _typeshed import Incomplete
 from typing_extensions import Literal, TypeAlias, TypedDict
+
+import numpy
+import numpy.typing
 
 from .coco import COCO
 
-# TODO: Use numpy types when #5768 is resolved.
-# import numpy as np
-# import numpy.typing as npt
-
-_NDArray: TypeAlias = Incomplete
 _TIOU: TypeAlias = Literal["segm", "bbox", "keypoints"]
 
 class _EvaluationResult(TypedDict):
@@ -17,15 +14,11 @@ class _EvaluationResult(TypedDict):
     maxDet: int
     dtIds: list[int]
     gtIds: list[int]
-    dtMatches: _NDArray
-    # dtMatches: npt.NDArray[np.float64]
-    gtMatches: _NDArray
-    # gtMatches: npt.NDArray[np.float64]
+    dtMatches: numpy.typing.NDArray[numpy.float64]
+    gtMatches: numpy.typing.NDArray[numpy.float64]
     dtScores: list[float]
-    gtIgnore: _NDArray
-    # gtIgnore: npt.NDArray[np.float64]
-    dtIgnore: _NDArray
-    # dtIgnore: npt.NDArray[np.float64]
+    gtIgnore: numpy.typing.NDArray[numpy.float64]
+    dtIgnore: numpy.typing.NDArray[numpy.float64]
 
 class COCOeval:
     cocoGt: COCO
@@ -33,14 +26,12 @@ class COCOeval:
     evalImgs: list[_EvaluationResult]
     eval: _EvaluationResult
     params: Params
-    stats: _NDArray
-    # stats: npt.NDArray[np.float64]
+    stats: numpy.typing.NDArray[numpy.float64]
     ious: dict[tuple[int, int], list[float]]
     def __init__(self, cocoGt: COCO | None = ..., cocoDt: COCO | None = ..., iouType: _TIOU = ...) -> None: ...
     def evaluate(self) -> None: ...
     def computeIoU(self, imgId: int, catId: int) -> list[float]: ...
-    def computeOks(self, imgId: int, catId: int) -> _NDArray: ...
-    # def computeOks(self, imgId: int, catId: int) -> npt.NDArray[np.float64]: ...
+    def computeOks(self, imgId: int, catId: int) -> numpy.typing.NDArray[numpy.float64]: ...
     def evaluateImg(self, imgId: int, catId: int, aRng: list[int], maxDet: int) -> _EvaluationResult: ...
     def accumulate(self, p: Params | None = ...) -> None: ...
     def summarize(self) -> None: ...
@@ -48,16 +39,13 @@ class COCOeval:
 class Params:
     imgIds: list[int]
     catIds: list[int]
-    iouThrs: _NDArray
-    # iouThrs: npt.NDArray[np.float64]
-    recThrs: _NDArray
-    # recThrs: npt.NDArray[np.float64]
+    iouThrs: numpy.typing.NDArray[numpy.float64]
+    recThrs: numpy.typing.NDArray[numpy.float64]
     maxDets: list[int]
     areaRng: list[int]
     areaRngLbl: list[str]
     useCats: int
-    kpt_oks_sigmas: _NDArray
-    # kpt_oks_sigmas: npt.NDArray[np.float64]
+    kpt_oks_sigmas: numpy.typing.NDArray[numpy.float64]
     iouType: _TIOU
     useSegm: int | None
     def __init__(self, iouType: _TIOU = ...) -> None: ...
