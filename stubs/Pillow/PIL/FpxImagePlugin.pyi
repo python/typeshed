99f9a5fe1af2de906a361ad2ec0551adb9c66690
Avasam <samuel.06@hotmail.com>
2023-01-14 05:27:13 -0500

    stubtest fixes in pillow

--- stubs/Pillow/PIL/FpxImagePlugin.pyi
+++ stubs/Pillow/PIL/FpxImagePlugin.pyi
@@ -1,12 +1,16 @@
 from typing import Any, ClassVar
-from typing_extensions import Literal
+from typing_extensions import Literal, TypeAlias
 
 from .ImageFile import ImageFile
 
-MODES: Any
+_OleFileIO: TypeAlias = Any  # olefile.OleFileIO
+_OleStream: TypeAlias = Any  # olefile.OleStream
+
+MODES: dict[tuple[int, ...], tuple[str, str]]
 
 class FpxImageFile(ImageFile):
+    ole: _OleFileIO
     format: ClassVar[Literal["FPX"]]
     format_description: ClassVar[str]
-    fp: Any
+    fp: _OleStream | None
     def load(self): ...
--- stubs/Pillow/PIL/MicImagePlugin.pyi
+++ stubs/Pillow/PIL/MicImagePlugin.pyi
@@ -1,11 +1,15 @@
 from typing import Any, ClassVar
-from typing_extensions import Literal
+from typing_extensions import Literal, TypeAlias
 
 from .TiffImagePlugin import TiffImageFile
 
+_OleFileIO: TypeAlias = Any  # olefile.OleFileIO
+_OleStream: TypeAlias = Any  # olefile.OleStream
+
 class MicImageFile(TiffImageFile):
+    ole: _OleFileIO
     format: ClassVar[Literal["MIC"]]
-    fp: Any
-    frame: Any
-    def seek(self, frame) -> None: ...
-    def tell(self): ...
+    fp: _OleStream
+    frame: int
+    def seek(self, frame: int) -> None: ...
+    def tell(self) -> int: ...
