cac3d488bfaeb7755804752b1154ab7c7de7c97d
AlexWaygood <alex.waygood@gmail.com>
2023-01-26 18:37:04 +0000

    PEP 688

--- test_cases/stdlib/check_re.py
+++ test_cases/stdlib/check_re.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import mmap
 import re
 import typing as t
 from typing_extensions import assert_type
@@ -9,6 +10,7 @@ def check_search(str_pat: re.Pattern[str], bytes_pat: re.Pattern[bytes]) -> None
     assert_type(str_pat.search("x"), t.Optional[t.Match[str]])
     assert_type(bytes_pat.search(b"x"), t.Optional[t.Match[bytes]])
     assert_type(bytes_pat.search(bytearray(b"x")), t.Optional[t.Match[bytes]])
+    assert_type(bytes_pat.search(mmap.mmap(0, 10)), t.Optional[t.Match[bytes]])
 
 
 def check_search_with_AnyStr(pattern: re.Pattern[t.AnyStr], string: t.AnyStr) -> re.Match[t.AnyStr]:
@@ -21,3 +23,4 @@ def check_search_with_AnyStr(pattern: re.Pattern[t.AnyStr], string: t.AnyStr) ->
 
 def check_no_ReadableBuffer_false_negatives() -> None:
     re.compile("foo").search(bytearray(b"foo"))  # type: ignore
+    re.compile("foo").search(mmap.mmap(0, 10))  # type: ignore
