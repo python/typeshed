897752e09966ad801aeefbb2f1b9eaf4e3638dda
AlexWaygood <alex.waygood@gmail.com>
2023-01-26 17:47:36 +0000

    Move test cases around

--- test_cases/stdlib/check_re.py
+++ test_cases/stdlib/check_re.py
@@ -5,7 +5,13 @@ import typing as t
 from typing_extensions import assert_type
 
 
-def check_re_search(pattern: re.Pattern[t.AnyStr], string: t.AnyStr) -> re.Match[t.AnyStr]:
+def check_search(str_pat: re.Pattern[str], bytes_pat: re.Pattern[bytes]) -> None:
+    assert_type(str_pat.search("x"), t.Optional[t.Match[str]])
+    assert_type(bytes_pat.search(b"x"), t.Optional[t.Match[bytes]])
+    assert_type(bytes_pat.search(bytearray(b"x")), t.Optional[t.Match[bytes]])
+
+
+def check_search_with_AnyStr(pattern: re.Pattern[t.AnyStr], string: t.AnyStr) -> re.Match[t.AnyStr]:
     """See issue #9591"""
     match = pattern.search(string)
     if match is None:
@@ -18,6 +24,3 @@ def check_no_ReadableBuffer_false_negatives() -> None:
 
     string_pattern = re.compile("foo")
     string_pattern.search(b)  # type: ignore
-
-    bytes_pattern = re.compile(b"foo")
-    assert_type(bytes_pattern.search(b), t.Optional[t.Match[bytes]])
