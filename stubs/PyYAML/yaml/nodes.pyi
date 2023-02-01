27b9a2bd11b921d3c43cd5fd1da0399872dbc201
Avasam <samuel.06@hotmail.com>
2023-02-01 13:49:57 -0500

    Fix primer issue

--- stubs/PyYAML/yaml/nodes.pyi
+++ stubs/PyYAML/yaml/nodes.pyi
@@ -9,19 +9,19 @@ from yaml.error import Mark
 class Node:
     tag: str
     value: Incomplete
-    start_mark: Mark | None | Any
-    end_mark: Mark | None | Any
+    start_mark: Mark | Any
+    end_mark: Mark | Any
     def __init__(self, tag: str, value, start_mark: Mark | None, end_mark: Mark | None) -> None: ...
 
 class ScalarNode(Node):
     id: ClassVar[str]
-    style: str | None | Any
+    style: str | Any
     def __init__(
         self, tag: str, value, start_mark: Mark | None = ..., end_mark: Mark | None = ..., style: str | None = ...
     ) -> None: ...
 
 class CollectionNode(Node):
-    flow_style: bool | None | Any
+    flow_style: bool | Any
     def __init__(
         self, tag: str, value, start_mark: Mark | None = ..., end_mark: Mark | None = ..., flow_style: bool | None = ...
     ) -> None: ...
