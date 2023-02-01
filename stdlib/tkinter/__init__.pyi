7cd7ae46c018c342cdf9d0c8997a017825b4a6ed
Alex Waygood <Alex.Waygood@Gmail.com>
2023-02-01 13:02:26 +0000

    Improve a few version-dependent tkinter functions (#9637)

--- stdlib/tkinter/__init__.pyi
+++ stdlib/tkinter/__init__.pyi
@@ -736,8 +736,8 @@ class Tk(Misc, Wm):
         screenName: str | None = None,
         baseName: str | None = None,
         className: str = "Tk",
-        useTk: bool = ...,
-        sync: bool = ...,
+        useTk: bool = True,
+        sync: bool = False,
         use: str | None = None,
     ) -> None: ...
     @overload
@@ -800,7 +800,7 @@ class Tk(Misc, Wm):
     def wantobjects(self, *args, **kwargs): ...
     def willdispatch(self): ...
 
-def Tcl(screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = ...) -> Tk: ...
+def Tcl(screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = False) -> Tk: ...
 
 _InMiscTotal = TypedDict("_InMiscTotal", {"in": Misc})
 _InMiscNonTotal = TypedDict("_InMiscNonTotal", {"in": Misc}, total=False)
