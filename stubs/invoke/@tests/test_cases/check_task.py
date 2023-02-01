0891ef1633639a523b8659d5b275924e9040c3b0
AlexWaygood <alex.waygood@gmail.com>
2023-02-01 15:19:21 +0000

    Add some unnecessary `type: ignore`s to some test cases to show that pyright now fails CI

--- stubs/invoke/@tests/test_cases/check_task.py
+++ stubs/invoke/@tests/test_cases/check_task.py
@@ -13,5 +13,5 @@ def docker_build(context: Context) -> None:
 
 
 @task(docker_build)
-def docker_push(context: Context) -> None:
+def docker_push(context: Context) -> None:  # type: ignore
     pass
