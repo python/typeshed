140bba3425229a69cf4d5ccf7f5348ff85f66c16
Avasam <samuel.06@hotmail.com>
2023-02-14 08:11:56 -0500

    Add comments when subclassing Any (#9732)

--- stubs/mock/mock/mock.pyi
+++ stubs/mock/mock/mock.pyi
@@ -81,6 +81,8 @@ class _CallList(list[_Call]):
 class Base:
     def __init__(self, *args: Any, **kwargs: Any) -> None: ...
 
+# We subclass with "Any" because mocks are explicitly designed to stand in for other types,
+# something that can't be expressed with our static type system.
 class NonCallableMock(Base, Any):
     def __new__(
         cls: type[Self],
