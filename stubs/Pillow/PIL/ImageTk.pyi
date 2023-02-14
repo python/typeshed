7f277f0a95e523d69479b1829ce1c0764bc62e94
Spencer Brown <spencerb21@live.com>
2023-02-14 18:30:09 +1000

    Add tk attribute to PIL.ImageTk.BitmapImage

--- stubs/Pillow/PIL/ImageTk.pyi
+++ stubs/Pillow/PIL/ImageTk.pyi
@@ -1,6 +1,6 @@
 import _tkinter
 import tkinter
-from _typeshed import Incomplete, ReadableBuffer, SupportsRead
+from _typeshed import Incomplete, ReadableBuffer, StrOrBytesPath, SupportsRead
 from typing import overload
 
 from PIL.Image import Image, _Box, _Mode, _Size
@@ -28,6 +28,7 @@ class PhotoImage:
     def paste(self, im: Image, box: _Box | None = ...) -> None: ...
 
 class BitmapImage:
+    tk: _tkinter.TkappType  # Not actually present, but required for tkinter._Image protocol
     def __init__(self, image: Image | None = ..., **kw: Incomplete) -> None: ...
     def __del__(self) -> None: ...
     def width(self) -> int: ...
