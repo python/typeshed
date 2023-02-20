5f205bfe22cbfd9f2c0d08850a47d68368447d5f
AlexWaygood <alex.waygood@gmail.com>
2023-02-20 15:22:33 +0000

    Do Sebastian's suggestion

--- stdlib/builtins.pyi
+++ stdlib/builtins.pyi
@@ -1394,7 +1394,7 @@ def iter(__function: Callable[[], _T | None], __sentinel: None) -> Iterator[_T]:
 @overload
 def iter(__function: Callable[[], _T], __sentinel: object) -> Iterator[_T]: ...
 
-# Keep this alias in sync with unittest.case._IsInstanceClassInfo
+# Keep this alias in sync with unittest.case._ClassInfo
 if sys.version_info >= (3, 10):
     _ClassInfo: TypeAlias = type | types.UnionType | tuple[_ClassInfo, ...]
 else:
