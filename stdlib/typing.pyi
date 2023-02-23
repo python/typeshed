6ba28ae54707db9be1a89119bd6eebcc9237395b
Alex Waygood <Alex.Waygood@Gmail.com>
2023-02-23 20:59:50 +0000

    Remove unused `type: ignore` comments (#9801)

--- stdlib/typing.pyi
+++ stdlib/typing.pyi
@@ -254,7 +254,7 @@ _T_contra = TypeVar("_T_contra", contravariant=True)  # Ditto contravariant.
 _TC = TypeVar("_TC", bound=Type[object])
 
 def no_type_check(arg: _F) -> _F: ...
-def no_type_check_decorator(decorator: Callable[_P, _T]) -> Callable[_P, _T]: ...  # type: ignore[misc]
+def no_type_check_decorator(decorator: Callable[_P, _T]) -> Callable[_P, _T]: ...
 
 # Type aliases and type constructors
 
