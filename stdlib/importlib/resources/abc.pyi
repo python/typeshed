b71f32dfb5b825ff170c632cc1c0d5810afb0397
Alex Waygood <Alex.Waygood@Gmail.com>
2023-02-27 15:39:37 +0000

    spelling

--- stdlib/importlib/resources/abc.pyi
+++ stdlib/importlib/resources/abc.pyi
@@ -4,7 +4,7 @@ if sys.version_info >= (3, 11):
     # These are all actually defined in this file on 3.11+,
     # and re-exported from importlib.abc,
     # but it's much less code duplication for typeshed if we pretend that they're still defined
-    # in `mportlib.abc` on 3.11+, and re-exported from this file
+    # in importlib.abc on 3.11+, and re-exported from this file
     from importlib.abc import (
         ResourceReader as ResourceReader,
         Traversable as Traversable,
