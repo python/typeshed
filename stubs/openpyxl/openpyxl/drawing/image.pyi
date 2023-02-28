d1e618e5b419c39a250217bd98f4c055fcaf7cf2
Avasam <samuel.06@hotmail.com>
2023-02-27 19:19:42 -0500

    Apply suggestions from code review
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- stubs/openpyxl/openpyxl/drawing/image.pyi
+++ stubs/openpyxl/openpyxl/drawing/image.pyi
@@ -3,7 +3,6 @@ from typing_extensions import Literal
 
 from PIL import Image as ImageModule
 
-# Using "object" because modules can't by used as type
 PILImage: ModuleType | Literal[False]
 
 class Image:
