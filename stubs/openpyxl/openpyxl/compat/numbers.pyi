bfb6c60260dc07078aa6bb589f59c639ba08058a
Avasam <samuel.06@hotmail.com>
2023-02-27 22:43:13 -0500

    Make stubuploader tests pass

--- stubs/openpyxl/openpyxl/compat/numbers.pyi
+++ stubs/openpyxl/openpyxl/compat/numbers.pyi
@@ -1,18 +1,17 @@
-import sys
 from decimal import Decimal
-from typing import Any
 from typing_extensions import TypeAlias
 
-import numpy
+# NOTE: Can't specify numpy as a dependency because openpyxl doesn't declare it as one
+# import numpy
+# if sys.version_info >= (3, 8):
+#     import numpy._typing
 
-if sys.version_info >= (3, 8):
-    import numpy._typing
+#     _NBitBase: TypeAlias = numpy._typing.NBitBase
+# else:
+#     _NBitBase: TypeAlias = Any
+# _NumericTypes: TypeAlias = int | float | Decimal | numpy.bool_ | numpy.floating[_NBitBase] | numpy.integer[_NBitBase]
 
-    _NBitBase: TypeAlias = numpy._typing.NBitBase
-else:
-    _NBitBase: TypeAlias = Any
-
-_NumericTypes: TypeAlias = int | float | Decimal | numpy.bool_ | numpy.floating[_NBitBase] | numpy.integer[_NBitBase]
+_NumericTypes: TypeAlias = int | float | Decimal
 NUMERIC_TYPES: tuple[type[_NumericTypes], ...]
 
 NUMPY: bool
--- stubs/openpyxl/openpyxl/drawing/image.pyi
+++ stubs/openpyxl/openpyxl/drawing/image.pyi
@@ -1,13 +1,15 @@
 from types import ModuleType
-from typing_extensions import Literal
+from typing import Any
+from typing_extensions import Literal, TypeAlias
 
-from PIL import Image as ImageModule
+# Is actually PIL.Image.Image
+_PILImageImage: TypeAlias = Any
 
 PILImage: ModuleType | Literal[False]
 
 class Image:
     anchor: str
-    ref: ImageModule.Image | str
+    ref: _PILImageImage | str
     format: str
     def __init__(self, img) -> None: ...
     @property
