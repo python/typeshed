7f10fc821abc40a7be0e0a19789b329235a2fcf4
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-03-09 19:42:54 -0800

    no picklebuffer in 3.7

--- test_cases/stdlib/typing/check_io.py
+++ test_cases/stdlib/typing/check_io.py
@@ -1,8 +1,8 @@
-import pickle
+import mmap
 from typing import IO, AnyStr
 
 
-def check_write(io_bytes: IO[bytes], io_str: IO[str], io_anystr: IO[AnyStr], any_str: AnyStr, buf: pickle.PickleBuffer) -> None:
+def check_write(io_bytes: IO[bytes], io_str: IO[str], io_anystr: IO[AnyStr], any_str: AnyStr, buf: mmap.mmap) -> None:
     io_bytes.write(b"")
     io_bytes.write(buf)
     io_bytes.write("")  # type: ignore
