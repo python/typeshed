6977474fedd8b5ccc3c03363845915d68750ace6
Tom Parker-Shemilt <palfrey@tevp.net>
2023-03-18 13:55:41 +0000

    [django-filter] Explicitly export all the filters from the rest_framework module

--- stubs/django-filter/django_filters/rest_framework/filters.pyi
+++ stubs/django-filter/django_filters/rest_framework/filters.pyi
@@ -1,37 +1,70 @@
 from typing import Any
 
-from django_filters import filters
+from django_filters import (
+    AllValuesFilter,
+    AllValuesMultipleFilter,
+    BaseCSVFilter,
+    BaseInFilter,
+    BaseRangeFilter,
+    CharFilter,
+    ChoiceFilter,
+    DateFilter,
+    DateFromToRangeFilter,
+    DateRangeFilter,
+    DateTimeFilter,
+    DateTimeFromToRangeFilter,
+    DurationFilter,
+    Filter,
+    IsoDateTimeFilter,
+    IsoDateTimeFromToRangeFilter,
+    LookupChoiceFilter,
+    ModelChoiceFilter,
+    ModelMultipleChoiceFilter,
+    MultipleChoiceFilter,
+    NumberFilter,
+    NumericRangeFilter,
+    OrderingFilter,
+    RangeFilter,
+    TimeFilter,
+    TimeRangeFilter,
+    TypedChoiceFilter,
+    TypedMultipleChoiceFilter,
+    UUIDFilter,
+    filters,
+)
 
 class BooleanFilter(filters.BooleanFilter):
     def __init__(self, *args: Any, **kwargs: Any) -> None: ...
 
-# Names in __all__ with no definition:
-#   AllValuesFilter
-#   AllValuesMultipleFilter
-#   BaseCSVFilter
-#   BaseInFilter
-#   BaseRangeFilter
-#   CharFilter
-#   ChoiceFilter
-#   DateFilter
-#   DateFromToRangeFilter
-#   DateRangeFilter
-#   DateTimeFilter
-#   DateTimeFromToRangeFilter
-#   DurationFilter
-#   Filter
-#   IsoDateTimeFilter
-#   IsoDateTimeFromToRangeFilter
-#   LookupChoiceFilter
-#   ModelChoiceFilter
-#   ModelMultipleChoiceFilter
-#   MultipleChoiceFilter
-#   NumberFilter
-#   NumericRangeFilter
-#   OrderingFilter
-#   RangeFilter
-#   TimeFilter
-#   TimeRangeFilter
-#   TypedChoiceFilter
-#   TypedMultipleChoiceFilter
-#   UUIDFilter
+__all__ = [
+    "BooleanFilter",
+    "AllValuesFilter",
+    "AllValuesMultipleFilter",
+    "BaseCSVFilter",
+    "BaseInFilter",
+    "BaseRangeFilter",
+    "CharFilter",
+    "ChoiceFilter",
+    "DateFilter",
+    "DateFromToRangeFilter",
+    "DateRangeFilter",
+    "DateTimeFilter",
+    "DateTimeFromToRangeFilter",
+    "DurationFilter",
+    "Filter",
+    "IsoDateTimeFilter",
+    "IsoDateTimeFromToRangeFilter",
+    "LookupChoiceFilter",
+    "ModelChoiceFilter",
+    "ModelMultipleChoiceFilter",
+    "MultipleChoiceFilter",
+    "NumberFilter",
+    "NumericRangeFilter",
+    "OrderingFilter",
+    "RangeFilter",
+    "TimeFilter",
+    "TimeRangeFilter",
+    "TypedChoiceFilter",
+    "TypedMultipleChoiceFilter",
+    "UUIDFilter",
+]
