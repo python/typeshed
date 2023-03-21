d9248401610543e80674559fc2e05de45a656145
Nikita Sobolev <mail@sobolevn.me>
2023-03-21 19:36:11 +0300

    Update datetime.pyi

--- stdlib/datetime.pyi
+++ stdlib/datetime.pyi
@@ -1,9 +1,8 @@
 import sys
-from _typeshed import SupportsIndex
 from abc import abstractmethod
 from time import struct_time
 from typing import ClassVar, NamedTuple, NoReturn, TypeVar, overload
-from typing_extensions import Literal, Self, TypeAlias, final
+from typing_extensions import Literal, Self, SupportsIndex, TypeAlias, final
 
 if sys.version_info >= (3, 11):
     __all__ = ("date", "datetime", "time", "timedelta", "timezone", "tzinfo", "MINYEAR", "MAXYEAR", "UTC")
