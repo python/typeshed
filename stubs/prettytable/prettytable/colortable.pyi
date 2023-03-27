6d6c70d75b6cd70c2679a45fc842a828d79af9ab
AlexWaygood <alex.waygood@gmail.com>
2023-03-27 17:57:17 +0100

    Run stubdefaulter

--- stubs/prettytable/prettytable/colortable.pyi
+++ stubs/prettytable/prettytable/colortable.pyi
@@ -18,13 +18,13 @@ class Theme:
     junction_color: str
     def __init__(
         self,
-        default_color: str = ...,
-        vertical_char: str = ...,
-        vertical_color: str = ...,
-        horizontal_char: str = ...,
-        horizontal_color: str = ...,
-        junction_char: str = ...,
-        junction_color: str = ...,
+        default_color: str = '',
+        vertical_char: str = '|',
+        vertical_color: str = '',
+        horizontal_char: str = '-',
+        horizontal_color: str = '',
+        junction_char: str = '+',
+        junction_color: str = '',
     ) -> None: ...
     # The following method is broken in upstream code.
     def format_code(s: str) -> str: ...  # type: ignore[misc]  # pyright: ignore[reportGeneralTypeIssues]
@@ -34,7 +34,7 @@ class Themes:
     OCEAN: ClassVar[Theme]
 
 class ColorTable(PrettyTable):
-    def __init__(self, field_names: Incomplete | None = ..., **kwargs) -> None: ...
+    def __init__(self, field_names: Incomplete | None = None, **kwargs) -> None: ...
     @property
     def theme(self) -> Theme: ...
     @theme.setter
