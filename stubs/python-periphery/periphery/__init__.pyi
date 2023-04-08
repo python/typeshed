7d2f98143f42f0a89551aa34f75489b7a9127f24
Juho Kim <juho-kim@outlook.com>
2023-04-08 04:38:34 -0400

    Create baseline stubs for python-periphery

---
+++ stubs/python-periphery/periphery/__init__.pyi
@@ -0,0 +1,21 @@
+from _typeshed import Incomplete
+
+from periphery.gpio import (
+    GPIO as GPIO,
+    CdevGPIO as CdevGPIO,
+    EdgeEvent as EdgeEvent,
+    GPIOError as GPIOError,
+    SysfsGPIO as SysfsGPIO,
+)
+from periphery.i2c import I2C as I2C, I2CError as I2CError
+from periphery.led import LED as LED, LEDError as LEDError
+from periphery.mmio import MMIO as MMIO, MMIOError as MMIOError
+from periphery.pwm import PWM as PWM, PWMError as PWMError
+from periphery.serial import Serial as Serial, SerialError as SerialError
+from periphery.spi import SPI as SPI, SPIError as SPIError
+
+version: Incomplete
+
+def sleep(seconds) -> None: ...
+def sleep_ms(milliseconds) -> None: ...
+def sleep_us(microseconds) -> None: ...
