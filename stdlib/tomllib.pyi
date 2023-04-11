d82f697cf2045d0c84c546c380320dfe35fbef95
Alex Waygood <Alex.Waygood@Gmail.com>
2023-04-11 11:54:18 +0100

    CI fixes

--- stdlib/tomllib.pyi
+++ stdlib/tomllib.pyi
@@ -1,13 +1,14 @@
 from _typeshed import SupportsRead
 from collections.abc import Callable
 from datetime import date, datetime, time
-from typing import Any, TypeAlias
+from typing import Any
+from typing_extensions import TypeAlias
 
 __all__ = ("loads", "load", "TOMLDecodeError")
 
 class TOMLDecodeError(ValueError): ...
 
-TOMLValue: TypeAlias = int | float | str | bool | list["TOMLValue"] | dict["TOMLValue", "TOMLValue"] | datetime | date | time
+_TOMLValue: TypeAlias = int | float | str | bool | list[_TOMLValue] | dict[_TOMLValue, _TOMLValue] | datetime | date | time
 
-def load(__fp: SupportsRead[bytes], *, parse_float: Callable[[str], Any] = ...) -> dict[str, TOMLValue]: ...
-def loads(__s: str, *, parse_float: Callable[[str], Any] = ...) -> dict[str, TOMLValue]: ...
+def load(__fp: SupportsRead[bytes], *, parse_float: Callable[[str], Any] = ...) -> dict[str, _TOMLValue]: ...
+def loads(__s: str, *, parse_float: Callable[[str], Any] = ...) -> dict[str, _TOMLValue]: ...
