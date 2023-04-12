f0d85854567ddf35595dfae337d979bd5550267c
semen603089 <semen603089@mail.ru>
2023-04-12 05:24:42 +0300

    flake8 fix

--- stdlib/zipfile.pyi
+++ stdlib/zipfile.pyi
@@ -222,9 +222,6 @@ class ZipInfo:
     def is_dir(self) -> bool: ...
     def FileHeader(self, zip64: bool | None = None) -> bytes: ...
 
-class _PathOpenProtocol(Protocol):
-    def __call__(self, mode: _ReadWriteMode = ..., pwd: bytes | None = ..., *, force_zip64: bool = ...) -> IO[bytes]: ...
-
 if sys.version_info >= (3, 8):
     class Path:
         @property
