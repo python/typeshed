4ab2e6faab5b1caf906f72b90405812f141b0f0b
hauntsaninja <hauntsaninja@gmail.com>
2023-04-20 23:22:24 -0600

    fix

--- stubs/setuptools/setuptools/__init__.pyi
+++ stubs/setuptools/setuptools/__init__.pyi
@@ -2,7 +2,6 @@ from abc import abstractmethod
 from collections.abc import Iterable, Mapping, Sequence
 from typing import Any
 
-from setuptools._deprecation_warning import SetuptoolsDeprecationWarning as SetuptoolsDeprecationWarning
 from setuptools.depends import Require as Require
 from setuptools.dist import Distribution as Distribution
 from setuptools.extension import Extension as Extension
