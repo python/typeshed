ccafb9362b09173d5854876f0115d848db358a80
Wesley Collin Wright <wesleyw@dropbox.com>
2023-04-22 22:17:18 +0000

    fix toxml => toprettyxml in test cases

--- test_cases/stdlib/check_xml.py
+++ test_cases/stdlib/check_xml.py
@@ -20,12 +20,12 @@ if sys.version_info >= (3, 9):
 # determines the return type, the proper stub typing isn't immediately obvious. This is a basic
 # brute-force sanity check.
 # Test cases like toxml
-assert_type(document.toxml(), str)
-assert_type(document.toxml(encoding=None), str)
-assert_type(document.toxml(encoding="UTF8"), bytes)
+assert_type(document.toprettyxml(), str)
+assert_type(document.toprettyxml(encoding=None), str)
+assert_type(document.toprettyxml(encoding="UTF8"), bytes)
 if sys.version_info >= (3, 9):
-    assert_type(document.toxml(standalone=True), str)
-    assert_type(document.toxml(encoding="UTF8", standalone=True), bytes)
+    assert_type(document.toprettyxml(standalone=True), str)
+    assert_type(document.toprettyxml(encoding="UTF8", standalone=True), bytes)
 # Test cases unique to toprettyxml
 assert_type(document.toprettyxml("  "), str)
 assert_type(document.toprettyxml("  ", "\r\n"), str)
