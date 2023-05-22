0c87093e519c54c9d955e64a2d9513f0a9eba6cf
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-05-22 07:48:34 -0700

    Update stubs/tensorflow/tensorflow/experimental/__init__.pyi

--- stubs/tensorflow/tensorflow/experimental/__init__.pyi
+++ stubs/tensorflow/tensorflow/experimental/__init__.pyi
@@ -2,9 +2,9 @@ from _typeshed import Incomplete
 from abc import ABC
 from typing import Generic, TypeVar
 
-_T1 = TypeVar("_T1", covariant=True)
+_T_co = TypeVar("_T_co", covariant=True)
 
-class Optional(Generic[_T1], ABC):
+class Optional(Generic[_T_co], ABC):
     def __getattr__(self, name: str) -> Incomplete: ...
 
 def __getattr__(name: str) -> Incomplete: ...
