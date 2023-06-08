8f509bd3b11d9cb589d5d648125159f436760f8d
Sebastian Rittau <srittau@rittau.biz>
2023-06-08 12:39:26 +0200

    Only test return types on non-Windows platforms

--- test_cases/stdlib/check_tempfile.py
+++ test_cases/stdlib/check_tempfile.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
 import io
+import sys
 from tempfile import TemporaryFile
 from typing_extensions import assert_type
 
-assert_type(TemporaryFile(), io.BufferedRandom)
-assert_type(TemporaryFile(buffering=0), io.FileIO)
-assert_type(TemporaryFile(mode="w+"), io.TextIOWrapper)
-assert_type(TemporaryFile(mode="w+b"), io.BufferedRandom)
-assert_type(TemporaryFile(mode="wb"), io.BufferedWriter)
-assert_type(TemporaryFile(mode="rb"), io.BufferedReader)
-assert_type(TemporaryFile("wb"), io.BufferedWriter)
-assert_type(TemporaryFile("wb", 0), io.FileIO)
+if sys.platform != "win32":
+    assert_type(TemporaryFile(), io.BufferedRandom)
+    assert_type(TemporaryFile(buffering=0), io.FileIO)
+    assert_type(TemporaryFile(mode="w+"), io.TextIOWrapper)
+    assert_type(TemporaryFile(mode="w+b"), io.BufferedRandom)
+    assert_type(TemporaryFile(mode="wb"), io.BufferedWriter)
+    assert_type(TemporaryFile(mode="rb"), io.BufferedReader)
+    assert_type(TemporaryFile("wb"), io.BufferedWriter)
+    assert_type(TemporaryFile("wb", 0), io.FileIO)
