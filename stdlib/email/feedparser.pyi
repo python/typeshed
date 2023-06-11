ac5cbd9c2984e340daa14a9e9132ea9e1b2c0e43
oleg.hoefling <oleg.hoefling@gmail.com>
2023-06-11 17:12:00 +0200

    fix: use correct base classes for classes in email.feedparser and email.generator
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- stdlib/email/feedparser.pyi
+++ stdlib/email/feedparser.pyi
@@ -1,24 +1,25 @@
 from collections.abc import Callable
 from email.message import Message
 from email.policy import Policy
-from typing import Generic, TypeVar, overload
+from typing import Generic, Protocol, TypeVar, overload
 
 __all__ = ["FeedParser", "BytesFeedParser"]
 
 _MessageT = TypeVar("_MessageT", bound=Message)
+_T_contra = TypeVar("_T_contra", contravariant=True)
 
-class FeedParser(Generic[_MessageT]):
+class _SupportsFeed(Protocol[_T_contra]):
+    def feed(self, data: _T_contra) -> None: ...
+
+class FeedParser(Generic[_MessageT], _SupportsFeed[str]):
     @overload
     def __init__(self: FeedParser[Message], _factory: None = None, *, policy: Policy = ...) -> None: ...
     @overload
     def __init__(self, _factory: Callable[[], _MessageT], *, policy: Policy = ...) -> None: ...
-    def feed(self, data: str) -> None: ...
     def close(self) -> _MessageT: ...
 
-class BytesFeedParser(Generic[_MessageT]):
+class BytesFeedParser(FeedParser[_MessageT], _SupportsFeed[bytes | bytearray]):
     @overload
     def __init__(self: BytesFeedParser[Message], _factory: None = None, *, policy: Policy = ...) -> None: ...
     @overload
     def __init__(self, _factory: Callable[[], _MessageT], *, policy: Policy = ...) -> None: ...
-    def feed(self, data: bytes | bytearray) -> None: ...
-    def close(self) -> _MessageT: ...
