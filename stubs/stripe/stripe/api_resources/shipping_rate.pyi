928512772ef05c55ddb65b58a6e88cce57d15669
Sergio <sergio.bost@pilot.com>
2023-06-15 19:53:57 -0700

    add shipping_rate type

---
+++ stubs/stripe/stripe/api_resources/shipping_rate.pyi
@@ -0,0 +1,14 @@
+from _typeshed import Incomplete
+from typing import NoReturn
+
+from stripe import api_requestor as api_requestor
+from stripe.api_resources.abstract import (
+    CreateableAPIResource as CreateableAPIResource,
+    ListableAPIResource as ListableAPIResource,
+    UpdateableAPIResource as UpdateableAPIResource,
+)
+
+class ShippingRate(CreateableAPIResource, ListableAPIResource, UpdateableAPIResource):
+    OBJECT_NAME: str
+    @classmethod
+    def retrieve(cls, id, api_key: Incomplete | None = None, **params) -> NoReturn: ...
