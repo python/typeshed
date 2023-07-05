6f7419ba1473aa42e91d5e033209543f680f02c0
Sergio <sergio.bost@pilot.com>
2023-07-05 14:48:11 -0700

    remove redundant method

--- stubs/stripe/stripe/api_resources/shipping_rate.pyi
+++ stubs/stripe/stripe/api_resources/shipping_rate.pyi
@@ -1,6 +1,3 @@
-from _typeshed import Incomplete
-from typing import NoReturn
-
 from stripe import api_requestor as api_requestor
 from stripe.api_resources.abstract import (
     CreateableAPIResource as CreateableAPIResource,
@@ -10,5 +7,3 @@ from stripe.api_resources.abstract import (
 
 class ShippingRate(CreateableAPIResource, ListableAPIResource, UpdateableAPIResource):
     OBJECT_NAME: str
-    @classmethod
-    def retrieve(cls, id, api_key: Incomplete | None = None, **params) -> NoReturn: ...
