37946c7ea21751e0afe96e305e495da0c7040ff1
sobolevn <mail@sobolevn.me>
2023-07-18 13:21:38 +0300

    Update `mock` to `5.1.*`

--- stubs/mock/mock/__init__.pyi
+++ stubs/mock/mock/__init__.pyi
@@ -1,3 +1,23 @@
 from .mock import *
 
+__all__ = (
+    "__version__",
+    "version_info",
+    "Mock",
+    "MagicMock",
+    "patch",
+    "sentinel",
+    "DEFAULT",
+    "ANY",
+    "call",
+    "create_autospec",
+    "AsyncMock",
+    "ThreadingMock",
+    "FILTER_DIR",
+    "NonCallableMock",
+    "NonCallableMagicMock",
+    "mock_open",
+    "PropertyMock",
+    "seal",
+)
 version_info: tuple[int, int, int]
