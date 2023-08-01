bcc66f1b3663ddd929563ab6d265244e484e508f
Daverball <dave@daverball.com>
2023-08-02 00:09:50 +0200

    Some more fixes

--- stubs/gevent/gevent/_monitor.pyi
+++ stubs/gevent/gevent/_monitor.pyi
@@ -3,7 +3,7 @@ from collections.abc import Callable, Sequence
 from gevent.events import IPeriodicMonitorThread, MemoryUsageThresholdExceeded, MemoryUsageUnderThreshold
 from gevent.hub import Hub
 from greenlet import greenlet
-from zope.interface import implementer  # type: ignore[import]  # pyright: ignore[reportMissingImports]
+from zope.interface import implementer  # type: ignore[import]  # pyright: ignore[reportMissingImports, reportUnknownVariableType]
 
 class MonitorWarning(RuntimeWarning): ...
 
