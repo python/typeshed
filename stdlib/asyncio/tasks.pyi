81d136224755b9f3bbb9a8b9dfff88615a2e8ea0
Nikita Sobolev <mail@sobolevn.me>
2023-08-04 16:12:51 +0300

    Fix `asyncio.Task.__init__` signature (#10532)

--- stdlib/asyncio/tasks.pyi
+++ stdlib/asyncio/tasks.pyi
@@ -285,7 +285,26 @@ else:
 # since the only reason why `asyncio.Future` is invariant is the `set_result()` method,
 # and `asyncio.Task.set_result()` always raises.
 class Task(Future[_T_co], Generic[_T_co]):  # type: ignore[type-var]  # pyright: ignore[reportGeneralTypeIssues]
-    if sys.version_info >= (3, 8):
+    if sys.version_info >= (3, 12):
+        def __init__(
+            self,
+            coro: _TaskCompatibleCoro[_T_co],
+            *,
+            loop: AbstractEventLoop = ...,
+            name: str | None,
+            context: Context | None = None,
+            eager_start: bool = False,
+        ) -> None: ...
+    elif sys.version_info >= (3, 11):
+        def __init__(
+            self,
+            coro: _TaskCompatibleCoro[_T_co],
+            *,
+            loop: AbstractEventLoop = ...,
+            name: str | None,
+            context: Context | None = None,
+        ) -> None: ...
+    elif sys.version_info >= (3, 8):
         def __init__(
             self, coro: _TaskCompatibleCoro[_T_co], *, loop: AbstractEventLoop = ..., name: str | None = ...
         ) -> None: ...
