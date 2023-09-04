7cf01aa0a03d3a0ea9fca72dfb3924bd981dc931
hauntsaninja <hauntsaninja@gmail.com>
2023-09-04 16:46:38 -0700

    .

--- test_cases/stdlib/check_pathlib.py
+++ test_cases/stdlib/check_pathlib.py
@@ -1,4 +1,7 @@
 # pyright: strict
+from __future__ import annotations
+
 from pathlib import Path
 
-if Path("asdf") == "asdf": ...  # type: ignore
+if Path("asdf") == "asdf":  # type: ignore
+    ...
