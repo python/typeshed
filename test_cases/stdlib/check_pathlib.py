c94c946326a24164bfc6c74c416438cc638b666d
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2023-09-04 17:15:16 -0700

    Apply suggestions from code review
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- test_cases/stdlib/check_pathlib.py
+++ test_cases/stdlib/check_pathlib.py
@@ -1,7 +1,8 @@
-# pyright: strict
 from __future__ import annotations
 
 from pathlib import Path
 
+# make sure this causes mypy to emit a --strict-equality error,
+# and pyright to report a reportUnnecessaryComparison error
 if Path("asdf") == "asdf":  # type: ignore
     ...
