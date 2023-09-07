98fb95fad3d5dcfcc96ef56df6e13520c2d6093d
AlexWaygood <alex.waygood@gmail.com>
2023-09-07 21:07:10 +0100

    try something other than `os.environ`

--- test_cases/stdlib/builtins/check_dict-py39.py
+++ test_cases/stdlib/builtins/check_dict-py39.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import os
 import sys
 from typing import Mapping, TypeVar, Union
-from typing_extensions import assert_type
+from typing_extensions import Self, assert_type
 
 _KT = TypeVar("_KT")
 _VT = TypeVar("_VT")
@@ -19,7 +19,23 @@ if sys.version_info >= (3, 9):
     class CustomDictSubclass(dict[_KT, _VT]):
         pass
 
-    def test_dict_dot_or(a: dict[int, int], b: CustomDictSubclass[int, int], c: dict[str, str], d: Mapping[int, int]) -> None:
+    class CustomMappingWithDunderOr(Mapping[_KT, _VT]):
+        def __or__(self, other: Mapping[_KT, _VT]) -> dict[_KT, _VT]:
+            return {}
+
+        def __ror__(self, other: Mapping[_KT, _VT]) -> dict[_KT, _VT]:
+            return {}
+
+        def __ior__(self, other: Mapping[_KT, _VT]) -> Self:
+            return self
+
+    def test_dict_dot_or(
+        a: dict[int, int],
+        b: CustomDictSubclass[int, int],
+        c: dict[str, str],
+        d: Mapping[int, int],
+        e: CustomMappingWithDunderOr[str, str],
+    ) -> None:
         # dict.__(r)or__ always returns a dict, even if called on a subclass of dict:
         assert_type(a | b, dict[int, int])
         assert_type(b | a, dict[int, int])
@@ -30,14 +46,19 @@ if sys.version_info >= (3, 9):
         # it has to be a subclass of `dict`
         a | d  # type: ignore
 
-        # but Mappings such as `os._Environ`,
+        # but Mappings such as `os._Environ` or `CustomMappingWithDunderOr`,
         # which define `__ror__` methods that accept `dict`, are fine:
         assert_type(a | os.environ, dict[Union[str, int], Union[str, int]])
         assert_type(os.environ | a, dict[Union[str, int], Union[str, int]])
-        assert_type(c | os.environ, dict[str, str])
-        assert_type(os.environ | c, dict[str, str])
 
-        os.environ |= c
-        os.environ |= a  # type: ignore
-        c |= os.environ
+        assert_type(c | os.environ, dict[str, str])
+        assert_type(c | e, dict[str, str])
+
+        assert_type(os.environ | c, dict[str, str])
+        assert_type(e | c, dict[str, str])
+
+        e |= c
+        e |= a  # type: ignore
+
+        c |= e
         c |= a  # type: ignore
--- test_cases/stdlib/collections/check_defaultdict-py39.py
+++ test_cases/stdlib/collections/check_defaultdict-py39.py
@@ -9,7 +9,7 @@ import os
 import sys
 from collections import defaultdict
 from typing import Mapping, TypeVar, Union
-from typing_extensions import assert_type
+from typing_extensions import Self, assert_type
 
 _KT = TypeVar("_KT")
 _VT = TypeVar("_VT")
@@ -20,8 +20,22 @@ if sys.version_info >= (3, 9):
     class CustomDefaultDictSubclass(defaultdict[_KT, _VT]):
         pass
 
+    class CustomMappingWithDunderOr(Mapping[_KT, _VT]):
+        def __or__(self, other: Mapping[_KT, _VT]) -> dict[_KT, _VT]:
+            return {}
+
+        def __ror__(self, other: Mapping[_KT, _VT]) -> dict[_KT, _VT]:
+            return {}
+
+        def __ior__(self, other: Mapping[_KT, _VT]) -> Self:
+            return self
+
     def test_defaultdict_dot_or(
-        a: defaultdict[int, int], b: CustomDefaultDictSubclass[int, int], c: defaultdict[str, str], d: Mapping[int, int]
+        a: defaultdict[int, int],
+        b: CustomDefaultDictSubclass[int, int],
+        c: defaultdict[str, str],
+        d: Mapping[int, int],
+        e: CustomMappingWithDunderOr[str, str],
     ) -> None:
         assert_type(a | b, defaultdict[int, int])
 
@@ -34,15 +48,20 @@ if sys.version_info >= (3, 9):
         # it has to be a subclass of `dict`
         a | d  # type: ignore
 
-        # but Mappings such as `os._Environ`,
+        # but Mappings such as `os._Environ` or `CustomMappingWithDunderOr`,
         # which define `__ror__` methods that accept `dict`, are fine
         # (`os._Environ.__(r)or__` always returns `dict`, even if a `defaultdict` is passed):
         assert_type(a | os.environ, dict[Union[str, int], Union[str, int]])
         assert_type(os.environ | a, dict[Union[str, int], Union[str, int]])
-        assert_type(c | os.environ, dict[str, str])
-        assert_type(os.environ | c, dict[str, str])
 
-        os.environ |= c
-        os.environ |= a  # type: ignore
-        c |= os.environ
+        assert_type(c | os.environ, dict[str, str])
+        assert_type(c | e, dict[str, str])
+
+        assert_type(os.environ | c, dict[str, str])
+        assert_type(e | c, dict[str, str])
+
+        e |= c
+        e |= a  # type: ignore
+
+        c |= e
         c |= a  # type: ignore
