2666728299b769c61d80624de0e92117e787237a
AlexWaygood <alex.waygood@gmail.com>
2023-09-11 10:53:46 +0100

    Add some non-exhaustive tests

--- test_cases/stdlib/check_enum.py
+++ test_cases/stdlib/check_enum.py
@@ -2,7 +2,28 @@ from __future__ import annotations
 
 import enum
 import sys
+from typing import Type
 from typing_extensions import Literal, assert_type
+
+
+A = enum.Enum("A", names="spam eggs bacon")
+B = enum.Enum("B", names=["spam", "eggs", "bacon"])
+C = enum.Enum("Bar", names=[("spam", 1), ("eggs", 2), ("bacon", 3)])
+D = enum.Enum("Bar", names={"spam": 1, "eggs": 2})
+
+assert_type(A, Type[A])
+assert_type(B, Type[B])
+assert_type(C, Type[C])
+assert_type(D, Type[D])
+
+
+class EnumOfTuples(enum.Enum):
+    X = 1, 2, 3
+    Y = 4, 5, 6
+
+
+assert_type(EnumOfTuples((1, 2, 3)), EnumOfTuples)
+
 
 if sys.version_info >= (3, 11):
 
