6eb79feec31cdc3545797db138307baee3b97d12
Markis Taylor <m@rkis.net>
2023-09-22 16:56:25 -0400

    test: check real world examples and examples in the readme

--- stubs/python-decouple/@tests/test_cases/check_config.py
+++ stubs/python-decouple/@tests/test_cases/check_config.py
@@ -14,13 +14,13 @@ def _test_post_process(_: Iterable[Any]) -> int:
     return 0
 
 
-assert_type(config(""), str)
-assert_type(config("", cast=int), int)
-assert_type(config("", cast=_test_cast), int)
-assert_type(config("", default="localhost"), str)
-assert_type(config("", cast=bool, default="false"), bool)
-assert_type(config("", cast=Csv(), default="foo,bar"), List[str])
-assert_type(config("", cast=Csv(int), default="1,2"), List[int])
-assert_type(config("", cast=Csv(int, post_process=_test_post_process), default="1,2"), int)
-assert_type(config("", cast=Choices([1, 2], int)), int)
-assert_type(config("", cast=Choices(["foo", "bar"])), str)
+assert_type(config("NAME"), str)
+assert_type(config("NAME", cast=int), int)
+assert_type(config("NAME", cast=_test_cast), int)
+assert_type(config("NAME", default="localhost"), str)
+assert_type(config("NAME", cast=bool, default="false"), bool)
+assert_type(config("NAME", cast=Csv(), default="foo,bar"), List[str])
+assert_type(config("NAME", cast=Csv(int), default="1,2"), List[int])
+assert_type(config("NAME", cast=Csv(int, post_process=_test_post_process), default="1,2"), int)
+assert_type(config("NAME", cast=Choices([1, 2], int)), int)
+assert_type(config("NAME", cast=Choices(["foo", "bar"])), str)
