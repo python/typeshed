bf423b38704ad93d39a1b4b45625f43d74c62d89
sobolevn <mail@sobolevn.me>
2023-09-23 14:10:16 +0300

    Fix `contextlib.asynccontextmanager` to work with coroutine functions

--- test_cases/stdlib/check_contextlib.py
+++ test_cases/stdlib/check_contextlib.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from contextlib import ExitStack
+from contextlib import ExitStack, asynccontextmanager
+from typing import AsyncIterator, Awaitable
 from typing_extensions import assert_type
 
 
@@ -18,3 +19,17 @@ with stack as cm:
     assert_type(cm, ExitStack)
 with thing as cm2:
     assert_type(cm2, Thing)
+
+
+@asynccontextmanager
+async def async_context() -> AsyncGenerator[str, None]:
+    yield 'example'
+
+
+async def async_gen() -> AsyncGenerator[str, None]:
+    yield 'async gen'
+
+
+@asynccontextmanager
+def async_cm_func() -> AsyncGenerator[str, None]:
+    return async_gen()
