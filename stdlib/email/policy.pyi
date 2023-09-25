b0d50d39046537bd8f52ef5eae929b4675ee4f9d
Sebastian Rittau <srittau@rittau.biz>
2023-09-25 13:26:34 +0200

    Make email.policy classes generic
    Cf. #10762

--- stdlib/email/policy.pyi
+++ stdlib/email/policy.pyi
@@ -3,19 +3,33 @@ from collections.abc import Callable
 from email.contentmanager import ContentManager
 from email.errors import MessageDefect
 from email.header import Header
-from email.message import Message
-from typing import Any
+from email.message import EmailMessage, Message
+from typing import Any, TypeVar, overload
 from typing_extensions import Self
 
 __all__ = ["Compat32", "compat32", "Policy", "EmailPolicy", "default", "strict", "SMTP", "HTTP"]
 
-class Policy(metaclass=ABCMeta):
+_MessageT = TypeVar("_MessageT", bound=Message)
+
+class Policy(Generic[_MessageT], metaclass=ABCMeta):
     max_line_length: int | None
     linesep: str
     cte_type: str
     raise_on_defect: bool
     mangle_from_: bool
-    message_factory: Callable[[Policy], Message] | None
+    message_factory: Callable[[Policy], _MessageT] | None
+    @overload
+    def __init__(
+        self: Policy[Message],
+        *,
+        max_line_length: int | None = ...,
+        linesep: str = ...,
+        cte_type: str = ...,
+        raise_on_defect: bool = ...,
+        mangle_from_: bool = ...,
+        message_factory: None = None,
+    ) -> None: ...
+    @overload
     def __init__(
         self,
         *,
@@ -24,11 +38,11 @@ class Policy(metaclass=ABCMeta):
         cte_type: str = ...,
         raise_on_defect: bool = ...,
         mangle_from_: bool = ...,
-        message_factory: Callable[[Policy], Message] | None = ...,
+        message_factory: Callable[[Policy], _MessageT],
     ) -> None: ...
     def clone(self, **kw: Any) -> Self: ...
-    def handle_defect(self, obj: Message, defect: MessageDefect) -> None: ...
-    def register_defect(self, obj: Message, defect: MessageDefect) -> None: ...
+    def handle_defect(self, obj: _MessageT, defect: MessageDefect) -> None: ...
+    def register_defect(self, obj: _MessageT, defect: MessageDefect) -> None: ...
     def header_max_count(self, name: str) -> int | None: ...
     @abstractmethod
     def header_source_parse(self, sourcelines: list[str]) -> tuple[str, str]: ...
@@ -41,20 +55,36 @@ class Policy(metaclass=ABCMeta):
     @abstractmethod
     def fold_binary(self, name: str, value: str) -> bytes: ...
 
-class Compat32(Policy):
+class Compat32(Policy[_MessageT]):
     def header_source_parse(self, sourcelines: list[str]) -> tuple[str, str]: ...
     def header_store_parse(self, name: str, value: str) -> tuple[str, str]: ...
     def header_fetch_parse(self, name: str, value: str) -> str | Header: ...  # type: ignore[override]
     def fold(self, name: str, value: str) -> str: ...
     def fold_binary(self, name: str, value: str) -> bytes: ...
 
-compat32: Compat32
+compat32: Compat32[Message]
 
-class EmailPolicy(Policy):
+class EmailPolicy(Policy[_MessageT]):
     utf8: bool
     refold_source: str
     header_factory: Callable[[str, Any], Any]
     content_manager: ContentManager
+    @overload
+    def __init__(
+        self: EmailPolicy[EmailMessage],
+        *,
+        max_line_length: int | None = ...,
+        linesep: str = ...,
+        cte_type: str = ...,
+        raise_on_defect: bool = ...,
+        mangle_from_: bool = ...,
+        message_factory: None = None,
+        utf8: bool = ...,
+        refold_source: str = ...,
+        header_factory: Callable[[str, str], str] = ...,
+        content_manager: ContentManager = ...,
+    ) -> None: ...
+    @overload
     def __init__(
         self,
         *,
@@ -63,7 +93,7 @@ class EmailPolicy(Policy):
         cte_type: str = ...,
         raise_on_defect: bool = ...,
         mangle_from_: bool = ...,
-        message_factory: Callable[[Policy], Message] | None = ...,
+        message_factory: Callable[[Policy], _MessageT] | None = ...,
         utf8: bool = ...,
         refold_source: str = ...,
         header_factory: Callable[[str, str], str] = ...,
@@ -75,8 +105,8 @@ class EmailPolicy(Policy):
     def fold(self, name: str, value: str) -> Any: ...
     def fold_binary(self, name: str, value: str) -> bytes: ...
 
-default: EmailPolicy
-SMTP: EmailPolicy
-SMTPUTF8: EmailPolicy
-HTTP: EmailPolicy
-strict: EmailPolicy
+default: EmailPolicy[EmailMessage]
+SMTP: EmailPolicy[EmailMessage]
+SMTPUTF8: EmailPolicy[EmailMessage]
+HTTP: EmailPolicy[EmailMessage]
+strict: EmailPolicy[EmailMessage]
