79e15c92ad936ecb75b786e0c3bf338a301fcaed
plokmijnuhby <39633434+plokmijnuhby@users.noreply.github.com>
2023-09-26 15:44:18 +0100

    Add more checks

--- test_cases/stdlib/builtins/check_reversed.py
+++ test_cases/stdlib/builtins/check_reversed.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from typing import Generic, TypeVar
 from typing_extensions import assert_type
 
 x: list[int] = []
@@ -8,14 +9,20 @@ assert_type(list(reversed(x)), list[int])
 iterator = iter(x)
 assert_type(list(reversed(iterator)), list[int])
 
+class MyIterable:
+    def __iter__(self):
+        yield "blah"
+assert_type(list(reversed(MyIterable())), list[str])
 
-class LenAndGetItem:
+_T = TypeVar('T')
+
+class MyLenAndGetItem(Generic[_T]):
     def __len__(self):
         return 0
 
-    def __getitem__(self, item: int) -> int:
+    def __getitem__(self, item: int) -> _T:
         raise KeyError
 
 
-len_and_get_item = LenAndGetItem()
+len_and_get_item: MyLenAndGetItem[int] = LenAndGetItem()
 assert_type(list(reversed(len_and_get_item)), list[int])
