4dad5311d56e0b90ab82756a8c2a9ce645a788b1
Nikita Sobolev <mail@sobolevn.me>
2023-09-27 12:15:32 +0300

    Update `tty` to python 3.12 (#10784)

--- stdlib/tty.pyi
+++ stdlib/tty.pyi
@@ -1,9 +1,11 @@
 import sys
-from typing import IO
+from typing import IO, Any
 from typing_extensions import TypeAlias
 
 if sys.platform != "win32":
     __all__ = ["setraw", "setcbreak"]
+    if sys.version_info >= (3, 12):
+        __all__ += ["cfmakeraw", "cfmakecbreak"]
 
     _FD: TypeAlias = int | IO[str]
 
@@ -17,3 +19,10 @@ if sys.platform != "win32":
     CC: int
     def setraw(fd: _FD, when: int = 2) -> None: ...
     def setcbreak(fd: _FD, when: int = 2) -> None: ...
+
+    if sys.version_info >= (3, 12):
+        # It is: `list[int, int, int, int, int, int, list[str]]
+        _Mode: TypeAlias = list[Any]
+
+        def cfmakeraw(mode: _Mode) -> None: ...
+        def cfmakecbreak(mode: _Mode) -> None: ...
