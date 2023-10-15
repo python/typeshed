f2ea753321b741b8ded0cbd80a9eafc1f11f62a3
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-10-15 22:40:39 +0200

    Add `sys.monitoring` from Python 3.12

---
+++ stdlib/sys/_monitoring.pyi
@@ -0,0 +1,44 @@
+from collections.abc import Callable
+from types import CodeType, SimpleNamespace
+from typing import Any
+
+DEBUGGER_ID: int
+COVERAGE_ID: int
+PROFILER_ID: int
+OPTIMIZER_ID: int
+
+def use_tool_id(id: int, name: str) -> None: ...
+def free_tool_id(id: int) -> None: ...
+def get_tool(id: int) -> str | None: ...
+
+class events(SimpleNamespace):
+    BRANCH: int
+    CALL: int
+    C_RAISE: int
+    C_RETURN: int
+    EXCEPTION_HANDLED: int
+    INSTRUCTION: int
+    JUMP: int
+    LINE: int
+    NO_EVENTS: int
+    PY_RESUME: int
+    PY_RETURN: int
+    PY_START: int
+    PY_THROW: int
+    PY_UNWIND: int
+    PY_YIELD: int
+    RAISE: int
+    RERAISE: int
+    STOP_ITERATION: int
+
+def get_events(tool_id: int) -> int: ...
+def set_events(tool_id: int, event_set: int) -> None: ...
+def get_local_events(tool_id: int, code: CodeType) -> int: ...
+def set_local_events(tool_id: int, code: CodeType, event_set: int) -> int: ...
+def restart_events() -> None: ...  # undocumented
+def _all_events() -> dict[str, int]: ...  # undocumented
+
+DISABLE: object
+MISSING: object
+
+def register_callback(tool_id: int, event: int, func: Callable[..., Any] | None) -> Callable[..., Any] | None: ...
