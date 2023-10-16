11446be1cb245d54afc3ed691e15eca4c5f4b494
Avasam <samuel.06@hotmail.com>
2023-10-16 14:44:48 -0400

    tagname stays classvar

--- stubs/openpyxl/openpyxl/styles/protection.pyi
+++ stubs/openpyxl/openpyxl/styles/protection.pyi
@@ -1,10 +1,11 @@
+from typing import ClassVar
 from typing_extensions import Literal
 
 from openpyxl.descriptors.base import Bool, _ConvertibleToBool
 from openpyxl.descriptors.serialisable import Serialisable
 
 class Protection(Serialisable):
-    tagname: str
+    tagname: ClassVar[str]
     locked: Bool[Literal[False]]
     hidden: Bool[Literal[False]]
     def __init__(self, locked: _ConvertibleToBool = True, hidden: _ConvertibleToBool = False) -> None: ...
