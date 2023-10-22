cb02f2c1b365339a52ebf407c835f6f66fc2f7c2
Audrey Dutcher <audrey@rhelmot.io>
2023-10-22 15:02:09 -0700

    Fix typevar names

--- stubs/networkx/networkx/convert.pyi
+++ stubs/networkx/networkx/convert.pyi
@@ -5,14 +5,14 @@ from typing_extensions import TypeAlias
 import numpy
 
 # import scipy
-from networkx.classes.graph import EdgePlus, Graph, _Node
+from networkx.classes.graph import _EdgePlus, Graph, _Node
 
 # this is imported from other stub files
 _Data: TypeAlias = (  # noqa: Y047
     Graph[_Node]
     | dict[_Node, dict[_Node, dict[str, Incomplete]]]
     | dict[_Node, Iterable[_Node]]
-    | Iterable[EdgePlus[_Node]]
+    | Iterable[_EdgePlus[_Node]]
     | numpy.ndarray[_Node, Incomplete]
     # | scipy.sparse.base.spmatrix
 )
