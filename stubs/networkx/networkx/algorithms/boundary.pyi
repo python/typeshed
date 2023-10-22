cf6dad57212633d769bb176a63b2add6012b4268
Audrey Dutcher <audrey@rhelmot.io>
2023-10-22 14:44:08 -0700

    Update stubs/networkx/networkx/algorithms/boundary.pyi
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- stubs/networkx/networkx/algorithms/boundary.pyi
+++ stubs/networkx/networkx/algorithms/boundary.pyi
@@ -12,54 +12,89 @@ _U = TypeVar("_U")
 def edge_boundary(
     G: Graph[_T],
     nbunch1: Iterable[_T],
-    nbunch2: Iterable[_T] | None = ...,
-    data: Literal[False] = ...,
-    keys: Literal[False] = ...,
-    default: Incomplete = ...,
+    nbunch2: Iterable[_T] | None = None,
+    data: Literal[False] = False,
+    keys: Literal[False] = False,
+    default=None,
 ) -> Iterable[tuple[_T, _T]]: ...
 @overload
 def edge_boundary(
     G: Graph[_T],
     nbunch1: Iterable[_T],
-    nbunch2: Iterable[_T] | None = ...,
-    data: Literal[True] = ...,
-    keys: Literal[False] = ...,
-    default: Incomplete = ...,
+    nbunch2: Iterable[_T] | None,
+    data: Literal[True],
+    keys: Literal[False] = False,
+    default=None,
 ) -> Iterable[tuple[_T, _T, dict[str, Incomplete]]]: ...
 @overload
 def edge_boundary(
     G: Graph[_T],
     nbunch1: Iterable[_T],
-    nbunch2: Iterable[_T] | None = ...,
-    data: str = ...,
-    keys: Literal[False] = ...,
+    nbunch2: Iterable[_T] | None = None,
+    *,
+    data: Literal[True],
+    keys: Literal[False] = False,
+    default=None,
+) -> Iterable[tuple[_T, _T, dict[str, Incomplete]]]: ...
+@overload
+def edge_boundary(
+    G: Graph[_T],
+    nbunch1: Iterable[_T],
+    nbunch2: Iterable[_T] | None,
+    data: str,
+    keys: Literal[False] = False,
     default: _U | None = None,
 ) -> Iterable[tuple[_T, _T, dict[str, _U]]]: ...
 @overload
 def edge_boundary(
     G: Graph[_T],
     nbunch1: Iterable[_T],
-    nbunch2: Iterable[_T] | None = ...,
-    data: Literal[False] = ...,
-    keys: Literal[True] = ...,
-    default: Incomplete = ...,
+    nbunch2: Iterable[_T] | None = None,
+    *,
+    data: str,
+    keys: Literal[False] = False,
+    default: _U | None = None,
+) -> Iterable[tuple[_T, _T, dict[str, _U]]]: ...
+@overload
+def edge_boundary(
+    G: Graph[_T], nbunch1: Iterable[_T], nbunch2: Iterable[_T] | None, data: Literal[False], keys: Literal[True], default=None
 ) -> Iterable[tuple[_T, _T, int]]: ...
 @overload
 def edge_boundary(
     G: Graph[_T],
     nbunch1: Iterable[_T],
-    nbunch2: Iterable[_T] | None = ...,
-    data: Literal[True] = ...,
-    keys: Literal[True] = ...,
-    default: Incomplete = ...,
+    nbunch2: Iterable[_T] | None = None,
+    data: Literal[False] = False,
+    *,
+    keys: Literal[True],
+    default=None,
+) -> Iterable[tuple[_T, _T, int]]: ...
+@overload
+def edge_boundary(
+    G: Graph[_T], nbunch1: Iterable[_T], nbunch2: Iterable[_T] | None, data: Literal[True], keys: Literal[True], default=None
 ) -> Iterable[tuple[_T, _T, int, dict[str, Incomplete]]]: ...
 @overload
 def edge_boundary(
     G: Graph[_T],
     nbunch1: Iterable[_T],
-    nbunch2: Iterable[_T] | None = ...,
-    data: str = ...,
-    keys: Literal[True] = ...,
+    nbunch2: Iterable[_T] | None = None,
+    *,
+    data: Literal[True],
+    keys: Literal[True],
+    default=None,
+) -> Iterable[tuple[_T, _T, int, dict[str, Incomplete]]]: ...
+@overload
+def edge_boundary(
+    G: Graph[_T], nbunch1: Iterable[_T], nbunch2: Iterable[_T] | None, data: str, keys: Literal[True], default: _U | None = None
+) -> Iterable[tuple[_T, _T, int, dict[str, _U]]]: ...
+@overload
+def edge_boundary(
+    G: Graph[_T],
+    nbunch1: Iterable[_T],
+    nbunch2: Iterable[_T] | None = None,
+    *,
+    data: str,
+    keys: Literal[True],
     default: _U | None = None,
 ) -> Iterable[tuple[_T, _T, int, dict[str, _U]]]: ...
 def node_boundary(G: Graph[_T], nbunch1: Iterable[_T], nbunch2: Iterable[_T] | None = ...) -> Iterable[_T]: ...
