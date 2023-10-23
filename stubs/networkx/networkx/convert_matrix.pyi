ebef1076798cd43a143f7de85518f34c9e81ca7a
Avasam <samuel.06@hotmail.com>
2023-10-23 18:04:52 -0400

    Work around stubuploader issue (#6)
    https://github.com/typeshed-internal/stub_uploader/issues/90
    
    Co-authored-by: Audrey Dutcher <audrey@rhelmot.io>

--- stubs/networkx/networkx/convert_matrix.pyi
+++ stubs/networkx/networkx/convert_matrix.pyi
@@ -1,11 +1,15 @@
 from _typeshed import Incomplete
 from collections.abc import Callable, Iterable
-from typing_extensions import Literal
+from typing_extensions import Literal, TypeAlias
 
 import numpy
 from networkx.classes.graph import Graph, _Node
-from pandas import DataFrame
-from pandas.core.dtypes.base import ExtensionDtype
+
+# stub_uploader won't allow pandas-stubs in the requires field https://github.com/typeshed-internal/stub_uploader/issues/90
+# from pandas import DataFrame
+_DataFrame: TypeAlias = Incomplete
+# from pandas.core.dtypes.base import ExtensionDtype
+_ExtensionDtype: TypeAlias = Incomplete
 
 def to_pandas_adjacency(
     G: Graph[_Node],
@@ -15,18 +19,18 @@ def to_pandas_adjacency(
     multigraph_weight: Callable[[Iterable[float]], float] = ...,
     weight: str = ...,
     nonedge: float = ...,
-) -> DataFrame: ...
-def from_pandas_adjacency(df: DataFrame, create_using: type[Graph[Incomplete]] = ...) -> Graph[Incomplete]: ...
+) -> _DataFrame: ...
+def from_pandas_adjacency(df: _DataFrame, create_using: type[Graph[Incomplete]] = ...) -> Graph[Incomplete]: ...
 def to_pandas_edgelist(
     G: Graph[_Node],
     source: str | int = ...,
     target: str | int = ...,
     nodelist: list[_Node] | None = ...,
-    dtype: ExtensionDtype | None = ...,
+    dtype: _ExtensionDtype | None = ...,
     edge_key: str | int | None = ...,
-) -> DataFrame: ...
+) -> _DataFrame: ...
 def from_pandas_edgelist(
-    df: DataFrame,
+    df: _DataFrame,
     source: str | int = ...,
     target: str | int = ...,
     edge_attr: str | int | Iterable[str | int] | Literal[True] | None = ...,
