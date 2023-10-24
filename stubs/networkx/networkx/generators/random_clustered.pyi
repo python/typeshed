cfa3ff6e752912e13ab59a0e2dcc5d7e4524240f
Audrey Dutcher <audrey@rhelmot.io>
2023-10-23 19:08:30 -0700

    Improve two create_using cases

--- stubs/networkx/networkx/generators/random_clustered.pyi
+++ stubs/networkx/networkx/generators/random_clustered.pyi
@@ -1,3 +1,14 @@
-from _typeshed import Incomplete
+from typing import Any, TypeVar, overload
 
-def random_clustered_graph(joint_degree_sequence, create_using: Incomplete | None = None, seed: Incomplete | None = None): ...
+from networkx import MultiGraph
+
+_G = TypeVar("_G", bound=MultiGraph[int])
+
+@overload
+def random_clustered_graph(
+    joint_degree_sequence: list[tuple[int, int]], create_using: None = None, seed: int | tuple[Any, ...] | None = None
+) -> MultiGraph[int]: ...
+@overload
+def random_clustered_graph(
+    joint_degree_sequence: list[tuple[int, int]], create_using: type[_G], seed: int | tuple[Any, ...] | None = None
+) -> _G: ...
