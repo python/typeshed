c5cefbbc6dd0b6f3e6591a9f933554524cc8c327
Audrey Dutcher <audrey@rhelmot.io>
2023-10-23 19:31:07 -0700

    Improve fidelity of graphviews

--- stubs/networkx/networkx/classes/multigraph.pyi
+++ stubs/networkx/networkx/classes/multigraph.pyi
@@ -1,8 +1,11 @@
 from _typeshed import Incomplete
 from typing import Generic
+from typing_extensions import TypeAlias
 
 from networkx.classes.graph import Graph, _Node
 from networkx.classes.multidigraph import MultiDiGraph
+
+_MultiEdge: TypeAlias = tuple[_Node, _Node, int]  # noqa: Y047
 
 class MultiGraph(Graph[_Node], Generic[_Node]):
     def __init__(self, incoming_graph_data: Incomplete | None = None, multigraph_input: bool | None = None, **attr) -> None: ...
--- stubs/networkx/networkx/relabel.pyi
+++ stubs/networkx/networkx/relabel.pyi
@@ -1,13 +1,23 @@
 from _typeshed import Incomplete
 from collections.abc import Hashable, Mapping
-from typing import TypeVar
+from typing import TypeVar, overload
 from typing_extensions import Literal
 
+from networkx.classes.digraph import DiGraph
 from networkx.classes.graph import Graph
+from networkx.classes.multidigraph import MultiDiGraph
+from networkx.classes.multigraph import MultiGraph
 
 _X = TypeVar("_X")
 _Y = TypeVar("_Y")
 
+@overload
+def relabel_nodes(G: MultiDiGraph[_X], mapping: Mapping[_X, _Y], copy: bool = True) -> MultiDiGraph[_X | _Y]: ...
+@overload
+def relabel_nodes(G: DiGraph[_X], mapping: Mapping[_X, _Y], copy: bool = True) -> DiGraph[_X | _Y]: ...
+@overload
+def relabel_nodes(G: MultiGraph[_X], mapping: Mapping[_X, _Y], copy: bool = True) -> MultiGraph[_X | _Y]: ...
+@overload
 def relabel_nodes(G: Graph[_X], mapping: Mapping[_X, _Y], copy: bool = True) -> Graph[_X | _Y]: ...
 def convert_node_labels_to_integers(
     G: Graph[Hashable],
