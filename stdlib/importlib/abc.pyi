b1bcede92211a0f27045c16a404985850dc8f7c5
Sebastian Rittau <srittau@rittau.biz>
2023-10-26 21:06:57 +0200

    Typo

--- stdlib/importlib/abc.pyi
+++ stdlib/importlib/abc.pyi
@@ -1,12 +1,13 @@
 import _ast
 import sys
 import types
-from _typeshed import ReadableBuffer, StrPath
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterator, Mapping, Sequence
 from importlib.machinery import ModuleSpec
 from io import BufferedReader
 from typing import IO, Any, Protocol, overload, runtime_checkable
+
+from _typeshed import ReadableBuffer, StrPath
 from typing_extensions import Literal
 
 if sys.version_info >= (3, 11):
@@ -134,7 +135,7 @@ if sys.version_info >= (3, 9):
             def joinpath(self, child: str) -> Traversable: ...
 
         # The documentation and runtime protocol allows *args, **kwargs arguments,
-        # but this would mean that all implementators would have to support them,
+        # but this would mean that all implementors would have to support them,
         # which is not the case.
         @overload
         @abstractmethod
