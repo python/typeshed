643b6890056a55f388cbf102b5e56926a4c75537
Alex Lowe <alex.lowe@canonical.com>
2023-11-04 13:52:55 +0200

    add pymacaroons.verifier and __init__

---
+++ stubs/pymacaroons/pymacaroons/verifier.pyi
@@ -0,0 +1,19 @@
+from _typeshed import Incomplete
+from collections.abc import Callable, Iterable
+from typing import Any, Literal
+
+from pymacaroons import Macaroon
+from pymacaroons.caveat_delegates import FirstPartyCaveatVerifierDelegate, ThirdPartyCaveatVerifierDelegate
+
+class Verifier:
+    predicates: list[str]
+    callbacks: list[Callable[[Any], bool]]
+    calculated_signature: bytes | None
+    first_party_caveat_verifier_delegate: FirstPartyCaveatVerifierDelegate
+    third_party_caveat_verifier_delegate: ThirdPartyCaveatVerifierDelegate
+    def __init__(self) -> None: ...
+    def satisfy_exact(self, predicate: str | bytes | None) -> None: ...
+    def satisfy_general(self, func: Callable[[Any], bool]) -> None: ...
+    def verify_exact(self, predicate: str) -> bool: ...
+    def verify(self, macaroon: Macaroon, key: bytes | bytearray, discharge_macaroons: Iterable[Macaroon] | None = None) -> Literal[True]: ...
+    def verify_discharge(self, root: Macaroon, discharge: Macaroon, key: bytes | bytearray, discharge_macaroons: Iterable[Macaroon] | None = None) -> Literal[True]: ...
