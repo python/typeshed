d1925f2bb67d9231e7ecc0ae294b33f22d903b08
Oleh Prypin <oleh@pryp.in>
2023-11-15 14:06:24 +0100

    Various improvements to `Markdown` stubs (#10972)

--- stubs/Markdown/markdown/core.pyi
+++ stubs/Markdown/markdown/core.pyi
@@ -33,6 +33,8 @@ class Markdown:
     block_level_elements: list[str]
     registeredExtensions: list[Extension]
     ESCAPED_CHARS: list[str]
+    doc_tag: ClassVar[str]
+    stripTopLevelTags: bool
     def __init__(
         self,
         *,
