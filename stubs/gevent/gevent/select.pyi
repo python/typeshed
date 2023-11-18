835198b8580c40bb3a2c3fb17c14ffe136c51a07
Daverball <dave@daverball.com>
2023-11-18 10:49:28 +0100

    Bump gevent to 23.9.*

--- stubs/gevent/gevent/select.pyi
+++ stubs/gevent/gevent/select.pyi
@@ -1,13 +1,10 @@
 import sys
-from collections.abc import Collection
+from collections.abc import Iterable
 from select import error as error
 from typing import Any
 
-# this implementation is slightly more strict because it reuses the lists, so it can't
-# just be any iterable, this is a little bit dangerous in terms of interoperability with
-# the stdlib select module. link to github issue: https://github.com/gevent/gevent/issues/1979
 def select(
-    rlist: Collection[Any], wlist: Collection[Any], xlist: Collection[Any], timeout: float | None = None
+    rlist: Iterable[Any], wlist: Iterable[Any], xlist: Iterable[Any], timeout: float | None = None
 ) -> tuple[list[Any], list[Any], list[Any]]: ...
 
 if sys.platform != "win32":
