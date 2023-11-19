c4abab74403a2d2c1abda7873a150e268e6cb67e
sobolevn <mail@sobolevn.me>
2023-11-19 11:50:45 +0300

    Fix error

--- stdlib/typing.pyi
+++ stdlib/typing.pyi
@@ -2,7 +2,7 @@ import collections  # Needed by aliases like DefaultDict, see mypy issue 2986
 import sys
 import typing_extensions
 from _collections_abc import dict_items, dict_keys, dict_values
-from _typeshed import IdentityFunction, Incomplete, ReadableBuffer, SupportsKeysAndGetItem
+from _typeshed import IdentityFunction, Incomplete, ReadableBuffer, Self as _typeshed_Self, SupportsKeysAndGetItem
 from abc import ABCMeta, abstractmethod
 from contextlib import AbstractAsyncContextManager, AbstractContextManager
 from re import Match as Match, Pattern as Pattern
@@ -860,7 +860,7 @@ if sys.version_info >= (3, 11):
 # Type constructors
 
 class NamedTupleMeta(type):
-    def __new__(cls, typename: str, bases: tuple[type, ...], ns: dict[str, Any]) -> typing_extensions.Self: ...
+    def __new__(cls: type[_typeshed_Self], typename: str, bases: tuple[type, ...], ns: dict[str, Any]) -> _typeshed_Self: ...
 
 # We `type: ignore` here, because `tuple` base-class has `ABCMeta` in stubs,
 # while in runtime it does not. So, ignoring this error.
