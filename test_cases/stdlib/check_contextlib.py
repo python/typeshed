083e471707b41d4bcbf21642d271ef153e5a6c06
Daverball <dave@daverball.com>
2023-11-20 23:38:24 +0100

    Fixes use of union syntax in test cases

--- test_cases/stdlib/check_contextlib.py
+++ test_cases/stdlib/check_contextlib.py
@@ -1,20 +1,21 @@
 from __future__ import annotations
 
 from contextlib import ExitStack
+from typing import Optional
 from typing_extensions import assert_type
 
 
 # See issue #7961
-class Thing(ExitStack[bool | None]):
+class Thing(ExitStack[Optional[bool]]):
     pass
 
 
-stack: ExitStack[bool | None] = ExitStack()
+stack: ExitStack[Optional[bool]] = ExitStack()
 thing = Thing()
 assert_type(stack.enter_context(Thing()), Thing)
-assert_type(thing.enter_context(ExitStack()), ExitStack[bool | None])
+assert_type(thing.enter_context(ExitStack()), ExitStack[Optional[bool]])
 
 with stack as cm:
-    assert_type(cm, ExitStack[bool | None])
+    assert_type(cm, ExitStack[Optional[bool]])
 with thing as cm2:
     assert_type(cm2, Thing)
