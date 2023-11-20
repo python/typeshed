8bb852ef2109925e12ed10d5043485dce25f94df
Daverball <dave@daverball.com>
2023-11-20 23:32:52 +0100

    Fixes `ExitStack` test cases

--- test_cases/stdlib/check_contextlib.py
+++ test_cases/stdlib/check_contextlib.py
@@ -5,16 +5,16 @@ from typing_extensions import assert_type
 
 
 # See issue #7961
-class Thing(ExitStack):
+class Thing(ExitStack[bool | None]):
     pass
 
 
-stack = ExitStack()
+stack: ExitStack[bool | None] = ExitStack()
 thing = Thing()
 assert_type(stack.enter_context(Thing()), Thing)
-assert_type(thing.enter_context(ExitStack()), ExitStack)
+assert_type(thing.enter_context(ExitStack()), ExitStack[bool | None])
 
 with stack as cm:
-    assert_type(cm, ExitStack)
+    assert_type(cm, ExitStack[bool | None])
 with thing as cm2:
     assert_type(cm2, Thing)
