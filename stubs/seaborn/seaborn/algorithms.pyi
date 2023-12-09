31002ec6a780448999d150805f9d43db190413cc
Ali Hamdan <ali.hamdan.dev@gmail.com>
2023-12-09 19:01:52 +0100

    Make stubtest happy

--- stubs/seaborn/seaborn/algorithms.pyi
+++ stubs/seaborn/seaborn/algorithms.pyi
@@ -1,22 +1,22 @@
 from collections.abc import Callable
-from typing import Any, overload
-from typing_extensions import deprecated
+from typing import Any
 
 from numpy.typing import ArrayLike, NDArray
 
 from .utils import _Seed
 
-@overload
-def bootstrap(
-    *args: ArrayLike,
-    n_boot: int = 10000,
-    func: str | Callable[..., Any] = "mean",
-    axis: int | None = None,
-    units: ArrayLike | None = None,
-    seed: _Seed | None = None,
-) -> NDArray[Any]: ...
-@overload
-@deprecated("Parameter `random_seed` is deprecated in favor of `seed`")
+# TODO: Uncomment when stubtest crash is fixed
+# @overload
+# def bootstrap(
+#     *args: ArrayLike,
+#     n_boot: int = 10000,
+#     func: str | Callable[..., Any] = "mean",
+#     axis: int | None = None,
+#     units: ArrayLike | None = None,
+#     seed: _Seed | None = None,
+# ) -> NDArray[Any]: ...
+# @overload
+# @deprecated("Parameter `random_seed` is deprecated in favor of `seed`")
 def bootstrap(
     *args: ArrayLike,
     n_boot: int = 10000,
