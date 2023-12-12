255774a4024e6737716ea2974a0a676f421dd26f
Stephen Morton <git@tungol.org>
2023-12-12 10:39:32 -0800

    code review

--- stdlib/pyclbr.pyi
+++ stdlib/pyclbr.pyi
@@ -13,6 +13,9 @@ class _Object:
         end_lineno: int | None
 
     parent: _Object | None
+
+    # This is a dict at runtime, but we're typing it as Mapping to
+    # avoid variance issues in the subclasses
     children: Mapping[str, _Object]
 
     if sys.version_info >= (3, 10):
@@ -21,6 +24,28 @@ class _Object:
         ) -> None: ...
     else:
         def __init__(self, module: str, name: str, file: str, lineno: int, parent: _Object | None) -> None: ...
+
+class Function(_Object):
+    if sys.version_info >= (3, 10):
+        is_async: bool
+
+    parent: Function | Class | None
+    children: dict[str, Class | Function]
+
+    if sys.version_info >= (3, 10):
+        def __init__(
+            self,
+            module: str,
+            name: str,
+            file: str,
+            lineno: int,
+            parent: Function | Class | None = None,
+            is_async: bool = False,
+            *,
+            end_lineno: int | None = None,
+        ) -> None: ...
+    else:
+        def __init__(self, module: str, name: str, file: str, lineno: int, parent: Function | Class | None = None) -> None: ...
 
 class Class(_Object):
     super: list[Class | str] | None
@@ -44,28 +69,6 @@ class Class(_Object):
         def __init__(
             self, module: str, name: str, super: list[Class | str] | None, file: str, lineno: int, parent: Class | None = None
         ) -> None: ...
-
-class Function(_Object):
-    if sys.version_info >= (3, 10):
-        is_async: bool
-
-    parent: Function | Class | None
-    children: dict[str, Class | Function]
-
-    if sys.version_info >= (3, 10):
-        def __init__(
-            self,
-            module: str,
-            name: str,
-            file: str,
-            lineno: int,
-            parent: Function | Class | None = None,
-            is_async: bool = False,
-            *,
-            end_lineno: int | None = None,
-        ) -> None: ...
-    else:
-        def __init__(self, module: str, name: str, file: str, lineno: int, parent: Function | Class | None = None) -> None: ...
 
 def readmodule(module: str, path: Sequence[str] | None = None) -> dict[str, Class]: ...
 def readmodule_ex(module: str, path: Sequence[str] | None = None) -> dict[str, Class | Function | list[str]]: ...
