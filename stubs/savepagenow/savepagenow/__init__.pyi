0f72b0ff129c7154b9c698f820308672ffbb7bb5
Max Friedman <mr.awesome10000@gmail.com>
2023-12-16 08:33:11 -0800

    fix __init__.pyi trying to pull exceptions from api

--- stubs/savepagenow/savepagenow/__init__.pyi
+++ stubs/savepagenow/savepagenow/__init__.pyi
@@ -1,6 +1,8 @@
-from .api import (
+from .exceptions import (
     BlockedByRobots as BlockedByRobots,
     CachedPage as CachedPage,
+)
+from .api import (
     capture as capture,
     capture_or_cache as capture_or_cache,
 )
