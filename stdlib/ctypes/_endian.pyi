1b216991915ba0533849eddb5400619788f7e982
Stephen Morton <git@tungol.org>
2023-12-18 18:17:12 -0800

    fix windows stubtest

--- stdlib/ctypes/_endian.pyi
+++ stdlib/ctypes/_endian.pyi
@@ -5,6 +5,9 @@ from ctypes import DEFAULT_MODE as DEFAULT_MODE, cdll as cdll, pydll as pydll, p
 if sys.version_info >= (3, 12):
     from _ctypes import SIZEOF_TIME_T as SIZEOF_TIME_T
 
+if sys.platform == "win32":
+    from ctypes import oledll as oledll, windll as windll
+
 # At runtime, the native endianness is an alias for Structure,
 # while the other is a subclass with a metaclass added in.
 class BigEndianStructure(Structure): ...
