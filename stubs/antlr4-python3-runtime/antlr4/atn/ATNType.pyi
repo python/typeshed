cda790f8b27bae5fa57ff7bcbe456fe50f9b8c57
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-23 07:39:39 -0600

    Fixed strict annotations

--- stubs/antlr4-python3-runtime/antlr4/atn/ATNType.pyi
+++ stubs/antlr4-python3-runtime/antlr4/atn/ATNType.pyi
@@ -1,7 +1,9 @@
+
+from _typeshed import Incomplete
 from enum import IntEnum
 
 class ATNType(IntEnum):
     LEXER: int
     PARSER: int
     @classmethod
-    def fromOrdinal(cls, i: int): ...
+    def fromOrdinal(cls, i: int) -> Incomplete: ...
