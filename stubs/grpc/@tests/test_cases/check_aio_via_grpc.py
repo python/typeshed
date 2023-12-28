0ac8415074eb3e2c7bf1ec54b2cfff8783ce9b8a
Blake Williams <code@shabbyrobe.org>
2023-12-28 13:30:05 +1100

    Add check for alternate aio import

---
+++ stubs/grpc/@tests/test_cases/check_aio_via_grpc.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing_extensions import assert_type
+
+# Both import methods work, see the comment in grpc/__init__.py.
+from grpc import aio
+
+assert_type(aio.Channel(), aio.Channel)
+assert_type(aio.Server(), aio.Server)
