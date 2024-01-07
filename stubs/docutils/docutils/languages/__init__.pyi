6dcb4999909508674303957057ae64764e2c5511
Philipp A <flying-sheep@web.de>
2024-01-07 14:44:03 +0100

    Discard changes to stubs/docutils/docutils/languages/__init__.pyi

--- stubs/docutils/docutils/languages/__init__.pyi
+++ stubs/docutils/docutils/languages/__init__.pyi
@@ -1,8 +1,7 @@
-from typing import ClassVar, Protocol
+from _typeshed import Incomplete
+from typing import Protocol
 
-from docutils.utils import Reporter, normalize_language_tag
-
-__all__ = ["get_language", "normalize_language_tag", "LanguageImporter"]
+from docutils.utils import Reporter
 
 class _LanguageModule(Protocol):
     labels: dict[str, str]
@@ -10,14 +9,7 @@ class _LanguageModule(Protocol):
     bibliographic_fields: list[str]
 
 class LanguageImporter:
-    packages: ClassVar[tuple[str, ...]]
-    warn_msg: ClassVar[str]
-    fallback: ClassVar[str]
-
-    cache: dict[str, _LanguageModule]
-
     def __call__(self, language_code: str, reporter: Reporter | None = None) -> _LanguageModule: ...
-    def import_from_packages(self, name: str, reporter: Reporter | None = None) -> _LanguageModule: ...
-    def check_content(self, module: _LanguageModule) -> None: ...
+    def __getattr__(self, __name: str) -> Incomplete: ...
 
 get_language: LanguageImporter
