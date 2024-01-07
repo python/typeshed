81a85ed52b2742f199faa4830837953e1804b29f
Philipp A. <flying-sheep@web.de>
2024-01-07 14:42:53 +0100

    Fixes

--- stubs/docutils/docutils/__init__.pyi
+++ stubs/docutils/docutils/__init__.pyi
@@ -1,8 +1,13 @@
-from typing import Any, ClassVar, NamedTuple
+from collections.abc import Callable, Iterable, Mapping, Sequence
+from typing import Any, ClassVar, Literal, NamedTuple
 from typing_extensions import Self
+
+from docutils.nodes import Node
+from docutils.transforms import Transform
 
 __docformat__: str
 __version__: str
+__version_details__: str
 
 class _VersionInfo(NamedTuple):
     major: int
@@ -18,25 +23,25 @@ class VersionInfo(_VersionInfo):
     ) -> Self: ...
 
 __version_info__: VersionInfo
-__version_details__: str
 
 class ApplicationError(Exception): ...
 class DataError(ApplicationError): ...
 
 class SettingsSpec:
     settings_spec: ClassVar[tuple[Any, ...]]
-    settings_defaults: ClassVar[dict[Any, Any] | None]
-    settings_default_overrides: ClassVar[dict[Any, Any] | None]
-    relative_path_settings: ClassVar[tuple[Any, ...]]
+    settings_defaults: ClassVar[Mapping[str, Any] | None]
+    settings_default_overrides: ClassVar[Mapping[str, Any] | None]
+    relative_path_settings: ClassVar[tuple[str, ...]]
     config_section: ClassVar[str | None]
     config_section_dependencies: ClassVar[tuple[str, ...] | None]
 
 class TransformSpec:
-    def get_transforms(self) -> list[Any]: ...
-    default_transforms: ClassVar[tuple[Any, ...]]
-    unknown_reference_resolvers: ClassVar[list[Any]]
+    def get_transforms(self) -> Iterable[Transform]: ...
+    # @deprecated("Use get_transforms() instead.")
+    default_transforms: ClassVar[tuple[Transform, ...]]
+    unknown_reference_resolvers: ClassVar[Sequence[Callable[[Node], bool]]]
 
 class Component(SettingsSpec, TransformSpec):
-    component_type: ClassVar[str | None]
+    component_type: ClassVar[Literal['reader', 'parser', 'writer'] | None]
     supported: ClassVar[tuple[str, ...]]
     def supports(self, format: str) -> bool: ...
--- stubs/docutils/docutils/languages/__init__.pyi
+++ stubs/docutils/docutils/languages/__init__.pyi
@@ -1,7 +1,8 @@
-from _typeshed import Incomplete
-from typing import Protocol
+from typing import ClassVar, Protocol
 
-from docutils.utils import Reporter
+from docutils.utils import Reporter, normalize_language_tag
+
+__all__ = ["get_language", "normalize_language_tag", "LanguageImporter"]
 
 class _LanguageModule(Protocol):
     labels: dict[str, str]
@@ -9,7 +10,14 @@ class _LanguageModule(Protocol):
     bibliographic_fields: list[str]
 
 class LanguageImporter:
+    packages: ClassVar[tuple[str, ...]]
+    warn_msg: ClassVar[str]
+    fallback: ClassVar[str]
+
+    cache: dict[str, _LanguageModule]
+
     def __call__(self, language_code: str, reporter: Reporter | None = None) -> _LanguageModule: ...
-    def __getattr__(self, __name: str) -> Incomplete: ...
+    def import_from_packages(self, name: str, reporter: Reporter | None = None) -> _LanguageModule: ...
+    def check_content(self, module: _LanguageModule) -> None: ...
 
 get_language: LanguageImporter
