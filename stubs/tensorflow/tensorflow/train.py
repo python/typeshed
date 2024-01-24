b7622518a543d67db4728bbfd90dd2c83981723b
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-24 22:22:04 +0900

    fix: file name train.py -> train.pyi

--- stubs/tensorflow/tensorflow/train.py
+++
@@ -1,9 +0,0 @@
-from collections.abc import Callable
-from typing import Any
-
-
-class CheckpointOptions:
-    experimental_io_device: None | str = None
-    experimental_enable_async_checkpoint: bool = False
-    experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]] = None
-    enable_async: bool = False
