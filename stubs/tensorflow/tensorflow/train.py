eb3098f694c6393e58dc30f5664a761d00d27a43
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-24 22:15:40 +0900

    Fix CheckpointOptions's callbacks type.

--- stubs/tensorflow/tensorflow/train.py
+++ stubs/tensorflow/tensorflow/train.py
@@ -1,8 +1,9 @@
 from collections.abc import Callable
+from typing import Any
 
 
 class CheckpointOptions:
     experimental_io_device: None | str = None
     experimental_enable_async_checkpoint: bool = False
-    experimental_write_callbacks: None | list[Callable] = None
+    experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]] = None
     enable_async: bool = False
