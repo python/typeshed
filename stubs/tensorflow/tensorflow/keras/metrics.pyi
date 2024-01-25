38f84808504230679136f0443b61a2d6d19b9b6a
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-25 21:13:49 +0900

    feat: add empty Metric class

--- stubs/tensorflow/tensorflow/keras/metrics.pyi
+++ stubs/tensorflow/tensorflow/keras/metrics.pyi
@@ -1,12 +1,13 @@
-from typing import Any
-from typing_extensions import Self
-
+from _typeshed import Incomplete
 from tensorflow import Tensor, _TensorCompatible
 from tensorflow.dtypes import DType
 
 class Metric:
     def __init__(self, name: str, dtype: DType) -> None: ...
     def __new__(cls, *args: Any, **kwargs: Any) -> Self: ...
+
+class Metric(Incomplete):
+    ...
 
 def binary_crossentropy(
     y_true: _TensorCompatible, y_pred: _TensorCompatible, from_logits: bool = False, label_smoothing: float = 0.0, axis: int = -1
