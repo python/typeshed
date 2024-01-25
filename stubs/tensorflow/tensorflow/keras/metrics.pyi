0326484456bcba8d39f8be381edfe7f33d079290
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-25 22:47:39 +0900

    fix: add __init__ and __new__ to Metric

--- stubs/tensorflow/tensorflow/keras/metrics.pyi
+++ stubs/tensorflow/tensorflow/keras/metrics.pyi
@@ -1,8 +1,11 @@
-from _typeshed import Incomplete
+from typing import Any, Self
 
 from tensorflow import Tensor, _TensorCompatible
+from tensorflow.dtypes import DType
 
-class Metric(Incomplete): ...
+class Metric:
+    def __init__(self, name: str, dtype: DType) -> None: ...
+    def __new__(cls, *args: Any, **kwargs: Any) -> Self: ...
 
 def binary_crossentropy(
     y_true: _TensorCompatible, y_pred: _TensorCompatible, from_logits: bool = False, label_smoothing: float = 0.0, axis: int = -1
