d9f9669c2f062a36476577cb62a6894daba49af8
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-25 22:46:56 +0900

    fix: fix CheckpointOptions (add init)

--- stubs/tensorflow/tensorflow/train.pyi
+++ stubs/tensorflow/tensorflow/train.pyi
@@ -2,7 +2,15 @@ from collections.abc import Callable
 from typing import Any
 
 class CheckpointOptions:
-    experimental_io_device: None | str = None
-    experimental_enable_async_checkpoint: bool = False
-    experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]] = None
-    enable_async: bool = False
+    experimental_io_device: None | str
+    experimental_enable_async_checkpoint: bool
+    experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]]
+    enable_async: bool
+
+    def __init__(
+        self,
+        experimental_io_device: None | str = None,
+        experimental_enable_async_checkpoint: bool = False,
+        experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]] = None,
+        enable_async: bool = False,
+    ) -> None: ...
