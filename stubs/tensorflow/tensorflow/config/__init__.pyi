03dd434547a64657fce5176d0deacab058a68383
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-27 19:19:56 +0900

    Add some functions from the config module.

---
+++ stubs/tensorflow/tensorflow/config/__init__.pyi
@@ -0,0 +1,11 @@
+from typing import NamedTuple
+
+from tensorflow.config import experimental as experimental
+
+class PhysicalDevice(NamedTuple):
+    name: str
+    device_type: str
+
+def list_physical_devices(device_type: None | str = None) -> list[PhysicalDevice]: ...
+def get_visible_devices(device_type: None | str = None) -> list[PhysicalDevice]: ...
+def set_visible_devices(devices: list[PhysicalDevice], device_type: None | str = None) -> None: ...
