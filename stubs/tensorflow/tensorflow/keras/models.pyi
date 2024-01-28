d1d9a56109d2d6b52e7f6b80b7e7d548f7b123a7
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-28 19:31:30 +0900

    fix model

--- stubs/tensorflow/tensorflow/keras/models.pyi
+++ stubs/tensorflow/tensorflow/keras/models.pyi
@@ -1,6 +1,3 @@
 from _typeshed import Incomplete
 
-import tensorflow as tf
-from tensorflow.keras.layers import Layer, _InputT, _OutputT
-
-class Model(Layer[_InputT, _OutputT], tf.Module, Incomplete): ...
+Model = Incomplete
