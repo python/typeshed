145c6d58c431c8c6b6235a83fb6b749e3a45e9ec
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-28 20:24:51 +0900

    begin adding models.Model

--- stubs/tensorflow/tensorflow/keras/__init__.pyi
+++ stubs/tensorflow/tensorflow/keras/__init__.pyi
@@ -1,5 +1,8 @@
 from _typeshed import Incomplete
+from typing import Callable
 
+import tensorflow as tf
+from tensorflow._aliases import _TensorCompatible
 from tensorflow.keras import (
     activations as activations,
     constraints as constraints,
@@ -7,8 +10,13 @@ from tensorflow.keras import (
     layers as layers,
     losses as losses,
     metrics as metrics,
+    models as models,
     optimizers as optimizers,
     regularizers as regularizers,
 )
+from tensorflow.keras.models import Model as Model
+
+_Loss = str | tf.keras.losses.Loss | Callable[[_TensorCompatible, _TensorCompatible], tf._Tensor]
+_Metric = str | tf.keras.metrics.Metric | Callable[[_TensorCompatible, _TensorCompatible], tf._Tensor] | None
 
 def __getattr__(name: str) -> Incomplete: ...
