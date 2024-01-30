d97a4e4708d47306f0a415faf13c5382d7a1601f
Avasam <samuel.06@hotmail.com>
2024-01-30 16:23:55 -0500

    clearer kwarg param name

--- stubs/networkx/@tests/test_cases/check_dispatch_decorator.py
+++ stubs/networkx/@tests/test_cases/check_dispatch_decorator.py
@@ -12,12 +12,12 @@ def some_method(int_p: int, str_p: str) -> float:
 some_method("", 0)  # type: ignore
 # backend is kw-only
 some_method(0, "", None)  # type: ignore
-# No backend means no backend_kwargs allowed
-some_method(0, "", backend_arg="")  # type: ignore
-some_method(0, "", backend=None, backend_arg="")  # type: ignore
+# No backend means no **backend_kwargs allowed
+some_method(0, "", backend_specific_kwarg="")  # type: ignore
+some_method(0, "", backend=None, backend_specific_kwarg="")  # type: ignore
 
 # Correct usage
 assert_type(some_method(0, ""), float)
 # type system doesn't allow this yet (see comment in networkx/utils/backends.pyi)
 # assert_type(some_method(0, "", backend=None), float)
-assert_type(some_method(0, "", backend="custom backend", backend_arg=""), float)
+assert_type(some_method(0, "", backend="custom backend", backend_specific_kwarg=""), float)
