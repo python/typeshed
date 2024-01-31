85b4d57ef43ec2a63e78ddece3d9f7be13c40c96
Hoel Bagard <hoel.bagard@gmail.com>
2024-01-31 21:51:07 +0900

    fix: Remove tf.distribute.experimental.coordinator.RemoteValue since it prevents the CI from passing.

--- stubs/tensorflow/tensorflow/distribute/experimental/coordinator.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Generic, TypeVar
-
-from tensorflow._aliases import AnyArray
-
-_Value = TypeVar("_Value", covariant=True)
-
-class RemoteValue(Generic[_Value]):
-    def fetch(self) -> AnyArray: ...
-    def get(self) -> _Value: ...
