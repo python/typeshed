4dfca8278ee3b22fec1b548e6733fbc16e48d8dc
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-03 22:10:13 +0900

    fix: use 'object' when return type is ignored.
    If return value does not matter for a callable prefer object over Any. Return argument is covariant so in practice it behaves same. Just that conventionally object as a return means anything is fine, Any when correct type is difficult to describe.

--- stubs/tensorflow/tensorflow/train/__init__.pyi
+++ stubs/tensorflow/tensorflow/train/__init__.pyi
@@ -20,14 +20,14 @@ from tensorflow.python.trackable.base import Trackable
 class CheckpointOptions:
     experimental_io_device: None | str
     experimental_enable_async_checkpoint: bool
-    experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]]
+    experimental_write_callbacks: None | list[Callable[[str], object] | Callable[[], object]]
     enable_async: bool
 
     def __init__(
         self,
         experimental_io_device: None | str = None,
         experimental_enable_async_checkpoint: bool = False,
-        experimental_write_callbacks: None | list[Callable[[str], Any] | Callable[[], Any]] = None,
+        experimental_write_callbacks: None | list[Callable[[str], object] | Callable[[], object]] = None,
         enable_async: bool = False,
     ) -> None: ...
 
