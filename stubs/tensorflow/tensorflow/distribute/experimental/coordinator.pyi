e51face833e48ffd265885fe5c4b44b9354f875e
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-04 12:52:28 +0900

    fix: add __getattr__

--- stubs/tensorflow/tensorflow/distribute/experimental/coordinator.pyi
+++ stubs/tensorflow/tensorflow/distribute/experimental/coordinator.pyi
@@ -1,3 +1,4 @@
+from _typeshed import Incomplete
 from typing import Generic, TypeVar
 
 from tensorflow._aliases import AnyArray
@@ -7,3 +8,5 @@ _Value = TypeVar("_Value", covariant=True)
 class RemoteValue(Generic[_Value]):
     def fetch(self) -> AnyArray: ...
     def get(self) -> _Value: ...
+
+def __getattr__(name: str) -> Incomplete: ...
