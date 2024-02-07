041a182ef7dc39788c04584ccc5ab781d858c298
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-07 23:41:25 +0900

    add some tf.strings functions.

--- stubs/tensorflow/tensorflow/strings.pyi
+++ stubs/tensorflow/tensorflow/strings.pyi
@@ -3,8 +3,6 @@ from typing import Any, Literal, overload
 
 from tensorflow import RaggedTensor, Tensor, TensorCompatible
 
-def join(inputs: Sequence[TensorCompatible | RaggedTensor], separator: str = "", name: str | None = None) -> Tensor: ...
-
 # None corresponds to "" for split.
 def split(
     input: TensorCompatible | RaggedTensor, sep: str | None = None, maxsplit: int = -1, name: str | None = None
@@ -27,6 +25,28 @@ def as_string(
     shortest: bool = False,
     width: int = -1,
     fill: str = "",
+    name: str | None = None,
+) -> RaggedTensor: ...
+def bytes_split(input: TensorCompatible | RaggedTensor, name: str | None = None) -> RaggedTensor: ...
+def format(
+    template: str, inputs: TensorCompatible, placeholder: str = "{}", summarize: int = 3, name: str | None = None
+) -> Tensor: ...
+def join(inputs: Sequence[TensorCompatible | RaggedTensor], separator: str = "", name: str | None = None) -> Tensor: ...
+@overload
+def length(input: TensorCompatible, unit: Literal["BYTE", "UTF8_CHAR"] = "BYTE", name: str | None = None) -> Tensor: ...
+@overload
+def length(input: RaggedTensor, unit: Literal["BYTE", "UTF8_CHAR"] = "BYTE", name: str | None = None) -> RaggedTensor: ...
+@overload
+def lower(input: TensorCompatible, encoding: Literal["utf-8", "", " "] = "", name: str | None = None) -> Tensor: ...
+@overload
+def lower(input: RaggedTensor, encoding: Literal["utf-8", "", " "] = "", name: str | None = None) -> RaggedTensor: ...
+def ngrams(
+    data: TensorCompatible | RaggedTensor,
+    ngram_width: int | Sequence[int],
+    separator: str = " ",
+    pad_values: tuple[int, int] | str | None = None,
+    padding_width: int | None = None,
+    preserve_short_sequences: bool = False,
     name: str | None = None,
 ) -> RaggedTensor: ...
 @overload
