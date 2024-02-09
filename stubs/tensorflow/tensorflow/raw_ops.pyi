e6160ae59145526b19ec15b9cf34eaa7d02bcfca
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-10 00:58:07 +0900

    add names

--- stubs/tensorflow/tensorflow/raw_ops.pyi
+++ stubs/tensorflow/tensorflow/raw_ops.pyi
@@ -5,7 +5,7 @@ from tensorflow import Tensor
 from tensorflow._aliases import TensorCompatible
 
 def Fingerprint(data: TensorCompatible, method: Literal["farmhash64"], name: str | None = None) -> Tensor: ...
-def Snapshot(*, input: TensorCompatible, name: str | None = None) -> Tensor: ...
+def Snapshot(input: TensorCompatible, name: str | None = None) -> Tensor: ...
 def ResourceApplyAdagradV2(
     var: Tensor,
     accum: Tensor,
@@ -14,6 +14,7 @@ def ResourceApplyAdagradV2(
     grad: TensorCompatible,
     use_locking: bool = False,
     update_slots: bool = True,
+    name: str | None = None,
 ) -> None: ...
 def ResourceSparseApplyAdagradV2(
     var: Tensor,
@@ -24,6 +25,7 @@ def ResourceSparseApplyAdagradV2(
     indices: TensorCompatible,
     use_locking: bool = False,
     update_slots: bool = True,
+    name: str | None = None,
 ) -> None: ...
 def ResourceApplyAdam(
     var: Tensor,
@@ -38,5 +40,6 @@ def ResourceApplyAdam(
     grad: TensorCompatible,
     use_locking: bool = False,
     use_nesterov: bool = False,
+    name: str | None = None,
 ) -> None: ...
 def __getattr__(name: str) -> Incomplete: ...
