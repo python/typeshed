669a209043f93880b6fac4d7cc6194dcbdf34100
Alex Waygood <Alex.Waygood@Gmail.com>
2024-02-11 15:50:42 +0000

    py38 compat

--- test_cases/stdlib/builtins/check_set.py
+++ test_cases/stdlib/builtins/check_set.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
+from typing import FrozenSet, Set
 from typing_extensions import assert_type
 
 # We special case `AbstractSet[None] in set.__sub__ and frozenset.__sub__
 # so that it can be used for narrowing `set[T|None]` to `set[T]`
 x = {"foo", "bar", None}
 y = frozenset(x)
-assert_type(x - {None}, set[str])
-assert_type(y - {None}, frozenset[str])
+assert_type(x - {None}, Set[str])
+assert_type(y - {None}, FrozenSet[str])
 
 # For most other cases of set subtraction, we're pretty restrictive about what's allowed.
 # `set[T] - set[S]` is an error, even though it won't cause an exception at runtime,
@@ -16,7 +17,7 @@ assert_type(y - {None}, frozenset[str])
 
 # But subtracting set[T|None] from set[T] is allowed, as a convenience;
 # this comes up a lot in real-life code:
-assert_type({"foo", "bar"} - {"foo", None}, set[str])
+assert_type({"foo", "bar"} - {"foo", None}, Set[str])
 x = {"foo", "bar"}
 x.difference_update({"foo", "bar", None})
 name: str | None = "foo"
