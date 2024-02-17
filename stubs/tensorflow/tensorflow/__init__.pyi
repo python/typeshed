28ae404a5be7f888feed3971ec6ec412f07e3c14
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-17 11:57:43 +0900

    fix: re-export eye from tf

--- stubs/tensorflow/tensorflow/__init__.pyi
+++ stubs/tensorflow/tensorflow/__init__.pyi
@@ -37,6 +37,7 @@ from tensorflow.core.protobuf import struct_pb2
 from tensorflow.dtypes import *
 from tensorflow.dtypes import DType as DType
 from tensorflow.keras import losses as losses
+from tensorflow.linalg import eye as eye
 
 # Most tf.math functions are exported as tf, but sadly not all are.
 from tensorflow.math import (
