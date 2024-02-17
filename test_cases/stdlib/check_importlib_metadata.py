bd0e85c2c9d1a560d85e32f392e684fd6aca9841
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-02-17 15:05:52 -0800

    Extend test, fix protocol

--- test_cases/stdlib/check_importlib_metadata.py
+++ test_cases/stdlib/check_importlib_metadata.py
@@ -1,9 +1,16 @@
 import sys
-from importlib.metadata._meta import SimplePath
 from pathlib import Path
+from typing import Any
 
-if sys.version_info >= (3, 9):
+if sys.version_info >= (3, 10):
+    from importlib.metadata._meta import SimplePath
     from zipfile import Path as ZipPath
 
-    _: SimplePath = Path()
-    _ = ZipPath("")
+    if sys.version_info >= (3, 12):
+        def takes_simple_path(p: SimplePath[Any]) -> None: ...
+    else:
+        def takes_simple_path(p: SimplePath) -> None: ...
+
+    takes_simple_path(Path())
+    takes_simple_path(ZipPath(""))
+    takes_simple_path("some string")  # type: ignore
