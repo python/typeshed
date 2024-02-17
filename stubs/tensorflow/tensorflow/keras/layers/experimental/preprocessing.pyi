37c668ba1943d116099bedb75d05274a7fdd31b1
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-17 20:07:13 +0900

    add PreprocessingLayer methods/overloads

--- stubs/tensorflow/tensorflow/keras/layers/experimental/preprocessing.pyi
+++ stubs/tensorflow/tensorflow/keras/layers/experimental/preprocessing.pyi
@@ -1,6 +1,18 @@
 import abc
+from typing import overload
 
-from tensorflow._aliases import TensorLike
+import tensorflow as tf
+from tensorflow._aliases import AnyArray, TensorLike
 from tensorflow.keras.layers import Layer
 
-class PreprocessingLayer(Layer[TensorLike, TensorLike], metaclass=abc.ABCMeta): ...
+class PreprocessingLayer(Layer[TensorLike, TensorLike], metaclass=abc.ABCMeta):
+    @overload
+    def __call__(self, inputs: tf.Tensor) -> tf.Tensor: ...
+    @overload
+    def __call__(self, inputs: tf.SparseTensor) -> tf.SparseTensor: ...
+    @overload
+    def __call__(self, inputs: tf.RaggedTensor) -> tf.RaggedTensor: ...  # type: ignore
+    def adapt(
+        self, data: tf.data.Dataset[TensorLike] | AnyArray, batch_size: int | None = None, steps: int | None = None
+    ) -> None: ...
+    def compile(self, run_eagerly: bool | None = None, steps_per_execution: int | None = None) -> None: ...
