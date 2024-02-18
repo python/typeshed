18995cad1cf3e08b0675859544aefb7f64904478
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-18 20:35:44 +0900

    add pre-existing stubs.

---
+++ stubs/tensorflow/tensorflow/bitwise.pyi
@@ -0,0 +1,13 @@
+from typing import overload
+
+import tensorflow as tf
+from tensorflow._aliases import TensorCompatible
+
+@overload
+def bitwise_or(x: TensorCompatible, y: TensorCompatible, name: str | None = None) -> tf.Tensor: ...
+@overload
+def bitwise_or(x: tf.RaggedTensor, y: tf.RaggedTensor, name: str | None = None) -> tf.RaggedTensor: ...
+@overload
+def bitwise_and(x: TensorCompatible, y: TensorCompatible, name: str | None = None) -> tf.Tensor: ...
+@overload
+def bitwise_and(x: tf.RaggedTensor, y: tf.RaggedTensor, name: str | None = None) -> tf.RaggedTensor: ...
