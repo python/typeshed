f71a2ba122565e264b24d46f86c35f21e1b33c9a
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-18 21:46:45 +0900

    add autograph members

---
+++ stubs/tensorflow/tensorflow/autograph/__init__.pyi
@@ -0,0 +1,26 @@
+from collections.abc import Callable
+from typing import Any, ParamSpec, TypeVar, overload
+
+from tensorflow.autograph.experimental import Feature
+
+_Param = ParamSpec("_Param")
+_RetType = TypeVar("_RetType")
+_Type = TypeVar("_Type")
+
+def set_verbosity(level: int, alsologtostdout: bool = False) -> None: ...
+def to_code(
+    entity: Callable[_Param, Any] | type,
+    recursive: bool = True,
+    experimental_optional_features: None | Feature | tuple[Feature, ...] = None,
+) -> str: ...
+@overload
+def to_graph(
+    entity: Callable[_Param, _RetType],
+    recursive: bool = True,
+    experimental_optional_features: None | Feature | tuple[Feature, ...] = None,
+) -> Callable[_Param, _RetType]: ...
+@overload
+def to_graph(
+    entity: _Type, recursive: bool = True, experimental_optional_features: None | Feature | tuple[Feature, ...] = None
+) -> _Type: ...
+def trace(*args: Any) -> None: ...
