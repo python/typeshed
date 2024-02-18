b82892078be100fba792635d999607374ee81847
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-02-17 16:08:51 -0800

    Add MyPath

--- test_cases/stdlib/check_importlib_metadata.py
+++ test_cases/stdlib/check_importlib_metadata.py
@@ -1,10 +1,21 @@
 import sys
+from _typeshed import StrPath
+from os import PathLike
 from pathlib import Path
 from typing import Any
+from zipfile import Path as ZipPath
 
 if sys.version_info >= (3, 10):
     from importlib.metadata._meta import SimplePath
-    from zipfile import Path as ZipPath
+
+    # Simplified version of zipfile.Path
+    class MyPath:
+        @property
+        def parent(self) -> PathLike[str]: ...  # undocumented
+
+        def read_text(self, encoding: str | None = ..., errors: str | None = ...) -> str: ...
+        def joinpath(self, *other: StrPath) -> Path: ...
+        def __truediv__(self, add: StrPath) -> Path: ...
 
     if sys.version_info >= (3, 12):
 
@@ -16,4 +27,5 @@ if sys.version_info >= (3, 10):
 
     takes_simple_path(Path())
     takes_simple_path(ZipPath(""))
+    takes_simple_path(MyPath())
     takes_simple_path("some string")  # type: ignore
