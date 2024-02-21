e354ef7fc59fb21bfa7fcb0ec5822557cd535a11
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-21 21:04:41 +0900

    add wurlitzer stubs.
    --fixup=ea82d69

---
+++ stubs/wurlitzer/wurlitzer/__init__.pyi
@@ -0,0 +1,43 @@
+import contextlib
+import io
+from _typeshed import Incomplete
+from collections.abc import Iterator
+from contextlib import contextmanager
+from types import TracebackType
+from typing import Final, Literal, TextIO, TypeAlias
+
+from IPython.core.interactiveshell import InteractiveShell
+
+STDOUT: Final = 2
+PIPE: Final = 3
+_STDOUT: TypeAlias = Literal[2]
+_PIPE: TypeAlias = Literal[3]
+
+class Wurlitzer:
+    flush_interval: float = 0.2
+
+    def __init__(
+        self,
+        stdout: TextIO | io.IOBase | None = None,
+        stderr: _STDOUT | TextIO | io.IOBase | None = None,
+        encoding: str = ...,
+        bufsize: int | None = ...,
+    ) -> None: ...
+    def __enter__(self): ...
+    def __exit__(
+        self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None
+    ) -> None: ...
+
+def dup2(a: int, b: int, timeout: int = 3) -> int: ...
+def sys_pipes(
+    encoding: str = ..., bufsize: int | None = None
+) -> contextlib._GeneratorContextManager[tuple[TextIO | io.BytesIO | io.StringIO, TextIO | io.BytesIO | io.StringIO | None]]: ...
+@contextmanager
+def pipes(
+    stdout: _PIPE | TextIO = 3, stderr: _STDOUT | _PIPE | TextIO = 3, encoding: str = ..., bufsize: int | None = None
+) -> Iterator[tuple[TextIO | io.BytesIO | io.StringIO, TextIO | io.BytesIO | io.StringIO | None]]: ...
+def sys_pipes_forever(encoding: str = ..., bufsize: int | None = None): ...
+def stop_sys_pipes() -> None: ...
+def load_ipython_extension(ip: InteractiveShell) -> None: ...
+def unload_ipython_extension(ip: InteractiveShell) -> None: ...
+def __getattr__(name: str) -> Incomplete: ...
