1a774204d2307d8f452af5b5ec5f0c755523649b
Robert Schweizer <robert.schweizer@astrazeneca.com>
2024-02-21 10:33:49 +0100

    fix: Remove inheritance of NotImplementedType from Any

---
+++ test_cases/stdlib/check_types-py310.py
@@ -0,0 +1,22 @@
+from collections.abc import Callable
+from types import NotImplementedType
+from typing import Any
+from typing_extensions import assert_type
+
+
+def check_not_implemented_type() -> None:
+    union_with_callable: Callable[[], None] | NotImplementedType
+    if callable(union_with_callable):
+        assert_type(union_with_callable, Callable[[], None])
+
+    class _Class:
+        def __call__(self, *args: Any, **kwds: Any) -> Any:
+            pass
+    
+    union_with_class: _Class | NotImplementedType
+    if callable(union_with_class):
+        assert_type(union_with_class, _Class)
+    if isinstance(union_with_class, NotImplementedType):
+        assert_type(union_with_class, NotImplementedType)
+    if not isinstance(union_with_class, NotImplementedType):
+        assert_type(union_with_class, _Class)
