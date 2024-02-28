2d741c901416599928a7c917e41d3c39dc37f018
WANG Xuerui <git@xen0n.name>
2024-02-28 19:43:15 +0800

    pygit2: Fix signatures per review

--- stubs/pygit2/pygit2/_build.pyi
+++ stubs/pygit2/pygit2/_build.pyi
@@ -1,6 +1,5 @@
 from pathlib import Path
-from typing import Any
 
 __version__: str
 
-def get_libgit2_paths() -> tuple[Path, dict[str, Any]]: ...
+def get_libgit2_paths() -> tuple[Path, dict[str, list[str]]]: ...
--- stubs/pygit2/pygit2/_run.pyi
+++ stubs/pygit2/pygit2/_run.pyi
@@ -1,5 +1,5 @@
+from _typeshed import Incomplete
 from pathlib import Path
-from typing import Any
 
 from cffi import FFI
 
@@ -7,9 +7,9 @@ dir_path: Path
 h_files: list[str]
 h_source: list[str]
 h_file: Path
-f: Any
+f: Incomplete
 C_HEADER_SRC: str
 C_PREAMBLE: str
 _: Path
-libgit2_kw: dict[str, Any]
+libgit2_kw: dict[str, list[str]]
 ffi: FFI
