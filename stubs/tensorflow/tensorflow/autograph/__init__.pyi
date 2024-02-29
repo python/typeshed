baf73245dc14d556fc307e263248038fc6af5f6f
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-29 18:36:51 +0900

    fix: remove to_graph overload, fix to_code's type hint

--- stubs/tensorflow/tensorflow/autograph/__init__.pyi
+++ stubs/tensorflow/tensorflow/autograph/__init__.pyi
@@ -1,26 +1,16 @@
 from collections.abc import Callable
-from typing import Any, TypeVar, overload
-from typing_extensions import ParamSpec
+from typing import Any, TypeVar
 
 from tensorflow.autograph.experimental import Feature
 
-_Param = ParamSpec("_Param")
-_RetType = TypeVar("_RetType")
 _Type = TypeVar("_Type")
 
 def set_verbosity(level: int, alsologtostdout: bool = False) -> None: ...
 def to_code(
-    entity: Callable[_Param, Any] | type,
+    entity: Callable[..., Any],
     recursive: bool = True,
     experimental_optional_features: None | Feature | tuple[Feature, ...] = None,
 ) -> str: ...
-@overload
-def to_graph(
-    entity: Callable[_Param, _RetType],
-    recursive: bool = True,
-    experimental_optional_features: None | Feature | tuple[Feature, ...] = None,
-) -> Callable[_Param, _RetType]: ...
-@overload
 def to_graph(
     entity: _Type, recursive: bool = True, experimental_optional_features: None | Feature | tuple[Feature, ...] = None
 ) -> _Type: ...
