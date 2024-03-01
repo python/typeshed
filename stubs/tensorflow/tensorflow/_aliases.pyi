ea2002d171ba35731767f100e912e2d461bc00d9
Hoël Bagard <34478245+hoel-bagard@users.noreply.github.com>
2024-03-02 00:17:28 +0900

    `tensorflow`: add `tensorflow.keras.activations` members (#11444)
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- stubs/tensorflow/tensorflow/_aliases.pyi
+++ stubs/tensorflow/tensorflow/_aliases.pyi
@@ -34,6 +34,7 @@ IntDataSequence: TypeAlias = Sequence[int] | Sequence[IntDataSequence]
 StrDataSequence: TypeAlias = Sequence[str] | Sequence[StrDataSequence]
 ScalarTensorCompatible: TypeAlias = tf.Tensor | str | float | np.ndarray[Any, Any] | np.number[Any]
 UIntTensorCompatible: TypeAlias = tf.Tensor | int | UIntArray
+FloatTensorCompatible: TypeAlias = tf.Tensor | int | IntArray | float | FloatArray | np.number[Any]
 StringTensorCompatible: TypeAlias = tf.Tensor | str | npt.NDArray[np.str_] | Sequence[StringTensorCompatible]
 
 TensorCompatible: TypeAlias = ScalarTensorCompatible | Sequence[TensorCompatible]
