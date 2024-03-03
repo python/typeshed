e27686d331849c8283933a8d16c8196d0156e399
Daniel Eades <danieleades@hotmail.com>
2024-03-03 16:59:47 +0000

    fixup

--- stubs/docutils/docutils/parsers/rst/directives/misc.pyi
+++ stubs/docutils/docutils/parsers/rst/directives/misc.pyi
@@ -2,7 +2,7 @@ from _typeshed import Incomplete
 from collections.abc import Callable
 from pathlib import Path
 from re import Pattern
-from typing import Any, ClassVar, Literal
+from typing import Any, ClassVar
 
 from docutils.parsers.rst import Directive
 from docutils.parsers.rst.states import SpecializedBody
@@ -10,60 +10,60 @@ from docutils.parsers.rst.states import SpecializedBody
 __docformat__: str
 
 class Include(Directive):
-    required_arguments: ClassVar[Literal[1]]
-    optional_arguments: ClassVar[Literal[0]]
-    final_argument_whitespace: ClassVar[Literal[True]]
+    required_arguments: ClassVar[int]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
     option_spec: dict[str, Callable[[str], Any]]
     standard_include_path: Path
 
 class Raw(Directive):
-    required_arguments: ClassVar[Literal[1]]
-    optional_arguments: ClassVar[Literal[0]]
-    final_argument_whitespace: ClassVar[Literal[True]]
+    required_arguments: ClassVar[int]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
     option_spec: dict[str, Callable[[str], Any]]
-    has_content: ClassVar[Literal[True]]
+    has_content: ClassVar[bool]
 
 class Replace(Directive):
-    has_content: ClassVar[Literal[True]]
+    has_content: ClassVar[bool]
 
 class Unicode(Directive):
-    required_arguments: ClassVar[Literal[1]]
-    optional_arguments: ClassVar[Literal[0]]
-    final_argument_whitespace: ClassVar[Literal[True]]
+    required_arguments: ClassVar[int]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
     option_spec: dict[str, Callable[[str], Any]]
     comment_pattern: Pattern[str]
 
 class Class(Directive):
-    required_arguments: ClassVar[Literal[1]]
-    optional_arguments: ClassVar[Literal[0]]
-    final_argument_whitespace: ClassVar[Literal[True]]
-    has_content: ClassVar[Literal[True]]
+    required_arguments: ClassVar[int]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
+    has_content: ClassVar[bool]
 
 class Role(Directive):
-    has_content: ClassVar[Literal[True]]
+    has_content: ClassVar[bool]
     argument_pattern: Pattern[str]
 
 class DefaultRole(Directive):
-    optional_arguments: ClassVar[Literal[1]]
-    final_argument_whitespace: ClassVar[Literal[False]]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
 
 class Title(Directive):
-    required_arguments: ClassVar[Literal[1]]
-    optional_arguments: ClassVar[Literal[0]]
-    final_argument_whitespace: ClassVar[Literal[True]]
+    required_arguments: ClassVar[int]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
 
 class MetaBody(SpecializedBody):
     def __getattr__(self, name: str) -> Incomplete: ...
 
 class Meta(Directive):
-    has_content: ClassVar[Literal[True]]
+    has_content: ClassVar[bool]
     SMkwargs: ClassVar[dict[str, tuple[MetaBody]]]
 
 class Date(Directive):
     has_content: bool
 
 class TestDirective(Directive):
-    optional_arguments: ClassVar[Literal[1]]
-    final_argument_whitespace: ClassVar[Literal[True]]
+    optional_arguments: ClassVar[int]
+    final_argument_whitespace: ClassVar[bool]
     option_spec: dict[str, Callable[[str], Any]]
-    has_content: ClassVar[Literal[True]]
+    has_content: ClassVar[bool]
