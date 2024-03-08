fc5e417be94268cad8a06e45648c8106ef40d2ad
daniel.eades <daniel.eades@seebyte.com>
2024-03-08 17:22:42 +0000

    fixup

--- stubs/docutils/docutils/parsers/rst/directives/misc.pyi
+++ stubs/docutils/docutils/parsers/rst/directives/misc.pyi
@@ -10,60 +10,30 @@ from docutils.parsers.rst.states import SpecializedBody
 __docformat__: str
 
 class Include(Directive):
-    required_arguments: ClassVar[int]
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
-    option_spec: dict[str, Callable[[str], Any]]
     standard_include_path: Path
 
-class Raw(Directive):
-    required_arguments: ClassVar[int]
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
-    option_spec: dict[str, Callable[[str], Any]]
-    has_content: ClassVar[bool]
+class Raw(Directive): ...
 
-class Replace(Directive):
-    has_content: ClassVar[bool]
+class Replace(Directive): ...
 
 class Unicode(Directive):
-    required_arguments: ClassVar[int]
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
-    option_spec: dict[str, Callable[[str], Any]]
     comment_pattern: Pattern[str]
 
-class Class(Directive):
-    required_arguments: ClassVar[int]
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
-    has_content: ClassVar[bool]
+class Class(Directive): ...
 
 class Role(Directive):
-    has_content: ClassVar[bool]
     argument_pattern: Pattern[str]
 
-class DefaultRole(Directive):
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
+class DefaultRole(Directive): ...
 
-class Title(Directive):
-    required_arguments: ClassVar[int]
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
+class Title(Directive): ...
 
 class MetaBody(SpecializedBody):
     def __getattr__(self, name: str) -> Incomplete: ...
 
 class Meta(Directive):
-    has_content: ClassVar[bool]
     SMkwargs: ClassVar[dict[str, tuple[MetaBody]]]
 
-class Date(Directive):
-    has_content: bool
+class Date(Directive): ...
 
-class TestDirective(Directive):
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
-    option_spec: dict[str, Callable[[str], Any]]
-    has_content: ClassVar[bool]
+class TestDirective(Directive): ...
