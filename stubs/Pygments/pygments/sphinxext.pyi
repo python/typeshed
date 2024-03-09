a52564cab36be0dc76c8d6c0333ee7927dce1785
Tony Narlock <tony@git-pull.com>
2024-03-09 06:50:59 -0600

    `pygments`: Remove redundant annotations for `PygmentsDoc`
    `Directives` adding these as `ClassVar` eliminates the
    need for its subclasses to.
    
    Co-authored-by: danieleades <33452915+danieleades@users.noreply.github.com>

--- stubs/Pygments/pygments/sphinxext.pyi
+++ stubs/Pygments/pygments/sphinxext.pyi
@@ -1,4 +1,4 @@
-from typing import Any, ClassVar
+from typing import Any
 
 from docutils.parsers.rst import Directive
 
@@ -8,13 +8,7 @@ FMTERDOC: str
 FILTERDOC: str
 
 class PygmentsDoc(Directive):
-    has_content: ClassVar[bool]
-    required_arguments: ClassVar[int]
-    optional_arguments: ClassVar[int]
-    final_argument_whitespace: ClassVar[bool]
-    option_spec: ClassVar[Any]
     filenames: Any
-    def run(self): ...
     def document_lexers(self): ...
     def document_formatters(self): ...
     def document_filters(self): ...
