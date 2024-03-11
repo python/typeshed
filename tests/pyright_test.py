ec5f66af4371a00bc02ef5fb31cecc035b7890b3
Sebastian Rittau <srittau@rittau.biz>
2024-03-11 17:11:09 +0100

    Install pyright from PyPI

--- tests/pyright_test.py
+++ tests/pyright_test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
 import subprocess
 import sys
 from pathlib import Path
 
-import tomli
+from utils import colored
 
 _WELL_KNOWN_FILE = Path("tests", "pyright_test.py")
 
@@ -16,26 +14,12 @@ def main() -> None:
         print("pyright_test.py must be run from the typeshed root directory", file=sys.stderr)
         sys.exit(1)
 
-    # subprocess.run on Windows does not look in PATH.
-    npx = shutil.which("npx")
+    print(colored("Running: pyright --version", "blue"))
+    subprocess.run(["pyright", "--version"], check=True)
+    print()
 
-    if npx is None:
-        print("error finding npx; is Node.js installed?", file=sys.stderr)
-        sys.exit(1)
-
-    try:
-        subprocess.run([npx, "--version"])
-    except OSError:
-        print("error running npx; is Node.js installed?", file=sys.stderr)
-        sys.exit(1)
-
-    with open("pyproject.toml", "rb") as config:
-        pyright_version: str = tomli.load(config)["tool"]["typeshed"]["pyright_version"]
-
-    os.environ["PYRIGHT_PYTHON_FORCE_VERSION"] = pyright_version
-    command = [npx, f"pyright@{pyright_version}"] + sys.argv[1:]
-    print("Running:", " ".join(command))
-
+    command = ["pyright"] + sys.argv[1:]
+    print(colored(f"Running: {' '.join(command)}", "blue"))
     ret = subprocess.run(command).returncode
     sys.exit(ret)
 
