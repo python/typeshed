1acbacbe98fb4bc30701d27767aa62d9cf5bb872
Sebastian Rittau <srittau@rittau.biz>
2024-03-15 16:48:59 +0100

    Fix import

--- stubs/influxdb-client/influxdb_client/client/util/helpers.pyi
+++ stubs/influxdb-client/influxdb_client/client/util/helpers.pyi
@@ -1,4 +1,4 @@
-from influxdb_client.client.influx_db_client import InfluxDBClient
+from influxdb_client.client.influxdb_client import InfluxDBClient
 from influxdb_client.domain.organization import Organization
 
 def get_org_query_param(org: Organization | str | None, client: InfluxDBClient, required_id: bool = False) -> str: ...
