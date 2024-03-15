afe0f91d82787daa3ae504faff85e1d4cd740fd8
Daverball <dave@daverball.com>
2024-03-15 22:20:46 +0100

    Reverts changes to test_cases, since they're no longer necessary
    We could consider adding new ones, to test the functionality though

--- test_cases/stdlib/check_contextlib.py
+++ test_cases/stdlib/check_contextlib.py
@@ -1,21 +1,20 @@
 from __future__ import annotations
 
 from contextlib import ExitStack
-from typing import Optional
 from typing_extensions import assert_type
 
 
 # See issue #7961
-class Thing(ExitStack[Optional[bool]]):
+class Thing(ExitStack):
     pass
 
 
-stack: ExitStack[bool | None] = ExitStack()
+stack = ExitStack()
 thing = Thing()
 assert_type(stack.enter_context(Thing()), Thing)
-assert_type(thing.enter_context(ExitStack()), ExitStack[Optional[bool]])
+assert_type(thing.enter_context(ExitStack()), ExitStack)
 
 with stack as cm:
-    assert_type(cm, ExitStack[Optional[bool]])
+    assert_type(cm, ExitStack)
 with thing as cm2:
     assert_type(cm2, Thing)
