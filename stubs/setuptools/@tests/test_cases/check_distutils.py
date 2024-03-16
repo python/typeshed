566352593aeebaa3f31cb09740b36643a471c810
Alex Waygood <Alex.Waygood@Gmail.com>
2024-03-16 15:07:08 +0000

    Update stubs/setuptools/@tests/test_cases/check_distutils.py
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- stubs/setuptools/@tests/test_cases/check_distutils.py
+++ stubs/setuptools/@tests/test_cases/check_distutils.py
@@ -1,5 +1,7 @@
 import distutils.command.sdist
 import distutils.config
+from distutils.util import split_version
 
+s = split_version("")
 d = distutils.config.PyPIRCCommand
 c = distutils.command.sdist.sdist
