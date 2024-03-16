2783e7bc8c0178d9fa83308efaf95d761b8f91a6
Alex Waygood <alex.waygood@gmail.com>
2024-03-16 15:16:13 +0000

    Revert "Update stubs/setuptools/@tests/test_cases/check_distutils.py"
    This reverts commit 566352593aeebaa3f31cb09740b36643a471c810.

--- stubs/setuptools/@tests/test_cases/check_distutils.py
+++ stubs/setuptools/@tests/test_cases/check_distutils.py
@@ -1,7 +1,5 @@
 import distutils.command.sdist
 import distutils.config
-from distutils.util import split_version
 
-s = split_version("")
 d = distutils.config.PyPIRCCommand
 c = distutils.command.sdist.sdist
