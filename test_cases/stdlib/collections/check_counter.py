923d8e20cc5e74b19966aa7e7ec1365fcb354bf1
Sebastian Rittau <srittau@rittau.biz>
2024-03-20 07:02:35 +0100

    Add more tests

--- test_cases/stdlib/collections/check_counter.py
+++ test_cases/stdlib/collections/check_counter.py
@@ -13,12 +13,14 @@ assert_type(Counter([1, 2, 3]), Counter[int, int])
 
 int_c: Counter[str] = Counter()
 assert_type(int_c, Counter[str, int])
+assert_type(int_c["a"], int)
 int_c["a"] = 1
 int_c["a"] += 3
 int_c["a"] += 3.5  # type: ignore
 
 float_c = Counter(foo=42.2)
 assert_type(float_c, Counter[str, float])
+assert_type(float_c["a"], float)
 float_c["a"] = 1.0
 float_c["a"] += 3.0
 float_c["a"] += 42
@@ -26,6 +28,7 @@ float_c["a"] += "42"  # type: ignore
 
 custom_c: Counter[str, Foo] = Counter()
 assert_type(custom_c, Counter[str, Foo])
+assert_type(custom_c["a"], Foo)
 custom_c["a"] = Foo()
 custom_c["a"] += Foo()  # type: ignore
 custom_c["a"] += 42  # type: ignore
