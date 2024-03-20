e3369269901d010d97cf10a9d5f391d5b3210a8c
Sebastian Rittau <srittau@rittau.biz>
2024-03-20 07:14:40 +0100

    pyright is quite unhappy today ...

--- test_cases/stdlib/collections/check_counter.py
+++ test_cases/stdlib/collections/check_counter.py
@@ -7,19 +7,19 @@ class Foo: ...
 
 # Test the constructor
 # assert_type(Counter(), Counter[Never, int])  # pyright derives "Unknown" instead of "Never"
-assert_type(Counter(foo=42.2), Counter[str, float])
-assert_type(Counter({42: "bar"}), Counter[int, str])
-assert_type(Counter([1, 2, 3]), Counter[int, int])
+assert_type(Counter(foo=42.2), "Counter[str, float]")
+assert_type(Counter({42: "bar"}), "Counter[int, str]")
+assert_type(Counter([1, 2, 3]), "Counter[int, int]")
 
 int_c: Counter[str] = Counter()
-assert_type(int_c, Counter[str, int])
+assert_type(int_c, "Counter[str, int]")
 assert_type(int_c["a"], int)
 int_c["a"] = 1
 int_c["a"] += 3
 int_c["a"] += 3.5  # type: ignore
 
 float_c = Counter(foo=42.2)
-assert_type(float_c, Counter[str, float])
+assert_type(float_c, "Counter[str, float]")
 assert_type(float_c["a"], float)
 float_c["a"] = 1.0
 float_c["a"] += 3.0
@@ -27,7 +27,7 @@ float_c["a"] += 42
 float_c["a"] += "42"  # type: ignore
 
 custom_c: Counter[str, Foo] = Counter()
-assert_type(custom_c, Counter[str, Foo])
+assert_type(custom_c, "Counter[str, Foo]")
 assert_type(custom_c["a"], Foo)
 custom_c["a"] = Foo()
 custom_c["a"] += Foo()  # type: ignore
