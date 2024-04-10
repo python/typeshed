ca543ba22589abe6be8e64ccc16cf2975b6bea78
Avasam <samuel.06@hotmail.com>
2024-04-10 00:11:51 -0400

    Improved protoc call

--- scripts/sync_tensorflow_protobuf_stubs.sh
+++ scripts/sync_tensorflow_protobuf_stubs.sh
@@ -47,20 +47,20 @@ python3 -m pip install pre-commit mypy-protobuf=="$MYPY_PROTOBUF_VERSION"
 # Remove existing pyi
 find "$REPO_ROOT/stubs/tensorflow/" -name "*_pb2.pyi" -delete
 
-cd $TENSORFLOW_DIR
 # Folders here cover the more commonly used protobufs externally and
 # their dependencies. Tensorflow has more protobufs and can be added if requested.
-../protoc_install/bin/protoc \
+protoc_install/bin/protoc \
+  --proto_path="$TENSORFLOW_DIR" \
   --mypy_out "relax_strict_optional_primitives:$REPO_ROOT/stubs/tensorflow" \
-  tensorflow/compiler/xla/*.proto \
-  tensorflow/compiler/xla/service/*.proto \
-  tensorflow/core/example/*.proto \
-  tensorflow/core/framework/*.proto \
-  tensorflow/core/protobuf/*.proto \
-  tensorflow/core/protobuf/tpu/*.proto \
-  tensorflow/core/util/*.proto \
-  tensorflow/python/keras/protobuf/*.proto \
-  tensorflow/tsl/protobuf/*.proto
+  $TENSORFLOW_DIR/tensorflow/compiler/xla/*.proto \
+  $TENSORFLOW_DIR/tensorflow/compiler/xla/service/*.proto \
+  $TENSORFLOW_DIR/tensorflow/core/example/*.proto \
+  $TENSORFLOW_DIR/tensorflow/core/framework/*.proto \
+  $TENSORFLOW_DIR/tensorflow/core/protobuf/*.proto \
+  $TENSORFLOW_DIR/tensorflow/core/protobuf/tpu/*.proto \
+  $TENSORFLOW_DIR/tensorflow/core/util/*.proto \
+  $TENSORFLOW_DIR/tensorflow/python/keras/protobuf/*.proto \
+  $TENSORFLOW_DIR/tensorflow/tsl/protobuf/*.proto \
 
 # Cleanup after ourselves, this is a temp dir, but it can still grow fast if run multiple times
 rm -rf "$TMP_DIR"
@@ -71,21 +71,22 @@ cd "$REPO_ROOT"
 # included in the python wheel. They are likely only used for other
 # language builds. stubtest was used to identify them by looking for
 # ModuleNotFoundError.
-rm stubs/tensorflow/tensorflow/compiler/xla/service/hlo_execution_profile_data_pb2.pyi \
-   stubs/tensorflow/tensorflow/compiler/xla/service/hlo_profile_printer_data_pb2.pyi \
-   stubs/tensorflow/tensorflow/compiler/xla/service/test_compilation_environment_pb2.pyi \
-   stubs/tensorflow/tensorflow/compiler/xla/xla_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/autotuning_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/conv_autotuning_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/critical_section_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/eager_service_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/master_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/master_service_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/replay_log_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/tpu/compile_metadata_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/worker_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/protobuf/worker_service_pb2.pyi \
-   stubs/tensorflow/tensorflow/core/util/example_proto_fast_parsing_test_pb2.pyi
+rm \
+  stubs/tensorflow/tensorflow/compiler/xla/service/hlo_execution_profile_data_pb2.pyi \
+  stubs/tensorflow/tensorflow/compiler/xla/service/hlo_profile_printer_data_pb2.pyi \
+  stubs/tensorflow/tensorflow/compiler/xla/service/test_compilation_environment_pb2.pyi \
+  stubs/tensorflow/tensorflow/compiler/xla/xla_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/autotuning_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/conv_autotuning_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/critical_section_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/eager_service_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/master_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/master_service_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/replay_log_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/tpu/compile_metadata_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/worker_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/protobuf/worker_service_pb2.pyi \
+  stubs/tensorflow/tensorflow/core/util/example_proto_fast_parsing_test_pb2.pyi \
 
 sed --in-place="" \
     "s/extra_description = .*$/extra_description = \"Partially generated using [mypy-protobuf==$MYPY_PROTOBUF_VERSION](https:\/\/github.com\/nipunn1313\/mypy-protobuf\/tree\/v$MYPY_PROTOBUF_VERSION) on tensorflow==$TENSORFLOW_VERSION\"/" \
