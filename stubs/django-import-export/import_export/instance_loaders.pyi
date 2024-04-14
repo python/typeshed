0248aa5bee184e5bc19d4fb27b8e2fe2fcc884c4
Viicos <65306057+Viicos@users.noreply.github.com>
2024-04-14 11:05:10 +0200

    Finish work

--- stubs/django-import-export/import_export/instance_loaders.pyi
+++ stubs/django-import-export/import_export/instance_loaders.pyi
@@ -1,4 +1,3 @@
-from collections.abc import Mapping
 from typing import Any
 
 from django.db.models import Model, QuerySet
@@ -8,13 +7,13 @@ from .fields import Field
 from .resources import Resource
 
 class BaseInstanceLoader:
-    resource: Resource
+    resource: Resource[Any]
     dataset: Dataset | None
-    def __init__(self, resource: Resource, dataset: Dataset | None = None) -> None: ...
-    def get_instance(self, row: Mapping[str, Any]) -> Model | None: ...
+    def __init__(self, resource: Resource[Any], dataset: Dataset | None = None) -> None: ...
+    def get_instance(self, row: dict[str, Any]) -> Model | None: ...
 
 class ModelInstanceLoader(BaseInstanceLoader):
-    def get_queryset(self) -> QuerySet: ...
+    def get_queryset(self) -> QuerySet[Any]: ...
 
 class CachedInstanceLoader(ModelInstanceLoader):
     pk_field: Field
