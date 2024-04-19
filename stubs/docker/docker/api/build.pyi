c6457521b61fa9b43f71f22065d164a722006975
Kai Mueller <kai.mueller01@sap.com>
2024-04-19 12:24:45 +0000

    Fully type docker/api/build

--- stubs/docker/docker/api/build.pyi
+++ stubs/docker/docker/api/build.pyi
@@ -1,39 +1,53 @@
-from _typeshed import Incomplete
+from collections.abc import Generator
+from logging import Logger
+from typing import IO, Any, TypedDict
+from typing_extensions import NotRequired
 
-log: Incomplete
+log: Logger
+
+class _ContainerLimits(TypedDict):
+    memory: NotRequired[int]
+    memswap: NotRequired[int]
+    cpushares: NotRequired[int]
+    cpusetcpus: NotRequired[str]
+
+class _Filers(TypedDict):
+    dangling: NotRequired[bool]
+    until: NotRequired[str]
 
 class BuildApiMixin:
     def build(
         self,
-        path: Incomplete | None = None,
+        path: str | None = None,
         tag: str | None = None,
         quiet: bool = False,
-        fileobj: Incomplete | None = None,
+        fileobj: IO[Any] | None = None,
         nocache: bool = False,
         rm: bool = False,
-        timeout: Incomplete | None = None,
+        timeout: int | None = None,
         custom_context: bool = False,
-        encoding: Incomplete | None = None,
+        encoding: str | None = None,
         pull: bool = False,
         forcerm: bool = False,
-        dockerfile: Incomplete | None = None,
-        container_limits: Incomplete | None = None,
+        dockerfile: str | None = None,
+        container_limits: _ContainerLimits | None = None,
         decode: bool = False,
-        buildargs: Incomplete | None = None,
+        buildargs: dict[str, Any] | None = None,
         gzip: bool = False,
-        shmsize: Incomplete | None = None,
-        labels: Incomplete | None = None,
-        cache_from: Incomplete | None = None,
-        target: Incomplete | None = None,
-        network_mode: Incomplete | None = None,
-        squash: Incomplete | None = None,
-        extra_hosts: Incomplete | None = None,
-        platform: Incomplete | None = None,
-        isolation: Incomplete | None = None,
+        shmsize: int | None = None,
+        labels: dict[str, Any] | None = None,
+        # need to use list, because the type must be json serializable
+        cache_from: list[str] | None = None,
+        target: str | None = None,
+        network_mode: str | None = None,
+        squash: bool | None = None,
+        extra_hosts: dict[str, Any] | None = None,
+        platform: str | None = None,
+        isolation: str | None = None,
         use_config_proxy: bool = True,
-    ): ...
+    ) -> Generator[Any, None, None]: ...
     def prune_builds(
-        self, filters: Incomplete | None = None, keep_storage: Incomplete | None = None, all: Incomplete | None = None
-    ): ...
+        self, filters: _Filers | None = None, keep_storage: int | None = None, all: bool | None = None
+    ) -> dict[str, Any]: ...
 
-def process_dockerfile(dockerfile, path): ...
+def process_dockerfile(dockerfile: str | None, path: str) -> tuple[str | None, str | None]: ...
