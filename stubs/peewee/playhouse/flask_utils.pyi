0364730ca84c72949038814a9ba98f9a1d90ea6e
P M <10617122+pylipp@users.noreply.github.com>
2024-04-19 22:59:11 +0200

    Update stubs/peewee/playhouse/flask_utils.pyi
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- stubs/peewee/playhouse/flask_utils.pyi
+++ stubs/peewee/playhouse/flask_utils.pyi
@@ -1,20 +1,24 @@
 from _typeshed import Unused
 from collections.abc import Container
 
-from flask import Flask
+from typing import Any
+from typing_extensions import TypeAlias
 from peewee import Database, Model, Proxy
+
+# Is actually flask.Flask
+_Flask: TypeAlias = Any
 
 class FlaskDB:
     # Omitting undocumented base_model_class on purpose, use FlaskDB.Model instead
     database: Database | Proxy
     def __init__(
         self,
-        app: Flask | None = None,
+        app: _Flask | None = None,
         database: Database | Proxy | None = None,
         model_class: type[Model] = ...,
         excluded_routes: Container[str] | None = None,
     ) -> None: ...
-    def init_app(self, app: Flask) -> None: ...
+    def init_app(self, app: _Flask) -> None: ...
     def get_model_class(self) -> type[Model]: ...
     @property
     def Model(self) -> type[Model]: ...
