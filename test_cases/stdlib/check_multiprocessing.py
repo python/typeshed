78280b80e34f8619d8ae6d7ed29f4554c10f278c
Patrick Rauscher <prauscher@prauscher.de>
2024-04-25 12:27:51 +0200

    extend test cases for multiprocessing after suggestion by @srittau

--- test_cases/stdlib/check_multiprocessing.py
+++ test_cases/stdlib/check_multiprocessing.py
@@ -7,6 +7,8 @@ from typing_extensions import assert_type
 
 string = Array(c_char, 12)
 assert_type(string, SynchronizedString)
+assert_type(string.value, bytes)
 
 field = Value(c_float, 0.0)
 assert_type(field, Synchronized[float])
+field.value = 1.2
