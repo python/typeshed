855f0679b2c4eae0167f1f015efbb89ac2191a9b
Philipp <49057914+truePhilipp@users.noreply.github.com>
2024-05-02 20:02:18 +0200

    Update formatter.pyi

--- stubs/beautifulsoup4/bs4/formatter.pyi
+++ stubs/beautifulsoup4/bs4/formatter.pyi
@@ -34,6 +34,7 @@ class HTMLFormatter(Formatter):
         entity_substitution: _EntitySubstitution | None = ...,
         void_element_close_prefix: str = ...,
         cdata_containing_tags: list[str] | None = ...,
+        empty_attributes_are_booleans: bool = False,
         indent: int = 1,
     ) -> None: ...
 
@@ -44,5 +45,6 @@ class XMLFormatter(Formatter):
         entity_substitution: _EntitySubstitution | None = ...,
         void_element_close_prefix: str = ...,
         cdata_containing_tags: list[str] | None = ...,
+        empty_attributes_are_booleans: bool = False,
         indent: int = 1,
     ) -> None: ...
