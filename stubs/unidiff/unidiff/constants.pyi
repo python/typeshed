f331ee60335e56a88884028fa22b7f1b633ab25a
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-05-04 07:11:41 -0700

    Final constants

--- stubs/unidiff/unidiff/constants.pyi
+++ stubs/unidiff/unidiff/constants.pyi
@@ -1,4 +1,5 @@
 from re import Pattern
+from typing import Final
 
 RE_SOURCE_FILENAME: Pattern[str]
 RE_TARGET_FILENAME: Pattern[str]
@@ -12,11 +13,12 @@ RE_HUNK_BODY_LINE: Pattern[str]
 RE_HUNK_EMPTY_BODY_LINE: Pattern[str]
 RE_NO_NEWLINE_MARKER: Pattern[str]
 RE_BINARY_DIFF: Pattern[str]
-DEFAULT_ENCODING: str
-DEV_NULL: str
-LINE_TYPE_ADDED: str
-LINE_TYPE_REMOVED: str
-LINE_TYPE_CONTEXT: str
-LINE_TYPE_EMPTY: str
-LINE_TYPE_NO_NEWLINE: str
-LINE_VALUE_NO_NEWLINE: str
+
+DEFAULT_ENCODING: Final = "UTF-8"
+DEV_NULL: Final = "/dev/null"
+LINE_TYPE_ADDED: Final = "+"
+LINE_TYPE_REMOVED: Final = "-"
+LINE_TYPE_CONTEXT: Final = " "
+LINE_TYPE_EMPTY: Final = ""
+LINE_TYPE_NO_NEWLINE: Final = "\\"
+LINE_VALUE_NO_NEWLINE: Final = " No newline at end of file"
