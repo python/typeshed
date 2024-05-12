ede966fa80172fe98d07468c08b71e394274c331
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-05-13 01:22:24 +0530

    stubs: [pyflakes] add backwards compatibility module for pyflakes

--- stubs/pyflakes/pyflakes/scripts/pyflakes.pyi
+++ stubs/pyflakes/pyflakes/scripts/pyflakes.pyi
@@ -1,2 +1,2 @@
-__all__ = ['check', 'checkPath', 'checkRecursive', 'iterSourceCode', 'main']
+__all__ = ["check", "checkPath", "checkRecursive", "iterSourceCode", "main"]
 from pyflakes.api import check, checkPath, checkRecursive, iterSourceCode, main
