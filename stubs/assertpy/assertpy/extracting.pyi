dcf10b5d1c5c29e74ed8fea2f2521df29428fa2b
Raphael Castro <rafaphcastro@gmail.com>
2024-05-15 04:02:10 +0000

    Fix str_types for extracting

--- stubs/assertpy/assertpy/extracting.pyi
+++ stubs/assertpy/assertpy/extracting.pyi
@@ -1,10 +1,9 @@
-from _typeshed import Incomplete
 from collections.abc import Iterable as _Iterable
 from typing import Any
 from typing_extensions import Self, TypeAlias
 
 Iterable: TypeAlias = _Iterable[Any]
-str_types: Incomplete
+str_types: tuple[type[str]]
 __tracebackhide__: bool
 
 class ExtractingMixin:
