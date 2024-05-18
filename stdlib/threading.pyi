3848f982e2165f9215f45d4f9fb05789e57efdef
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2024-05-18 13:27:51 -0700

    Update stdlib/threading.pyi
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- stdlib/threading.pyi
+++ stdlib/threading.pyi
@@ -109,10 +109,9 @@ class Lock:
     def acquire(self, blocking: bool = ..., timeout: float = ...) -> bool: ...
     def release(self) -> None: ...
     def locked(self) -> bool: ...
-    if sys.version_info >= (3, 13):
-        def acquire_lock(self, blocking: bool = ..., timeout: float = ...) -> bool: ...  # undocumented
-        def release_lock(self) -> None: ...  # undocumented
-        def locked_lock(self) -> bool: ...  # undocumented
+    def acquire_lock(self, blocking: bool = ..., timeout: float = ...) -> bool: ...  # undocumented
+    def release_lock(self) -> None: ...  # undocumented
+    def locked_lock(self) -> bool: ...  # undocumented
 
 @final
 class _RLock:
