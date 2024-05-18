b02dc7514cba5b415587a9888674a3a5e7bfc248
Avasam <samuel.06@hotmail.com>
2024-05-18 13:24:23 -0400

    Update dep_util

--- stubs/setuptools/setuptools/_distutils/dep_util.pyi
+++ stubs/setuptools/setuptools/_distutils/dep_util.pyi
@@ -1,12 +1,1 @@
-from _typeshed import FileDescriptorOrPath, StrOrBytesPath, SupportsLenAndGetItem
-from collections.abc import Iterable
-from typing import Literal, TypeVar
-
-_T1 = TypeVar("_T1")
-_T2 = TypeVar("_T2")
-
-def newer(source: StrOrBytesPath, target: FileDescriptorOrPath) -> bool: ...
-def newer_pairwise(sources: SupportsLenAndGetItem[_T1], targets: SupportsLenAndGetItem[_T2]) -> tuple[list[_T1], list[_T2]]: ...
-def newer_group(
-    sources: Iterable[FileDescriptorOrPath], target: FileDescriptorOrPath, missing: Literal["error", "ignore", "newer"] = "error"
-) -> bool: ...
+from ._modified import newer as newer, newer_group as newer_group, newer_pairwise as newer_pairwise
