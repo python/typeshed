c8a5205b91d3420b54aaf6d1dedcf665a2d9bc10
hauntsaninja <hauntsaninja@gmail.com>
2024-05-18 18:35:01 -0400

    .

--- stdlib/stat.pyi
+++ stdlib/stat.pyi
@@ -1,5 +1,7 @@
+import sys
 from _stat import *
 from typing import Literal
 
-# https://github.com/python/cpython/issues/114081#issuecomment-2119017790
-SF_RESTRICTED: Literal[0x00080000]
+if sys.version_info >= (3, 13):
+    # https://github.com/python/cpython/issues/114081#issuecomment-2119017790
+    SF_RESTRICTED: Literal[0x00080000]
