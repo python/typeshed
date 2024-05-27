985a6037023eee6d162955978c82fd87b1fae296
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-05-26 19:28:54 -0700

    Fix

--- stdlib/@tests/test_cases/check_math.py
+++ stdlib/@tests/test_cases/check_math.py
@@ -1,14 +1,12 @@
-from __future__ import annotations
-
 import math
-from typing import Literal
+from typing import Literal, Union
 from decimal import Decimal
 from typing_extensions import assert_type
 
 
 assert_type(math.prod([]), Literal[1])
 assert_type(math.prod([1]), int)
-assert_type(math.prod([34.34]), float | Literal[1])
-assert_type(math.prod([34.34, 1]), float | int)
-assert_type(math.prod([34.34, Decimal(1)]), float | Decimal | Literal[1])
+assert_type(math.prod([34.34]), Union[float, Literal[1]])
+assert_type(math.prod([34.34, 1]), Union[float, int])
+assert_type(math.prod([34.34, Decimal(1)]), Union[float, Decimal, Literal[1]])
 assert_type(math.prod([Decimal(1)], start=Decimal(1)), Decimal)
