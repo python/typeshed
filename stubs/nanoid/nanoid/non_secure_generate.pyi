86da076744c980cd8c58caa94b1eca4b01b0394e
Oliver Lambson <oliverlambson@gmail.com>
2024-05-31 22:16:18 +0100

    fmt

--- stubs/nanoid/nanoid/non_secure_generate.pyi
+++ stubs/nanoid/nanoid/non_secure_generate.pyi
@@ -1,4 +1,3 @@
 def non_secure_generate(
-    alphabet: str = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",  # noqa: Y053
-    size: int = 21,
+    alphabet: str = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", size: int = 21  # noqa: Y053
 ) -> str: ...
