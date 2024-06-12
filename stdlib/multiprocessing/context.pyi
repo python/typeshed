4ea5e41871382a922bb2a9c409e45defbfb9f463
Patrick Rauscher <prauscher@prauscher.de>
2024-06-12 13:39:32 +0200

    remove double Array-overload

--- stdlib/multiprocessing/context.pyi
+++ stdlib/multiprocessing/context.pyi
@@ -93,10 +93,6 @@ class BaseContext:
     def Value(self, typecode_or_type: str | type[_CData], *args: Any, lock: bool | _LockLike = True) -> Any: ...
     @overload
     def Array(
-        self, typecode_or_type: type[c_char], size_or_initializer: int | Sequence[Any], *, lock: Literal[True] | _LockLike = True
-    ) -> SynchronizedString: ...
-    @overload
-    def Array(
         self, typecode_or_type: type[_CT], size_or_initializer: int | Sequence[Any], *, lock: Literal[False]
     ) -> SynchronizedArray[_CT]: ...
     @overload
