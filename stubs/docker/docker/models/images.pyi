28d4d228077dc1ec6e2db08a9cc8f1380f375fbe
Adam Dangoor <adamdangoor@gmail.com>
2024-06-24 10:52:33 +0100

    Add parameter types to docker.models.images.ImageCollection.build
    At
    https://github.com/docker/docker-py/blob/a3652028b1ead708bd9191efb286f909ba6c2a49/docker/models/images.py#L296
    the kwargs are passed through to docker.api.build.BuildApiMixin,
    so I copied the types from there.

--- stubs/docker/docker/models/images.pyi
+++ stubs/docker/docker/models/images.pyi
@@ -1,10 +1,21 @@
 from collections.abc import Iterator
-from typing import Any, Literal, overload
+from io import StringIO
+from typing import IO, Any, Literal, TypedDict, overload, type_check_only
 from typing_extensions import TypeAlias
 
 from .resource import Collection, Model
 
 _ImageList: TypeAlias = list[Image]  # To resolve conflicts with a method called "list"
+# Type alias for JSON, explained at:
+# https://github.com/python/typing/issues/182#issuecomment-1320974824.
+_JSON: TypeAlias = dict[str, _JSON] | list[_JSON] | str | int | float | bool | None
+
+@type_check_only
+class _ContainerLimits(TypedDict, total=False):
+    memory: int
+    memswap: int
+    cpushares: int
+    cpusetcpus: str
 
 class Image(Model):
     @property
@@ -31,7 +42,35 @@ class RegistryData(Model):
 
 class ImageCollection(Collection[Image]):
     model: type[Image]
-    def build(self, **kwargs) -> tuple[Image, Iterator[Any]]: ...
+    def build(
+        self,
+        *,
+        path: str | None = None,
+        fileobj: StringIO | IO[bytes] | None = None,
+        tag: str | None = None,
+        quiet: bool = False,
+        nocache: bool = False,
+        rm: bool = False,
+        timeout: int | None = None,
+        custom_context: bool = False,
+        encoding: str | None = None,
+        pull: bool = False,
+        forcerm: bool = False,
+        dockerfile: str | None = None,
+        buildargs: dict[str, Any] | None = None,
+        container_limits: _ContainerLimits | None = None,
+        shmsize: int | None = None,
+        labels: dict[str, Any] | None = None,
+        # need to use list, because the type must be json serializable
+        cache_from: list[str] | None = None,
+        target: str | None = None,
+        network_mode: str | None = None,
+        squash: bool | None = None,
+        extra_hosts: list[str] | dict[str, str] | None = None,
+        platform: str | None = None,
+        isolation: str | None = None,
+        use_config_proxy: bool = True,
+    ) -> tuple[Image, Iterator[_JSON]]: ...
     def get(self, name: str) -> Image: ...
     def get_registry_data(self, name, auth_config: dict[str, Any] | None = None) -> RegistryData: ...
     def list(self, name: str | None = None, all: bool = False, filters: dict[str, Any] | None = None) -> _ImageList: ...
