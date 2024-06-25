88563b95cd453d2a4db411a9b660f122bbff9c7b
Sebastian Rittau <srittau@rittau.biz>
2024-06-25 13:53:54 +0200

    Revert changes to re

--- stdlib/re.pyi
+++ stdlib/re.pyi
@@ -233,9 +233,17 @@ if sys.version_info >= (3, 11):
     NOFLAG = RegexFlag.NOFLAG
 _FlagsType: TypeAlias = int | RegexFlag
 
+# Type-wise the compile() overloads are unnecessary, they could also be modeled using
+# unions in the parameter types. However mypy has a bug regarding TypeVar
+# constraints (https://github.com/python/mypy/issues/11880),
+# which limits us here because AnyStr is a constrained TypeVar.
+
 # pattern arguments do *not* accept arbitrary buffers such as bytearray,
 # because the pattern must be hashable.
-def compile(pattern: AnyStr | Pattern[AnyStr], flags: _FlagsType = 0) -> Pattern[AnyStr]: ...
+@overload
+def compile(pattern: AnyStr, flags: _FlagsType = 0) -> Pattern[AnyStr]: ...
+@overload
+def compile(pattern: Pattern[AnyStr], flags: _FlagsType = 0) -> Pattern[AnyStr]: ...
 @overload
 def search(pattern: str | Pattern[str], string: str, flags: _FlagsType = 0) -> Match[str] | None: ...
 @overload
