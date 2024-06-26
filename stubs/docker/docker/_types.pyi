0c66a4590c458b5fa2d9a2d9123e7f023848d06c
Adam Dangoor <adamdangoor@gmail.com>
2024-06-26 07:00:26 +0100

    Add kwarg types to Docker container run and create
    Implementations:
    
    * `run`: https://github.com/docker/docker-py/blob/a3652028b1ead708bd9191efb286f909ba6c2a49/docker/models/containers.py#L534
    * `create`: https://github.com/docker/docker-py/blob/a3652028b1ead708bd9191efb286f909ba6c2a49/docker/models/containers.py#L913
    
    Notes:
    
    `create`'s docstring says:
    
    ```
    Takes the same arguments as :py:meth:`run`, except for ``stdout``,
    ``stderr``, and ``remove``.
    ```
    
    However, the implementation includes:
    
    ```
    kwargs['version'] = self.client.api._version
    ```
    
    so I did not include `version` for `create`.

--- stubs/docker/docker/_types.pyi
+++ stubs/docker/docker/_types.pyi
@@ -1,8 +1,15 @@
 # Internal-use module for types shared by multiple modules.
 # This does not match a module in docker-py.
 
+from pathlib import Path
+from typing import TypedDict, type_check_only
 from typing_extensions import TypeAlias
 
 # Type alias for JSON, explained at:
 # https://github.com/python/typing/issues/182#issuecomment-1320974824.
 JSON: TypeAlias = dict[str, JSON] | list[JSON] | str | int | float | bool | None
+
+@type_check_only
+class ContainerWeightDevice(TypedDict):
+    Path: Path
+    Weight: int
