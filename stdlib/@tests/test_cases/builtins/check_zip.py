ff837242529cf45b4f027ca83131e962d9f5edc7
Alexander Daryin <shurick@google.com>
2024-06-28 13:55:51 +0200

    Update check_zip.py

--- stdlib/@tests/test_cases/builtins/check_zip.py
+++ stdlib/@tests/test_cases/builtins/check_zip.py
@@ -1,8 +1,13 @@
-from typing import Tuple
+from typing import Sequence, Tuple
 from typing_extensions import assert_type
 
-assert_type(zip((1, 2)), zip[Tuple[int]])
-assert_type(zip([1, 2], ("3", "4")), zip[Tuple[int, str]])
-assert_type(zip([1], ["2"], [3.0]), zip[Tuple[int, str, float]])
-assert_type(zip(["1"], [2], [3.0], [4]), zip[Tuple[str, int, float, int]])
-assert_type(zip(["1"], [2], [3.0], [4], [("5",)]), zip[Tuple[str, int, float, int, Tuple[str]]])
+ints: Sequence[int] = [1, 2, 3]
+strs: Sequence[str] = ["one", "two", "three"]
+floats: Sequence[float] = [1.0, 2.0, 3.0]
+str_tuples: Sequence[Tuple[str]] = [("one",), ("two",), ("three",)]
+
+assert_type(zip(ints), zip[Tuple[int]])
+assert_type(zip(ints, strs), zip[Tuple[int, str]])
+assert_type(zip(ints, strs, floats), zip[Tuple[int, str, float]])
+assert_type(zip(strs, ints, floats, ints), zip[Tuple[str, int, float, int]])
+assert_type(zip(strs, ints, floats, ints, str_tuples)]), zip[Tuple[str, int, float, int, Tuple[str]]])
