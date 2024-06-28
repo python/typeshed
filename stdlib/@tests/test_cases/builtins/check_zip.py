28ee4e58be3f0c7a9c3c4249ffb70b6da27233b1
Alexander Daryin <shurick@google.com>
2024-06-28 13:10:57 +0200

    Create check_zip.py

---
+++ stdlib/@tests/test_cases/builtins/check_zip.py
@@ -0,0 +1,9 @@
+from typing_extensions import assert_type
+
+from typing import Tuple
+
+assert_type(zip((1, 2)), zip[Tuple[int]])
+assert_type(zip([1, 2], ("3", "4")), zip[Tuple[int, str]])
+assert_type(zip([1], ["2"], [3.0]), zip[Tuple[int, str, float]])
+assert_type(zip(["1"], [2], [3.0], [4]), zip[Tuple[str, int, float, int]])
+assert_type(zip(["1"], [2], [3.0], [4], [("5",)]), zip[Tuple[str, int, float, int, Tuple[str]]])
