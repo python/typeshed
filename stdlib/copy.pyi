892c7b89fe9d427042571d8e22e3fb073d498417
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-07-02 19:26:43 -0500

    Add `copy.replace`

--- stdlib/copy.pyi
+++ stdlib/copy.pyi
@@ -1,3 +1,4 @@
+import sys
 from typing import Any, TypeVar
 
 __all__ = ["Error", "copy", "deepcopy"]
@@ -10,6 +11,9 @@ PyStringMap: Any
 # Note: memo and _nil are internal kwargs.
 def deepcopy(x: _T, memo: dict[int, Any] | None = None, _nil: Any = []) -> _T: ...
 def copy(x: _T) -> _T: ...
+
+if sys.version_info >= (3, 13):
+    def replace(obj: _T, /, **changes: Any) -> _T: ...
 
 class Error(Exception): ...
 
