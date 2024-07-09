4d1dfca791bf6ae2b4daf1f893644c8096a96fe0
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-07-08 23:09:14 -0500

    Add tests

---
+++ stdlib/@tests/test_cases/check_importlib_resources.py
@@ -0,0 +1,33 @@
+import sys
+import importlib.resources
+import pathlib
+
+
+class _CustomPathLike:
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def __fspath__(self) -> str:
+        return self.path
+
+
+if sys.version_info >= (3, 13):
+
+    def f(pth: pathlib.Path | str | _CustomPathLike) -> None:
+        importlib.resources.open_binary("pkg", pth)
+        # Encoding defaults to "utf-8" for one arg.
+        importlib.resources.open_text("pkg", pth)
+        # Otherwise, it must be specified.
+        importlib.resources.open_text("pkg", pth, pth)  # type: ignore
+        importlib.resources.open_text("pkg", pth, pth, encoding="utf-8")
+
+        # Encoding defaults to "utf-8" for one arg.
+        importlib.resources.read_text("pkg", pth)
+        # Otherwise, it must be specified.
+        importlib.resources.read_text("pkg", pth, pth)  # type: ignore
+        importlib.resources.read_text("pkg", pth, pth, encoding="utf-8")
+
+        importlib.resources.read_binary("pkg", pth)
+        importlib.resources.path("pkg", pth)
+        importlib.resources.is_resource("pkg", pth)
+        importlib.resources.contents("pkg", pth)
