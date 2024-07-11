76720eca1f4b3c46779432c3cf7d535063886481
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-07-10 23:59:37 -0500

    PyType bug?

--- stdlib/re.pyi
+++ stdlib/re.pyi
@@ -230,7 +230,7 @@ if sys.version_info < (3, 13):
     T: Final = RegexFlag.T
     TEMPLATE: Final = RegexFlag.TEMPLATE
 if sys.version_info >= (3, 11):
-    NOFLAG: Final = RegexFlag.NOFLAG
+    NOFLAG = RegexFlag.NOFLAG
 _FlagsType: TypeAlias = int | RegexFlag
 
 # Type-wise the compile() overloads are unnecessary, they could also be modeled using
