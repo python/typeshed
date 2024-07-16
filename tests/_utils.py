6529a9a29863b5a556d584a15503a2ee126ce5ac
Sebastian Rittau <srittau@rittau.biz>
2024-07-16 12:44:43 +0200

    Py 3.8 compat

--- tests/_utils.py
+++ tests/_utils.py
@@ -7,7 +7,7 @@ import sys
 from collections.abc import Iterable, Mapping
 from functools import lru_cache
 from pathlib import Path
-from typing import Any, Final, NamedTuple, Tuple
+from typing import Any, Dict, Final, NamedTuple, Tuple
 from typing_extensions import TypeAlias
 
 import pathspec
@@ -108,7 +108,7 @@ def get_mypy_req() -> str:
 # ====================================================================
 
 VersionTuple: TypeAlias = Tuple[int, int]
-SupportedVersionsDict: TypeAlias = dict[str, tuple[VersionTuple, VersionTuple]]
+SupportedVersionsDict: TypeAlias = Dict[str, Tuple[VersionTuple, VersionTuple]]
 
 VERSIONS_PATH = STDLIB_PATH / "VERSIONS"
 VERSION_LINE_RE = re.compile(r"^([a-zA-Z_][a-zA-Z0-9_.]*): ([23]\.\d{1,2})-([23]\.\d{1,2})?$")
