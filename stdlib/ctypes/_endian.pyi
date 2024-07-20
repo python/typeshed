8829053501246bf8c578933d52c15a691a0673a1
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-07-20 12:21:23 -0500

    Tweak

--- stdlib/ctypes/_endian.pyi
+++ stdlib/ctypes/_endian.pyi
@@ -6,10 +6,10 @@ else:
     from _ctypes import RTLD_GLOBAL as RTLD_GLOBAL, RTLD_LOCAL as RTLD_LOCAL, Structure, Union
     from ctypes import DEFAULT_MODE as DEFAULT_MODE, cdll as cdll, pydll as pydll, pythonapi as pythonapi
 
-if sys.version_info >= (3, 12):
+if sys.version_info >= (3, 12) and sys.version_info < (3, 13):
     from _ctypes import SIZEOF_TIME_T as SIZEOF_TIME_T
 
-if sys.platform == "win32":
+if sys.platform == "win32" and sys.version_info < (3, 13):
     from ctypes import oledll as oledll, windll as windll
 
 # At runtime, the native endianness is an alias for Structure,
