adb8d1059194c829cae6ea9a1ee03bec85746639
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-07-20 23:59:18 -0500

    Remove as imports

--- stdlib/ctypes/_endian.pyi
+++ stdlib/ctypes/_endian.pyi
@@ -1,15 +1,5 @@
 import sys
-from _ctypes import Structure, Union
-
-if sys.version_info < (3, 13):
-    from _ctypes import RTLD_GLOBAL as RTLD_GLOBAL, RTLD_LOCAL as RTLD_LOCAL
-    from ctypes import DEFAULT_MODE as DEFAULT_MODE, cdll as cdll, pydll as pydll, pythonapi as pythonapi
-
-if sys.version_info >= (3, 12) and sys.version_info < (3, 13):
-    from _ctypes import SIZEOF_TIME_T as SIZEOF_TIME_T
-
-if sys.platform == "win32" and sys.version_info < (3, 13):
-    from ctypes import oledll as oledll, windll as windll
+from ctypes import Structure, Union
 
 # At runtime, the native endianness is an alias for Structure,
 # while the other is a subclass with a metaclass added in.
