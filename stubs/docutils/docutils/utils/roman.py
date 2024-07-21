3dd37db4f767274ea0cb84b9f4226b4b6ca1af0f
Daniel Eades <danieleades@hotmail.com>
2024-07-21 13:27:04 +0100

    add type annotations for 'docutils.utils.roman'

---
+++ stubs/docutils/docutils/utils/roman.py
@@ -0,0 +1,10 @@
+import re
+
+# Define digit mapping
+romanNumeralMap: tuple[tuple[str, int],...]
+
+def toRoman(n: int) -> str: ...
+
+romanNumeralPattern: re.Pattern[str]
+
+def fromRoman(s: str) -> int: ...
