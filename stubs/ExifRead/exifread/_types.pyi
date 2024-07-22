7bd36133d44c58d4934ef8777bca7b37a85e1a3f
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-07-22 09:07:36 -0700

    Update stubs/ExifRead/exifread/_types.pyi

--- stubs/ExifRead/exifread/_types.pyi
+++ stubs/ExifRead/exifread/_types.pyi
@@ -1,6 +1,7 @@
 # Stubs-only module with type aliases for ExifRead.
 
-from typing import Any, Iterator, Literal, Protocol
+from collections.abc import Iterator
+from typing import Any, Literal, Protocol
 from typing_extensions import TypeAlias
 
 # The second item of the value tuple - if it exists - can be a variety of types,
