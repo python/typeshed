8065ada4e76abddca6fdacc1ff4272d1fa3d3f0e
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-22 12:49:49 -0300

    fix(exifreader): Makes Reader.__iter__() return Iterator[bytes]

--- stubs/ExifRead/exifread/_types.pyi
+++ stubs/ExifRead/exifread/_types.pyi
@@ -1,6 +1,6 @@
 # Stubs-only module with type aliases for ExifRead.
 
-from typing import Any, Literal, Protocol
+from typing import Any, Literal, Iterator, Protocol
 from typing_extensions import TypeAlias
 
 # The second item of the value tuple - if it exists - can be a variety of types,
@@ -8,7 +8,7 @@ from typing_extensions import TypeAlias
 TagDict: TypeAlias = dict[int, tuple[str] | tuple[str, Any]]
 
 class Reader(Protocol):
-    def __iter__(self) -> bytes: ...
+    def __iter__(self) -> Iterator[bytes]: ...
     def read(self, size: int, /) -> bytes: ...
     def tell(self) -> int: ...
     def seek(self, offset: int, whence: Literal[0, 1] = ..., /) -> object: ...
