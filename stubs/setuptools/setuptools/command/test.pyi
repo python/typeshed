f5d66771e90fe7a0d4f552698945afd2fb400f03
Sebastian Rittau <srittau@rittau.biz>
2024-07-30 18:14:09 +0200

    Update stubs/setuptools/setuptools/command/test.pyi
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- stubs/setuptools/setuptools/command/test.pyi
+++ stubs/setuptools/setuptools/command/test.pyi
@@ -4,7 +4,10 @@ from typing_extensions import deprecated
 from .. import Command
 
 @deprecated(
-    "The test command is disabled and references to it are deprecated. Please remove any references to `setuptools.command.test` in all supported versions of the affected package."
+    """\
+The test command is disabled and references to it are deprecated. \
+Please remove any references to `setuptools.command.test` in all supported versions of the affected package.\
+"""
 )
 class _test(Command):
     description: ClassVar[str]
