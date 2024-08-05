a002c84c2236e7a5995819e70fdc84dbae0c77e5
Moritz Marseu <moritz.marseu@festo.com>
2024-08-05 16:03:19 +0200

    Update jsonschema.protocols

--- stubs/jsonschema/jsonschema/protocols.pyi
+++ stubs/jsonschema/jsonschema/protocols.pyi
@@ -1,29 +1,34 @@
 from _typeshed import Incomplete
 from collections.abc import Iterator, Mapping, Sequence
 from typing import ClassVar, Protocol
-from typing_extensions import TypeAlias
+from typing_extensions import TypeAlias, deprecated
 
+from jsonschema import _typing
 from jsonschema._format import FormatChecker
 from jsonschema._types import TypeChecker
 from jsonschema.exceptions import ValidationError
 from jsonschema.validators import RefResolver
+import referencing
 
 _JsonParameter: TypeAlias = str | int | float | bool | None | Mapping[str, _JsonParameter] | Sequence[_JsonParameter]
 
 class Validator(Protocol):
-    META_SCHEMA: ClassVar[dict[Incomplete, Incomplete]]
-    VALIDATORS: ClassVar[dict[Incomplete, Incomplete]]
+    META_SCHEMA: ClassVar[Mapping[Incomplete, Incomplete]]
+    VALIDATORS: ClassVar[Mapping[Incomplete, Incomplete]g]
     TYPE_CHECKER: ClassVar[TypeChecker]
     FORMAT_CHECKER: ClassVar[FormatChecker]
-    schema: dict[Incomplete, Incomplete] | bool
+    ID_OF: _typing.id_of
+    schema: Mapping[Incomplete, Incomplete] | bool
     def __init__(
         self,
-        schema: dict[Incomplete, Incomplete] | bool,
+        schema: Mapping[Incomplete, Incomplete] | bool,
+        registry: referencing.jsonschema.SchemaRegistry,
+        @deprecated("'RefResolver' has been deprecated in favor of 'referencing'")
         resolver: RefResolver | None = None,
         format_checker: FormatChecker | None = None,
     ) -> None: ...
     @classmethod
-    def check_schema(cls, schema: dict[Incomplete, Incomplete]) -> None: ...
+    def check_schema(cls, schema: Mapping[Incomplete, Incomplete]) -> None: ...
     def is_type(self, instance: _JsonParameter, type: str) -> bool: ...
     def is_valid(self, instance: _JsonParameter) -> bool: ...
     def iter_errors(self, instance: _JsonParameter) -> Iterator[ValidationError]: ...
