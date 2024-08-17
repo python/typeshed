79cccc59f11fe33b34d67daca073927d588e2030
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-08-17 14:43:34 -0500

    Fix

--- stdlib/pyexpat/__init__.pyi
+++ stdlib/pyexpat/__init__.pyi
@@ -15,9 +15,9 @@ class ExpatError(Exception):
     offset: int
 
 error = ExpatError
-XML_PARAM_ENTITY_PARSING_NEVER = 0
-XML_PARAM_ENTITY_PARSING_UNLESS_STANDALONE = 1
-XML_PARAM_ENTITY_PARSING_ALWAYS = 2
+XML_PARAM_ENTITY_PARSING_NEVER: Final = 0
+XML_PARAM_ENTITY_PARSING_UNLESS_STANDALONE: Final = 1
+XML_PARAM_ENTITY_PARSING_ALWAYS: Final = 2
 
 _Model: TypeAlias = tuple[int, int, str | None, tuple[Any, ...]]
 
