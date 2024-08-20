de6acd4d694ad56fb3cb37e4156317b755dda0fd
Avasam <samuel.06@hotmail.com>
2024-08-20 14:33:16 -0400

    Bump setuptools to 73.0.*

--- stubs/setuptools/setuptools/command/test.pyi
+++ stubs/setuptools/setuptools/command/test.pyi
@@ -9,11 +9,9 @@ The test command is disabled and references to it are deprecated. \
 Please remove any references to `setuptools.command.test` in all supported versions of the affected package.\
 """
 )
-class _test(Command):
+class test(Command):
     description: ClassVar[str]
     user_options: ClassVar[list[tuple[str, str, str]]]
     def initialize_options(self) -> None: ...
     def finalize_options(self) -> None: ...
     def run(self) -> None: ...
-
-test = _test
