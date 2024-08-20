fd03979c80c958995ca66c58631ac0c6f76256ea
sobolevn <mail@sobolevn.me>
2024-08-20 22:33:41 +0300

    Fix CI

--- stubs/Markdown/markdown/extensions/abbr.pyi
+++ stubs/Markdown/markdown/extensions/abbr.pyi
@@ -14,19 +14,23 @@ class AbbrExtension(Extension):
     def reset_glossary(self) -> None: ...
     def load_glossary(self, dictionary: dict[str, str]) -> None: ...
 
-# Techinically it is the same type as `AbbrPreprocessor` just not deprecated.
-class AbbrBlockprocessor(BlockProcessor):
-    RE: ClassVar[Pattern[str]]
-
 class AbbrTreeprocessor(Treeprocessor):
     RE: Pattern[str]
     abbrs: dict[str, str]
     def __init__(self, md: Markdown | None = None, abbrs: dict[str, str] | None = None) -> None: ...
     def iter_element(self, el: Element, parent: Element | None = None) -> None: ...
 
+# Techinically it is the same type as `AbbrPreprocessor` just not deprecated.
+class AbbrBlockprocessor(BlockProcessor):
+    RE: ClassVar[Pattern[str]]
+    abbrs: dict[str, str]
+    def __init__(self, parser: BlockParser, abbrs: dict[str, str]) -> None: ...
+
 @deprecated("This class will be removed in the future; use `AbbrTreeprocessor` instead.")
 class AbbrPreprocessor(BlockProcessor):
     RE: ClassVar[Pattern[str]]
+    abbrs: dict[str, str]
+    def __init__(self, parser: BlockParser, abbrs: dict[str, str]) -> None: ...
 
 @deprecated("This class will be removed in the future; use `AbbrTreeprocessor` instead.")
 class AbbrInlineProcessor(InlineProcessor):
