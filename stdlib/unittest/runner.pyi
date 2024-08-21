fda43a2ebbf5c11c61d7657b17af595f16c9ec4f
Avasam <samuel.06@hotmail.com>
2024-08-21 17:28:28 -0400

    Try making a protocol from _TextIOLike

--- stdlib/unittest/runner.pyi
+++ stdlib/unittest/runner.pyi
@@ -3,34 +3,40 @@ import unittest.case
 import unittest.result
 import unittest.suite
 from collections.abc import Callable, Iterable
-from typing import Any, TextIO
+from typing import Any, Generic, Protocol, TextIO, TypeVar, type_check_only
 from typing_extensions import Never, TypeAlias
 
-_ResultClassType: TypeAlias = Callable[[TextIO, bool, int], unittest.result.TestResult]
+# Build a Protocol from TextIO's methods.
+# `TextTestRunner.stream` defaults to `sys.stderr` which is typed as `TextIO`
+@type_check_only
+class _TextIOLike(TextIO, Protocol): ...  # type: ignore[misc] # pyright: ignore[reportGeneralTypeIssues]
+
+_ResultClassType: TypeAlias = Callable[[_TextIOLike, bool, int], unittest.result.TestResult]
+_StreamT = TypeVar("_StreamT", bound=_TextIOLike, default=_TextIOLike)
 
 # Note: doesn't actually inherit TextIO, but re-exposes all methods of the stream passed to __init__
-class _WritelnDecorator(TextIO):  # type: ignore[misc] # Is not abstract
-    def __init__(self, stream: TextIO) -> None: ...
+class _WritelnDecorator(_TextIOLike):  # type: ignore[misc] # Is not abstract
+    def __init__(self, stream: _TextIOLike) -> None: ...
     def __getattr__(self, attr: str) -> Any: ...  # Any attribute from the stream type passed to __init__
     def writeln(self, arg: str | None = None) -> str: ...
     # These attributes are prevented by __getattr__
     stream: Never
     __getstate__: Never
 
-class TextTestResult(unittest.result.TestResult):
+class TextTestResult(unittest.result.TestResult, Generic[_StreamT]):
     descriptions: bool  # undocumented
     dots: bool  # undocumented
     separator1: str
     separator2: str
     showAll: bool  # undocumented
-    stream: TextIO  # undocumented
+    stream: _StreamT  # undocumented
     if sys.version_info >= (3, 12):
         durations: unittest.result._DurationsType | None
         def __init__(
-            self, stream: TextIO, descriptions: bool, verbosity: int, *, durations: unittest.result._DurationsType | None = None
+            self, stream: _StreamT, descriptions: bool, verbosity: int, *, durations: unittest.result._DurationsType | None = None
         ) -> None: ...
     else:
-        def __init__(self, stream: TextIO, descriptions: bool, verbosity: int) -> None: ...
+        def __init__(self, stream: _StreamT, descriptions: bool, verbosity: int) -> None: ...
 
     def getDescription(self, test: unittest.case.TestCase) -> str: ...
     def printErrorList(self, flavour: str, errors: Iterable[tuple[unittest.case.TestCase, str]]) -> None: ...
@@ -49,7 +55,7 @@ class TextTestRunner:
         durations: unittest.result._DurationsType | None
         def __init__(
             self,
-            stream: TextIO | None = None,
+            stream: _TextIOLike | None = None,
             descriptions: bool = True,
             verbosity: int = 1,
             failfast: bool = False,
@@ -63,7 +69,7 @@ class TextTestRunner:
     else:
         def __init__(
             self,
-            stream: TextIO | None = None,
+            stream: _TextIOLike | None = None,
             descriptions: bool = True,
             verbosity: int = 1,
             failfast: bool = False,
