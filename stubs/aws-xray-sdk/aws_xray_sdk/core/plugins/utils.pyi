818688b8afe5425d708ce9c207aa49278f1ee3e8
Shamil <ashm.tech@proton.me>
2024-08-25 00:50:42 +0300

    Add stubs for aws_xray_sdk

--- stubs/aws-xray-sdk/aws_xray_sdk/core/plugins/utils.pyi
+++ stubs/aws-xray-sdk/aws_xray_sdk/core/plugins/utils.pyi
@@ -1,4 +1,6 @@
+from _typeshed import Incomplete
+
 module_prefix: str
 PLUGIN_MAPPING: dict[str, str]
 
-def get_plugin_modules(plugins: tuple) -> tuple | None: ...
+def get_plugin_modules(plugins) -> Incomplete | None: ...
