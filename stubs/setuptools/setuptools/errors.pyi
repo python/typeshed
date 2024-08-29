c47764dbed9713f4638ca3335057aa77db01bc49
Avasam <samuel.06@hotmail.com>
2024-08-29 12:11:48 -0400

    Address PR comments

--- stubs/setuptools/setuptools/errors.pyi
+++ stubs/setuptools/setuptools/errors.pyi
@@ -1,24 +1,22 @@
-from typing_extensions import TypeAlias
-
 from ._distutils import errors as _distutils_errors
 
-ByteCompileError: TypeAlias = _distutils_errors.DistutilsByteCompileError
-CCompilerError: TypeAlias = _distutils_errors.CCompilerError
-ClassError: TypeAlias = _distutils_errors.DistutilsClassError
-CompileError: TypeAlias = _distutils_errors.CompileError
-ExecError: TypeAlias = _distutils_errors.DistutilsExecError
-FileError: TypeAlias = _distutils_errors.DistutilsFileError
-InternalError: TypeAlias = _distutils_errors.DistutilsInternalError
-LibError: TypeAlias = _distutils_errors.LibError
-LinkError: TypeAlias = _distutils_errors.LinkError
-ModuleError: TypeAlias = _distutils_errors.DistutilsModuleError
-OptionError: TypeAlias = _distutils_errors.DistutilsOptionError
-PlatformError: TypeAlias = _distutils_errors.DistutilsPlatformError
-PreprocessError: TypeAlias = _distutils_errors.PreprocessError
-SetupError: TypeAlias = _distutils_errors.DistutilsSetupError
-TemplateError: TypeAlias = _distutils_errors.DistutilsTemplateError
-UnknownFileError: TypeAlias = _distutils_errors.UnknownFileError
-BaseError: TypeAlias = _distutils_errors.DistutilsError
+ByteCompileError = _distutils_errors.DistutilsByteCompileError
+CCompilerError = _distutils_errors.CCompilerError
+ClassError = _distutils_errors.DistutilsClassError
+CompileError = _distutils_errors.CompileError
+ExecError = _distutils_errors.DistutilsExecError
+FileError = _distutils_errors.DistutilsFileError
+InternalError = _distutils_errors.DistutilsInternalError
+LibError = _distutils_errors.LibError
+LinkError = _distutils_errors.LinkError
+ModuleError = _distutils_errors.DistutilsModuleError
+OptionError = _distutils_errors.DistutilsOptionError
+PlatformError = _distutils_errors.DistutilsPlatformError
+PreprocessError = _distutils_errors.PreprocessError
+SetupError = _distutils_errors.DistutilsSetupError
+TemplateError = _distutils_errors.DistutilsTemplateError
+UnknownFileError = _distutils_errors.UnknownFileError
+BaseError = _distutils_errors.DistutilsError
 
 class InvalidConfigError(OptionError): ...
 class RemovedConfigError(OptionError): ...
