e68d701d88f2c9161e1f3618d2ba743d6e601bec
Peter Bierma <zintensitydev@gmail.com>
2024-08-31 14:04:26 -0400

    Add stubs for objgraph.

---
+++ stubs/objgraph/objgraph.pyi
@@ -0,0 +1,99 @@
+from _typeshed import Incomplete
+from collections import defaultdict
+from collections.abc import Callable, Container, Iterable
+from types import ModuleType
+from typing import Final, Literal, TextIO, TypedDict
+from typing_extensions import NotRequired, TypeGuard, Unpack
+
+IS_INTERACTIVE: bool
+
+__author__: Final[str]
+__copyright__: Final[str]
+__license__: Final[str]
+__version__: Final[str]
+__date__: Final[str]
+
+type _Filter = Callable[[object], bool]
+
+class _StateDict(TypedDict):
+    old: defaultdict[str, set[int]]
+    current: defaultdict[str, set[int]]
+    new: defaultdict[str, set[int]]
+    shortnames: bool
+
+class _ChainKeywords(TypedDict):
+    obj: NotRequired[object]
+    predicate: NotRequired[_Filter]
+    max_depth: NotRequired[int]
+    extra_ignore: NotRequired[Iterable[int]]
+
+def count(typename: str, objects: Iterable[object] | None = None) -> int: ...
+def typestats(
+    objects: Iterable[object] | None = None, shortnames: bool = True, filter: _Filter | None = None
+) -> dict[str, int]: ...
+def most_common_types(
+    limit: int = 10, objects: Iterable[object] | None = None, shortnames: bool = True, filter: _Filter | None = None
+) -> list[tuple[str, int]]: ...
+def show_most_common_types(
+    limit: int = 10,
+    objects: Iterable[object] | None = None,
+    shortnames: bool = True,
+    file: TextIO | None = None,
+    filter: _Filter | None = None,
+) -> None: ...
+def growth(
+    limit: int = 10, peak_stats: dict[str, int] = {}, shortnames: bool = True, filter: _Filter | None = None
+) -> list[tuple[str, int]]: ...
+def show_growth(
+    limit: int = 10,
+    peak_stats: dict[str, int] = {},
+    shortnames: bool = True,
+    file: TextIO | None = None,
+    filter: _Filter | None = None,
+) -> None: ...
+def get_new_ids(
+    skip_update: bool = False,
+    limit: int = 10,
+    sortby: Literal["old", "current", "new", "deltas"] = "deltas",
+    shortnames: bool | None = None,
+    file: TextIO | None = None,
+    _state: _StateDict | None = None,
+) -> defaultdict[str, set[int]]: ...
+def get_leaking_objects(objects: Iterable[object] | None = None) -> list[object]: ...
+def by_type(typename: str, objects: Iterable[object] | None = None) -> list[object]: ...
+def at(addr: int) -> object | None: ...
+def at_addrs(address_set: Container[int]) -> list[object]: ...
+def find_ref_chain(obj: object, predicate: _Filter, max_depth: int = 20, extra_ignore: Iterable[int] = ()) -> list[object]: ...
+def find_backref_chain(
+    obj: object, predicate: _Filter, max_depth: int = 20, extra_ignore: Iterable[int] = ()
+) -> list[object]: ...
+def show_backrefs(
+    objs: object | list[object] | tuple[object],
+    max_depth: int = 3,
+    extra_ignore: Iterable[int] = (),
+    filter: _Filter | None = None,
+    too_many: int = 10,
+    highlight: object | None = None,
+    filename: str | None = None,
+    extra_info: Callable[[object], str] | None = None,
+    refcounts: bool = False,
+    shortnames: bool = True,
+    output: TextIO | None = None,
+    extra_node_attrs: Callable[[object], dict[str, str]] | None = None,
+) -> None | Incomplete: ...  # Can return graphviz.Source, but we don't have stubs for that
+def show_refs(
+    objs: object | list[object] | tuple[object],
+    max_depth: int = 3,
+    extra_ignore: Iterable[int] = (),
+    filter: _Filter | None = None,
+    too_many: int = 10,
+    highlight: object | None = None,
+    filename: str | None = None,
+    extra_info: Callable[[object], str] | None = None,
+    refcounts: bool = False,
+    shortnames: bool = True,
+    output: TextIO | None = None,
+    extra_node_attrs: Callable[[object], dict[str, str]] | None = None,
+) -> None | Incomplete: ...  # Same story here
+def show_chain(*chains: list[object], **kw: Unpack[_ChainKeywords]) -> None: ...
+def is_proper_module(obj: object) -> TypeGuard[ModuleType]: ...
