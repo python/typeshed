5ab3a74e0e596229421ba021f4504f9cd4abbffe
kasium <15907922+kasium@users.noreply.github.com>
2024-09-06 13:33:53 +0200

    Update stubs/python-jenkins/jenkins/plugins.pyi
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- stubs/python-jenkins/jenkins/plugins.pyi
+++ stubs/python-jenkins/jenkins/plugins.pyi
@@ -1,5 +1,6 @@
 from typing import Any
 
+# Any: Union of possible plugin values is too complex
 class Plugin(dict[str, Any]):
     # __init__ wraps dict.__init__  w/o changing the type signature
     def __setitem__(self, key: str, value: Any) -> None: ...
