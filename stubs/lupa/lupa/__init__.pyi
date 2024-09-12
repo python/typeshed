54a9e802a6b162d43298232bf2c566ae6c68f46d
RedContritio <RedContritio@qq.com>
2024-09-13 03:23:07 +0800

    update lupa.__init__.pyi

--- stubs/lupa/lupa/__init__.pyi
+++ stubs/lupa/lupa/__init__.pyi
@@ -2,11 +2,8 @@ from collections.abc import Iterator
 from contextlib import contextmanager as _contextmanager
 
 from .lua54 import *
-from .version import __version__ as version
 
 __all__ = [
-    "allow_lua_module_loading",
-    "version",
     # from lua54 (newest lib)
     "LUA_VERSION",
     "LUA_MAXINTEGER",
@@ -21,6 +18,3 @@ __all__ = [
     "unpacks_lua_table",
     "unpacks_lua_table_method",
 ]
-
-@_contextmanager
-def allow_lua_module_loading() -> Iterator[None]: ...
