4d21f2dea2660b32cd97de4abe7f30553fd8771c
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-10-04 18:04:39 -0700

    _dispatch -> _dispatchable

--- stubs/networkx/networkx/algorithms/shortest_paths/weighted.pyi
+++ stubs/networkx/networkx/algorithms/shortest_paths/weighted.pyi
@@ -1,63 +1,63 @@
 from _typeshed import Incomplete
 from collections.abc import Generator
 
-from networkx.utils.backends import _dispatch
+from networkx.utils.backends import _dispatchable
 
-@_dispatch
+@_dispatchable
 def dijkstra_path(G, source, target, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def dijkstra_path_length(G, source, target, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def single_source_dijkstra_path(G, source, cutoff: Incomplete | None = None, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def single_source_dijkstra_path_length(G, source, cutoff: Incomplete | None = None, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def single_source_dijkstra(
     G, source, target: Incomplete | None = None, cutoff: Incomplete | None = None, weight: str = "weight"
 ): ...
-@_dispatch
+@_dispatchable
 def multi_source_dijkstra_path(G, sources, cutoff: Incomplete | None = None, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def multi_source_dijkstra_path_length(G, sources, cutoff: Incomplete | None = None, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def multi_source_dijkstra(
     G, sources, target: Incomplete | None = None, cutoff: Incomplete | None = None, weight: str = "weight"
 ): ...
-@_dispatch
+@_dispatchable
 def dijkstra_predecessor_and_distance(G, source, cutoff: Incomplete | None = None, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def all_pairs_dijkstra(G, cutoff: Incomplete | None = None, weight: str = "weight") -> Generator[Incomplete, None, None]: ...
-@_dispatch
+@_dispatchable
 def all_pairs_dijkstra_path_length(
     G, cutoff: Incomplete | None = None, weight: str = "weight"
 ) -> Generator[Incomplete, None, None]: ...
-@_dispatch
+@_dispatchable
 def all_pairs_dijkstra_path(G, cutoff: Incomplete | None = None, weight: str = "weight") -> Generator[Incomplete, None, None]: ...
-@_dispatch
+@_dispatchable
 def bellman_ford_predecessor_and_distance(
     G, source, target: Incomplete | None = None, weight: str = "weight", heuristic: bool = False
 ): ...
-@_dispatch
+@_dispatchable
 def bellman_ford_path(G, source, target, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def bellman_ford_path_length(G, source, target, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def single_source_bellman_ford_path(G, source, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def single_source_bellman_ford_path_length(G, source, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def single_source_bellman_ford(G, source, target: Incomplete | None = None, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def all_pairs_bellman_ford_path_length(G, weight: str = "weight") -> Generator[Incomplete, None, None]: ...
-@_dispatch
+@_dispatchable
 def all_pairs_bellman_ford_path(G, weight: str = "weight") -> Generator[Incomplete, None, None]: ...
-@_dispatch
+@_dispatchable
 def goldberg_radzik(G, source, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def negative_edge_cycle(G, weight: str = "weight", heuristic: bool = True): ...
-@_dispatch
+@_dispatchable
 def find_negative_cycle(G, source, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def bidirectional_dijkstra(G, source, target, weight: str = "weight"): ...
-@_dispatch
+@_dispatchable
 def johnson(G, weight: str = "weight"): ...
