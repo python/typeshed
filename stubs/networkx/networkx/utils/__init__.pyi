a3881f1689167268c5ffbdc258bae246333c16b1
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-10-04 19:44:39 -0700

    try this

--- stubs/networkx/networkx/utils/__init__.pyi
+++ stubs/networkx/networkx/utils/__init__.pyi
@@ -2,7 +2,8 @@ from networkx.utils.backends import *
 from networkx.utils.configs import *
 from networkx.utils.decorators import *
 from networkx.utils.heaps import *
-from networkx.utils.misc import *
+
+# should be import * but pytype doesn't understand that _clear_cache is part of __all__
 from networkx.utils.random_sequence import *
 from networkx.utils.rcm import *
 from networkx.utils.union_find import *
