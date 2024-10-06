083cc6772d34a09bcdbab2e5bd2c167d7e8a9aa4
Stephen Morton <git@tungol.org>
2024-10-05 19:22:40 -0700

    use explicit imports

--- stdlib/operator.pyi
+++ stdlib/operator.pyi
@@ -1,5 +1,58 @@
 import sys
-from _operator import *
+from _operator import (
+    abs as abs,
+    add as add,
+    and_ as and_,
+    call as call,
+    concat as concat,
+    contains as contains,
+    countOf as countOf,
+    delitem as delitem,
+    eq as eq,
+    floordiv as floordiv,
+    ge as ge,
+    getitem as getitem,
+    gt as gt,
+    iadd as iadd,
+    iand as iand,
+    iconcat as iconcat,
+    ifloordiv as ifloordiv,
+    ilshift as ilshift,
+    imatmul as imatmul,
+    imod as imod,
+    imul as imul,
+    index as index,
+    indexOf as indexOf,
+    inv as inv,
+    invert as invert,
+    ior as ior,
+    ipow as ipow,
+    irshift as irshift,
+    is_ as is_,
+    is_not as is_not,
+    isub as isub,
+    itruediv as itruediv,
+    ixor as ixor,
+    le as le,
+    length_hint as length_hint,
+    lshift as lshift,
+    lt as lt,
+    matmul as matmul,
+    mod as mod,
+    mul as mul,
+    ne as ne,
+    neg as neg,
+    not_ as not_,
+    or_ as or_,
+    pos as pos,
+    pow as pow,
+    rshift as rshift,
+    setitem as setitem,
+    sub as sub,
+    truediv as truediv,
+    truth as truth,
+    xor as xor,
+)
 from _typeshed import SupportsGetItem
 from typing import Any, Generic, TypeVar, final, overload
 from typing_extensions import TypeVarTuple, Unpack
@@ -68,9 +121,13 @@ __all__ = [
 ]
 
 if sys.version_info >= (3, 11):
+    from _operator import call as call
+
     __all__ += ["call"]
 
 if sys.version_info >= (3, 14):
+    from _operator import is_none as is_none, is_not_none as is_not_none
+
     __all__ += ["is_none", "is_not_none"]
 
 __lt__ = lt
