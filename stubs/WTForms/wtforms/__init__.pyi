844d87ca062c6d7f2896f0e3892705364c5c1464
Daverball <dave@daverball.com>
2024-10-21 12:06:08 +0200

    Bump WTForms to 3.2.*

--- stubs/WTForms/wtforms/__init__.pyi
+++ stubs/WTForms/wtforms/__init__.pyi
@@ -1,4 +1,41 @@
 from wtforms import validators as validators, widgets as widgets
-from wtforms.fields import *
+from wtforms.fields.choices import (
+    RadioField as RadioField,
+    SelectField as SelectField,
+    SelectMultipleField as SelectMultipleField,
+)
+from wtforms.fields.core import Field as Field, Flags as Flags, Label as Label
+from wtforms.fields.datetime import (
+    DateField as DateField,
+    DateTimeField as DateTimeField,
+    DateTimeLocalField as DateTimeLocalField,
+    MonthField as MonthField,
+    TimeField as TimeField,
+    WeekField as WeekField,
+)
+from wtforms.fields.form import FormField as FormField
+from wtforms.fields.list import FieldList as FieldList
+from wtforms.fields.numeric import (
+    DecimalField as DecimalField,
+    DecimalRangeField as DecimalRangeField,
+    FloatField as FloatField,
+    IntegerField as IntegerField,
+    IntegerRangeField as IntegerRangeField,
+)
+from wtforms.fields.simple import (
+    BooleanField as BooleanField,
+    ColorField as ColorField,
+    EmailField as EmailField,
+    FileField as FileField,
+    HiddenField as HiddenField,
+    MultipleFileField as MultipleFileField,
+    PasswordField as PasswordField,
+    SearchField as SearchField,
+    StringField as StringField,
+    SubmitField as SubmitField,
+    TelField as TelField,
+    TextAreaField as TextAreaField,
+    URLField as URLField,
+)
 from wtforms.form import Form as Form
 from wtforms.validators import ValidationError as ValidationError
