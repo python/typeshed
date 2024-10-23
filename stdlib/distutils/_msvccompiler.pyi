a73176086e2ad908c47053a3c5437dd4a81ea615
Avasam <samuel.06@hotmail.com>
2024-10-23 14:41:11 -0400

    Add missing names

--- stdlib/distutils/_msvccompiler.pyi
+++ stdlib/distutils/_msvccompiler.pyi
@@ -1,3 +1,12 @@
+from _typeshed import Incomplete
 from distutils.ccompiler import CCompiler
+from typing import ClassVar, Final
 
-class MSVCCompiler(CCompiler): ...
+PLAT_SPEC_TO_RUNTIME: Final[dict[str, str]]
+PLAT_TO_VCVARS: Final[dict[str, str]]
+
+class MSVCCompiler(CCompiler):
+    compiler_type: ClassVar[str]
+    executables: ClassVar[dict[Incomplete, Incomplete]]
+    res_extension: ClassVar[str]
+    initialized: bool
--- stubs/setuptools/setuptools/_distutils/_msvccompiler.pyi
+++ stubs/setuptools/setuptools/_distutils/_msvccompiler.pyi
@@ -1,3 +1,14 @@
+from binascii import Incomplete
+from typing import ClassVar, Final
+
 from .ccompiler import CCompiler
 
-class MSVCCompiler(CCompiler): ...
+PLAT_SPEC_TO_RUNTIME: Final[dict[str, str]]
+
+class MSVCCompiler(CCompiler):
+    compiler_type: ClassVar[str]
+    executables: ClassVar[dict[Incomplete, Incomplete]]
+    res_extension: ClassVar[str]
+    initialized: bool
+    @property
+    def out_extensions(self) -> dict[str, str]: ...
