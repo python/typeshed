4a9141a9dd4c8064eec01a12a4bc28632be1dea3
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-06 23:53:36 +0530

    Experiment with more combined types

--- stubs/setuptools/@tests/test_cases/check_distutils.py
+++ stubs/setuptools/@tests/test_cases/check_distutils.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import distutils.command.sdist
 from _typeshed import StrPath
+from os import PathLike
 from pathlib import Path
-from typing import List
 
 from setuptools._distutils.ccompiler import CCompiler
 
@@ -12,10 +12,20 @@ c = distutils.command.sdist.sdist
 # Test CCompiler().compile with varied sources
 
 compiler = CCompiler()
+
+str_list: list[str] = ["file1.c", "file2.c"]
+compiler.compile(sources=str_list)
+
+path_list: list[Path] = [Path("file1.c"), Path("file2.c")]
+compiler.compile(sources=path_list)
+
+pathlike_list: list[PathLike[str]] = [Path("file1.c"), Path("file2.c")]
+compiler.compile(sources=pathlike_list)
+
+strpath_list: list[StrPath] = [Path("file1.c"), "file2.c"]
+compiler.compile(sources=strpath_list)
+
+# Direct literals should also work
 compiler.compile(sources=["file1.c", "file2.c"])
-
-paths: List[StrPath] = [Path("file1.c"), Path("file2.c")]
-compiler.compile(sources=paths)
-
-mixed: List[StrPath] = [Path("file1.c"), "file2.c"]
-compiler.compile(sources=mixed)
+compiler.compile(sources=[Path("file1.c"), Path("file2.c")])
+compiler.compile(sources=[Path("file1.c"), "file2.c"])
