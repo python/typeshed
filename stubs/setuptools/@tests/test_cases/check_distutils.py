47e149ca611b70918811abef61a39df748ea14e0
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-06 18:08:56 +0530

    Add a few tests for `CCompiler().compile`

--- stubs/setuptools/@tests/test_cases/check_distutils.py
+++ stubs/setuptools/@tests/test_cases/check_distutils.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
+
 import distutils.command.sdist
+from _typeshed import StrPath
+from pathlib import Path
+from typing import List
+
+from setuptools._distutils.ccompiler import CCompiler
 
 c = distutils.command.sdist.sdist
+
+# Test CCompiler().compile with varied sources
+
+compiler = CCompiler()
+compiler.compile(sources=["file1.c", "file2.c"])
+
+paths: List[StrPath] = [Path("file1.c"), Path("file2.c")]
+compiler.compile(sources=paths)
+
+mixed: List[StrPath] = [Path("file1.c"), "file2.c"]
+compiler.compile(sources=mixed)
