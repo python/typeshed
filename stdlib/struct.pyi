b38a3d9e1f0ab35ab8324b2af7a90cdbf9f2764c
Stephen Morton <github@tungol.org>
2024-11-07 18:51:31 -0800

    Update stdlib/struct.pyi
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- stdlib/struct.pyi
+++ stdlib/struct.pyi
@@ -1,12 +1,4 @@
-from _struct import (
-    Struct as Struct,
-    calcsize as calcsize,
-    iter_unpack as iter_unpack,
-    pack as pack,
-    pack_into as pack_into,
-    unpack as unpack,
-    unpack_from as unpack_from,
-)
+from _struct import *
 
 __all__ = ["calcsize", "pack", "pack_into", "unpack", "unpack_from", "iter_unpack", "Struct", "error"]
 
