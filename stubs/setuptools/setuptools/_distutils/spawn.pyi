09249087e82fb24b117f43f5e8376367c2491038
Avasam <samuel.06@hotmail.com>
2024-11-18 14:52:52 -0500

    setuptools&distutils: more accurate spawn method

---
+++ stubs/setuptools/setuptools/_distutils/spawn.pyi
@@ -0,0 +1,7 @@
+from subprocess import _ENV
+from typing import MutableSequence
+
+def spawn(
+    cmd: MutableSequence[str], search_path: bool = True, verbose: bool = False, dry_run: bool = False, env: _ENV | None = None
+) -> None: ...
+def find_executable(executable: str, path: str | None = None) -> str | None: ...
