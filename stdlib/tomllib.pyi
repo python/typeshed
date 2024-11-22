5e0f6010217de8c5373bad526007f13220834834
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2024-11-22 02:40:41 +0200

    Update `TOMLDecodeError` for Python 3.14

--- stdlib/tomllib.pyi
+++ stdlib/tomllib.pyi
@@ -1,10 +1,20 @@
 from _typeshed import SupportsRead
 from collections.abc import Callable
+import sys
 from typing import Any
 
 __all__ = ("loads", "load", "TOMLDecodeError")
 
-class TOMLDecodeError(ValueError): ...
+if sys.version_info >= (3, 14):
+    class TOMLDecodeError(ValueError):
+        msg: str
+        doc: str
+        pos: int
+        lineno: int
+        colno: int
+        def __init__(self, msg: str, doc: str, pos: int) -> None: ...
+else:
+    class TOMLDecodeError(ValueError): ...
 
 def load(fp: SupportsRead[bytes], /, *, parse_float: Callable[[str], Any] = ...) -> dict[str, Any]: ...
 def loads(s: str, /, *, parse_float: Callable[[str], Any] = ...) -> dict[str, Any]: ...
