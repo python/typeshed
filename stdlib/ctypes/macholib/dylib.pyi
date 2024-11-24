52c69404f6d2ba6cc76ac5daee29f5a7978dce69
Stephen Morton <git@tungol.org>
2024-11-23 17:41:32 -0800

    type_check_only

--- stdlib/ctypes/macholib/dylib.pyi
+++ stdlib/ctypes/macholib/dylib.pyi
@@ -1,8 +1,9 @@
-from typing import TypedDict
+from typing import TypedDict, type_check_only
 
 __all__ = ["dylib_info"]
 
 # Actual result is produced by re.match.groupdict()
+@type_check_only
 class _DylibInfo(TypedDict):
     location: str
     name: str
--- stdlib/ctypes/macholib/framework.pyi
+++ stdlib/ctypes/macholib/framework.pyi
@@ -1,8 +1,9 @@
-from typing import TypedDict
+from typing import TypedDict, type_check_only
 
 __all__ = ["framework_info"]
 
 # Actual result is produced by re.match.groupdict()
+@type_check_only
 class _FrameworkInfo(TypedDict):
     location: str
     name: str
