b135d7d1de8c5c483d8f7d279733345785d7abce
Avasam <samuel.06@hotmail.com>
2024-11-25 17:11:31 -0500

    Allow any sequence as getopt args

--- stdlib/distutils/fancy_getopt.pyi
+++ stdlib/distutils/fancy_getopt.pyi
@@ -1,10 +1,11 @@
-from collections.abc import Iterable, Mapping
+from collections.abc import Iterable, Mapping, Sequence
 from re import Pattern
-from typing import Any, Final, overload
+from typing import Any, Final, TypeVar, overload
 from typing_extensions import TypeAlias
 
 _Option: TypeAlias = tuple[str, str | None, str]
-_GR: TypeAlias = tuple[list[str], OptionDummy]
+_StrSequenceT = TypeVar("_StrSequenceT", bound=Sequence[str], default=list[str])
+_GR: TypeAlias = tuple[_StrSequenceT, OptionDummy]
 
 longopt_pat: Final = r"[a-zA-Z](?:[a-zA-Z0-9-]*)"
 longopt_re: Final[Pattern[str]]
@@ -15,15 +16,21 @@ class FancyGetopt:
     def __init__(self, option_table: list[_Option] | None = None) -> None: ...
     # TODO kinda wrong, `getopt(object=object())` is invalid
     @overload
-    def getopt(self, args: list[str] | None = None) -> _GR: ...
+    def getopt(self, args: _StrSequenceT | None = None, object: None = None) -> _GR[_StrSequenceT]: ...
     @overload
-    def getopt(self, args: list[str] | None, object: Any) -> list[str]: ...
+    def getopt(self, args: _StrSequenceT | None, object: Any) -> _StrSequenceT: ...
     def get_option_order(self) -> list[tuple[str, str]]: ...
     def generate_help(self, header: str | None = None) -> list[str]: ...
 
+# Same note as FancyGetopt.getopt
+@overload
 def fancy_getopt(
-    options: list[_Option], negative_opt: Mapping[_Option, _Option], object: Any, args: list[str] | None
-) -> list[str] | _GR: ...
+    options: list[_Option], negative_opt: Mapping[_Option, _Option], object: None, args: _StrSequenceT | None
+) -> _GR[_StrSequenceT]: ...
+@overload
+def fancy_getopt(
+    options: list[_Option], negative_opt: Mapping[_Option, _Option], object: Any, args: _StrSequenceT | None
+) -> _StrSequenceT: ...
 
 WS_TRANS: Final[dict[int, str]]
 
