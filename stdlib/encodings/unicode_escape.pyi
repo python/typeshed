a051b4c891b7ec26c04d70b2f388b2204cb676ed
Stephen Morton <git@tungol.org>
2024-11-25 13:06:09 -0800

    add encodings.raw_unicode_escape and encodings.unicode_escape

---
+++ stdlib/encodings/unicode_escape.pyi
@@ -0,0 +1,19 @@
+import codecs
+from _typeshed import ReadableBuffer
+
+class Codec(codecs.Codec):
+    encode = codecs.unicode_escape_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+    decode = codecs.unicode_escape_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+class IncrementalEncoder(codecs.IncrementalEncoder):
+    def encode(self, input: str, final: bool = False) -> bytes: ...
+
+class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
+    def _buffer_decode(self, input: str | ReadableBuffer, errors: str | None, final: bool) -> tuple[str, int]: ...
+
+class StreamWriter(Codec, codecs.StreamWriter): ...
+
+class StreamReader(Codec, codecs.StreamReader):
+    def decode(self, input: str | ReadableBuffer, errors: str = "strict") -> tuple[str, int]: ...  # type: ignore[override]
+
+def getregentry() -> codecs.CodecInfo: ...
