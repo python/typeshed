bbb396a6f3b04bc562cd6c50003f68e930cb8b17
Stephen Morton <git@tungol.org>
2024-11-25 12:25:34 -0800

    stubtest

--- stdlib/encodings/utf_16_le.pyi
+++ stdlib/encodings/utf_16_le.pyi
@@ -8,12 +8,13 @@ def decode(input: ReadableBuffer, errors: str | None = "strict") -> tuple[str, i
 class IncrementalEncoder(codecs.IncrementalEncoder):
     def encode(self, input: str, final: bool = False) -> bytes: ...
 
-class IncrementalDecoder(codecs.BufferedIncrementalDecoder): ...
+class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
+    _buffer_decode = codecs.utf_16_le_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
 class StreamWriter(codecs.StreamWriter):
-    encode = codecs.utf_16_le_encode
+    encode = codecs.utf_16_le_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
 class StreamReader(codecs.StreamReader):
-    decode = codecs.utf_16_le_decode
+    decode = codecs.utf_16_le_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
 def getregentry() -> codecs.CodecInfo: ...
--- stdlib/encodings/utf_8.pyi
+++ stdlib/encodings/utf_8.pyi
@@ -8,12 +8,13 @@ def decode(input: ReadableBuffer, errors: str | None = "strict") -> tuple[str, i
 class IncrementalEncoder(codecs.IncrementalEncoder):
     def encode(self, input: str, final: bool = False) -> bytes: ...
 
-class IncrementalDecoder(codecs.BufferedIncrementalDecoder): ...
+class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
+    _buffer_decode = codecs.utf_8_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
 class StreamWriter(codecs.StreamWriter):
-    encode = codecs.utf_8_encode
+    encode = codecs.utf_8_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
 class StreamReader(codecs.StreamReader):
-    decode = codecs.utf_8_decode
+    decode = codecs.utf_8_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
 def getregentry() -> codecs.CodecInfo: ...
