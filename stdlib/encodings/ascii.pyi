1139a32b264316a9f92afb4727c0d252aa13b7a0
Stephen Morton <git@tungol.org>
2024-11-25 13:01:30 -0800

    add encodings.ascii and encodings.latin_1

---
+++ stdlib/encodings/ascii.pyi
@@ -0,0 +1,21 @@
+import codecs
+from _typeshed import ReadableBuffer
+
+class Codec(codecs.Codec):
+    encode = codecs.ascii_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+    decode = codecs.ascii_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+class IncrementalEncoder(codecs.IncrementalEncoder):
+    def encode(self, input: str, final: bool = False) -> bytes: ...
+
+class IncrementalDecoder(codecs.IncrementalDecoder):
+    def decode(self, input: ReadableBuffer, final: bool = False) -> str: ...
+
+class StreamWriter(Codec, codecs.StreamWriter): ...
+class StreamReader(Codec, codecs.StreamReader): ...
+
+class StreamConverter(StreamWriter, StreamReader):  # type: ignore[misc]  # incompatible methods in base classes
+    encode = codecs.ascii_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+    decode = codecs.ascii_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+def getregentry() -> codecs.CodecInfo: ...
