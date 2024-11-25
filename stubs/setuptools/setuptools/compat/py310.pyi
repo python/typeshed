e7bb14a406a2a2ab44920e281ed5d47fb1f7a1eb
Sebastian Rittau <srittau@rittau.biz>
2024-11-25 17:15:07 +0100

    revert

--- stubs/setuptools/setuptools/compat/py310.pyi
+++ stubs/setuptools/setuptools/compat/py310.pyi
@@ -6,4 +6,4 @@ if sys.version_info >= (3, 11):
     import tomllib
 else:
     # This is actually vendored
-    import tomli as tomllib  # type: ignore[import-not-found]
+    import tomli as tomllib  # type: ignore[import-not-found] # pyright: ignore[reportMissingImports]
