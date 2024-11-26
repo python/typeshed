db12fbfea53b97e149a1567b54051f0351ff1f86
Stephen Morton <git@tungol.org>
2024-11-26 11:50:50 -0800

    use override methods instead of directly assigning the function.

--- stdlib/encodings/ascii.pyi
+++ stdlib/encodings/ascii.pyi
@@ -2,8 +2,12 @@ import codecs
 from _typeshed import ReadableBuffer
 
 class Codec(codecs.Codec):
-    encode = codecs.ascii_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
-    decode = codecs.ascii_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+    # At runtime, this is codecs.ascii_encode
+    @staticmethod
+    def encode(str: str, errors: str | None = None, /) -> tuple[bytes, int]: ...  # type: ignore[override]
+    # At runtime, this is codecs.ascii_decode
+    @staticmethod
+    def decode(data: ReadableBuffer, errors: str | None = None, /) -> tuple[str, int]: ...  # type: ignore[override]
 
 class IncrementalEncoder(codecs.IncrementalEncoder):
     def encode(self, input: str, final: bool = False) -> bytes: ...
@@ -14,8 +18,13 @@ class IncrementalDecoder(codecs.IncrementalDecoder):
 class StreamWriter(Codec, codecs.StreamWriter): ...
 class StreamReader(Codec, codecs.StreamReader): ...
 
+# Note: encode being a decode function and decode being an encode function is accurate to runtime.
 class StreamConverter(StreamWriter, StreamReader):  # type: ignore[misc]  # incompatible methods in base classes
-    encode = codecs.ascii_decode  # type: ignore[assignment]
-    decode = codecs.ascii_encode  # type: ignore[assignment]
+    # At runtime, this is codecs.ascii_decode
+    @staticmethod
+    def encode(data: ReadableBuffer, errors: str | None = None, /) -> tuple[str, int]: ...  # type: ignore[override]
+    # At runtime, this is codecs.ascii_encode
+    @staticmethod
+    def decode(str: str, errors: str | None = None, /) -> tuple[bytes, int]: ...  # type: ignore[override]
 
 def getregentry() -> codecs.CodecInfo: ...
--- stdlib/encodings/latin_1.pyi
+++ stdlib/encodings/latin_1.pyi
@@ -2,8 +2,12 @@ import codecs
 from _typeshed import ReadableBuffer
 
 class Codec(codecs.Codec):
-    encode = codecs.latin_1_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
-    decode = codecs.latin_1_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+    # At runtime, this is codecs.latin_1_encode
+    @staticmethod
+    def encode(str: str, errors: str | None = None, /) -> tuple[bytes, int]: ...  # type: ignore[override]
+    # At runtime, this is codecs.latin_1_decode
+    @staticmethod
+    def decode(data: ReadableBuffer, errors: str | None = None, /) -> tuple[str, int]: ...  # type: ignore[override]
 
 class IncrementalEncoder(codecs.IncrementalEncoder):
     def encode(self, input: str, final: bool = False) -> bytes: ...
@@ -14,8 +18,13 @@ class IncrementalDecoder(codecs.IncrementalDecoder):
 class StreamWriter(Codec, codecs.StreamWriter): ...
 class StreamReader(Codec, codecs.StreamReader): ...
 
+# Note: encode being a decode function and decode being an encode function is accurate to runtime.
 class StreamConverter(StreamWriter, StreamReader):  # type: ignore[misc]  # incompatible methods in base classes
-    encode = codecs.latin_1_decode  # type: ignore[assignment]
-    decode = codecs.latin_1_encode  # type: ignore[assignment]
+    # At runtime, this is codecs.latin_1_decode
+    @staticmethod
+    def encode(data: ReadableBuffer, errors: str | None = None, /) -> tuple[str, int]: ...  # type: ignore[override]
+    # At runtime, this is codecs.latin_1_encode
+    @staticmethod
+    def decode(str: str, errors: str | None = None, /) -> tuple[bytes, int]: ...  # type: ignore[override]
 
 def getregentry() -> codecs.CodecInfo: ...
