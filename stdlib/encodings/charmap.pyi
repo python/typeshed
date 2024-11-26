352f42b564feb3457de00b8b4c7ad8a3c1dbe0b9
Stephen Morton <git@tungol.org>
2024-11-26 11:20:35 -0800

    from review

--- stdlib/encodings/charmap.pyi
+++ stdlib/encodings/charmap.pyi
@@ -3,26 +3,30 @@ from _codecs import _CharMap
 from _typeshed import ReadableBuffer
 
 class Codec(codecs.Codec):
-    encode = codecs.charmap_encode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
-    decode = codecs.charmap_decode  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+    # At runtime, this is codecs.charmap_encode
+    @staticmethod
+    def encode(str: str, errors: str | None = None, mapping: _CharMap | None = None, /) -> tuple[bytes, int]: ...  # type: ignore[override]
+    # At runtime, this is codecs.charmap_decode
+    @staticmethod
+    def decode(data: ReadableBuffer, errors: str | None = None, mapping: _CharMap | None = None, /) -> tuple[str, int]: ...  # type: ignore[override]
 
 class IncrementalEncoder(codecs.IncrementalEncoder):
-    mapping: _CharMap
+    mapping: _CharMap | None
     def __init__(self, errors: str = "strict", mapping: _CharMap | None = None) -> None: ...
     def encode(self, input: str, final: bool = False) -> bytes: ...
 
 class IncrementalDecoder(codecs.IncrementalDecoder):
-    mapping: _CharMap
+    mapping: _CharMap | None
     def __init__(self, errors: str = "strict", mapping: _CharMap | None = None) -> None: ...
     def decode(self, input: ReadableBuffer, final: bool = False) -> str: ...
 
 class StreamWriter(Codec, codecs.StreamWriter):
-    mapping: _CharMap
+    mapping: _CharMap | None
     def __init__(self, stream: codecs._WritableStream, errors: str = "strict", mapping: _CharMap | None = None) -> None: ...
     def encode(self, input: str, errors: str = "strict") -> tuple[bytes, int]: ...  # type: ignore[override]
 
 class StreamReader(Codec, codecs.StreamReader):
-    mapping: _CharMap
+    mapping: _CharMap | None
     def __init__(self, stream: codecs._ReadableStream, errors: str = "strict", mapping: _CharMap | None = None) -> None: ...
     def decode(self, input: ReadableBuffer, errors: str = "strict") -> tuple[str, int]: ...  # type: ignore[override]
 
