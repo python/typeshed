356bc06847cfd7d5c5d899a0c62023d149d3aa8f
Stephen Morton <git@tungol.org>
2024-11-25 17:33:28 -0800

    add byte-to-byte encodings submodules

---
+++ stdlib/encodings/uu_codec.pyi
@@ -0,0 +1,24 @@
+import codecs
+from _typeshed import ReadableBuffer
+
+# This codec is bytes to bytes.
+
+def uu_encode(
+    input: ReadableBuffer, errors: str = "strict", filename: str = "<data>", mode: int = 0o666
+) -> tuple[bytes, int]: ...
+def uu_decode(input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...
+
+class Codec(codecs.Codec):
+    def encode(self, input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...  # type: ignore[override]
+    def decode(self, input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...  # type: ignore[override]
+
+class IncrementalEncoder(codecs.IncrementalEncoder):
+    def encode(self, input: ReadableBuffer, final: bool = False) -> bytes: ...  # type: ignore[override]
+
+class IncrementalDecoder(codecs.IncrementalDecoder):
+    def decode(self, input: ReadableBuffer, final: bool = False) -> bytes: ...  # type: ignore[override]
+
+class StreamWriter(Codec, codecs.StreamWriter): ...
+class StreamReader(Codec, codecs.StreamReader): ...
+
+def getregentry() -> codecs.CodecInfo: ...
---
+++ stdlib/encodings/zlib_codec.pyi
@@ -0,0 +1,22 @@
+import codecs
+from _typeshed import ReadableBuffer
+
+# This codec is bytes to bytes.
+
+def zlib_encode(input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...
+def zlib_decode(input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...
+
+class Codec(codecs.Codec):
+    def encode(self, input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...  # type: ignore[override]
+    def decode(self, input: ReadableBuffer, errors: str = "strict") -> tuple[bytes, int]: ...  # type: ignore[override]
+
+class IncrementalEncoder(codecs.IncrementalEncoder):
+    def encode(self, input: ReadableBuffer, final: bool = False) -> bytes: ...  # type: ignore[override]
+
+class IncrementalDecoder(codecs.IncrementalDecoder):
+    def decode(self, input: ReadableBuffer, final: bool = False) -> bytes: ...  # type: ignore[override]
+
+class StreamWriter(Codec, codecs.StreamWriter): ...
+class StreamReader(Codec, codecs.StreamReader): ...
+
+def getregentry() -> codecs.CodecInfo: ...
