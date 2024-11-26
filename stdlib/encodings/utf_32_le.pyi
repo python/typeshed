246cce0d614134042c634ce90739d7243ab7c648
Stephen Morton <git@tungol.org>
2024-11-26 12:04:15 -0800

    unneeded ignores

--- stdlib/encodings/utf_32_le.pyi
+++ stdlib/encodings/utf_32_le.pyi
@@ -11,16 +11,16 @@ class IncrementalEncoder(codecs.IncrementalEncoder):
 class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
     # At runtime, this is codecs.utf_32_le_decode
     @staticmethod
-    def _buffer_decode(data: ReadableBuffer, errors: str | None = None, final: bool = False, /) -> tuple[str, int]: ...  # type: ignore[override]
+    def _buffer_decode(data: ReadableBuffer, errors: str | None = None, final: bool = False, /) -> tuple[str, int]: ...
 
 class StreamWriter(codecs.StreamWriter):
     # At runtime, this is codecs.utf_32_le_encode
     @staticmethod
-    def encode(str: str, errors: str | None = None, /) -> tuple[bytes, int]: ...  # type: ignore[override]
+    def encode(str: str, errors: str | None = None, /) -> tuple[bytes, int]: ...
 
 class StreamReader(codecs.StreamReader):
     # At runtime, this is codecs.utf_32_le_decode
     @staticmethod
-    def decode(data: ReadableBuffer, errors: str | None = None, final: bool = False, /) -> tuple[str, int]: ...  # type: ignore[override]
+    def decode(data: ReadableBuffer, errors: str | None = None, final: bool = False, /) -> tuple[str, int]: ...
 
 def getregentry() -> codecs.CodecInfo: ...
