89959bce0e3c1edf16e2d69f15fd062ed7a00999
Sebastian Rittau <srittau@rittau.biz>
2024-11-27 16:17:12 +0100

    Use TypeGuard

--- stubs/icalendar/icalendar/tools.pyi
+++ stubs/icalendar/icalendar/tools.pyi
@@ -1,6 +1,6 @@
 import datetime
 from typing import Final
-from typing_extensions import TypeIs
+from typing_extensions import TypeGuard
 
 from pytz.tzinfo import BaseTzInfo
 
@@ -19,5 +19,5 @@ def is_date(dt: datetime.date) -> bool: ...  # and not datetime.date
 def is_datetime(dt: datetime.date) -> TypeIs[datetime.datetime]: ...
 def to_datetime(dt: datetime.date) -> datetime.datetime: ...
 def is_pytz(tz: datetime.tzinfo) -> TypeIs[BaseTzInfo]: ...
-def is_pytz_dt(dt: datetime.date) -> TypeIs[datetime.datetime]: ...  # and dt.tzinfo is BaseTZInfo
+def is_pytz_dt(dt: datetime.date) -> TypeGuard[datetime.datetime]: ...  # and dt.tzinfo is BaseTZInfo
 def normalize_pytz(dt: datetime.date) -> datetime.datetime: ...
