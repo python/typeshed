08254f7081fbf86b7e2c78062208b945ffb852b5
Akuli <akuviljanen17@gmail.com>
2024-12-04 22:33:58 +0200

    asd

--- stdlib/tkinter/__init__.pyi
+++ stdlib/tkinter/__init__.pyi
@@ -148,7 +148,7 @@ if sys.version_info >= (3, 9):
         "PanedWindow",
     ]
 
-# Using anything from tkinter.font in this file means that 'import tkinter'
+# Using anything from tkinter.font in this file means that 'import tkinter
 # seems to also load tkinter.font. That's not how it actually works, but
 # unfortunately not much can be done about it. https://github.com/python/typeshed/pull/4346
 
