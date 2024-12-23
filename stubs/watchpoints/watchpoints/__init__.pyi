7ae5b5cc68bbac71bb289d79ce082f972b1a977c
bzoracler <bzoracler@gmail.com>
2024-12-24 12:46:44 +1300

    fix: remove misspelled runtime variable

--- stubs/watchpoints/watchpoints/__init__.pyi
+++ stubs/watchpoints/watchpoints/__init__.pyi
@@ -1,11 +1,10 @@
 from collections.abc import Callable
-from typing import Final, Literal
+from typing import Final
 from typing_extensions import LiteralString, Unpack
 
 from .watch import Watch
 
 __version__: Final[LiteralString]
 
-all: Final[list[Literal["watch", "unwatch"]]]
 watch: Watch
 unwatch: Final[Callable[[Unpack[tuple[object, ...]]], None]]
