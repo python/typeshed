e3ca7b11b5927d69c38b69ca952a68e6355f6a48
Daniel Eades <danieleades@hotmail.com>
2024-12-28 16:14:37 +0000

    improve type annotations in 'docutils.readers.standalone'

--- stubs/docutils/docutils/readers/standalone.pyi
+++ stubs/docutils/docutils/readers/standalone.pyi
@@ -1,3 +1,15 @@
-from _typeshed import Incomplete
+__docformat__: str
 
-def __getattr__(name: str) -> Incomplete: ...
+
+from typing import ClassVar, Literal, TypeVar
+from docutils import readers
+
+
+_S = TypeVar("_S")
+
+class Reader(readers.Reader[_S]):
+
+    supported: ClassVar[tuple[Literal["standalone"]]]
+
+    config_section: ClassVar[Literal["standalone reader"]]
+    config_section_dependencies: ClassVar[tuple[Literal["readers"]]]
