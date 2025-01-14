b5de0c4ba8399b7cf03626b223984bf30810a2ef
Avasam <samuel.06@hotmail.com>
2025-01-13 22:09:59 -0500

    Use TypedDict argument unpacking for setuptools/distutils's setup

--- stubs/setuptools/setuptools/__init__.pyi
+++ stubs/setuptools/setuptools/__init__.pyi
@@ -1,8 +1,8 @@
-from _typeshed import Incomplete
 from abc import abstractmethod
-from collections.abc import Mapping, Sequence
+from collections.abc import Sequence
+from distutils.core import _SetupArgs as _DistutilsSetupArgs
 from typing import Any, Literal, TypedDict, TypeVar, overload, type_check_only
-from typing_extensions import NotRequired
+from typing_extensions import NotRequired, Unpack
 
 from ._distutils.cmd import Command as _Command
 from .command.alias import alias
@@ -55,54 +55,17 @@ class _BuildInfo(TypedDict):
     include_dirs: NotRequired[list[str]]
     cflags: NotRequired[list[str]]
 
+@type_check_only
+class _SetupArgs(_DistutilsSetupArgs, total=False):  # total=False for custom Distributions that could accept more arguments
+    long_description_content_type: str
+    ext_modules: Sequence[Extension]  # type: ignore[misc] # Overwriting on purpose
+    # libraries for `Distribution` or `build_clib`, not `Extension`, `build_ext` or `CCompiler`
+    libraries: list[tuple[str, _BuildInfo]]  # type: ignore[misc] # Overwriting on purpose
+
 find_packages = _Finder.find
 find_namespace_packages = _Finder.find
 
-def setup(
-    *,
-    name: str = ...,
-    version: str = ...,
-    description: str = ...,
-    long_description: str = ...,
-    long_description_content_type: str = ...,
-    author: str = ...,
-    author_email: str = ...,
-    maintainer: str = ...,
-    maintainer_email: str = ...,
-    url: str = ...,
-    download_url: str = ...,
-    packages: list[str] = ...,
-    py_modules: list[str] = ...,
-    scripts: list[str] = ...,
-    ext_modules: Sequence[Extension] = ...,
-    classifiers: list[str] = ...,
-    distclass: type[Distribution] = ...,
-    script_name: str = ...,
-    script_args: list[str] = ...,
-    options: Mapping[str, Incomplete] = ...,
-    license: str = ...,
-    keywords: list[str] | str = ...,
-    platforms: list[str] | str = ...,
-    cmdclass: Mapping[str, type[_Command]] = ...,
-    data_files: list[tuple[str, list[str]]] = ...,
-    package_dir: Mapping[str, str] = ...,
-    obsoletes: list[str] = ...,
-    provides: list[str] = ...,
-    requires: list[str] = ...,
-    command_packages: list[str] = ...,
-    command_options: Mapping[str, Mapping[str, tuple[Incomplete, Incomplete]]] = ...,
-    package_data: Mapping[str, list[str]] = ...,
-    include_package_data: bool = ...,
-    # libraries for `Distribution` or `build_clib`, not `Extension`, `build_ext` or `CCompiler`
-    libraries: list[tuple[str, _BuildInfo]] = ...,
-    headers: list[str] = ...,
-    ext_package: str = ...,
-    include_dirs: list[str] = ...,
-    password: str = ...,
-    fullname: str = ...,
-    # Custom Distributions could accept more params
-    **attrs: Any,
-) -> Distribution: ...
+def setup(**attrs: Unpack[_SetupArgs]) -> Distribution: ...
 
 class Command(_Command):
     command_consumes_arguments: bool
