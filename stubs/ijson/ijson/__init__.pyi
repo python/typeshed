54bca9961516572c97dff44469a8d3a9983cdfbb
DinhHuy2010 <huy04052010@outlook.com>
2025-01-15 18:19:19 +0700

    add typing to __init__.py

--- stubs/ijson/ijson/__init__.pyi
+++ stubs/ijson/ijson/__init__.pyi
@@ -1,22 +1,21 @@
-from _typeshed import Incomplete
-
 from ijson.common import IncompleteJSONError as IncompleteJSONError, JSONError as JSONError, ObjectBuilder as ObjectBuilder
 from ijson.utils import coroutine as coroutine, sendable_list as sendable_list
 
+from ._abstract_backend import _BackendModule
 from .version import __version__ as __version__
 
-def get_backend(backend): ...
+def get_backend(backend: str) -> _BackendModule: ...
 
-backend: Incomplete
-basic_parse: Incomplete
-basic_parse_coro: Incomplete
-parse: Incomplete
-parse_coro: Incomplete
-items: Incomplete
-items_coro: Incomplete
-kvitems: Incomplete
-kvitems_coro: Incomplete
-basic_parse_async: Incomplete
-parse_async: Incomplete
-items_async: Incomplete
-kvitems_async: Incomplete
+backend: _BackendModule = ...
+basic_parse = backend.basic_parse
+basic_parse_coro = backend.basic_parse_coro
+parse = backend.parse
+parse_coro = backend.parse_coro
+items = backend.items
+items_coro = backend.items_coro
+kvitems = backend.kvitems
+kvitems_coro = backend.kvitems_coro
+basic_parse_async = backend.basic_parse_async
+parse_async = backend.parse_async
+items_async = backend.items_async
+kvitems_async = backend.kvitems_async
