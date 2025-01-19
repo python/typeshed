e9ba1a27b206ea5e3bacc8e0d31d029e374c5ba3
Viicos <65306057+Viicos@users.noreply.github.com>
2025-01-19 13:41:46 +0100

    Tackle "Removed unused method `utils.original()`"

--- stubs/django-import-export/import_export/utils.pyi
+++ stubs/django-import-export/import_export/utils.pyi
@@ -1,10 +1,6 @@
-from collections.abc import Callable
 from types import TracebackType
-from typing import Any, TypeVar
 
 from django.db.transaction import Atomic
-
-_C = TypeVar("_C", bound=Callable[..., Any])
 
 class atomic_if_using_transaction:
     using_transactions: bool
@@ -14,5 +10,3 @@ class atomic_if_using_transaction:
     def __exit__(
         self, exc_type: type[BaseException] | None, exc_value: BaseException | None, exc_tb: TracebackType | None
     ) -> None: ...
-
-def original(method: _C) -> _C: ...
