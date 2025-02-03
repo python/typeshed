fc9be72ae09112753626e9a0690816e2c7ae3669
DinhHuy2010 <huy04052010@outlook.com>
2025-02-03 16:54:08 +0700

    add stubs for utils35.pyi

--- stubs/ijson/ijson/utils35.pyi
+++ stubs/ijson/ijson/utils35.pyi
@@ -1,21 +1,26 @@
 import collections
-from _typeshed import Incomplete
+from _typeshed import SupportsRead
+from typing import Any, Callable, TypeVar
 
 from ijson import compat
+from ijson.utils import _CoroPipelineArgs
+
+_T = TypeVar("_T")
 
 class utf8reader_async(compat.utf8reader):
-    async def read(self, n): ...
+    async def read(self, n: int) -> bytes: ...  # type: ignore[override]
 
-class sendable_deque(collections.deque):
-    send: Incomplete
+class sendable_deque(collections.deque[_T]):
+    # send = collections.deque.append
+    def send(self, x: _T, /) -> None: ...
 
 class async_iterable:
-    events: Incomplete
-    coro: Incomplete
+    events: sendable_deque[Any]
+    coro: list[Any]
     coro_finished: bool
-    f: Incomplete
-    buf_size: Incomplete
-    read: Incomplete
-    def __init__(self, f, buf_size, *coro_pipeline) -> None: ...
+    f: SupportsRead[Any]
+    buf_size: int
+    read: Callable[[int], bytes] | None
+    def __init__(self, f: SupportsRead[Any], buf_size: int, *coro_pipeline: _CoroPipelineArgs) -> None: ...
     def __aiter__(self): ...
     async def __anext__(self): ...
