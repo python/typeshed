6e03d3db53b6058ad2a6f93f02b282639cae1551
Daniel Darabos <darabos.daniel@gmail.com>
2025-02-03 18:41:17 +0100

    Remove stubs for unreleased modules.

--- stubs/networkx/networkx/algorithms/approximation/density.pyi
+++
@@ -1,5 +0,0 @@
-from networkx.classes.graph import Graph, _Node
-from networkx.utils.backends import _dispatchable
-
-@_dispatchable
-def densest_subgraph(G: Graph[_Node], iterations: int | None = 1, *, method: str | None = "greedy++"): ...
