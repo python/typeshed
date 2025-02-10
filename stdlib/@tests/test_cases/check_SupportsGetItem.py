c22ec9f861091b8ac2944dfcefee0d35c7c467a9
Avasam <samuel.06@hotmail.com>
2025-02-10 16:27:08 -0500

    Improve operator.itemgetter generic following mypy 1.11 fix

---
+++ stdlib/@tests/test_cases/check_SupportsGetItem.py
@@ -0,0 +1,30 @@
+from _typeshed import SupportsGetItem
+from operator import itemgetter
+from collections.abc import Callable
+from typing import TypeVar
+from _typeshed import SupportsDunderGT, SupportsDunderLT
+from collections.abc import Callable
+from typing import Any, Tuple, TypeVar, Union
+
+_T = TypeVar("_T")
+
+
+# This should be equivalent to itemgetter.__call__
+def standalone_call(obj: SupportsGetItem[int, _T]) -> _T: ...
+
+
+# Regression tests for https://github.com/python/mypy/issues/14032
+
+test_dict = min({"first": 1, "second": 2}, key=itemgetter(1))
+test_items = min({"first": 1, "second": 2}.items(), key=itemgetter(1))
+test_dict_standalone = min({"first": 1, "second": 2}, key=standalone_call)
+test_items_standalone = min({"first": 1, "second": 2}.items(), key=standalone_call)
+
+expected_type_form_min_param_1: Callable[[Tuple[str, int]], Union[SupportsDunderLT[Any], SupportsDunderGT[Any]]]
+revealed_type_itemgetter_call: Callable[[SupportsGetItem[Any, _T]], _T]  # pyright: ignore[reportGeneralTypeIssues]
+
+revealed_type_itemgetter_call = itemgetter(1)
+expected_type_form_min_param_1 = itemgetter(1)
+
+revealed_type_itemgetter_call = standalone_call
+expected_type_form_min_param_1 = standalone_call
