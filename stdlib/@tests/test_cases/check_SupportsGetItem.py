ad8f4d18d388270dcab1a28b7d8e5ba03ed8528d
Avasam <samuel.06@hotmail.com>
2025-02-10 18:11:52 -0500

    clearer, more relevant tests

--- stdlib/@tests/test_cases/check_SupportsGetItem.py
+++ stdlib/@tests/test_cases/check_SupportsGetItem.py
@@ -3,27 +3,34 @@ from __future__ import annotations
 from _typeshed import SupportsDunderGT, SupportsDunderLT, SupportsGetItem
 from collections.abc import Callable
 from operator import itemgetter
-from typing import Any, TypeVar
+from typing import Any, TypeVar, assert_type
 
 _T = TypeVar("_T")
 
 
-# This should be equivalent to itemgetter.__call__
-def standalone_call(obj: SupportsGetItem[int, _T]) -> _T: ...
+# This should be equivalent to itemgetter().__call__
+def standalone_call(obj: SupportsGetItem[Any, _T]) -> _T: ...
+
+
+# Expected type of itemgetter(1).__call__
+expected_type_itemgetter_call: Callable[[SupportsGetItem[int, _T]], _T]  # pyright: ignore[reportGeneralTypeIssues]
+
+# Expecting itemgetter(1) to be assignable to this
+# based on the example below: min({"first": 1, "second": 2}.items(), key=itemgetter(1))
+expected_assignable_to: Callable[[tuple[str, int]], SupportsDunderLT[Any] | SupportsDunderGT[Any]]
 
 
 # Regression tests for https://github.com/python/mypy/issues/14032
+assert_type(itemgetter("")({"first": 1, "second": 2}), int)
+assert_type(min({"first": 1, "second": 2}, key=itemgetter(1)), str)
+assert_type(min({"first": 1, "second": 2}.items(), key=itemgetter(1)), tuple[str, int])
+assert_type(standalone_call({"first": 1, "second": 2}), int)
+assert_type(min({"first": 1, "second": 2}, key=standalone_call), str)
+assert_type(min({"first": 1, "second": 2}.items(), key=standalone_call), tuple[str, int])
 
-test_dict = min({"first": 1, "second": 2}, key=itemgetter(1))
-test_items = min({"first": 1, "second": 2}.items(), key=itemgetter(1))
-test_dict_standalone = min({"first": 1, "second": 2}, key=standalone_call)
-test_items_standalone = min({"first": 1, "second": 2}.items(), key=standalone_call)
+expected_itemgetter_call_type = itemgetter(1).__call__
+expected_itemgetter_call_type = itemgetter(1)
+expected_assignable_to = itemgetter(1)
 
-expected_type_form_min_param_1: Callable[[tuple[str, int]], SupportsDunderLT[Any] | SupportsDunderGT[Any]]
-revealed_type_itemgetter_call: Callable[[SupportsGetItem[Any, _T]], _T]  # pyright: ignore[reportGeneralTypeIssues]
-
-revealed_type_itemgetter_call = itemgetter(1)
-expected_type_form_min_param_1 = itemgetter(1)
-
-revealed_type_itemgetter_call = standalone_call
-expected_type_form_min_param_1 = standalone_call
+expected_itemgetter_call_type = standalone_call
+expected_assignable_to = standalone_call
