4cdcb955fcc4f23d84e6923a24c9403323a98802
Jia Chen <grievejia@fb.com>
2025-02-20 15:15:20 -0800

    Make `Any` a proper class instead of an alias to `object()`

--- stdlib/typing.pyi
+++ stdlib/typing.pyi
@@ -130,7 +130,8 @@ if sys.version_info >= (3, 12):
 if sys.version_info >= (3, 13):
     __all__ += ["get_protocol_members", "is_protocol", "NoDefault", "TypeIs", "ReadOnly"]
 
-Any = object()
+class Any:
+    pass
 
 class _Final: ...
 
