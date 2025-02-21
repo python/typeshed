1294d4fbc0748ed91b9af8a78f80bb209250d60c
Sam Bull <git@sambull.org>
2025-02-21 18:17:03 +0000

    Return BufferedIncrementalReader for some encodings

--- stdlib/codecs.pyi
+++ stdlib/codecs.pyi
@@ -3,7 +3,7 @@ from _codecs import *
 from _typeshed import ReadableBuffer
 from abc import abstractmethod
 from collections.abc import Callable, Generator, Iterable
-from typing import Any, BinaryIO, ClassVar, Final, Literal, Protocol, TextIO
+from typing import Any, BinaryIO, ClassVar, Final, Literal, Protocol, TextIO, overload
 from typing_extensions import Self
 
 __all__ = [
@@ -57,6 +57,8 @@ BOM32_BE: Final = b"\xfe\xff"
 BOM32_LE: Final = b"\xff\xfe"
 BOM64_BE: Final = b"\x00\x00\xfe\xff"
 BOM64_LE: Final = b"\xff\xfe\x00\x00"
+
+_BufferedEncoding = Literal["idna", "raw-unicode-escape", "unicode-escape", "utf-16", "utf-16-be", "utf-16-le", "utf-32", "utf-32-be", "utf-32-le", "utf-7", "utf-8", "utf-8-sig"]
 
 class _WritableStream(Protocol):
     def write(self, data: bytes, /) -> object: ...
@@ -125,6 +127,9 @@ class CodecInfo(tuple[_Encoder, _Decoder, _StreamReader, _StreamWriter]):
 def getencoder(encoding: str) -> _Encoder: ...
 def getdecoder(encoding: str) -> _Decoder: ...
 def getincrementalencoder(encoding: str) -> _IncrementalEncoder: ...
+@overload
+def getincrementaldecoder(encoding: _BufferedEncoding) -> BufferedIncrementalDecoder: ...
+@overload
 def getincrementaldecoder(encoding: str) -> _IncrementalDecoder: ...
 def getreader(encoding: str) -> _StreamReader: ...
 def getwriter(encoding: str) -> _StreamWriter: ...
