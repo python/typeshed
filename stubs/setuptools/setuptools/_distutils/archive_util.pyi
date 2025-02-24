e0046743316a14a8d25a2f47f8ba4467a8790b9d
Avasam <samuel.06@hotmail.com>
2025-02-24 17:33:18 -0500

    setuptools._distutils typing improvements from merging types upstream

--- stubs/setuptools/setuptools/_distutils/archive_util.pyi
+++ stubs/setuptools/setuptools/_distutils/archive_util.pyi
@@ -1,22 +1,10 @@
 from _typeshed import StrOrBytesPath, StrPath
-from typing import Literal, overload
+from typing import Literal
 
-@overload
 def make_archive(
     base_name: str,
     format: str,
     root_dir: StrOrBytesPath | None = None,
-    base_dir: str | None = None,
-    verbose: bool = False,
-    dry_run: bool = False,
-    owner: str | None = None,
-    group: str | None = None,
-) -> str: ...
-@overload
-def make_archive(
-    base_name: StrPath,
-    format: str,
-    root_dir: StrOrBytesPath,
     base_dir: str | None = None,
     verbose: bool = False,
     dry_run: bool = False,
@@ -32,4 +20,4 @@ def make_tarball(
     owner: str | None = None,
     group: str | None = None,
 ) -> str: ...
-def make_zipfile(base_name: str, base_dir: str, verbose: bool = False, dry_run: bool = False) -> str: ...
+def make_zipfile(base_name: str, base_dir: StrPath, verbose: bool = False, dry_run: bool = False) -> str: ...
