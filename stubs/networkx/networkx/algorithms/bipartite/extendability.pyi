84c78c6442ed147e52849d39234b55dfa14aedf2
Daniel Darabos <darabos.daniel@gmail.com>
2025-02-26 15:03:13 +0100

    Extract NetworkX types from docstrings (#13458)

--- stubs/networkx/networkx/algorithms/bipartite/extendability.pyi
+++ stubs/networkx/networkx/algorithms/bipartite/extendability.pyi
@@ -1,6 +1,5 @@
 from networkx.classes.graph import Graph, _Node
 from networkx.utils.backends import _dispatchable
-from numpy.random import RandomState
 
 @_dispatchable
-def diameter(G: Graph[_Node], seed: int | RandomState | None = None): ...
+def maximal_extendability(G: Graph[_Node]): ...
