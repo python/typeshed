fe4628f4ae430a0ff551de66a35ab7650350be17
Sebastian Rittau <srittau@rittau.biz>
2025-02-26 12:15:24 +0100

    Update authlib to 1.5.*
    Closes: #13536

--- stubs/Authlib/authlib/__init__.pyi
+++ stubs/Authlib/authlib/__init__.pyi
@@ -1,4 +1,8 @@
-from .consts import homepage, version
+from typing import Final
+
+from .consts import author, homepage, version
 
 __version__ = version
 __homepage__ = homepage
+__author__ = author
+__license__: Final[str]
