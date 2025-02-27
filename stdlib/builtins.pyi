2f44dfaf0de6a2194c2e434a7e74745f7ddadd3d
jakkdl <h6+github@pm.me>
2025-02-27 18:04:44 +0100

    I should maybe figure out how to run all the proper tests locally

--- stdlib/builtins.pyi
+++ stdlib/builtins.pyi
@@ -2077,7 +2077,8 @@ if sys.version_info >= (3, 11):
     # See `check_exception_group.py` for use-cases and comments.
     class BaseExceptionGroup(BaseException, Generic[_BaseExceptionT_co]):
         @overload
-        def __new__(
+        # mypy thinks this is an invalid type for cls/self
+        def __new__(  # type: ignore[misc]
             cls: ExceptionGroup[_ExceptionT_co], message: str, exceptions: Sequence[_ExceptionT_co], /
         ) -> ExceptionGroup[_ExceptionT_co]: ...
         @overload
