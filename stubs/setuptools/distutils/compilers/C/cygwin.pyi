d4f3cceec0c78729e6853d135b34c3c59cd9adb6
Avasam <samuel.06@hotmail.com>
2025-03-15 13:46:06 -0400

    Teach our other stubs that our setuptools stubs is partial

--- stubs/setuptools/distutils/compilers/C/cygwin.pyi
+++ stubs/setuptools/distutils/compilers/C/cygwin.pyi
@@ -1,1 +1,1 @@
-from setuptools._distutils.compilers.C.cygwin import *  # pyright: ignore[reportMissingTypeStubs] # Submodule untyped, still expose it
+from setuptools._distutils.compilers.C.cygwin import *
