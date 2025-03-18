5d0b0fb5da2e8e2e9bdf8de276900ac59afa7018
Jia Chen <grievejia@fb.com>
2025-03-18 14:32:21 -0700

    Fix formatting and ignore and more ignores on Any* in stub tests

--- stdlib/typing.pyi
+++ stdlib/typing.pyi
@@ -130,9 +130,7 @@ if sys.version_info >= (3, 12):
 if sys.version_info >= (3, 13):
     __all__ += ["get_protocol_members", "is_protocol", "NoDefault", "TypeIs", "ReadOnly"]
 
-class Any:
-    pass
-
+class Any: ...
 class _Final: ...
 
 def final(f: _T) -> _T: ...
