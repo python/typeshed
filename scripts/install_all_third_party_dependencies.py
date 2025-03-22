1d59531b92b93ae42c085ee783b665ecbf9e1344
donBarbos <donbarbos@proton.me>
2025-03-22 18:53:26 +0400

    Support `uv` for installing third party dependencies

--- scripts/install_all_third_party_dependencies.py
+++ scripts/install_all_third_party_dependencies.py
@@ -1,6 +1,14 @@
 import subprocess
+import sys
 
 from ts_utils.requirements import get_external_stub_requirements
 
+use_uv = "--uv" in sys.argv
+if use_uv:
+    sys.argv.remove("--uv")
+    pip_command = ["uv", "pip", "install"]
+else:
+    pip_command = ["pip", "install"]
+
 requirements = get_external_stub_requirements()
-subprocess.check_call(("pip", "install", *[str(requirement) for requirement in requirements]))
+subprocess.check_call(pip_command + [str(requirement) for requirement in requirements])
