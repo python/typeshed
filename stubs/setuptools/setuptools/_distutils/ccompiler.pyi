fccf5cbd79104a1fd7b871a4348513aee187072e
Sebastian Rittau <srittau@rittau.biz>
2025-03-27 13:02:19 +0100

    Remove an item from __all__

--- stubs/setuptools/setuptools/_distutils/ccompiler.pyi
+++ stubs/setuptools/setuptools/_distutils/ccompiler.pyi
@@ -1,6 +1,5 @@
 from .compilers.C import base
 from .compilers.C.base import (
-    compiler_class as compiler_class,
     gen_lib_options,
     gen_preprocess_options,
     get_default_compiler,
@@ -12,7 +11,6 @@ from .compilers.C.errors import CompileError, LinkError
 __all__ = [
     "CompileError",
     "LinkError",
-    "compiler_class",
     "gen_lib_options",
     "gen_preprocess_options",
     "get_default_compiler",
