9802dcd5925f8037c76d3f02422c660a597232b1
Alex Waygood <alex.waygood@gmail.com>
2025-04-03 11:26:50 +0100

    add noqas

--- stdlib/typing_extensions.pyi
+++ stdlib/typing_extensions.pyi
@@ -32,7 +32,7 @@ from collections.abc import (
 from contextlib import AbstractAsyncContextManager as AsyncContextManager, AbstractContextManager as ContextManager
 from re import Match as Match, Pattern as Pattern
 from types import GenericAlias, ModuleType
-from typing import (  # noqa: Y022,Y037,Y038,Y039
+from typing import (  # noqa: Y022,Y037,Y038,Y039,UP035
     IO as IO,
     TYPE_CHECKING as TYPE_CHECKING,
     AbstractSet as AbstractSet,
