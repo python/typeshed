a00e8092097566fe69543e35ed932bbf23dbee65
donBarbos <donbarbos@proton.me>
2025-04-08 23:05:11 +0400

    Revert "Adding Jinja2"

--- stubs/click-web/click_web/__init__.pyi
+++ stubs/click-web/click_web/__init__.pyi
@@ -1,11 +1,12 @@
 import logging
 import types
+from _typeshed import Incomplete
 
 import click
 import flask
-import jinja2
 
-jinja_env: jinja2.Environment
+# This should be jinja2.Environment, but it does not have stubs and forbidden for requires in METADATA.toml
+jinja_env: Incomplete
 script_file: str | None
 click_root_cmd: str | None
 OUTPUT_FOLDER: str
