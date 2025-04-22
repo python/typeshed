ad863a810c18066cc54c540fc5e34a77e3cf0d8e
donBarbos <donbarbos@proton.me>
2025-04-22 04:57:27 +0400

    Improve `braintree`

--- stubs/braintree/braintree/unknown_payment_method.pyi
+++ stubs/braintree/braintree/unknown_payment_method.pyi
@@ -1,4 +1,6 @@
+from typing import Literal
+
 from braintree.resource import Resource
 
 class UnknownPaymentMethod(Resource):
-    def image_url(self): ...
+    def image_url(self) -> Literal["https://assets.braintreegateway.com/payment_method_logo/unknown.png"]: ...
