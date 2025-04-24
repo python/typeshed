ce2e073c8994db0a00e7ac74dc482901d12cf21b
donBarbos <donbarbos@proton.me>
2025-04-24 05:42:55 +0400

    Revert "Adding Pillow"

--- stubs/ttkthemes/ttkthemes/_imgops.pyi
+++ stubs/ttkthemes/ttkthemes/_imgops.pyi
@@ -1,4 +1,7 @@
-from PIL.Image import Image
+from typing import Any
+from typing_extensions import TypeAlias
+
+Image: TypeAlias = Any  # actually PIL.Image, but not worth adding a dependency
 
 def shift_hue(image: Image, hue: float) -> Image: ...
 def make_transparent(image: Image) -> Image: ...
