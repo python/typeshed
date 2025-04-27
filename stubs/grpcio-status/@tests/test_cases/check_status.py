b1caa0af590fc9a8cd7b18e5da4691378d98c6e7
Avasam <samuel.06@hotmail.com>
2025-04-27 18:40:30 -0400

    Remove broken test

--- stubs/grpcio-status/@tests/test_cases/check_status.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from grpc import Status
-from grpc_status import to_status
-
-# XXX: to_status actually expects a "google.rpc.status.Status",
-# but the stubs for that aren't present yet.
-status: Status = to_status(None)
