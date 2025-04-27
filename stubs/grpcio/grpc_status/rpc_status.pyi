05554dc2c61ebb6bab21b8cf87131d6b8d651c05
Avasam <samuel.06@hotmail.com>
2025-04-27 16:50:52 -0400

    Remove Any in stubs/grpcio/grpc_status/rpc_status.pyi

--- stubs/grpcio/grpc_status/rpc_status.pyi
+++ stubs/grpcio/grpc_status/rpc_status.pyi
@@ -1,13 +1,7 @@
-from typing import Any
-
 import grpc
 
-# XXX: don't yet know how to add a stub for google.rpc.status_pb2.Status
-# without affecting other stuff; may need to make a stub-only package for
-# google.rpc as well.
-
 # Returns a google.rpc.status.Status message corresponding to a given grpc.Call.
-def from_call(call: grpc.Call) -> Any: ...
+def from_call(call: grpc.Call): ...
 
 # Convert a google.rpc.status.Status message to grpc.Status.
-def to_status(status: Any) -> grpc.Status: ...
+def to_status(status) -> grpc.Status: ...
