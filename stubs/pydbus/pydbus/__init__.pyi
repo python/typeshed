fb0e57b64a930d275a76c992871225c657dcf46e
Andrew Udvare <audvare@gmail.com>
2025-05-07 07:12:57 -0400

    pydbus: do not export re-export Variant

--- stubs/pydbus/pydbus/__init__.pyi
+++ stubs/pydbus/pydbus/__init__.pyi
@@ -1,5 +1,3 @@
-from gi.repository.GLib import Variant as Variant
-
 from .bus import SessionBus as SessionBus, SystemBus as SystemBus, connect as connect
 
-__all__ = ["SessionBus", "SystemBus", "Variant", "connect"]
+__all__ = ["SessionBus", "SystemBus", "connect"]
