6060876c3945f2443efb44a48b98f834e89d8aa8
Max Muoto <maxmuoto@gmail.com>
2025-05-08 18:57:41 -0500

    feat: Improve `json.dumps`, `json.dump`  typing

--- stdlib/json/__init__.pyi
+++ stdlib/json/__init__.pyi
@@ -1,14 +1,19 @@
 from _typeshed import SupportsRead, SupportsWrite
 from collections.abc import Callable
-from typing import Any
+from typing import Any, TypeAlias, TypeVar, overload
 
 from .decoder import JSONDecodeError as JSONDecodeError, JSONDecoder as JSONDecoder
 from .encoder import JSONEncoder as JSONEncoder
 
 __all__ = ["dump", "dumps", "load", "loads", "JSONDecoder", "JSONDecodeError", "JSONEncoder"]
 
+_T = TypeVar("_T")
+
+_JSON: TypeAlias = dict[str, _JSON] | list[_JSON] | str | int | float | bool | None
+
+@overload
 def dumps(
-    obj: Any,
+    obj: _JSON,
     *,
     skipkeys: bool = False,
     ensure_ascii: bool = True,
@@ -17,12 +22,28 @@ def dumps(
     cls: type[JSONEncoder] | None = None,
     indent: None | int | str = None,
     separators: tuple[str, str] | None = None,
-    default: Callable[[Any], Any] | None = None,
+    default: None = None,
     sort_keys: bool = False,
     **kwds: Any,
 ) -> str: ...
+@overload
+def dumps(
+    obj: _T,
+    *,
+    skipkeys: bool = False,
+    ensure_ascii: bool = True,
+    check_circular: bool = True,
+    allow_nan: bool = True,
+    cls: type[JSONEncoder] | None = None,
+    indent: None | int | str = None,
+    separators: tuple[str, str] | None = None,
+    default: Callable[[_T], _JSON],
+    sort_keys: bool = False,
+    **kwds: Any,
+) -> str: ...
+@overload
 def dump(
-    obj: Any,
+    obj: _JSON,
     fp: SupportsWrite[str],
     *,
     skipkeys: bool = False,
@@ -32,7 +53,23 @@ def dump(
     cls: type[JSONEncoder] | None = None,
     indent: None | int | str = None,
     separators: tuple[str, str] | None = None,
-    default: Callable[[Any], Any] | None = None,
+    default: None = None,
+    sort_keys: bool = False,
+    **kwds: Any,
+) -> None: ...
+@overload
+def dump(
+    obj: _T,
+    fp: SupportsWrite[str],
+    *,
+    skipkeys: bool = False,
+    ensure_ascii: bool = True,
+    check_circular: bool = True,
+    allow_nan: bool = True,
+    cls: type[JSONEncoder] | None = None,
+    indent: None | int | str = None,
+    separators: tuple[str, str] | None = None,
+    default: Callable[[_T], _JSON],
     sort_keys: bool = False,
     **kwds: Any,
 ) -> None: ...
