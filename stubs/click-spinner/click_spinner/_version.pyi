6dc982d2342bd66e84210f374c982542b580fb47
donBarbos <donbarbos@proton.me>
2025-05-09 19:11:50 +0400

    Use TypedDict

--- stubs/click-spinner/click_spinner/_version.pyi
+++ stubs/click-spinner/click_spinner/_version.pyi
@@ -1,5 +1,11 @@
-from typing import Literal
+from typing import TypedDict
+
+class _Versions(TypedDict):
+    dirty: bool
+    error: None
+    full_revisionid: str
+    version: str
 
 version_json: str
 
-def get_versions() -> dict[Literal["dirty", "error", "full-revisionid", "version"], str | bool | None]: ...
+def get_versions() -> _Versions: ...
