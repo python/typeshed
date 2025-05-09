a05d94a6dbe3ae7e519b94ca880157bc3fc0c67d
Max Muoto <maxmuoto@gmail.com>
2025-05-08 19:39:22 -0500

    Add json.dump tests

--- stdlib/@tests/test_cases/check_json.py
+++ stdlib/@tests/test_cases/check_json.py
@@ -1,7 +1,12 @@
-from __future__ import annotations
-
 import json
 from decimal import Decimal
+
+
+class _File:
+    def write(self, s: str) -> int: ...
+
+
+fp = _File()
 
 
 # By default, json.dumps() will not accept any non JSON-serializable objects.
@@ -9,11 +14,15 @@ class CustomClass: ...
 
 
 json.dumps(CustomClass())  # type: ignore
+json.dump(CustomClass(), fp)  # type: ignore
 json.dumps(object())  # type: ignore
+json.dump(object(), fp)  # type: ignore
 json.dumps(Decimal(1))  # type: ignore
+json.dump(Decimal(1), fp)  # type: ignore
 
 # Serializable types are supported, included nested JSON.
 json.dumps({"a": 34, "b": [1, 2, 3], "c": {"d": "hello", "e": False}})
+json.dump({"a": 34, "b": [1, 2, 3], "c": {"d": "hello", "e": False}}, fp)
 json.dumps(
     {
         "numbers": [1, 2, 3, 4, 5],
@@ -23,18 +32,31 @@ json.dumps(
         "nested": {"array": [[1, 2], [3, 4.34]], "object": {"x": 1, "y": 2}},
     }
 )
+json.dump(
+    {
+        "numbers": [1, 2, 3, 4, 5],
+        "strings": ["hello", "world"],
+        "booleans": [True, False],
+        "null": None,
+        "nested": {"array": [[1, 2], [3, 4.34]], "object": {"x": 1, "y": 2}},
+    },
+    fp,
+)
 json.dumps(1)
+json.dump(1, fp)
 json.dumps(1.23)
+json.dump(1.23, fp)
 json.dumps(True)
-json.dumps(False)
-json.dumps(None)
-json.dumps("hello")
+json.dump(True, fp)
 
+# Test explicit nested types that might cause variance issues.
 x: dict[str, float | int] = {"a": 1, "b": 2.0}
 json.dumps(x)
+json.dump(x, fp)
 
 z: dict[str, dict[str, dict[str, list[int]]]] = {"a": {"b": {"c": [1, 2, 3]}}}
 json.dumps(z)
+json.dump(z, fp)
 
 
 # Custom types are supported when a custom encoder is provided.
@@ -43,6 +65,7 @@ def decimal_encoder(obj: Decimal) -> float:
 
 
 json.dumps(Decimal(1), default=decimal_encoder)
+json.dump(Decimal(1), fp, default=decimal_encoder)
 
 
 # If the custom encoder doesn't return JSON, it will lead a typing error..
@@ -51,3 +74,4 @@ def custom_encoder(obj: Decimal) -> Decimal:
 
 
 json.dumps(Decimal(1), default=custom_encoder)  #  type: ignore
+json.dump(Decimal(1), fp, default=custom_encoder)  #  type: ignore
