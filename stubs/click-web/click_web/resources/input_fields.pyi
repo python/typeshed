8b4985a79cf878eb96d913bfe42161f2b07a90ed
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-05-10 15:59:54 -0700

    ?

--- stubs/click-web/click_web/resources/input_fields.pyi
+++ stubs/click-web/click_web/resources/input_fields.pyi
@@ -1,3 +1,4 @@
+import sys
 from typing import Any, ClassVar, Final
 
 import click
@@ -46,7 +47,10 @@ class BaseInput:
     def _build_name(self, name: str): ...
 
 class ChoiceInput(BaseInput):
-    param_type_cls: type[click.Choice[Any]]
+    if sys.version_info >= (3, 10):
+        param_type_cls: type[click.Choice[Any]]
+    else:
+        param_type_cls: type[click.Choice]
 
 class FlagInput(BaseInput):
     param_type_cls: None
