cf87104c075b92e8b7becc8e81ca8ec2e7f0f4cb
Anton Pilipenko <anton.pilipenko@retechlabs.com>
2025-05-10 11:01:33 +0400

    feat PhotoImage->put to x1 y1 ?x2 y2?

--- stdlib/tkinter/__init__.pyi
+++ stdlib/tkinter/__init__.pyi
@@ -3744,7 +3744,7 @@ class PhotoImage(Image, _PhotoImageLike):
             | tuple[list[str], ...]
             | tuple[tuple[str, ...], ...]
         ),
-        to: tuple[int, int] | None = None,
+        to: tuple[int, int] | tuple[int, int, int, int] | None = None,
     ) -> None: ...
     if sys.version_info >= (3, 13):
         def read(
