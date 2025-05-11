0668ed6fa139bfa6dd7d8a8a44d443af8cab8b5f
Sebastian Rittau <srittau@rittau.biz>
2025-05-11 19:02:38 +0200

    `BufferedWriter` can handle non-blocking I/O

--- stdlib/@tests/test_cases/check_io.py
+++ stdlib/@tests/test_cases/check_io.py
@@ -1,10 +1,15 @@
 from _io import BufferedReader, BufferedRWPair, BufferedWriter
 from gzip import GzipFile
 from io import FileIO, RawIOBase, TextIOWrapper
+from socket import SocketIO
+from typing import Any
 from typing_extensions import assert_type
+
+socket: Any = None
 
 BufferedReader(RawIOBase())
 BufferedWriter(RawIOBase())
+BufferedWriter(SocketIO(socket, "r"))
 
 BufferedRWPair(open("", "rb"), open("", "wb"))
 
