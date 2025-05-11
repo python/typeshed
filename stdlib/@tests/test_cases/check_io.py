e4ccafe27cb689b9336e637637d4b5e3d251d545
Sebastian Rittau <srittau@rittau.biz>
2025-05-11 18:23:38 +0200

    Add a test

--- stdlib/@tests/test_cases/check_io.py
+++ stdlib/@tests/test_cases/check_io.py
@@ -1,10 +1,12 @@
-from _io import BufferedReader, BufferedWriter
+from _io import BufferedReader, BufferedRWPair, BufferedWriter
 from gzip import GzipFile
 from io import FileIO, RawIOBase, TextIOWrapper
 from typing_extensions import assert_type
 
 BufferedReader(RawIOBase())
 BufferedWriter(RawIOBase())
+
+BufferedRWPair(open("", "rb"), open("", "wb"))
 
 assert_type(TextIOWrapper(FileIO("")).buffer, FileIO)
 assert_type(TextIOWrapper(FileIO(13)).detach(), FileIO)
