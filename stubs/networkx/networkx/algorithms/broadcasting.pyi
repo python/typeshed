78edaf4460417a63a68b07f98992d3607b4b3596
Avasam <samuel.06@hotmail.com>
2025-05-16 15:07:18 -0400

    Clearer re-exports for pytype

--- stubs/networkx/networkx/algorithms/broadcasting.pyi
+++ stubs/networkx/networkx/algorithms/broadcasting.pyi
@@ -1,9 +1,9 @@
+import networkx as nx
 from networkx.classes.graph import Graph, _Node
-from networkx.utils.backends import _dispatchable
 
 __all__ = ["tree_broadcast_center", "tree_broadcast_time"]
 
-@_dispatchable
+@nx._dispatchable
 def tree_broadcast_center(G: Graph[_Node]) -> tuple[int, set[_Node]]: ...
-@_dispatchable
+@nx._dispatchable
 def tree_broadcast_time(G: Graph[_Node], node: int | None = None) -> int: ...
