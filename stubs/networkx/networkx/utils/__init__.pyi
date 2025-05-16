d8c3d555309905a7385f83cbb39b38538d19c4e8
Avasam <samuel.06@hotmail.com>
2025-05-16 13:29:46 -0400

    networkx: Add symbols from python-type-stubs

--- stubs/networkx/networkx/utils/__init__.pyi
+++ stubs/networkx/networkx/utils/__init__.pyi
@@ -1,4 +1,4 @@
-from networkx.utils.backends import _dispatchable as _dispatchable
+from networkx.utils.backends import *
 from networkx.utils.configs import *
 from networkx.utils.configs import NetworkXConfig
 from networkx.utils.decorators import *
