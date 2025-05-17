25ae6d39ea5e00577bfee0790b794efe9f4ca387
donbarbos <donbarbos@proton.me>
2025-05-17 18:58:39 +0400

    draft: impove `docutils`

--- stubs/docutils/docutils/examples.pyi
+++ stubs/docutils/docutils/examples.pyi
@@ -1,1 +1,42 @@
-def __getattr__(name: str): ...  # incomplete module
+from _typeshed import Incomplete
+from typing import Literal, overload
+
+from docutils.core import Publisher, _WriterParts
+from docutils.io import FileInput, StringInput
+from docutils.nodes import document
+
+def html_parts(
+    input_string: str,
+    source_path=None,
+    destination_path=None,
+    input_encoding: str = "unicode",
+    doctitle: bool = True,
+    initial_header_level: Literal[1, 2, 3, 4, 5, 6] = 1,
+) -> _WriterParts: ...
+@overload
+def html_body(
+    input_string: str,
+    source_path=None,
+    destination_path=None,
+    input_encoding: str = "unicode",
+    output_encoding: Literal["unicode"] = "unicode",
+    doctitle: bool = True,
+    initial_header_level: Literal[1, 2, 3, 4, 5, 6] = 1,
+) -> str: ...
+@overload
+def html_body(
+    input_string: str,
+    source_path=None,
+    destination_path=None,
+    input_encoding: str = "unicode",
+    output_encoding: str = "unicode",
+    doctitle: bool = True,
+    initial_header_level: Literal[1, 2, 3, 4, 5, 6] = 1,
+) -> str | bytes: ...
+def internals(
+    input_string,
+    source_path: FileInput | StringInput | None = None,
+    destination_path: FileInput | StringInput | None = None,
+    input_encoding: str = "unicode",
+    settings_overrides: dict[str, Incomplete] | None = None,
+) -> tuple[document | None, Publisher]: ...
