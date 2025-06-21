d3bdca5bdb023ebe640a43818a5df82ee77cc741
donBarbos <donbarbos@proton.me>
2025-06-21 16:39:35 +0400

    Use `Final`

--- stubs/flake8/flake8/_compat.pyi
+++ stubs/flake8/flake8/_compat.pyi
@@ -4,6 +4,6 @@ FSTRING_START: Final[int]
 FSTRING_MIDDLE: Final[int]
 FSTRING_END: Final[int]
 
-TSTRING_START: int
-TSTRING_MIDDLE: int
-TSTRING_END: int
+TSTRING_START: Final[int]
+TSTRING_MIDDLE: Final[int]
+TSTRING_END: Final[int]
