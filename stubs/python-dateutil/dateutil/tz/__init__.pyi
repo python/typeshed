92c65142998d9c01edf5fd344427f6bcda74d1d3
donBarbos <donbarbos@proton.me>
2025-06-22 23:00:49 +0400

    [python-dateutil] Add missing stubs
    * Fix and improve few stubs

--- stubs/python-dateutil/dateutil/tz/__init__.pyi
+++ stubs/python-dateutil/dateutil/tz/__init__.pyi
@@ -1,6 +1,9 @@
+import sys
+
 from .tz import (
     datetime_ambiguous as datetime_ambiguous,
     datetime_exists as datetime_exists,
+    enfold as enfold,
     gettz as gettz,
     resolve_imaginary as resolve_imaginary,
     tzfile as tzfile,
@@ -12,4 +15,31 @@ from .tz import (
     tzutc as tzutc,
 )
 
+if sys.platform == "win32":
+    from .win import tzwin as tzwin, tzwinlocal as tzwinlocal
+else:
+    tzwin: None
+    tzwinlocal: None
+
 UTC: tzutc
+
+__all__ = [
+    "tzutc",
+    "tzoffset",
+    "tzlocal",
+    "tzfile",
+    "tzrange",
+    "tzstr",
+    "tzical",
+    "tzwin",
+    "tzwinlocal",
+    "gettz",
+    "enfold",
+    "datetime_ambiguous",
+    "datetime_exists",
+    "resolve_imaginary",
+    "UTC",
+    "DeprecatedTzFormatWarning",
+]
+
+class DeprecatedTzFormatWarning(Warning): ...
---
+++ stubs/python-dateutil/dateutil/tz/win.pyi
@@ -0,0 +1,46 @@
+import sys
+import winreg
+from ctypes import _Pointer, c_wchar
+from datetime import datetime, timedelta
+from typing import Any, ClassVar, Final
+from typing_extensions import Self
+
+from ._common import tzrangebase
+
+if sys.platform == "win32":
+    __all__ = ["tzwin", "tzwinlocal", "tzres"]
+
+    ONEWEEK: timedelta
+    TZKEYNAMENT: Final[str]
+    TZKEYNAME9X: Final[str]
+    TZLOCALKEYNAME: Final[str]
+    TZKEYNAME: Final[str]
+
+    class tzres:
+        p_wchar: ClassVar[type[_Pointer[c_wchar]]]
+        def __init__(self, tzres_loc="tzres.dll"): ...
+        def load_name(self, offset): ...
+        def name_from_string(self, tzname_str: str): ...
+
+    class tzwinbase(tzrangebase):
+        hasdst: bool
+        def __eq__(self, other: tzwinbase) -> bool: ...
+        @staticmethod
+        def list() -> list[str]: ...
+        def display(self) -> str | None: ...
+        def transitions(self, year: int) -> tuple[datetime, datetime] | None: ...
+
+    class tzwin(tzwinbase):
+        hasdst: bool
+        def __init__(self, name: str) -> None: ...
+        def __reduce__(self) -> tuple[type[Self], tuple[str, ...]]: ...
+
+    class tzwinlocal(tzwinbase):
+        hasdst: bool
+        def __init__(self) -> None: ...
+        def __reduce__(self) -> tuple[type[Self], tuple[str, ...]]: ...
+
+    def picknthweekday(year: int, month: int, dayofweek: int, hour: int, minute: int, whichweek: int) -> datetime: ...
+    def valuestodict(
+        key: winreg._KeyType,
+    ) -> dict[str, Any]: ...  # keys and values in dict are results of winreg.EnumValue() function
