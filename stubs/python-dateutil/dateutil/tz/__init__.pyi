0f653aa485ad72a5eec7307873a6df2419e37b2d
donBarbos <donbarbos@proton.me>
2025-06-23 20:39:56 +0400

    Fix import issues

--- stubs/python-dateutil/dateutil/tz/__init__.pyi
+++ stubs/python-dateutil/dateutil/tz/__init__.pyi
@@ -1,5 +1,8 @@
 import sys
+from datetime import datetime
+from typing_extensions import Self
 
+from ._common import tzrangebase
 from .tz import (
     datetime_ambiguous as datetime_ambiguous,
     datetime_exists as datetime_exists,
@@ -15,8 +18,27 @@ from .tz import (
     tzutc as tzutc,
 )
 
+# UTC, tzwin, tzwinlocal are defined in this class
+# otherwise pyright complains about unknown import symbol:
 if sys.platform == "win32":
-    from .win import tzwin as tzwin, tzwinlocal as tzwinlocal
+    class tzwinbase(tzrangebase):
+        hasdst: bool
+        def __eq__(self, other: tzwinbase) -> bool: ...  # type: ignore[override]
+        @staticmethod
+        def list() -> list[str]: ...
+        def display(self) -> str | None: ...
+        def transitions(self, year: int) -> tuple[datetime, datetime] | None: ...
+
+    class tzwin(tzwinbase):
+        hasdst: bool
+        def __init__(self, name: str) -> None: ...
+        def __reduce__(self) -> tuple[type[Self], tuple[str, ...]]: ...  # type: ignore[override]
+
+    class tzwinlocal(tzwinbase):
+        hasdst: bool
+        def __init__(self) -> None: ...
+        def __reduce__(self) -> tuple[type[Self], tuple[str, ...]]: ...  # type: ignore[override]
+
 else:
     tzwin: None
     tzwinlocal: None
--- stubs/python-dateutil/dateutil/tz/win.pyi
+++ stubs/python-dateutil/dateutil/tz/win.pyi
@@ -2,9 +2,8 @@ import sys
 from ctypes import _Pointer, c_wchar
 from datetime import datetime, timedelta
 from typing import Any, ClassVar, Final
-from typing_extensions import Self
 
-from ._common import tzrangebase
+from dateutil.tz import tzwin as tzwin, tzwinlocal as tzwinlocal
 
 if sys.platform == "win32":
     from winreg import _KeyType
@@ -22,24 +21,6 @@ if sys.platform == "win32":
         def __init__(self, tzres_loc="tzres.dll"): ...
         def load_name(self, offset): ...
         def name_from_string(self, tzname_str: str): ...
-
-    class tzwinbase(tzrangebase):
-        hasdst: bool
-        def __eq__(self, other: tzwinbase) -> bool: ...  # type: ignore[override]
-        @staticmethod
-        def list() -> list[str]: ...
-        def display(self) -> str | None: ...
-        def transitions(self, year: int) -> tuple[datetime, datetime] | None: ...
-
-    class tzwin(tzwinbase):
-        hasdst: bool
-        def __init__(self, name: str) -> None: ...
-        def __reduce__(self) -> tuple[type[Self], tuple[str, ...]]: ...  # type: ignore[override]
-
-    class tzwinlocal(tzwinbase):
-        hasdst: bool
-        def __init__(self) -> None: ...
-        def __reduce__(self) -> tuple[type[Self], tuple[str, ...]]: ...  # type: ignore[override]
 
     def picknthweekday(year: int, month: int, dayofweek: int, hour: int, minute: int, whichweek: int) -> datetime: ...
     def valuestodict(key: _KeyType) -> dict[str, Any]: ...  # keys and values in dict are results of winreg.EnumValue() function
