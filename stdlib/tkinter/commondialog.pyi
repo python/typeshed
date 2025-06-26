751014017b5455b2bc40af8e0f68db66346b5047
Marcell Perger <102254594+MarcellPerger1@users.noreply.github.com>
2025-06-26 22:26:11 +0100

    Add comment explaining use of `Any`
    Co-authored-by: Akuli <akuviljanen17@gmail.com>

--- stdlib/tkinter/commondialog.pyi
+++ stdlib/tkinter/commondialog.pyi
@@ -7,6 +7,8 @@ __all__ = ["Dialog"]
 class Dialog:
     command: ClassVar[str | None]
     master: Misc | None
+    # Types of options are very dynamic. They depend on the command and are
+    # sometimes changed to a different type.
     options: Mapping[str, Any]
     def __init__(self, master: Misc | None = None, **options: Any) -> None: ...
     def show(self, **options: Any): ...
