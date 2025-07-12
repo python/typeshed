824e1c0064fab3ca810471d174810dd838cbcc83
Max Muoto <maxmuoto@gmail.com>
2025-07-12 13:53:22 -0500

    Switch to `type_check_only`

--- stdlib/sys/_monitoring.pyi
+++ stdlib/sys/_monitoring.pyi
@@ -8,7 +8,7 @@
 import sys
 from collections.abc import Callable
 from types import CodeType
-from typing import Any, Final, final
+from typing import Any, Final, type_check_only
 from typing_extensions import deprecated
 
 DEBUGGER_ID: Final[int]
@@ -22,7 +22,7 @@ def get_tool(tool_id: int, /) -> str | None: ...
 
 events: Final[_events]
 
-@final
+@type_check_only
 class _events:
     CALL: Final[int]
     C_RAISE: Final[int]
