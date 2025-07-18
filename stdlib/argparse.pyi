0d8ec4081a2282b26650d82abb73a546056a8359
Semyon Moroz <donbarbos@proton.me>
2025-07-18 18:14:32 +0000

    Update stdlib/argparse.pyi
    Co-authored-by: Sebastian Rittau <srittau@rittau.biz>

--- stdlib/argparse.pyi
+++ stdlib/argparse.pyi
@@ -514,7 +514,7 @@ class _ArgumentGroup(_ActionsContainer):
             title: str | None = None,
             description: str | None = None,
             *,
-            prefix_chars: str = ...,
+            prefix_chars: str,
             argument_default: Any = ...,
             conflict_handler: str = ...,
         ) -> None: ...
