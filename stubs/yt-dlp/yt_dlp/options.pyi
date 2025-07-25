e6c440aedf2645f57faf9f68798e846c1a32e513
Andrew Udvare <audvare@gmail.com>
2025-07-24 21:36:39 -0400

    Fixing more Any types

--- stubs/yt-dlp/yt_dlp/options.pyi
+++ stubs/yt-dlp/yt_dlp/options.pyi
@@ -1,7 +1,8 @@
 import optparse
+from collections.abc import Sequence
 
 def parseOpts(
-    overrideArguments: object | None = None, ignore_config_files: str = "if_override"
+    overrideArguments: Sequence[str] | None = None, ignore_config_files: str = "if_override"
 ) -> tuple[_YoutubeDLOptionParser, optparse.Values, list[str]]: ...
 
 class _YoutubeDLOptionParser(optparse.OptionParser):
@@ -9,7 +10,7 @@ class _YoutubeDLOptionParser(optparse.OptionParser):
     ALIAS_TRIGGER_LIMIT: int
     def __init__(self) -> None: ...
     def parse_known_args(
-        self, args: list[str] | None = None, values: optparse.Values | None = None, strict: bool = True
+        self, args: Sequence[str] | None = None, values: optparse.Values | None = None, strict: bool = True
     ) -> tuple[optparse.Values, list[str]]: ...
 
 def create_parser() -> _YoutubeDLOptionParser: ...
