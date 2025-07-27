7e41a1650a8fcd6cb5b565eb6f774799d2867fef
donBarbos <donbarbos@proton.me>
2025-07-27 17:41:12 +0400

    Update version check

--- stdlib/glob.pyi
+++ stdlib/glob.pyi
@@ -9,7 +9,7 @@ __all__ = ["escape", "glob", "iglob"]
 if sys.version_info >= (3, 13):
     __all__ += ["translate"]
 
-if sys.version_info >= (3, 10):
+if sys.version_info >= (3, 13):
     @deprecated("Will be removed in Python 3.15; Use `glob.glob` and pass *root_dir* argument instead.")
     def glob0(dirname: AnyStr, pattern: AnyStr) -> list[AnyStr]: ...
     @deprecated("Will be removed in Python 3.15; Use `glob.glob` and pass *root_dir* argument instead.")
