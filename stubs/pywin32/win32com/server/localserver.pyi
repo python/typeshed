e9c036b23088bb6ed231942004b1cc0d222d926e
qwertycxz <gmail2@qq.com>
2025-07-29 19:59:00 +0800

    fix some stub error

--- stubs/pywin32/win32com/server/localserver.pyi
+++ stubs/pywin32/win32com/server/localserver.pyi
@@ -1,8 +1,9 @@
 from collections.abc import Iterable
+from typing import Final
 
 from _win32typing import PyIID
 
-usage: str
+usage: Final[str]
 
 def serve(clsids: Iterable[PyIID]) -> None: ...
 def main() -> None: ...
