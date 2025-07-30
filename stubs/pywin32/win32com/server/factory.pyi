4b911b9b251bb800485ea9b7c69b1f0c328d825a
qwertycxz <gmail2@qq.com>
2025-07-31 00:13:09 +0800

    Use `Unused` instead of `Any`

--- stubs/pywin32/win32com/server/factory.pyi
+++ stubs/pywin32/win32com/server/factory.pyi
@@ -1,9 +1,9 @@
+from _typeshed import Unused
 from collections.abc import Iterable
-from typing import Any
 
 from _win32typing import PyIClassFactory, PyIID
 
 def RegisterClassFactories(
     clsids: Iterable[PyIID], flags: int | None = None, clsctx: int | None = None
 ) -> list[tuple[PyIClassFactory, int]]: ...
-def RevokeClassFactories(infos: Iterable[tuple[Any, int]]) -> None: ...  # The first element is not used in the original code.
+def RevokeClassFactories(infos: Iterable[tuple[Unused, int]]) -> None: ...
