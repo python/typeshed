163cfa3bc0fafcda1a62ffee628a34d1b7930473
Ali Hamdan <ali.hamdan.dev@gmail.com>
2025-08-09 18:18:44 +0200

    networkx: annotate the linalg module

---
+++ stubs/networkx/networkx/_typing.pyi
@@ -0,0 +1,11 @@
+from typing import TypeVar
+from typing_extensions import TypeAlias
+
+import numpy as np
+
+_G = TypeVar("_G", bound=np.generic)
+
+# numpy aliases
+Array1D: TypeAlias = np.ndarray[tuple[int], np.dtype[_G]]
+Array2D: TypeAlias = np.ndarray[tuple[int, int], np.dtype[_G]]
+Seed: TypeAlias = int | np.random.Generator | np.random.RandomState
