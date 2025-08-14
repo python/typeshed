5d52b74fc41a1be303a2699a9ee7ee2342adb0e8
H4rryK4ne <39696020+H4rryK4ne@users.noreply.github.com>
2025-08-14 10:19:15 +0200

    Replace Collection by Sequence

--- stubs/django-filter/django_filters/filterset.pyi
+++ stubs/django-filter/django_filters/filterset.pyi
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from collections.abc import Collection
+from collections.abc import Sequence
 from enum import Enum
 from typing import Any, ClassVar
 
@@ -19,8 +19,8 @@ class UnknownFieldBehavior(Enum):
 
 class FilterSetOptions:
     model: type[Model] | None
-    fields: Collection[str] | dict[str, Collection[str]] | str | None
-    exclude: Collection[str] | None
+    fields: Sequence[str] | dict[str, Sequence[str]] | str | None
+    exclude: Sequence[str] | None
     filter_overrides: dict[type[models.Field[Any, Any]], dict[str, Any]]  # Field override mapping
     form: type[Form]
     unknown_field_behavior: UnknownFieldBehavior
@@ -82,7 +82,5 @@ class BaseFilterSet:
 class FilterSet(BaseFilterSet, metaclass=FilterSetMetaclass): ...
 
 def filterset_factory(
-    model: type[Model],
-    filterset: FilterSetMetaclass = ...,
-    fields: Collection[str] | dict[str, Collection[str]] | str | None = None,
+    model: type[Model], filterset: FilterSetMetaclass = ..., fields: Sequence[str] | dict[str, Sequence[str]] | str | None = None
 ) -> type[FilterSet]: ...
