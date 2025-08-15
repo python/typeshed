b052e8c80d03c335e68a79039bf14bafc2aeb83a
Ali Hamdan <ali.hamdan.dev@gmail.com>
2025-08-15 16:46:41 +0200

    .

--- stubs/networkx/networkx/_typing.pyi
+++ stubs/networkx/networkx/_typing.pyi
@@ -1,5 +1,6 @@
 # Stub-only module, can't be imported at runtime.
 
+import sys
 from collections.abc import Collection
 from typing import Any, Protocol, type_check_only
 from typing_extensions import TypeAlias, TypeVar
@@ -12,11 +13,16 @@ _GenericT_co = TypeVar("_GenericT_co", bound=np.generic, covariant=True)
 _ShapeT_co = TypeVar("_ShapeT_co", bound=tuple[int, ...], default=Any, covariant=True)
 
 # numpy aliases
-@type_check_only
-class SupportsArray(Protocol[_GenericT_co, _ShapeT_co]):
-    def __array__(self) -> np.ndarray[_ShapeT_co, np.dtype[_GenericT_co]]: ...
+if sys.version_info >= (3, 10):
+    @type_check_only
+    class SupportsArray(Protocol[_GenericT_co, _ShapeT_co]):
+        def __array__(self) -> np.ndarray[_ShapeT_co, np.dtype[_GenericT_co]]: ...
+
+    ArrayLike1D: TypeAlias = Collection[_ScalarT] | SupportsArray[_GenericT, tuple[int]]
+else:
+    # networkx does not support Python 3.9 but pyright still runs on 3.9 in CI
+    ArrayLike1D: TypeAlias = Collection[_ScalarT] | np.ndarray[tuple[int], np.dtype[_GenericT]]
 
 Array1D: TypeAlias = np.ndarray[tuple[int], np.dtype[_GenericT]]
 Array2D: TypeAlias = np.ndarray[tuple[int, int], np.dtype[_GenericT]]
-ArrayLike1D: TypeAlias = Collection[_ScalarT] | SupportsArray[_GenericT, tuple[int]]
 Seed: TypeAlias = int | np.random.Generator | np.random.RandomState
