f322f4411ca72030768f1d806a45434e9fa3bf92
Samuel Grayson <sam@samgrayson.me>
2025-08-20 12:47:51 -0500

    Replace IO with Protocol

--- stubs/networkx/networkx/drawing/nx_pydot.pyi
+++ stubs/networkx/networkx/drawing/nx_pydot.pyi
@@ -1,18 +1,16 @@
+from _typeshed import SupportsRead, SupportsWrite
 from os import PathLike
-from typing import IO, Any
-from typing_extensions import TypeAlias
+from typing import Any
 
 from networkx.classes.graph import Graph, _Node
 from networkx.utils.backends import _dispatchable
-from pydot import Dot  # type: ignore[import-not-found]  # pyright: ignore[reportMissingImports]
+from pydot import Dot  # type: ignore[import-not-found]
 
 __all__ = ["write_dot", "read_dot", "graphviz_layout", "pydot_layout", "to_pydot", "from_pydot"]
 
-_File: TypeAlias = str | PathLike[Any] | IO[str]
-
-def write_dot(G: Graph[_Node], path: _File) -> None: ...
+def write_dot(G: Graph[_Node], path: str | PathLike[Any] | SupportsWrite[str]) -> None: ...
 @_dispatchable
-def read_dot(path: _File) -> Graph[str]: ...
+def read_dot(path: str | PathLike[Any] | SupportsRead[str]) -> Graph[str]: ...
 @_dispatchable
 def from_pydot(P: Dot): ...
 def to_pydot(N: Graph[_Node]) -> Dot: ...
