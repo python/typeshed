a17c86bef4240c5286aa8b5cebbac659b33942b4
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-08-24 06:45:58 -0700

    ignore the Random thing for now

--- stdlib/_random.pyi
+++ stdlib/_random.pyi
@@ -6,7 +6,7 @@ _State: TypeAlias = tuple[int, ...]
 
 @disjoint_base
 class Random:
-    if sys.version_info >= (3, 11):
+    if sys.version_info >= (3, 10):
         def __init__(self, seed: object = ..., /) -> None: ...
     else:
         def __new__(self, seed: object = ..., /) -> Self: ...
