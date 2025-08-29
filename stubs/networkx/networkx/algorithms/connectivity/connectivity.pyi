347cbbfa716e003fe5b5081e57845b973e90eb50
GiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-08-29 12:23:51 -0700

    Fix int | floats in connectivity.pyi

--- stubs/networkx/networkx/algorithms/connectivity/connectivity.pyi
+++ stubs/networkx/networkx/algorithms/connectivity/connectivity.pyi
@@ -21,15 +21,15 @@ def local_node_connectivity(
     flow_func: Callable[[DiGraph[_Node], _Node, _Node], DiGraph[_Node]] | None = None,
     auxiliary: DiGraph[_Node] | None = None,
     residual: DiGraph[_Node] | None = None,
-    cutoff: int | float | None = None,
-) -> int | float: ...
+    cutoff: float | None = None,
+) -> float: ...
 @_dispatchable
 def node_connectivity(
     G: Graph[_Node],
     s: _Node | None = None,
     t: _Node | None = None,
     flow_func: Callable[[DiGraph[_Node], _Node, _Node], DiGraph[_Node]] | None = None,
-) -> int | float: ...
+) -> float: ...
 @_dispatchable
 def average_node_connectivity(
     G: Graph[_Node], flow_func: Callable[[DiGraph[_Node], _Node, _Node], DiGraph[_Node]] | None = None
@@ -39,7 +39,7 @@ def all_pairs_node_connectivity(
     G: Graph[_Node],
     nbunch: Iterable[tuple[_Node, _Node]] | None = None,
     flow_func: Callable[[DiGraph[_Node], _Node, _Node], DiGraph[_Node]] | None = None,
-) -> dict[_Node, dict[_Node, int | float]]: ...
+) -> dict[_Node, dict[_Node, float]]: ...
 @_dispatchable
 def local_edge_connectivity(
     G: Graph[_Node],
@@ -48,13 +48,13 @@ def local_edge_connectivity(
     flow_func: Callable[[DiGraph[_Node], _Node, _Node], DiGraph[_Node]] | None = None,
     auxiliary: DiGraph[_Node] | None = None,
     residual: DiGraph[_Node] | None = None,
-    cutoff: int | float | None = None,
-) -> int | float: ...
+    cutoff: float | None = None,
+) -> float: ...
 @_dispatchable
 def edge_connectivity(
     G: Graph[_Node],
     s: _Node,
     t: _Node,
     flow_func: Callable[[DiGraph[_Node], _Node, _Node], DiGraph[_Node]] | None = None,
-    cutoff: int | float | None = None,
-) -> int | float: ...
+    cutoff: float | None = None,
+) -> float: ...
