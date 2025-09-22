2a4fb364f98cdc25cc1baeb1eca53a4be11561d5
Sebastian Rittau <srittau@rittau.biz>
2025-09-22 12:37:41 +0200

    [reportlab] Update to ~= 4.4.4

--- stubs/reportlab/reportlab/platypus/tables.pyi
+++ stubs/reportlab/reportlab/platypus/tables.pyi
@@ -1,7 +1,7 @@
 from _typeshed import Incomplete
 from abc import abstractmethod
 from collections.abc import Collection, Iterable, Sequence
-from typing import Any, Literal, overload
+from typing import Any, Literal, NamedTuple, overload
 from typing_extensions import TypeAlias, Unpack
 
 from reportlab.lib.colors import Color
@@ -55,6 +55,13 @@ class TableStyle:
     def add(self, *cmd: Unpack[_RoundedCornersTableCommand]) -> None: ...
     def getCommands(self) -> list[_TableCommand]: ...
 
+class ShadowStyle(NamedTuple):
+    dx: int
+    dy: int
+    color0: _Color
+    color1: _Corner
+    nshades: int
+
 class Table(Flowable):
     ident: str | None
     repeatRows: int
@@ -89,6 +96,7 @@ class Table(Flowable):
         minRowHeights: Sequence[float] | None = None,
         cornerRadii: _CornerRadii | _UNSET_ | None = ...,
         renderCB: TableRenderCB | None = None,
+        shadow: ShadowStyle | None = None,
     ) -> None: ...
     def identity(self, maxLen: int | None = 30) -> str: ...
     def normalizeData(self, data: Iterable[Iterable[Any]]) -> list[list[Any]]: ...
