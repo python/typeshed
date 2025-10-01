55b5a1cdba054c50446cf6d650471ad9d5551d36
Alex Waygood <alex.waygood@gmail.com>
2025-10-01 10:51:55 +0100

    add comment, remove ignore, remove test

--- stdlib/@tests/test_cases/builtins/check_type.py
+++
@@ -1,8 +0,0 @@
-import sys
-from typing_extensions import assert_type
-
-if sys.version_info >= (3, 10):
-    from types import UnionType
-
-    # pyright has special-casing that causes this failure
-    assert_type(int | int, UnionType | type[int])  # pyright: ignore[reportAssertTypeFailure]
