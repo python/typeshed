f1f9c84634dc86bd01f24ceae74bfc4c625527e9
Pēteris Caune <cuu508@gmail.com>
2025-10-24 17:55:55 +0300

    Tighhten the return type in braintree.PaymentMethod.find()
    The possible return types are listed here:
    https://developer.paypal.com/braintree/docs/reference/response/payment-method/python/
    
    Annoyingly, these classes don't inherit from PaymentMethod,
    so need to be listed out.

--- stubs/braintree/braintree/payment_method.pyi
+++ stubs/braintree/braintree/payment_method.pyi
@@ -1,14 +1,22 @@
 from _typeshed import Incomplete
-
+from braintree.android_pay_card import AndroidPayCard
+from braintree.apple_pay_card import ApplePayCard
+from braintree.credit_card import CreditCard
 from braintree.error_result import ErrorResult
+from braintree.paypal_account import PayPalAccount
 from braintree.resource import Resource
 from braintree.successful_result import SuccessfulResult
+from braintree.us_bank_account import UsBankAccount
+from braintree.venmo_account import VenmoAccount
+from braintree.visa_checkout_card import VisaCheckoutCard
 
 class PaymentMethod(Resource):
     @staticmethod
     def create(params: dict[str, Incomplete] | None = None) -> SuccessfulResult | ErrorResult: ...
     @staticmethod
-    def find(payment_method_token: str) -> Resource: ...
+    def find(
+        payment_method_token: str,
+    ) -> AndroidPayCard | ApplePayCard | CreditCard | PayPalAccount | UsBankAccount | VenmoAccount | VisaCheckoutCard: ...
     @staticmethod
     def update(payment_method_token: str, params) -> SuccessfulResult | ErrorResult: ...
     @staticmethod
