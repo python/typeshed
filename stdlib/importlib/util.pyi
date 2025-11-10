01d3c9a44fed65c689554288de40fe5c9723f439
guoci <zguoci@gmail.com>
2025-11-10 17:16:41 -0500

    fix use of `Self`

--- stdlib/importlib/util.pyi
+++ stdlib/importlib/util.pyi
@@ -1,7 +1,7 @@
 import importlib.machinery
 import sys
 import types
-from _typeshed import ReadableBuffer, Self
+from _typeshed import ReadableBuffer
 from collections.abc import Callable
 from importlib._bootstrap import module_from_spec as module_from_spec, spec_from_loader as spec_from_loader
 from importlib._bootstrap_external import (
@@ -13,7 +13,7 @@ from importlib._bootstrap_external import (
 )
 from importlib.abc import Loader
 from types import TracebackType
-from typing_extensions import ParamSpec, deprecated
+from typing_extensions import ParamSpec, deprecated, Self
 
 _P = ParamSpec("_P")
 
