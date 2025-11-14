0a50c8e1f5309f3ec2a3781a4337bd347df1ea57
donBarbos <donbarbos@proton.me>
2025-11-15 01:01:59 +0400

    update final attrs

--- stubs/yt-dlp/yt_dlp/utils/_jsruntime.pyi
+++ stubs/yt-dlp/yt_dlp/utils/_jsruntime.pyi
@@ -18,21 +18,21 @@ class JsRuntime(abc.ABC):
     def info(self) -> JsRuntimeInfo | None: ...
 
 class DenoJsRuntime(JsRuntime):
-    MIN_SUPPORTED_VERSION: Final = (2, 0, 0)
+    MIN_SUPPORTED_VERSION: Final[tuple[int, int, int]]
     @cached_property
     def info(self) -> JsRuntimeInfo | None: ...
 
 class BunJsRuntime(JsRuntime):
-    MIN_SUPPORTED_VERSION: Final = (1, 0, 31)
+    MIN_SUPPORTED_VERSION: Final[tuple[int, int, int]]
     @cached_property
     def info(self) -> JsRuntimeInfo | None: ...
 
 class NodeJsRuntime(JsRuntime):
-    MIN_SUPPORTED_VERSION: Final = (20, 0, 0)
+    MIN_SUPPORTED_VERSION: Final[tuple[int, int, int]]
     @cached_property
     def info(self) -> JsRuntimeInfo | None: ...
 
 class QuickJsRuntime(JsRuntime):
-    MIN_SUPPORTED_VERSION: Final = (2023, 12, 9)
+    MIN_SUPPORTED_VERSION: Final[tuple[int, int, int]]
     @cached_property
     def info(self) -> JsRuntimeInfo | None: ...
