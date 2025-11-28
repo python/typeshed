45d2cc7b939c598e11c10bd18d9c9927d21d00e4
donBarbos <donbarbos@proton.me>
2025-11-28 21:51:39 +0400

    [psutil] Complete BSD

--- stubs/psutil/psutil/_psutil_bsd.pyi
+++ stubs/psutil/psutil/_psutil_bsd.pyi
@@ -1,73 +1,143 @@
-from _typeshed import Incomplete
+import sys
 
-# TODO: Add POSIX constants and check their aviability (see psutil_posix_add_constants)
-AF_LINK: Incomplete
+# sys.platform.startswith(("freebsd", "midnightbsd", "openbsd", "netbsd")):
+if sys.platform != "linux" and sys.platform != "win32" and sys.platform != "darwin":
+    from collections.abc import Sequence
+    from socket import AddressFamily, SocketKind
+    from typing import Final, Literal, overload
 
-def getpagesize() -> int: ...
-def net_if_addrs(): ...
-def net_if_flags(nic_name: str, /) -> list[str]: ...
-def net_if_is_running(nic_name: str, /) -> bool: ...
-def net_if_mtu(nic_name: str, /) -> int: ...
-def proc_priority_get(pid: int, /) -> int: ...
-def proc_priority_set(pid: int, priority: int, /) -> None: ...
-def net_if_duplex_speed(nic_name: str, /): ...
-def proc_is_zombie(pid: int, /) -> bool: ...
+    AF_LINK: Final[int]
+    RLIMIT_AS: Final[int]  # only FreeBSD
+    RLIMIT_CORE: Final[int]  # only FreeBSD
+    RLIMIT_CPU: Final[int]  # only FreeBSD
+    RLIMIT_DATA: Final[int]  # only FreeBSD
+    RLIMIT_FSIZE: Final[int]  # only FreeBSD
+    RLIMIT_MEMLOCK: Final[int]  # only FreeBSD
+    RLIMIT_NOFILE: Final[int]  # only FreeBSD
+    RLIMIT_NPROC: Final[int]  # only FreeBSD
+    RLIMIT_RSS: Final[int]  # only FreeBSD
+    RLIMIT_STACK: Final[int]  # only FreeBSD
+    RLIMIT_SWAP: Final[int]  # only FreeBSD
+    RLIMIT_SBSIZE: Final[int]  # only FreeBSD
+    RLIMIT_NPTS: Final[int]  # only FreeBSD
+    RLIM_INFINITY: Final[int]  # only FreeBSD
 
-SIDL: Incomplete
-SRUN: Incomplete
-SSLEEP: Incomplete
-SSTOP: Incomplete
-SZOMB: Incomplete
-SWAIT: Incomplete  # only FreeBSD
-SLOCK: Incomplete  # only FreeBSD
-SDEAD: Incomplete  # only OpenBSD and NetBSD
-SONPROC: Incomplete  # only OpenBSD and NetBSD
-SSUSPENDED: Incomplete  # only NetBSD
-TCPS_CLOSED: Incomplete
-TCPS_CLOSING: Incomplete
-TCPS_CLOSE_WAIT: Incomplete
-TCPS_LISTEN: Incomplete
-TCPS_ESTABLISHED: Incomplete
-TCPS_SYN_SENT: Incomplete
-TCPS_SYN_RECEIVED: Incomplete
-TCPS_FIN_WAIT_1: Incomplete
-TCPS_FIN_WAIT_2: Incomplete
-TCPS_LAST_ACK: Incomplete
-TCPS_TIME_WAIT: Incomplete
-PSUTIL_CONN_NONE: Incomplete
+    def getpagesize() -> int: ...
+    def net_if_addrs(): ...
+    def net_if_flags(nic_name: str, /) -> list[str]: ...
+    def net_if_is_running(nic_name: str, /) -> bool: ...
+    def net_if_mtu(nic_name: str, /) -> int: ...
+    def proc_priority_get(pid: int, /) -> int: ...
+    def proc_priority_set(pid: int, priority: int, /) -> None: ...
+    def net_if_duplex_speed(nic_name: str, /): ...
+    def proc_is_zombie(pid: int, /) -> bool: ...
 
-def proc_cmdline(*args, **kwargs): ...
-def proc_cwd(*args, **kwargs): ...
-def proc_environ(*args, **kwargs): ...
-def proc_name(*args, **kwargs): ...
-def proc_num_fds(*args, **kwargs): ...
-def proc_oneshot_info(*args, **kwargs): ...
-def proc_open_files(*args, **kwargs): ...
-def proc_threads(*args, **kwargs): ...
-def proc_num_threads(*args, **kwargs): ...  # only FreeBSD and OpenBSD
-def proc_cpu_affinity_get(*args, **kwargs): ...  # only FreeBSD
-def proc_cpu_affinity_set(*args, **kwargs): ...  # only FreeBSD
-def proc_exe(*args, **kwargs): ...  # only FreeBSD
-def proc_getrlimit(*args, **kwargs): ...  # only FreeBSD
-def proc_memory_maps(*args, **kwargs): ...  # only FreeBSD
-def proc_net_connections(*args, **kwargs): ...  # only FreeBSD
-def proc_setrlimit(*args, **kwargs): ...  # only FreeBSD
-def boot_time(*args, **kwargs): ...
-def cpu_count_logical(*args, **kwargs): ...
-def cpu_stats(*args, **kwargs): ...
-def cpu_times(*args, **kwargs): ...
-def disk_io_counters(*args, **kwargs): ...
-def disk_partitions(*args, **kwargs): ...
-def net_connections(*args, **kwargs): ...
-def net_io_counters(*args, **kwargs): ...
-def per_cpu_times(*args, **kwargs): ...
-def pids(*args, **kwargs): ...
-def swap_mem(*args, **kwargs): ...
-def users(*args, **kwargs): ...
-def virtual_mem(*args, **kwargs): ...
-def cpu_freq(*args, **kwargs): ...  # only FreeBSD and OpenBSD
-def cpu_topology(*args, **kwargs): ...  # only FreeBSD
-def sensors_battery(*args, **kwargs): ...  # only FreeBSD
-def sensors_cpu_temperature(*args, **kwargs): ...  # only FreeBSD
-def check_pid_range(*args, **kwargs): ...
-def set_debug(*args, **kwargs): ...
+    version: Final[int]
+    SIDL: Final[int]
+    SRUN: Final[int]
+    SSLEEP: Final[int]
+    SSTOP: Final[int]
+    SZOMB: Final[int]
+    SWAIT: Final[int]  # only FreeBSD
+    SLOCK: Final[int]  # only FreeBSD
+    SDEAD: Final[int]  # only OpenBSD and NetBSD
+    SONPROC: Final[int]  # only OpenBSD and NetBSD
+    SSUSPENDED: Final[int]  # only NetBSD
+    TCPS_CLOSED: Final[int]
+    TCPS_CLOSING: Final[int]
+    TCPS_CLOSE_WAIT: Final[int]
+    TCPS_LISTEN: Final[int]
+    TCPS_ESTABLISHED: Final[int]
+    TCPS_SYN_SENT: Final[int]
+    TCPS_SYN_RECEIVED: Final[int]
+    TCPS_FIN_WAIT_1: Final[int]
+    TCPS_FIN_WAIT_2: Final[int]
+    TCPS_LAST_ACK: Final[int]
+    TCPS_TIME_WAIT: Final[int]
+    PSUTIL_CONN_NONE: Final = 128
+
+    def proc_cmdline(pid: int, /) -> list[str]: ...
+    def proc_cwd(pid: int, /) -> str: ...
+    def proc_environ(pid: int, /) -> dict[str, str]: ...
+    def proc_name(pid: int, /) -> str: ...
+    def proc_num_fds(pid: int, /) -> int: ...
+    def proc_oneshot_info(
+        pid: int, /
+    ) -> tuple[
+        int,
+        int,
+        int,
+        int,
+        int,
+        int,
+        int,
+        int,
+        int,
+        float,
+        int,
+        int,
+        int,
+        int,
+        float,
+        float,
+        float,
+        float,
+        int,
+        int,
+        int,
+        int,
+        int,
+        int,
+        str,
+    ]: ...
+    def proc_open_files(pid: int, /) -> list[tuple[str, int]]: ...
+    def proc_threads(pid: int, /) -> list[tuple[int, float, float]]: ...
+    def proc_num_threads(pid: int, /) -> int: ...  # only FreeBSD and NetBSD
+    def proc_cpu_affinity_get(pid: int, /) -> list[int]: ...  # only FreeBSD
+    def proc_cpu_affinity_set(pid: int, cpu_set: Sequence[int], /) -> None: ...  # only FreeBSD
+    def proc_exe(pid: int, /) -> str: ...  # only FreeBSD
+    def proc_getrlimit(pid: int, resource: int, /) -> tuple[int, int]: ...  # only FreeBSD
+    def proc_memory_maps(pid: int, /) -> list[tuple[str, str, str, int, int, int, int]]: ...  # only FreeBSD
+    def proc_net_connections(  # only FreeBSD
+        pid: int, af_filter: Sequence[AddressFamily | int | None], type_filter: Sequence[SocketKind | int | None], /
+    ) -> list[
+        tuple[int, int, int, tuple[str, int], tuple[str, int] | tuple[()], int] | tuple[int, int, int, str, Literal[""], int]
+    ]: ...
+    def proc_setrlimit(pid: int, resource: int, soft: int, hard: int, /) -> None: ...  # only FreeBSD
+    def boot_time() -> float: ...
+    def cpu_count_logical() -> int | None: ...
+    def cpu_stats() -> tuple[int, ...]: ...  # tuple's length depends on OS
+    def cpu_times() -> tuple[float, float, float, float, float]: ...
+    def disk_io_counters() -> dict[str, tuple[int, ...]]: ...  # tuple's length depends on OS
+    def disk_partitions() -> list[tuple[str, str, str, str]]: ...
+    @overload  # for FreeBSD
+    def net_connections(
+        af_filter: Sequence[AddressFamily | int | None], type_filter: Sequence[SocketKind | int | None], /
+    ) -> list[
+        tuple[int, int, int, tuple[str, int], tuple[str, int] | tuple[()], int, int]
+        | tuple[int, int, int, str, Literal[""], int, int]
+    ]: ...
+    @overload  # for OpenBSD
+    def net_connections(
+        pid: int, af_filter: Sequence[AddressFamily | int | None], type_filter: Sequence[SocketKind | int | None], /
+    ) -> list[
+        tuple[int, int, int, tuple[str, int], tuple[str, int] | tuple[()], int, int]
+        | tuple[int, int, int, str, Literal[""], int, int]
+    ]: ...
+    @overload  # for NetBSD
+    def net_connections(pid: int, kind: str, /) -> list[tuple[int, int, int, str, str, int, int]]: ...
+    def net_io_counters() -> dict[str, tuple[int, int, int, int, int, int, int, int]]: ...
+    def per_cpu_times() -> list[tuple[float, float, float, float, float]]: ...
+    def pids() -> list[int]: ...
+    def swap_mem() -> tuple[int, int, int, int, int]: ...
+    def users() -> list[tuple[str, str, str, float, int | None]]: ...  # returns None only in OpenBSD
+    def virtual_mem() -> tuple[int, ...]: ...  # tuple's length depends on OS
+    @overload
+    def cpu_freq() -> int: ...  # only OpenBSD
+    @overload
+    def cpu_freq(core: int, /) -> tuple[int, str]: ...  # only FreeBSD
+    def cpu_topology() -> str | None: ...  # only FreeBSD
+    def sensors_battery() -> tuple[int, int, int]: ...  # only FreeBSD
+    def sensors_cpu_temperature(core: int, /) -> tuple[int, int]: ...  # only FreeBSD
+    def check_pid_range(pid: int, /) -> None: ...
+    def set_debug(value: bool, /) -> None: ...
