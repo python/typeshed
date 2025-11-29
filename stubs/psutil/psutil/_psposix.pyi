9c9ef15140e74b44f4992ab3705e30122a87a9d9
donBarbos <donbarbos@proton.me>
2025-11-29 21:53:47 +0400

    add sys.platform check to _psposix

--- stubs/psutil/psutil/_psposix.pyi
+++ stubs/psutil/psutil/_psposix.pyi
@@ -1,74 +1,76 @@
-import enum
 import sys
-from _typeshed import FileDescriptorOrPath, StrOrBytesPath, Unused
-from collections.abc import Callable
 
-from ._common import sdiskusage
+if sys.platform != "win32":
+    import enum
+    from _typeshed import FileDescriptorOrPath, StrOrBytesPath, Unused
+    from collections.abc import Callable
 
-def pid_exists(pid: int) -> bool: ...
+    from ._common import sdiskusage
 
-# Sync with `signal.Signals`, but with opposite values:
-class Negsignal(enum.IntEnum):
-    SIGABRT = -6
-    SIGFPE = -8
-    SIGILL = -4
-    SIGINT = -2
-    SIGSEGV = -11
-    SIGTERM = -15
-    SIGALRM = -14
-    SIGBUS = -7
-    SIGCHLD = -17
-    SIGCONT = -18
-    SIGHUP = -1
-    SIGIO = -29
-    SIGIOT = 6
-    SIGKILL = -9
-    SIGPIPE = -13
-    SIGPROF = -27
-    SIGQUIT = -3
-    SIGSTOP = -19
-    SIGSYS = -31
-    SIGTRAP = -5
-    SIGTSTP = -20
-    SIGTTIN = -21
-    SIGTTOU = -22
-    SIGURG = -23
-    SIGUSR1 = -10
-    SIGUSR2 = -12
-    SIGVTALRM = -26
-    SIGWINCH = -28
-    SIGXCPU = -24
-    SIGXFSZ = -25
-    if sys.platform != "linux":
-        SIGEMT = -7
-        SIGINFO = -29
-    if sys.platform != "darwin":
-        SIGCLD = -17
-        SIGPOLL = -29
-        SIGPWR = -30
-        SIGRTMAX = -64
-        SIGRTMIN = -34
-        if sys.version_info >= (3, 11):
-            SIGSTKFLT = -16
+    def pid_exists(pid: int) -> bool: ...
 
-def negsig_to_enum(num: int) -> int: ...
-def wait_pid(
-    pid: int,
-    timeout: float | None = None,
-    proc_name: str | None = None,
-    _waitpid: Unused = ...,
-    _timer: Callable[[], float] = ...,
-    _min=...,
-    _sleep: Callable[[float], None] = ...,
-    _pid_exists: Callable[[int], bool] = ...,
-) -> int | None: ...
+    # Sync with `signal.Signals`, but with opposite values:
+    class Negsignal(enum.IntEnum):
+        SIGABRT = -6
+        SIGFPE = -8
+        SIGILL = -4
+        SIGINT = -2
+        SIGSEGV = -11
+        SIGTERM = -15
+        SIGALRM = -14
+        SIGBUS = -7
+        SIGCHLD = -17
+        SIGCONT = -18
+        SIGHUP = -1
+        SIGIO = -29
+        SIGIOT = 6
+        SIGKILL = -9
+        SIGPIPE = -13
+        SIGPROF = -27
+        SIGQUIT = -3
+        SIGSTOP = -19
+        SIGSYS = -31
+        SIGTRAP = -5
+        SIGTSTP = -20
+        SIGTTIN = -21
+        SIGTTOU = -22
+        SIGURG = -23
+        SIGUSR1 = -10
+        SIGUSR2 = -12
+        SIGVTALRM = -26
+        SIGWINCH = -28
+        SIGXCPU = -24
+        SIGXFSZ = -25
+        if sys.platform != "linux":
+            SIGEMT = -7
+            SIGINFO = -29
+        if sys.platform != "darwin":
+            SIGCLD = -17
+            SIGPOLL = -29
+            SIGPWR = -30
+            SIGRTMAX = -64
+            SIGRTMIN = -34
+            if sys.version_info >= (3, 11):
+                SIGSTKFLT = -16
 
-if sys.platform == "darwin":
-    def disk_usage(path: StrOrBytesPath) -> sdiskusage: ...
+    def negsig_to_enum(num: int) -> int: ...
+    def wait_pid(
+        pid: int,
+        timeout: float | None = None,
+        proc_name: str | None = None,
+        _waitpid: Unused = ...,
+        _timer: Callable[[], float] = ...,
+        _min=...,
+        _sleep: Callable[[float], None] = ...,
+        _pid_exists: Callable[[int], bool] = ...,
+    ) -> int | None: ...
 
-else:
-    def disk_usage(path: FileDescriptorOrPath) -> sdiskusage: ...
+    if sys.platform == "darwin":
+        def disk_usage(path: StrOrBytesPath) -> sdiskusage: ...
 
-def get_terminal_map() -> dict[int, str]: ...
+    else:
+        def disk_usage(path: FileDescriptorOrPath) -> sdiskusage: ...
 
-__all__ = ["pid_exists", "wait_pid", "disk_usage", "get_terminal_map"]
+    def get_terminal_map() -> dict[int, str]: ...
+
+    __all__ = ["pid_exists", "wait_pid", "disk_usage", "get_terminal_map"]
