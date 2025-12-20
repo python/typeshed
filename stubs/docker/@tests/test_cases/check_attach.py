ac1daa005e7ed29480bc9c1aa0e8fd4d8fac2faf
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2025-12-21 00:16:09 +0200

    [docker] Fix `Container.attach()` return type
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- stubs/docker/@tests/test_cases/check_attach.py
+++ stubs/docker/@tests/test_cases/check_attach.py
@@ -4,8 +4,9 @@ from typing_extensions import assert_type
 
 from docker.models.containers import Container
 
-c: Container
-assert_type(c.attach(), bytes)
-assert_type(c.attach(stream=False), bytes)
-for line in c.attach(stream=True):
-    assert_type(line, bytes)
+
+def check_attach(c: Container) -> None:
+    assert_type(c.attach(), bytes)
+    assert_type(c.attach(stream=False), bytes)
+    for line in c.attach(stream=True):
+        assert_type(line, bytes)
