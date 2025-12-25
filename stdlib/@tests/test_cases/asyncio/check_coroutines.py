a75f50254db4590c4c1559754201af496082dd40
Max Muoto <maxmuoto@gmail.com>
2025-12-25 14:28:35 -0600

    Add asyncio 3.14 deprecations

--- stdlib/@tests/test_cases/asyncio/check_coroutines.py
+++ stdlib/@tests/test_cases/asyncio/check_coroutines.py
@@ -1,25 +1,61 @@
 from __future__ import annotations
 
+import inspect
 from asyncio import iscoroutinefunction
 from collections.abc import Awaitable, Callable, Coroutine
+from types import CoroutineType
 from typing import Any
 from typing_extensions import assert_type
+import sys
 
 
-def test_iscoroutinefunction(
+# asyncio.iscoroutinefunction is deprecated, expecting a warning.
+def test_iscoroutinefunction_asyncio(
     x: Callable[[str, int], Coroutine[str, int, bytes]],
     y: Callable[[str, int], Awaitable[bytes]],
     z: Callable[[str, int], str | Awaitable[bytes]],
     xx: object,
 ) -> None:
-    if iscoroutinefunction(x):
+    if sys.version_info >= (3, 11):
+        if iscoroutinefunction(x):  # pyright: ignore
+            assert_type(x, Callable[[str, int], Coroutine[str, int, bytes]])
+
+        if iscoroutinefunction(y):  # pyright: ignore
+            assert_type(y, Callable[[str, int], Coroutine[Any, Any, bytes]])
+
+        if iscoroutinefunction(z):  # pyright: ignore
+            assert_type(z, Callable[[str, int], Coroutine[Any, Any, Any]])
+
+        if iscoroutinefunction(xx):  # pyright: ignore
+            assert_type(xx, Callable[..., Coroutine[Any, Any, Any]])
+    else:
+        if iscoroutinefunction(x):
+            assert_type(x, Callable[[str, int], Coroutine[str, int, bytes]])
+
+        if iscoroutinefunction(y):
+            assert_type(y, Callable[[str, int], Coroutine[Any, Any, bytes]])
+
+        if iscoroutinefunction(z):
+            assert_type(z, Callable[[str, int], Coroutine[Any, Any, Any]])
+
+        if iscoroutinefunction(xx):
+            assert_type(xx, Callable[..., Coroutine[Any, Any, Any]])
+
+
+def test_iscoroutinefunction_inspect(
+    x: Callable[[str, int], Coroutine[str, int, bytes]],
+    y: Callable[[str, int], Awaitable[bytes]],
+    z: Callable[[str, int], str | Awaitable[bytes]],
+    xx: object,
+) -> None:
+    if inspect.iscoroutinefunction(x):
         assert_type(x, Callable[[str, int], Coroutine[str, int, bytes]])
 
-    if iscoroutinefunction(y):
-        assert_type(y, Callable[[str, int], Coroutine[Any, Any, bytes]])
+    if inspect.iscoroutinefunction(y):
+        assert_type(y, Callable[[str, int], CoroutineType[Any, Any, bytes]])
 
-    if iscoroutinefunction(z):
-        assert_type(z, Callable[[str, int], Coroutine[Any, Any, Any]])
+    if inspect.iscoroutinefunction(z):
+        assert_type(z, Callable[[str, int], CoroutineType[Any, Any, Any]])
 
-    if iscoroutinefunction(xx):
-        assert_type(xx, Callable[..., Coroutine[Any, Any, Any]])
+    if inspect.iscoroutinefunction(xx):
+        assert_type(xx, Callable[..., CoroutineType[Any, Any, Any]])
