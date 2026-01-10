9840c584515d25733dc6b60ac4afb608c7b0a808
Semyon Moroz <donbarbos@proton.me>
2026-01-10 23:20:27 +0000

    [sre_compile] Use TypeGuard for isstring (#15250)

--- stdlib/sre_compile.pyi
+++ stdlib/sre_compile.pyi
@@ -2,10 +2,14 @@ from re import Pattern
 from sre_constants import *
 from sre_constants import _NamedIntConstant
 from sre_parse import SubPattern
-from typing import Any, Final
+from typing import Any, Final, Literal, overload
+from typing_extensions import TypeGuard
 
 MAXCODE: Final[int]
 
 def dis(code: list[_NamedIntConstant]) -> None: ...
-def isstring(obj: Any) -> bool: ...
+@overload
+def isstring(obj: str | bytes) -> Literal[True]: ...
+@overload
+def isstring(obj: object) -> TypeGuard[str | bytes]: ...
 def compile(p: str | bytes | SubPattern, flags: int = 0) -> Pattern[Any]: ...
