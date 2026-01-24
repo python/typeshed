7201649f2fcdbd1ce3bb073ee72896f59f9b2743
Randolf Scholz <randolf.scholz@gmail.com>
2026-01-24 16:22:13 +0100

    make _T_contra default to Any

--- stdlib/typing.pyi
+++ stdlib/typing.pyi
@@ -403,7 +403,7 @@ _S = TypeVar("_S")
 _KT = TypeVar("_KT")  # Key type.
 _VT = TypeVar("_VT")  # Value type.
 _T_co = TypeVar("_T_co", covariant=True)  # Any type covariant containers.
-_T_contra = TypeVar("_T_contra", contravariant=True)  # Any type contravariant containers.
+_T_contra = TypeVar("_T_contra", contravariant=True, default=Any)  # Any type contravariant containers.
 _KT_co = TypeVar("_KT_co", covariant=True)  # Key type covariant containers.
 _VT_co = TypeVar("_VT_co", covariant=True)  # Value type covariant containers.
 _TC = TypeVar("_TC", bound=type[object])
