6087a978078eff5d2a1d5dd001dae9a4905caae2
b9788213 <b9788213@gmail.com>
2026-01-28 16:38:25 +0300

    .

--- stdlib/json/decoder.pyi
+++ stdlib/json/decoder.pyi
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from typing import Any
+from _typeshed import Incomplete
 
 __all__ = ["JSONDecoder", "JSONDecodeError"]
 
@@ -11,6 +12,24 @@ class JSONDecodeError(ValueError):
     colno: int
     def __init__(self, msg: str, doc: str, pos: int) -> None: ...
 
+def JSONObject(
+    s_and_end: tuple[str, int],
+    strict: bool,
+    scan_once: Callable[[Incomplete], Incomplete],
+    object_hook: Callable[[Incomplete], Incomplete] | None,
+    object_pairs_hook: Callable[[Incomplete], Incomplete] | None,
+    memo: dict[Incomplete, Incomplete] | None = None,
+    _w: Callable[[str, int], Incomplete] = ...,
+    _ws: str = ...
+) -> tuple[Incomplete, int]: ...
+
+def JSONArray(
+    s_and_end: tuple[str, int],
+    scan_once: Callable[[Incomplete], Incomplete],
+    _w: Callable[[str, int], Incomplete] = ...,
+    _ws: str = ...
+) -> tuple[list[Incomplete], int]: ...
+
 class JSONDecoder:
     object_hook: Callable[[dict[str, Any]], Any]
     parse_float: Callable[[str], Any]
@@ -18,10 +37,10 @@ class JSONDecoder:
     parse_constant: Callable[[str], Any]
     strict: bool
     object_pairs_hook: Callable[[list[tuple[str, Any]]], Any]
-    parse_object: Callable[..., Any]
-    parse_array: Callable[..., Any]
-    parse_string: Callable[..., Any]
-    memo: dict[Any, Any]
+    parse_object: Callable[[Incomplete], Incomplete]
+    parse_array: Callable[[Incomplete], Incomplete]
+    parse_string: Callable[[Incomplete], Incomplete]
+    memo: dict[Incomplete, Incomplete]
     def __init__(
         self,
         *,
