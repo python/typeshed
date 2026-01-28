aae4e27f491166af82aba1f66e5c3e29ed50bb8d
b9788213 <b9788213@gmail.com>
2026-01-28 14:54:26 +0300

    .

--- stdlib/json/decoder.pyi
+++ stdlib/json/decoder.pyi
@@ -18,10 +18,10 @@ class JSONDecoder:
     parse_constant: Callable[[str], Any]
     strict: bool
     object_pairs_hook: Callable[[list[tuple[str, Any]]], Any]
-    parse_object: Callable
-    parse_array: Callable
-    parse_string: Callable
-    memo: dict
+    parse_object: Callable[..., Any]
+    parse_array: Callable[..., Any]
+    parse_string: Callable[..., Any]
+    memo: dict[Any, Any]
     def __init__(
         self,
         *,
