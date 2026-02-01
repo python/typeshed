84782b7680768b8b813d2de70ca89f5f6c2defe6
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-02-01 00:26:25 -0500

    Remove internal API: `_resample_loop`.

--- stubs/resampy/resampy/interpn.pyi
+++ stubs/resampy/resampy/interpn.pyi
@@ -1,14 +1,5 @@
 import numpy as np
 
-def _resample_loop(
-    x: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
-    t_out: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
-    interp_win: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
-    interp_delta: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
-    num_table: int,
-    scale: float,
-    y: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
-) -> None: ...
 def resample_f_p(
     x: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
     t_out: np.ndarray[tuple[int, ...], np.dtype[np.floating]],
