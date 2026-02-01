e6c29a6bd1175df3128d1de0a4b6d28ebe08db62
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-01-31 21:15:51 -0500

    Update stubs/resampy/resampy/filters.pyi
    Co-authored-by: Semyon Moroz <donbarbos@proton.me>

--- stubs/resampy/resampy/filters.pyi
+++ stubs/resampy/resampy/filters.pyi
@@ -6,7 +6,7 @@ import numpy as np
 __all__ = ["get_filter", "clear_cache", "sinc_window"]
 
 # Dictionary to cache loaded filters
-FILTER_CACHE: dict[str, tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]], int, float]]
+FILTER_CACHE: dict[str, tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating]], int, float]]
 
 # List of filter functions available
 FILTER_FUNCTIONS: list[str]
@@ -14,12 +14,12 @@ FILTER_FUNCTIONS: list[str]
 def sinc_window(
     num_zeros: int = 64,
     precision: int = 9,
-    window: Callable[..., np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]]] | None = None,
+    window: Callable[..., np.ndarray[tuple[int, ...], np.dtype[np.floating]]] | None = None,
     rolloff: float = 0.945,
-) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]], int, float]: ...
+) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating]], int, float]: ...
 def get_filter(
-    name_or_function: str | Callable[..., tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]], int, float]],
-    **kwargs: Any,
-) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]], int, float]: ...
-def load_filter(filter_name: str) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating[Any]]], int, float]: ...
+    name_or_function: str | Callable[..., tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating]], int, float]],
+    **kwargs,
+) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating]], int, float]: ...
+def load_filter(filter_name: str) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.floating]], int, float]: ...
 def clear_cache() -> None: ...
