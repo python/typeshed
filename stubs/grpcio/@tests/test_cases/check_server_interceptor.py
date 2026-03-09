d81eaf795078e7b372cfe93fcfe37c06728691cd
李文和 <107758517+XiaXia009@users.noreply.github.com>
2026-03-10 02:15:56 +0800

    fix @type_check_only check

--- stubs/grpcio/@tests/test_cases/check_server_interceptor.py
+++ stubs/grpcio/@tests/test_cases/check_server_interceptor.py
@@ -34,7 +34,7 @@ class NoopAioInterceptor(grpc.aio.ServerInterceptor):
 
 grpc.aio.server(interceptors=[NoopAioInterceptor()])
 
-aio_handler: grpc.aio.RpcMethodHandler[bytes, bytes] = grpc.aio.RpcMethodHandler()
+aio_handler = cast(grpc.aio.RpcMethodHandler[bytes, bytes], object())
 aio_handler.unary_unary = cast(Callable[[bytes, grpc.aio.ServicerContext[bytes, bytes]], Awaitable[bytes]], None)
 aio_handler.unary_stream = cast(Callable[[bytes, grpc.aio.ServicerContext[bytes, bytes]], AsyncIterator[bytes]], None)
 aio_handler.stream_unary = cast(Callable[[AsyncIterator[bytes], grpc.aio.ServicerContext[bytes, bytes]], Awaitable[bytes]], None)
