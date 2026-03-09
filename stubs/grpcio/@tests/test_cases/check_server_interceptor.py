2626c88ecc34d4441cdc1b33e9a7954f69d24b5c
XiaXia009 <wli62412@gmail.com>
2026-03-10 00:51:13 +0800

    Fix grpc aio handler typing

--- stubs/grpcio/@tests/test_cases/check_server_interceptor.py
+++ stubs/grpcio/@tests/test_cases/check_server_interceptor.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from collections.abc import Callable
+from collections.abc import AsyncIterator, Callable
 from concurrent.futures.thread import ThreadPoolExecutor
-from typing import Awaitable, TypeVar
+from typing import Awaitable, TypeVar, cast
 
 import grpc
 import grpc.aio
@@ -26,10 +26,28 @@ grpc.server(interceptors=[NoopInterceptor()], thread_pool=ThreadPoolExecutor())
 class NoopAioInterceptor(grpc.aio.ServerInterceptor):
     async def intercept_service(
         self,
-        continuation: Callable[[grpc.HandlerCallDetails], Awaitable[grpc.RpcMethodHandler[RequestT, ResponseT]]],
+        continuation: Callable[[grpc.HandlerCallDetails], Awaitable[grpc.aio.RpcMethodHandler[RequestT, ResponseT]]],
         handler_call_details: grpc.HandlerCallDetails,
-    ) -> grpc.RpcMethodHandler[RequestT, ResponseT]:
+    ) -> grpc.aio.RpcMethodHandler[RequestT, ResponseT]:
         return await continuation(handler_call_details)
 
 
 grpc.aio.server(interceptors=[NoopAioInterceptor()])
+
+aio_handler = grpc.aio.RpcMethodHandler[RequestT, ResponseT]()
+aio_handler.unary_unary = cast(
+    Callable[[RequestT, grpc.aio.ServicerContext[RequestT, ResponseT]], Awaitable[ResponseT]],
+    None,
+)
+aio_handler.unary_stream = cast(
+    Callable[[RequestT, grpc.aio.ServicerContext[RequestT, ResponseT]], AsyncIterator[ResponseT]],
+    None,
+)
+aio_handler.stream_unary = cast(
+    Callable[[AsyncIterator[RequestT], grpc.aio.ServicerContext[RequestT, ResponseT]], Awaitable[ResponseT]],
+    None,
+)
+aio_handler.stream_stream = cast(
+    Callable[[AsyncIterator[RequestT], grpc.aio.ServicerContext[RequestT, ResponseT]], AsyncIterator[ResponseT]],
+    None,
+)
