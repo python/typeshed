310b1517ffdb04fdea797aae16ef7f6b2dfad1e1
Andrew Udvare <audvare@gmail.com>
2026-03-18 08:20:53 -0400

    [portage] fix method locations

--- stubs/portage/portage/dbapi/__init__.pyi
+++ stubs/portage/portage/dbapi/__init__.pyi
@@ -29,21 +29,4 @@ class dbapi:
         mytree: str | None = ...,
         myrepo: str | None = ...,
     ) -> list[str]: ...
-    def xmatch(
-        self,
-        level: Literal[
-            "bestmatch-visible",
-            "match-all-cpv-only",
-            "match-all",
-            "match-visible",
-            "minimum-all",
-            "minimum-visible",
-            "minimum-all-ignore-profile",
-        ],
-        origdep: str,
-    ) -> list[str] | str: ...
-    def findname(self, mycpv: str, mytree: str | None = ..., myrepo: str | None = ...) -> str: ...
-    def findname2(
-        self, mycpv: str, mytree: str | None = ..., myrepo: str | None = ...
-    ) -> tuple[None, Literal[0]] | tuple[str, str] | tuple[str, None]: ...
     def match(self, mydep: str, use_cache: Literal[0, 1] = ...) -> list[str] | str: ...
--- stubs/portage/portage/dbapi/porttree.pyi
+++ stubs/portage/portage/dbapi/porttree.pyi
@@ -6,6 +6,23 @@ class portdbapi(dbapi):
     def getFetchMap(
         self, mypkg: str, useflags: Sequence[str] | None = ..., mytree: str | None = ...
     ) -> dict[str, tuple[str, ...]]: ...
+    def xmatch(
+        self,
+        level: Literal[
+            "bestmatch-visible",
+            "match-all-cpv-only",
+            "match-all",
+            "match-visible",
+            "minimum-all",
+            "minimum-visible",
+            "minimum-all-ignore-profile",
+        ],
+        origdep: str,
+    ) -> list[str] | str: ...
+    def findname(self, mycpv: str, mytree: str | None = ..., myrepo: str | None = ...) -> str: ...
+    def findname2(
+        self, mycpv: str, mytree: str | None = ..., myrepo: str | None = ...
+    ) -> tuple[None, Literal[0]] | tuple[str, str] | tuple[str, None]: ...
 
 class portagetree:
     dbapi: portdbapi
