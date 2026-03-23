34c8cc78beeba1c1014b45032ba45f054350102f
Sedat <sedataraswork@gmail.com>
2026-03-23 20:20:38 +1100

    Fix RefResolver store parameter type in jsonschema stubs

--- stubs/jsonschema/jsonschema/validators.pyi
+++ stubs/jsonschema/jsonschema/validators.pyi
@@ -105,7 +105,7 @@ class RefResolver:
         self,
         base_uri: str,
         referrer: dict[str, Incomplete],
-        store: SupportsKeysAndGetItem[str, str] | Iterable[tuple[str, str]] = ...,
+        store: SupportsKeysAndGetItem[str, Mapping[str, Any]] | Iterable[tuple[str, Mapping[str, Any]]] = ...,
         cache_remote: bool = True,
         handlers: SupportsKeysAndGetItem[str, _Handler] | Iterable[tuple[str, _Handler]] = (),
         urljoin_cache=None,
