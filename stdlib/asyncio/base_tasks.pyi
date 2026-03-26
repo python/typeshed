caca9d03d50bd629c4b12cb918372393f67e5ba9
Jonathan Dung <jonathandung@yahoo.com>
2026-03-26 16:52:10 +0800

    Add missing _task_repr function

--- stdlib/asyncio/base_tasks.pyi
+++ stdlib/asyncio/base_tasks.pyi
@@ -2,8 +2,9 @@ from _typeshed import StrOrBytesPath
 from types import FrameType
 from typing import Any
 
-from . import tasks
+from .tasks import Task
 
-def _task_repr_info(task: tasks.Task[Any]) -> list[str]: ...  # undocumented
-def _task_get_stack(task: tasks.Task[Any], limit: int | None) -> list[FrameType]: ...  # undocumented
-def _task_print_stack(task: tasks.Task[Any], limit: int | None, file: StrOrBytesPath) -> None: ...  # undocumented
+def _task_repr_info(task: Task[Any]) -> list[str]: ...  # undocumented
+def _task_repr(task: Task[Any]) -> str: ... # undocumented
+def _task_get_stack(task: Task[Any], limit: int | None) -> list[FrameType]: ...  # undocumented
+def _task_print_stack(task: Task[Any], limit: int | None, file: StrOrBytesPath) -> None: ...  # undocumented
