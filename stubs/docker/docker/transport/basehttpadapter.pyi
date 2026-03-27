3d8d412b89a8d38349b72ec4253e35e7185ad18f
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-03-27 17:53:51 +0300

    [docker] Fix incomplete types in models and add missing method
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- stubs/docker/docker/transport/basehttpadapter.pyi
+++ stubs/docker/docker/transport/basehttpadapter.pyi
@@ -1,4 +1,17 @@
+from collections.abc import Mapping
+
 import requests.adapters
+from typing_extensions import override
+from urllib3 import ConnectionPool
 
 class BaseHTTPAdapter(requests.adapters.HTTPAdapter):
+    @override
     def close(self) -> None: ...
+    @override
+    def get_connection_with_tls_context(
+        self,
+        request: requests.PreparedRequest,
+        verify: bool | str | None,
+        proxies: Mapping[str, str] | None = None,
+        cert: tuple[str, str] | str | None = None,
+    ) -> ConnectionPool: ...
