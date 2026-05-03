a0a01f09edd8be3405409799ba4d01e317bf71b1
lambda-abstraction <enikita332@gmail.com>
2026-05-03 05:49:29 +0300

    Wow, i didnt resolve the merge conflict with the pre-commit CI correctly

--- stdlib/@tests/test_cases/sys/check_jit.py
+++ stdlib/@tests/test_cases/sys/check_jit.py
@@ -6,9 +6,6 @@ if sys.version_info >= (3, 14):
     assert_type(sys._jit.is_enabled(), bool)
     assert_type(sys._jit.is_active(), bool)
 
-    # sys is not a package, so this should be an error
-    import sys._jit  # type: ignore
-
     def sys_is_not_a_package():
         # This has to be put into a function, because otherwise pyright
         # applies the type: ignore on the import sys._jit to the above usages of it
