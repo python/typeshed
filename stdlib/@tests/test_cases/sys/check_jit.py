bcf0601de287e2f1f09261137d83c9216aa94fbc
lambda-abstraction <41861454+lambda-abstraction@users.noreply.github.com>
2026-05-04 02:27:49 +0300

    Remove the claim of the issue cause in the test case, as its not confirmed
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- stdlib/@tests/test_cases/sys/check_jit.py
+++ stdlib/@tests/test_cases/sys/check_jit.py
@@ -7,6 +7,6 @@ if sys.version_info >= (3, 14):
     assert_type(sys._jit.is_active(), bool)
 
     def sys_is_not_a_package() -> None:
-        # This has to be put into a function, because otherwise pyright
-        # applies the type: ignore on the import sys._jit to the above usages of it
+        # This has to be put into a function, because otherwise the presence
+        # of this import statement causes errors on the above usages of `sys._jit`.
         import sys._jit  # type: ignore
