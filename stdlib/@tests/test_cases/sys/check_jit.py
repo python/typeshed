ee2a5663627f05a231e81b97f441c7538afd1218
lambda-abstraction <enikita332@gmail.com>
2026-05-03 06:12:23 +0300

    I love wasting CPU time running CI just to get a stupid error

--- stdlib/@tests/test_cases/sys/check_jit.py
+++ stdlib/@tests/test_cases/sys/check_jit.py
@@ -6,7 +6,7 @@ if sys.version_info >= (3, 14):
     assert_type(sys._jit.is_enabled(), bool)
     assert_type(sys._jit.is_active(), bool)
 
-    def sys_is_not_a_package():
+    def sys_is_not_a_package() -> None:
         # This has to be put into a function, because otherwise pyright
         # applies the type: ignore on the import sys._jit to the above usages of it
         import sys._jit  # type: ignore
