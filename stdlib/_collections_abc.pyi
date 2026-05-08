e073292634438f80b00b9f51e47e87d98e9e9a66
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-05-08 10:24:17 -0700

    Add Python 3.15 typing updates

--- stdlib/_collections_abc.pyi
+++ stdlib/_collections_abc.pyi
@@ -7,7 +7,6 @@ from typing import (  # noqa: Y022,Y038,UP035,Y057
     AsyncIterable as AsyncIterable,
     AsyncIterator as AsyncIterator,
     Awaitable as Awaitable,
-    ByteString as ByteString,
     Callable as Callable,
     ClassVar,
     Collection as Collection,
@@ -60,8 +59,11 @@ __all__ = [
     "ValuesView",
     "Sequence",
     "MutableSequence",
-    "ByteString",
 ]
+if sys.version_info < (3, 15):
+    from typing import ByteString as ByteString  # noqa: Y022,Y038,UP035,Y057,RUF100
+
+    __all__ += ["ByteString"]
 if sys.version_info >= (3, 12):
     __all__ += ["Buffer"]
 
