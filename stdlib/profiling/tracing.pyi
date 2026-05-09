689f19baf0457aef7f338c1b899b22a72a2d7960
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-05-09 11:29:48 -0700

    Fix Python 3.15 CI after merge

--- stdlib/profiling/tracing.pyi
+++ stdlib/profiling/tracing.pyi
@@ -2,7 +2,7 @@ from cProfile import Profile as Profile, run as run, runctx as runctx
 from types import CodeType
 from typing import TypeAlias
 
-__all__ = ["run", "runctx", "Profile"]
+__all__ = ("run", "runctx", "Profile")
 
 _Label: TypeAlias = tuple[str, int, str]
 
