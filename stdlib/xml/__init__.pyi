3004a862115416b9ce8988b204bcb7a40d8406b2
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-05-10 07:18:37 -0700

    try it

--- stdlib/xml/__init__.pyi
+++ stdlib/xml/__init__.pyi
@@ -2,8 +2,8 @@
 # valid, and causes them to be included in a star import. See #6523
 import sys
 
+__all__ = ["dom", "parsers", "sax", "etree"]  # noqa: F822  # pyright: ignore[reportUnsupportedDunderAll]
+
 if sys.version_info >= (3, 15):
-    __all__ = ["dom", "parsers", "sax", "etree", "is_valid_name"]  # noqa: F822  # pyright: ignore[reportUnsupportedDunderAll]
+    __all__ += ["is_valid_name"]  # pyright: ignore[reportUnsupportedDunderAll]
     from xml.utils import is_valid_name as is_valid_name, is_valid_text as is_valid_text
-else:
-    __all__ = ["dom", "parsers", "sax", "etree"]  # noqa: F822  # pyright: ignore[reportUnsupportedDunderAll]
