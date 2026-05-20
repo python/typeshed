5de0fe406d3110d3adb86d82252eeba97f74c530
p-vdp <peter@corbelspatial.com>
2026-05-20 09:53:07 -0700

    Add stubs for `geojson` using scripts/create_baseline_stubs.py

---
+++ stubs/geojson/geojson/_version.pyi
@@ -0,0 +1,4 @@
+from _typeshed import Incomplete
+
+__version__: str
+__version_info__: Incomplete
---
+++ stubs/geojson/geojson/examples.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+
+class SimpleWebFeature:
+    id: Incomplete
+    geometry: Incomplete
+    properties: Incomplete
+    def __init__(self, id=None, geometry=None, title=None, summary=None, link=None) -> None: ...
+    def as_dict(self): ...
+    __geo_interface__: Incomplete
+
+def create_simple_web_feature(o): ...
