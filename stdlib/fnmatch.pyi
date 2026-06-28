f8349e585d42e47cd725d8f2fe4e6228a84019e3
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-06-28 05:10:43 -0500

    drop overload definitions. use narrow return.

--- stdlib/fnmatch.pyi
+++ stdlib/fnmatch.pyi
@@ -2,7 +2,7 @@ import sys
 from _typeshed import AnyOrLiteralStr
 from collections.abc import Iterable
 from os import PathLike
-from typing import AnyStr, overload
+from typing import AnyStr
 
 __all__ = ["filter", "fnmatch", "fnmatchcase", "translate"]
 if sys.version_info >= (3, 14):
@@ -10,18 +10,10 @@ if sys.version_info >= (3, 14):
 
 def fnmatch(name: AnyOrLiteralStr | PathLike[AnyStr], pat: AnyOrLiteralStr | PathLike[AnyStr]) -> bool: ...
 def fnmatchcase(name: AnyStr, pat: AnyStr) -> bool: ...
-
-@overload
-def filter(names: Iterable[AnyOrLiteralStr], pat: AnyOrLiteralStr) -> list[AnyOrLiteralStr]: ...
-@overload
 def filter(names: Iterable[AnyOrLiteralStr | PathLike[AnyStr]], pat: AnyOrLiteralStr | PathLike[AnyStr]) -> list[AnyStr]: ...
-
 def translate(pat: str) -> str: ...
 
 if sys.version_info >= (3, 14):
-    @overload
-    def filterfalse(names: Iterable[AnyOrLiteralStr], pat: AnyOrLiteralStr) -> list[AnyOrLiteralStr]: ...
-    @overload
     def filterfalse(
         names: Iterable[AnyOrLiteralStr | PathLike[AnyStr]], pat: AnyOrLiteralStr | PathLike[AnyStr]
     ) -> list[AnyStr]: ...
