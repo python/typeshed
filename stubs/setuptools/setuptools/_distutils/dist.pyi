7a1611a569f9d5cf5fb1b20d33b3b820bbac725c
Avasam <samuel.06@hotmail.com>
2026-07-04 23:07:27 -0400

    Bump setuptools to 83.0.*

--- stubs/setuptools/setuptools/_distutils/dist.pyi
+++ stubs/setuptools/setuptools/_distutils/dist.pyi
@@ -4,6 +4,7 @@ from re import Pattern
 from typing import IO, ClassVar, Literal, TypeAlias, TypeVar, overload
 
 from .cmd import Command
+from .extension import Extension
 
 command_re: Pattern[str]
 
@@ -78,18 +79,18 @@ class Distribution:
     script_args: list[str] | None
     command_options: dict[str, dict[str, tuple[str, str]]]
     dist_files: list[tuple[str, str, str]]
-    packages: Incomplete
+    packages: list[str] | None
     package_data: dict[str, list[str]]
-    package_dir: Incomplete
-    py_modules: Incomplete
+    package_dir: dict[str, str] | None
+    py_modules: list[str] | None
     libraries: Incomplete
     headers: Incomplete
-    ext_modules: Incomplete
+    ext_modules: list[Extension] | None
     ext_package: Incomplete
     include_dirs: Incomplete
     extra_path: Incomplete
     scripts: Incomplete
-    data_files: Incomplete
+    data_files: list[str | tuple[Incomplete, ...]] | None
     password: str
     command_obj: dict[str, Command]
     have_run: dict[str, bool]
@@ -111,14 +112,11 @@ class Distribution:
     def get_command_obj(self, command: str, create: Literal[True] = True) -> Command: ...
     @overload
     def get_command_obj(self, command: str, create: Literal[False]) -> Command | None: ...
-
     def get_command_class(self, command: str) -> type[Command]: ...
-
     @overload
     def reinitialize_command(self, command: str, reinit_subcommands: bool = False) -> Command: ...
     @overload
     def reinitialize_command(self, command: _CommandT, reinit_subcommands: bool = False) -> _CommandT: ...
-
     def announce(self, msg, level: int = 20) -> None: ...
     def run_commands(self) -> None: ...
     def run_command(self, command: str) -> None: ...
