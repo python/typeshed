990134b34a6a27461a03e84a6113ccc9bf0ab10c
TuringTux <hi@turingtux.me>
2026-07-13 22:56:18 +0200

    Revert "Delete files which we won't need type annotations for"
    This reverts commit 190b8a627d2d7116bf5a69ea50956520fab8691d.
    
    # Conflicts:
    #	stubs/weasyprint/weasyprint/css/counters.pyi

---
+++ stubs/weasyprint/weasyprint/logger.pyi
@@ -0,0 +1,4 @@
+from _typeshed import Incomplete
+
+LOGGER: Incomplete
+PROGRESS_LOGGER: Incomplete
---
+++ stubs/weasyprint/weasyprint/pdf/debug.pyi
@@ -0,0 +1,5 @@
+from _typeshed import Incomplete
+
+def debug(pdf, metadata, document, page_streams, attachments, compress) -> None: ...
+
+VARIANTS: Incomplete
