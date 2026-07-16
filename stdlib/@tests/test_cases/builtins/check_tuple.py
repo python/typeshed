2bd224d58d49744ba21b04384269a0a4b4275f69
Jonathan Dung <jonathandung@yahoo.com>
2026-07-16 21:14:43 +0800

    tests?

--- stdlib/@tests/test_cases/builtins/check_tuple.py
+++ stdlib/@tests/test_cases/builtins/check_tuple.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import Tuple
+import sys
+from collections.abc import MutableSequence
+from typing import Any, Tuple, TypeVar
 from typing_extensions import assert_type
 
 
@@ -11,3 +13,22 @@ class TupleSub(Tuple[int, ...]):
 
 assert_type(TupleSub(), TupleSub)
 assert_type(TupleSub([1, 2, 3]), TupleSub)
+
+# Hashability shenanigans, see #15852
+t: Tuple[int, int] = (1, 3)
+hash(t)
+u: Tuple[int, ...] = t
+hash(u)
+v: Tuple[int] = u
+hash(v)
+w: Tuple[()] = u
+hash(w)
+hash(tuple(sys.platform))
+hash(([],)) # type: ignore
+x: Tuple[Any, Any] = ({}, ())
+hash(x)
+y = (),
+assert_type(y, Tuple[Tuple[()]])
+hash(y)
+z = ({}, ())
+hash(z) # type: ignore
