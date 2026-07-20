ac81749a57ec09ce0f28d6e81df263d859678da5
Randolf Scholz <randolf.scholz@gmail.com>
2026-07-20 20:04:52 +0200

    check_callable: add unnecessary type ignore comment for pyright

--- stdlib/@tests/test_cases/builtins/check_callable.py
+++ stdlib/@tests/test_cases/builtins/check_callable.py
@@ -4,5 +4,5 @@ from typing import Any
 def check_gradual_guarantee(arg: Any) -> int:
     # if Any materialized to `Callable[[], int] | None`, this function type checks
     if callable(arg):
-        return arg()  # type: ignore[no-any-return]
+        return arg()  # type: ignore[no-any-return]  # pyright: ignore[reportUnnecessaryTypeIgnoreComment]
     return -1
