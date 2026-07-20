e0d30f9184418d2c04a272d7bfacb7d47733de1f
Randolf Scholz <randolf.scholz@gmail.com>
2026-07-20 19:53:32 +0200

    check_callable: add type ignore for no-any-return in callable check

--- stdlib/@tests/test_cases/builtins/check_callable.py
+++ stdlib/@tests/test_cases/builtins/check_callable.py
@@ -4,5 +4,5 @@ from typing import Any
 def check_gradual_guarantee(arg: Any) -> int:
     # if Any materialized to `Callable[[], int] | None`, this function type checks
     if callable(arg):
-        return arg()  # OK
+        return arg()  # type: ignore[no-any-return]
     return -1
