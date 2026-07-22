16e75d6b4df6cb5255a650dac97ae557e4dbfc6d
Eddie Roman <eddiesr93@gmail.com>
2026-07-22 21:33:51 +0300

    [et_xmlfile] Complete the writer_cm annotation

--- stubs/et_xmlfile/et_xmlfile/xmlfile.pyi
+++ stubs/et_xmlfile/et_xmlfile/xmlfile.pyi
@@ -1,8 +1,7 @@
 import types
 import xml.etree.ElementTree as ET
-from _typeshed import Incomplete
 from collections.abc import Generator
-from contextlib import contextmanager
+from contextlib import _GeneratorContextManager, contextmanager
 from typing import Any
 
 class LxmlSyntaxError(Exception): ...
@@ -27,7 +26,7 @@ class _IncrementalFileWriter:
 
 class xmlfile:
     encoding: str
-    writer_cm: Incomplete
+    writer_cm: _GeneratorContextManager[tuple[ET._FileWrite, str]] | None
     def __init__(
         self, output_file: ET._FileWrite, buffered: bool = False, encoding: str = "utf-8", close: bool = False
     ) -> None: ...
