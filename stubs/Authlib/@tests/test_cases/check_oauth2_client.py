0e21638646114f05f2ebf8eb205f350bec69bc6d
nightcityblade <nightcityblade@gmail.com>
2026-08-01 23:14:14 +0800

    [Authlib] Remove introspect_token regression test

--- stubs/Authlib/@tests/test_cases/check_oauth2_client.py
+++
@@ -1,8 +0,0 @@
-from authlib.integrations.httpx_client import AsyncOAuth2Client
-
-
-async def check_introspect_token(client: AsyncOAuth2Client) -> None:
-    # The response type depends on the HTTP client integration.
-    await client.introspect_token(  # pyright: ignore[reportUnknownMemberType]
-        "https://example.com/introspect", token="token", token_type_hint="access_token"
-    )
