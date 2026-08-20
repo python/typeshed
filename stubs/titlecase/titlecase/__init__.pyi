d1238de294c4b92fa148304598df60249c496ed4
Artur Mostowski <artur.mostowski@protonmail.com>
2026-08-20 13:46:59 +0200

    feedback addressed

--- stubs/titlecase/titlecase/__init__.pyi
+++ stubs/titlecase/titlecase/__init__.pyi
@@ -1,11 +1,11 @@
 import logging
 import re
-from typing import Final, Protocol, TypeAlias, overload
+from typing import Final, Protocol, TypeAlias, overload, type_check_only
 from typing_extensions import LiteralString
 
 import regex
 
-Pattern: TypeAlias = re.Pattern[str] | regex.Pattern[str]
+_Pattern: TypeAlias = re.Pattern[str] | regex.Pattern[str]
 
 __all__ = ["titlecase"]
 __version__: Final[str]
@@ -16,20 +16,21 @@ REGEX_AVAILABLE: Final[bool]
 SMALL: Final[str]
 PUNCT: Final[str]
 
-SMALL_WORDS: Pattern
-SMALL_FIRST: Pattern
-SMALL_LAST: Pattern
-SUBPHRASE: Pattern
+SMALL_WORDS: _Pattern
+SMALL_FIRST: _Pattern
+SMALL_LAST: _Pattern
+SUBPHRASE: _Pattern
 
-MAC_MC: Final[Pattern]
-MR_MRS_MS_DR: Final[Pattern]
-INLINE_PERIOD: Final[Pattern]
-UC_ELSEWHERE: Final[Pattern]
-CAPFIRST: Final[Pattern]
-APOS_SECOND: Final[Pattern]
-UC_INITIALS: Final[Pattern]
+MAC_MC: Final[_Pattern]
+MR_MRS_MS_DR: Final[_Pattern]
+INLINE_PERIOD: Final[_Pattern]
+UC_ELSEWHERE: Final[_Pattern]
+CAPFIRST: Final[_Pattern]
+APOS_SECOND: Final[_Pattern]
+UC_INITIALS: Final[_Pattern]
 
-class CallbackProtocol(Protocol):
+@type_check_only
+class _CallbackProtocol(Protocol):
     def __call__(self, word: str, /, *, all_caps: bool) -> str | None: ...
 
 class Immutable: ...
@@ -42,7 +43,7 @@ def set_small_word_list() -> None: ...
 def set_small_word_list(small: str) -> None: ...
 
 def titlecase(
-    text: str, callback: CallbackProtocol | None = None, small_first_last: bool = True, preserve_blank_lines: bool = False
+    text: str, callback: _CallbackProtocol | None = None, small_first_last: bool = True, preserve_blank_lines: bool = False
 ) -> LiteralString: ...
-def create_wordlist_filter_from_file(file_path: str | None) -> CallbackProtocol: ...
+def create_wordlist_filter_from_file(file_path: str | None) -> _CallbackProtocol: ...
 def cmd() -> None: ...
