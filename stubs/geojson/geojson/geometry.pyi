fd0f03cfc12c7ee9d52f20897a27f620042a3dce
Peter Von der Porten <peter@corbelspatial.com>
2026-09-02 13:43:18 -0700

    `int` not `SupportsIndex`

--- stubs/geojson/geojson/geometry.pyi
+++ stubs/geojson/geojson/geometry.pyi
@@ -1,13 +1,13 @@
 from collections.abc import Sequence
 from decimal import Decimal
-from typing import SupportsIndex, TypeAlias
+from typing import TypeAlias
 
 from geojson.base import GeoJSON
 
 _InputCoord: TypeAlias = float | Decimal | Geometry | Sequence[_InputCoord]
 _CleanCoord: TypeAlias = float | Decimal | list[_CleanCoord]
 
-DEFAULT_PRECISION: SupportsIndex  # same type as ndigits parameter of builtins.round
+DEFAULT_PRECISION: int
 
 class Geometry(GeoJSON):
     def __init__(
