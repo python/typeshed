c8c167ce3084d19bbf511d7c67ff20d3663c36b3
ryux1 <code@ryux1.dev>
2026-09-06 21:14:03 +0200

    Remove trivial yt-dlp options test

--- stubs/yt-dlp/@tests/test_cases/check_options.py
+++
@@ -1,6 +0,0 @@
-from yt_dlp import YoutubeDL, YoutubeDLOptions
-
-options: YoutubeDLOptions = {"quiet": True, "extract_flat": True}
-YoutubeDL(options)
-
-YoutubeDL({"quiet": "yes"})  # type: ignore[arg-type]
