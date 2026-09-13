39e1136d035a6b715a453fda15b708bf46c2b779
subotac <73706465+subotac@users.noreply.github.com>
2026-09-13 18:24:04 +0300

    Correct set xor signatures for abstract sets

--- stdlib/@tests/test_cases/builtins/check_set.py
+++ stdlib/@tests/test_cases/builtins/check_set.py
@@ -61,5 +61,9 @@ def test_frozenset_interface(s: frozenset[Literal["foo", "bar"]], y: frozenset[s
 def test_xor_with_abstract_set(
     s: set[Literal["foo", "bar"]], frozen: frozenset[Literal["foo", "bar"]], other: AbstractSet[str]
 ) -> None:
+    s.__xor__(other)  # type: ignore
+    frozen.__xor__(other)  # type: ignore
+    assert_type(other.__rxor__(s), AbstractSet[str])
+    assert_type(other.__rxor__(frozen), AbstractSet[str])
     assert_type(s ^ other, AbstractSet[str])
     assert_type(frozen ^ other, AbstractSet[str])
