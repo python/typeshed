5c1c641a45300b9c3925e252ff02df21deddd5a5
Sebastian Rittau <sebastian.rittau@zfutura.de>
2026-09-17 14:37:12 +0200

    Fix tests

--- stdlib/@tests/test_cases/check_types.py
+++ stdlib/@tests/test_cases/check_types.py
@@ -78,10 +78,10 @@ assert_type(types.EllipsisType(), types.EllipsisType)
 if sys.version_info >= (3, 11):
     union_type = int | list[_T]
 
-    # ideally this would be `_SpecialForm` (Union)
     # pyright and mypy infer different types here
+    # ideally this would be `_SpecialForm` (Union)
     assert_type(union_type | Literal[1], types.UnionType | Any)  # pyright: ignore[reportAssertTypeFailure]
-    assert_type(union_type | Literal[1], types.UnionType | type[Literal[1]])  # type: ignore[assert-type]
+    # assert_type(union_type | Literal[1], types.UnionType | type[Literal[1]])  # this is what pyright infers
     # Both mypy and pyright special-case this operation,
     # but in different ways, so we just check that no error is emitted:
     _ = union_type[int]
